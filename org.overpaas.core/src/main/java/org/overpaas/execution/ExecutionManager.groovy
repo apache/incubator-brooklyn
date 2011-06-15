cd1d31894d04cd485249788bf11818cd1dea6966
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2011-06-15 23:14:21 +0100

    tasks can be submitted (and resubmitted) etc should contain the results but needs tests

--- org.overpaas.core/src/main/java/org/overpaas/execution/ExecutionManager.groovy
+++ org.overpaas.core/src/main/java/org/overpaas/execution/ExecutionManager.groovy
@@ -1,18 +1,32 @@
 package org.overpaas.execution
 
 import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.overpaas.entities.Entity
 
 class ExecutionManager {
 	
-	Executor runner = Executors.newCachedThreadPool() 
+	ExecutorService runner = Executors.newCachedThreadPool() 
 	
-	Map knownTasks = new LinkedHashMap()
+	Set<Task> knownTasks = new LinkedHashSet()
 	
-	CompoundTask execute(Entity entity, CompoundTask tasks) {
-		tasks.subTasks.each { it.run(runner) }
-		tasks
+	
+	
+	Task submit(Entity entity, Task task) {
+		if (task.result!=null) return task
+		synchronized (task) {
+			if (task.result!=null) return task
+			task.result = runner.submit task.job
+			return task
+		}
 	}
+	
+
+//	CompoundTask execute(Entity entity, CompoundTask tasks) {
+//		tasks.subTasks.each { it.run(runner) }
+//		tasks
+//	}
+	
 }
