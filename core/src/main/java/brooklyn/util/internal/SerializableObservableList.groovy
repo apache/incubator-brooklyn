99c218f9e8f917536904a729e43857bb70987181
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2011-06-16 10:37:06 +0100

    attempt to fix InheritConstructor annotation problems problems by splitting out SerializableXxx classes

---
+++ core/src/main/java/brooklyn/util/internal/SerializableObservableList.groovy
@@ -0,0 +1,6 @@
+package brooklyn.util.internal;
+
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+public class SerializableObservableList extends ObservableList implements Serializable {} 
---
+++ core/src/main/java/brooklyn/util/internal/SerializableObservableMap.groovy
@@ -0,0 +1,6 @@
+package brooklyn.util.internal;
+
+import groovy.transform.InheritConstructors
+
+@InheritConstructors
+public class SerializableObservableMap extends ObservableMap implements Serializable {} 
