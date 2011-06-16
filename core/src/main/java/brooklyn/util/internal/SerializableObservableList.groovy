9e1ea56d670cc7f7dd1306c507b7097b241c23d3
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2011-06-16 10:48:20 +0100

    Revert "attempt to fix InheritConstructor annotation problems problems by splitting out SerializableXxx classes"
    This reverts commit 99c218f9e8f917536904a729e43857bb70987181.

--- core/src/main/java/brooklyn/util/internal/SerializableObservableList.groovy
+++
@@ -1,6 +0,0 @@
-package brooklyn.util.internal;
-
-import groovy.transform.InheritConstructors
-
-@InheritConstructors
-public class SerializableObservableList extends ObservableList implements Serializable {} 
--- core/src/main/java/brooklyn/util/internal/SerializableObservableMap.groovy
+++
@@ -1,6 +0,0 @@
-package brooklyn.util.internal;
-
-import groovy.transform.InheritConstructors
-
-@InheritConstructors
-public class SerializableObservableMap extends ObservableMap implements Serializable {} 
