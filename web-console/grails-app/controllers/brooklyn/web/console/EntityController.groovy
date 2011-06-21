fd97252bf88c355a1d4bbc355791329c0e073250
Don Tran <don.tran@cloudsoftcorp.com>
2011-06-21 16:02:47 +0100

    fixed up mock data/entityController cr Aled

--- web-console/grails-app/controllers/brooklyn/web/console/EntityController.groovy
+++ web-console/grails-app/controllers/brooklyn/web/console/EntityController.groovy
@@ -120,40 +120,40 @@ private class TestApplication extends AbstractApplication {
     TestApplication() {
         displayName = "Application";
         
-        addOwnedChildren( [
-                new TestGroupEntity("tomcat tier 1").addOwnedChildren( [
-                        new TestGroupEntity("tomcat cluster 1a").addOwnedChildren( [
+        addOwnedChildren(
+                new TestGroupEntity("tomcat tier 1").addOwnedChildren(
+                        new TestGroupEntity("tomcat cluster 1a").addOwnedChildren(
                                 new TestLeafEntity("tomcat node 1a.1"),
                                 new TestLeafEntity("tomcat node 1a.2"),
                                 new TestLeafEntity("tomcat node 1a.3"),
-                                new TestLeafEntity("tomcat node 1a.4") ]),
-                        new TestGroupEntity("tomcat cluster 1b").addOwnedChildren( [
+                                new TestLeafEntity("tomcat node 1a.4") ),
+                        new TestGroupEntity("tomcat cluster 1b").addOwnedChildren(
                             new TestLeafEntity("tomcat node 1b.1"),
                             new TestLeafEntity("tomcat node 1b.2"),
                             new TestLeafEntity("tomcat node 1b.3"),
-                            new TestLeafEntity("tomcat node 1b.4") ])
-                        ] ),
-                new TestGroupEntity("data tier 1").addOwnedChildren( [
-                    new TestGroupEntity("data cluster 1a").addOwnedChildren( [
+                            new TestLeafEntity("tomcat node 1b.4") )
+                         ),
+                new TestGroupEntity("data tier 1").addOwnedChildren(
+                    new TestGroupEntity("data cluster 1a").addOwnedChildren(
                             new TestLeafEntity("data node 1a.1"),
                             new TestLeafEntity("data node 1a.2"),
-                            new TestLeafEntity("data node 1a.3") ])
-                    ] )
-                ] )
+                            new TestLeafEntity("data node 1a.3") )
+                     )
+                )
     }
     
-    TestGroupEntity addOwnedChildren(Entity[] children) {
+    TestApplication addOwnedChildren(Entity... children) {
         children.each { addOwnedChild(it) }
         return this
     }
 }
 
 private class TestGroupEntity extends AbstractGroup {
-    TestGroupEntity(String displayName, ArrayList<String> groups) {
+    TestGroupEntity(String displayName) {
         this.displayName = displayName
     }
     
-    TestGroupEntity addOwnedChildren(Entity[] children) {
+    TestGroupEntity addOwnedChildren(Entity... children) {
         children.each { addOwnedChild(it) }
         return this
     }
