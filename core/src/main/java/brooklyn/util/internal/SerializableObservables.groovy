ec8f168d6ec5eb1681d346d90bb3c6d959404fe7
danikov <dan.lobianco@cloudsoftcorp.com>
2011-06-23 11:39:22 +0100

    added uids to serializable classes

--- core/src/main/java/brooklyn/util/internal/SerializableObservables.groovy
+++ core/src/main/java/brooklyn/util/internal/SerializableObservables.groovy
@@ -14,10 +14,12 @@ import java.util.List
 
 //@InheritConstructors
 public class SerializableObservableList extends ObservableList implements Serializable {
+	private static final long serialVersionUID = 8299640601890557184L;
 	public SerializableObservableList(List delegate) { super(delegate) }
 }
 
 //@InheritConstructors
 public class SerializableObservableMap extends ObservableMap implements Serializable {
+	private static final long serialVersionUID = 6731953672699661734L;
 	public SerializableObservableMap(Map delegate) { super(delegate) }
 }
