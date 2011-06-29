fa7ac67961242add77c0021c210871ab6045176c
Andrew Donald Kennedy <grkvlt@apache.org>
2011-06-29 12:49:20 +0100

    Fixing BasicSubscriptionContext ctor

--- core/src/main/java/brooklyn/management/internal/BasicSubscriptionContext.groovy
+++ core/src/main/java/brooklyn/management/internal/BasicSubscriptionContext.groovy
@@ -18,27 +18,26 @@ public class BasicSubscriptionContext implements SubscriptionContext {
     private Map flags;
     
     public BasicSubscriptionContext(Map<String, Object> flags=[:], SubscriptionManager manager, Object subscriber) {
-    	this.manager = m;
+    	this.manager = manager;
         this.subscriber = subscriber;
         this.flags = [subscriber:subscriber]
-    	this.flags.addAll(flags);
+    	this.flags << flags;
     }
     
     public <T> SubscriptionHandle subscribe(Map<String, Object> newFlags=[:], Entity producer, Sensor<T> sensor, EventListener<T> listener) {
         Map f2 = [:]
-        f2.addAll(flags)
-        f2.addAll(newFlags)
+        f2 << flags
+        f2 << newFlags
         manager.subscribe(f2, producer, sensor, listener)
     }
     
     public boolean unsubscribe(SubscriptionHandle subscriptionId) {
-        assert ((Subscription)subscriptionId).subscriber == subscriber
+        assert ((Subscription) subscriptionId).subscriber == subscriber
         manager.unsubscribe(subscriptionId)
     }
 
     /** @see SubscriptionManager#publish(SensorEvent) */
     public <T> void publish(SensorEvent<T> event) {
-        assert ((Subscription)subscriptionId).subscriber == event.source
         manager.publish(event);
     }
 
