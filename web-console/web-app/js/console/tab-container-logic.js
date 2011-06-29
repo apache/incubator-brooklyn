d94660c9b8d915a2727a4ce8aee2904f6e14ccff
Alistair MacLeod <alistair@biscuitsfruit.org.uk>
2011-06-29 13:25:37 +0100

    Made summary tab view an accordion

--- web-console/web-app/js/console/tab-container-logic.js
+++ web-console/web-app/js/console/tab-container-logic.js
@@ -1,3 +1,15 @@
 function initTabs() {
-    $("#tabs").tabs();
+    $("#sensors").html("Oh what lovely sensor data I see.");
+
+    $("#tabs").tabs({
+        show: function (event, ui) {
+            setAccordionLayout(ui);
+        }
+    });
+
+    $("#summary").accordion();
+}
+
+function setAccordionLayout(accordion) {
+    $("#summary").css('width', '300px');
 }
