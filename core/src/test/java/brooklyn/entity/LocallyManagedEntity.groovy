c0f800ddbb8d603be3711bb4a004e2602277156e
aplowe <adam@aplowe.com>
2011-06-29 12:42:01 +0100

    Fixing imports

--- core/src/test/java/brooklyn/entity/LocallyManagedEntity.groovy
+++ core/src/test/java/brooklyn/entity/LocallyManagedEntity.groovy
@@ -2,7 +2,8 @@ package brooklyn.entity;
 
 import brooklyn.entity.basic.AbstractEntity;
 import brooklyn.management.ManagementContext;
-import brooklyn.management.internal.LocalManagementContext;
+import brooklyn.management.internal.LocalManagementContext
+import brooklyn.management.internal.AbstractManagementContext;
 
 /** An entity for testing which has a local management context (not associated with an application) */
 public class LocallyManagedEntity extends AbstractEntity {
