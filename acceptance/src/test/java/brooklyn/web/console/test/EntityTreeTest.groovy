b559f5360bc7109cb8205feff4fb860453801ae1
Don Tran <don.tran@cloudsoftcorp.com>
2011-06-30 13:31:51 +0100

    ENGR-1566 removed CloudsoftWebsiteTest.groovy, and added todo

--- acceptance/src/test/java/brooklyn/web/console/test/EntityTreeTest.groovy
+++ acceptance/src/test/java/brooklyn/web/console/test/EntityTreeTest.groovy
@@ -11,7 +11,8 @@ public class EntityTreeTest {
     @Test
     public void entityControllerShowsEntities() throws Exception {
         FirefoxProfile profile = new FirefoxProfile()
-        profile.setPreference("network.http.phishy-userpass-length", 255) //hooky!
+        //TODO There must be another way to get past the confirmation dialog, needs investigating
+        profile.setPreference("network.http.phishy-userpass-length", 255)
         WebDriver driver = new FirefoxDriver(profile)
         driver.get("http://admin:password@localhost:9090/entity")
         driver.findElementById("demo1").text.contains("tomcat node 1a.1")
