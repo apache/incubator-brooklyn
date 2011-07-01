e8228a4c5d324fc68353c7174ae87f9220a5331a
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-01 13:34:20 +0100

    Added cluster controller

---
+++ extra/src/main/java/brooklyn/entity/cluster/Controller.groovy
@@ -0,0 +1,11 @@
+package brooklyn.entity.cluster
+
+import brooklyn.entity.group.Tier
+import brooklyn.entity.trait.Startable
+
+/**
+ * Represents a controller mechanism for a {@link Cluster}.
+ */
+public abstract class Controller extends Tier implements Startable {
+    
+}
