6a90d5a9bc1406135e3eb0c9f1dde1b0b629dd6f
Alistair MacLeod <alistair@biscuitsfruit.org.uk>
2011-07-01 15:46:52 +0100

    Added javascript to get sensor data and draw a table of it.

--- web-console/web-app/js/console/tab-container-logic.js
+++ web-console/web-app/js/console/tab-container-logic.js
@@ -1,3 +1,15 @@
+function drawSensorData(json) {
+    var table = "<table> <thead> <tr> <th>Sensor</th> <th>Value</th> </tr> </thead>\n";
+    table += "<tbody>\n";
+
+    for (name in json) {
+        var line = "<tr> <td> " + name + " </td> <td> " + json[name] + "</td> </tr>\n";
+        table += line;
+    }
+
+    $("#sensors").html(table);
+}
+
 function initTabs() {
     $("#sensors").html("Oh what lovely sensor data I see.");
 
@@ -8,6 +20,9 @@ function initTabs() {
     });
 
     $("#summary").accordion();
+
+    var url = "sensors";
+    $.getJSON(url, drawSensorData);
 }
 
 function setAccordionLayout(accordion) {
