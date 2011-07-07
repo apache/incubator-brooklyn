d9e26f0e8abf19e1c29733b65c06e06365d6b0e8
aplowe <adam@aplowe.com>
2011-07-07 14:41:46 +0100

    Gridding the Sensors

--- web-console/web-app/js/console/sensors.js
+++ web-console/web-app/js/console/sensors.js
@@ -1,40 +1,46 @@
 Brooklyn.sensors = (function() {
     var entity_id;
-
-    /* Fetch sensor readings for an entity and show them in an html table. */
-    function drawSensorData(json) {
-        var table = "<table><thead><tr><th>Sensor</th><th>Description</th><th>Value</th></tr></thead>\n";
-        table += "<tbody>\n";
-
-        for (name in json) {
-            table += "<tr> <td> " + name + " </td> <td>" + json[name].description + " </td> <td>" + json[name].value + "</td> </tr>\n";
-        }
-
-        table += "</tbody></table>";
-
-        $("#sensor-data").html(table);
-    }
-
-    function handleXHRError(jqXHR, textStatus, errorThrown) {
-        $(Brooklyn.eventBus).trigger('update_failed', "Could not get sensor data.");
-    }
+    var x;
 
     /* Make a GET request for sensor data for an entity of the given id. Then call
      * drawSensorData on the returned json.
      */
     function getAndDrawSensorData(entity_id) {
-        $.getJSON("sensors?id=" + entity_id,
-                  function (json) {drawSensorData(json);
-                                   $(Brooklyn.eventBus).trigger('update_ok');
-                                   }).error(handleXHRError);
+          $("#sensor-data").jqGrid({
+                url:'sensors?id=' + entity_id,
+                datatype: "json",
+    	        height: '100%',
+                width: '700px',
+                colNames:['Name', 'Description', 'Value'],
+                colModel:[
+                    {name:'name',index:'name', jsonmap:'name', width:200},
+                    {name:'description',index:'description', width:250},
+                    {name:'value',index:'value',sortable:false}
+                ],
+                rowNum:10,
+                sortname: 'name',
+                viewrecords: true,
+                sortorder: "desc",
+                caption:"Sensor Values",
+                multiselect: false,
+                jsonReader: {
+		            repeatitems : false,
+		            id: "0"
+            	}
+            });
     }
 
     function update() {
         if (typeof entity_id === 'undefined') {
             return;
         }
-
-        getAndDrawSensorData(entity_id);
+        if (typeof x == 'undefined') {
+            getAndDrawSensorData(entity_id);
+            x = 1;
+        } else {
+    	    $("#sensor-data").jqGrid('setGridParam',{url:"sensors?id=" + entity_id}).trigger("reloadGrid");
+    	}
+    	$(Brooklyn.eventBus).trigger('update_ok');
     }
 
     /* This method is intended to be called as an event handler. The e paramater is
