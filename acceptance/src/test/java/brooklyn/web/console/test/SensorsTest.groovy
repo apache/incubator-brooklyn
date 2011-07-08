28bde380b0a737068571b06873382107e568d940
Alistair MacLeod <alistair.macleod@cloudsoftcorp.com>
2011-07-08 18:01:09 +0100

    Refactor / cleanup tests.

--- acceptance/src/test/java/brooklyn/web/console/test/SensorsTest.groovy
+++ acceptance/src/test/java/brooklyn/web/console/test/SensorsTest.groovy
@@ -6,6 +6,13 @@ import org.testng.Assert;
 
 public class SensorsTest extends AbstractSeleniumTest {
 
+    @Test public void testInitialText() {
+        selenium.open("/entity/#summary");
+
+        waitFor({selenium.isTextPresent("tomcat node 1a.1")});
+        waitFor({selenium.isTextPresent("Select an entity in the tree to the left to work with it here.")});
+    }
+
     @Test public void testSensors() {
         selenium.open("/entity/");
 
@@ -20,5 +27,18 @@ public class SensorsTest extends AbstractSeleniumTest {
         assertTrue(selenium.isTextPresent("HTTP port"));
         assertTrue(selenium.isTextPresent("JMX host"));
         assertTrue(selenium.isTextPresent("JMX port"));
+    }
+
+    @Test public void statusIndicator() {
+        selenium.open("/entity/");
+
+        selenium.isTextPresent("No data yet.")
+
+        // Wait for tree to load
+        waitFor({selenium.isTextPresent("tomcat")});
+
+        selenium.click("jstree-node-id-leaf-4")
+
+        waitFor({selenium.isTextPresent("regexp:\d{2}:\d{2}:\d{2}")});
     }
 }
