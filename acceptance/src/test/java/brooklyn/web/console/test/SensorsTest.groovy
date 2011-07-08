7b7d1c24d2f699073757828c3084e906414f0066
Alistair MacLeod <alistair.macleod@cloudsoftcorp.com>
2011-07-08 16:56:22 +0100

    Converted SensorsTest to old Selenium. It works!

--- acceptance/src/test/java/brooklyn/web/console/test/SensorsTest.groovy
+++ acceptance/src/test/java/brooklyn/web/console/test/SensorsTest.groovy
@@ -1,95 +1,51 @@
 package brooklyn.web.console.test
 
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.firefox.FirefoxProfile
-import org.testng.annotations.AfterTest
-import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
-import static org.testng.Assert.assertTrue
-import org.openqa.selenium.By
-import org.openqa.selenium.support.ui.WebDriverWait
-import org.openqa.selenium.support.ui.Wait
-import org.openqa.selenium.support.ui.ExpectedCondition
-import org.openqa.selenium.chrome.ChromeDriver
+import static org.testng.Assert.*;
 
-public class SensorsTest {
-    WebDriver driver;
-    static Wait<WebDriver> wait;
+public class SensorsTest extends AbstractSeleniumTest {
 
+    private static int timeout = 10;
 
-    @BeforeTest
-    public void setUp() throws Exception {
-        //TODO There must be another way to get past the confirmation dialog, needs investigating
-        FirefoxProfile profile = new FirefoxProfile()
-        profile.setPreference("network.http.phishy-userpass-length", 255)
-        //System.properties.setProperty("webdriver.chrome.driver", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
-        //driver = new ChromeDriver()
-        driver = new FirefoxDriver(profile)
-        wait = new WebDriverWait(driver, 30);
-
-        driver.get("http://admin:password@localhost:9090/entity/")
+    void waitFor(c) {
+		for (int second = 0;; second++) {
+			if (second >= timeout) fail("timeout");
+			try {
+                if (c()) {
+                    break;
+                }
+            } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
     }
 
-    void clickId(String id) {
-        driver.findElement(By.id(id)).click();
+    @Test public void testInitialText() {
+        selenium.open("/entity/#summary");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("tomcat node 1a.1")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("No data yet.")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+        waitFor({selenium.isTextPresent("Select an entity in the tree to the left to work with it here.")});
     }
 
-    @Test
-    public void testInitalDisplay() {
-        assertTrue(!!(driver.findElement(By.id("status")).getText() =~ "No data yet"));
-        assertTrue(!!(driver.findElement(By.tagName("body")).getText() =~
-                "Select an entity in the tree to the left to work with it here."));
+    @Test public void testSensors() {
+        selenium.open("/entity/");
 
-        println driver.findElement(By.tagName("body")).getText().contains("pplica");
+        // Wait for tree to load
+        waitFor({selenium.isTextPresent("tomcat")});
 
-        wait.until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver webDriver) {
+		selenium.click("jstree-node-id-leaf-4")
+		selenium.click("link=Sensors");
+        waitFor({selenium.isTextPresent("HTTP port")});
 
-                System.out.println("Searching ...");
-                //return !!(driver.findElement(By.tagName("body")).getText().contains("pplica"));
-                return true;
-            }
-        });
-    }
-
-    @Test public void testUpdatesWhenEntitySelected () {
-        driver.findElement(By.id("jstree-node-id-leaf-5")).click();
-
-        wait.until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver webDriver) {
-
-                System.out.println("Searching ...");
-                return !!(driver.findElement(By.id("status-message")).getText() =~ /\d{2}:\d{2}:\d{2}/);
-            }
-        });
-    }
-
-    @Test public void testSummaryShown() {
-        driver.findElement(By.id("jstree-node-id-leaf-5")).click();
-        sleep(3000);
-        clickId("summary");
-
-        wait.until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver webDriver) {
-
-                System.out.println("Searching ...");
-                return !!(driver.findElement(By.tagName("body")).getText() =~ /HTTP Port/);
-            }
-        });
-    }
-
-/*
-
-        verifyEquals("8086", selenium.getText("css=tbody > tr:nth(2) > td:nth(2)"));
-        selenium.click("jstree-node-id-leaf-10");
-        selenium.click("jstree-node-id-leaf-11");
-        verifyEquals("8092", selenium.getText("css=tbody > tr:nth(2) > td:nth(2)"));
-
-        */
-
-    @AfterTest
-    public void tearDown() throws Exception {
-        driver.quit()
+		assertTrue(selenium.isTextPresent("HTTP port"));
+		assertTrue(selenium.isTextPresent("8085"));
     }
 }
