7875162b97f2e6ab15d4aef2980f23a48f5d92a6
aplowe <adam@aplowe.com>
2011-07-11 11:40:03 +0100

    Putting correct URL in test

--- acceptance/src/test/java/brooklyn/web/console/test/EntityTreeTest.groovy
+++ acceptance/src/test/java/brooklyn/web/console/test/EntityTreeTest.groovy
@@ -14,7 +14,7 @@ public class EntityTreeTest {
         //TODO There must be another way to get past the confirmation dialog, needs investigating
         profile.setPreference("network.http.phishy-userpass-length", 255)
         WebDriver driver = new FirefoxDriver(profile)
-        driver.get("http://admin:password@localhost:9090/entity")
+        driver.get("http://admin:password@localhost:9090/entity/")
         driver.findElementById("demo1").text.contains("tomcat node 1a.1")
         driver.quit()
     }
