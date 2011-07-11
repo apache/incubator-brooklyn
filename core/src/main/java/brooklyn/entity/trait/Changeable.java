46c7b18d4eb67437c84005614ffd6e98eaf61918
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-11 18:25:55 +0100

    Remove cargo-cult copypasta

--- core/src/main/java/brooklyn/entity/trait/Changeable.java
+++ core/src/main/java/brooklyn/entity/trait/Changeable.java
@@ -1,8 +1,5 @@
 package brooklyn.entity.trait;
 
-import groovy.lang.Closure;
-
-import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import brooklyn.util.internal.SerializableObservableList;
@@ -12,17 +9,6 @@ import brooklyn.util.internal.SerializableObservableMap;
  * A collection of entities that can change.
  */
 public interface Changeable {
-    /**
-     * Allows casting a {@link Closure} to a {@link PropertyChangeListener}.
-     */
-    class ClosurePropertyChangeListener implements PropertyChangeListener {
-        Closure<Void> closure;
-        public ClosurePropertyChangeListener(Closure<Void> c) { closure = c; }
-        public void propertyChange(PropertyChangeEvent event) {
-            closure.call(event);
-        }
-    }
-
     /**
      * Implement this method to add a {@link PropertyChangeListener} on
      * some collection of entities.
