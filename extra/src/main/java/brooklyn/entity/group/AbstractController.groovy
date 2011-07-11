3f9ae4da2efa542c3e9884310940b83e4b593887
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-11 18:27:02 +0100

    Update cluster controller with more change events

--- extra/src/main/java/brooklyn/entity/group/AbstractController.groovy
+++ extra/src/main/java/brooklyn/entity/group/AbstractController.groovy
@@ -6,31 +6,52 @@ import groovy.util.ObservableList.ElementEvent
 import java.beans.PropertyChangeListener
 import java.util.Map
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 import brooklyn.entity.Entity
 import brooklyn.entity.Group
 import brooklyn.entity.basic.AbstractEntity
-import brooklyn.entity.group.Cluster
 import brooklyn.entity.trait.Startable
+
+import com.google.common.base.Preconditions
 
 /**
  * Represents a controller mechanism for a {@link Cluster}.
  */
-public abstract class Controller extends AbstractEntity implements Startable {
-    public Cluster cluster
+public abstract class AbstractController extends AbstractEntity implements Startable {
+    protected static final Logger LOG = LoggerFactory.getLogger(AbstractController.class)
 
-    public Controller(Map properties=[:], Group owner=null, Cluster cluster=null) {
+    Cluster cluster
+
+    public AbstractController(Map properties=[:], Group owner=null, Cluster cluster) {
         super(properties, owner)
 
-        if (cluster) this.cluster = cluster
+        setCluster(cluster ?: properties.cluster)
+    }
 
+    public void setCluster(Cluster cluster) {
+        Preconditions.checkNotNull cluster, "The cluster cannot be null"
+        this.cluster = cluster
+        addOwnedChild(cluster)
         cluster.addEntityChangeListener({ ElementEvent event ->
+                LOG.trace "Entity change event for {} - {}", id, event.changeType
 	            switch (event.changeType) {
                     case ChangeType.ADDED:
                         add event.newValue
                         break
+                    case ChangeType.MULTI_ADD:
+                        event.values.each { add it }
+                        break
                     case ChangeType.REMOVED:
 	                    remove event.oldValue
                         break;
+                    case ChangeType.MULTI_REMOVE:
+                        event.values.each { remove it }
+                        break
+                    case ChangeType.CLEARED:
+                        event.values.each { remove it }
+                        break
                     default:
                         break;
                 }
