35ce578c9c25ee7a620e1cb4fec2f9217765fc42
Richard Downer <richard.downer@cloudsoftcorp.com>
2011-07-12 17:15:13 +0100

    Reinstate TomcatCluster as a shim over DynamicCluster with @Obsolete annotation. This allows classes (particularly in examples) that depended on this to continue to compile.

--- extra/src/main/java/brooklyn/entity/webapp/tomcat/TomcatCluster.groovy
+++ extra/src/main/java/brooklyn/entity/webapp/tomcat/TomcatCluster.groovy
@@ -1,12 +1,31 @@
 package brooklyn.entity.webapp.tomcat;
 
-import java.util.Map
+import brooklyn.entity.group.DynamicCluster
+import brooklyn.util.internal.EntityStartUtils
+import brooklyn.util.internal.LanguageUtils
 
-import brooklyn.entity.Group
-import brooklyn.entity.group.ClusterFromTemplate
-
-public class TomcatCluster extends ClusterFromTemplate {
+// This class is deprecated. Instead you should write your code to refer directly to DynamicCluster.
+@Deprecated
+public class TomcatCluster extends DynamicCluster {
     public TomcatCluster(Map props=[:], TomcatNode template=new TomcatNode()) {
-        super(props, template);
+        super(getPropertiesForSuperConstructor(props), template);
+    }
+
+    private static Map getPropertiesForSuperConstructor(Map props, TomcatNode template) {
+        Map superProps = [:]
+        superProps << props
+        superProps.newEntity = makeNewEntityClosure(template)
+    }
+
+    private void setTemplate(TomcatNode template) {
+        super.newEntity = makeNewEntityClosure(template)
+    }
+
+    private static Closure<TomcatNode> makeNewEntityClosure(TomcatNode template) {
+        return {
+            TomcatNode copy = EntityStartUtils.cloneTemplate(template);
+            copy.id = LanguageUtils.newUid()
+            return copy
+        }
     }
 }
