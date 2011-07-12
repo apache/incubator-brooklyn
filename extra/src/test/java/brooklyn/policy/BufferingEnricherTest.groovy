c3271a7695b993d61d97b4b00d597145e8692ab5
Richard Downer <richard.downer@cloudsoftcorp.com>
2011-07-12 15:25:03 +0100

    Reorganise the imports of BufferingEnricherTest in a way that keeps IntelliJ happy

--- extra/src/test/java/brooklyn/policy/BufferingEnricherTest.groovy
+++ extra/src/test/java/brooklyn/policy/BufferingEnricherTest.groovy
@@ -1,11 +1,8 @@
 package brooklyn.policy
 
-import static brooklyn.test.TestUtils.*
 import static org.testng.Assert.*
 
 import java.util.concurrent.CyclicBarrier
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 import org.testng.annotations.AfterMethod
 import org.testng.annotations.BeforeMethod
@@ -20,9 +17,11 @@ import brooklyn.event.SensorEvent
 import brooklyn.event.basic.BasicSensor
 import brooklyn.management.SubscriptionContext
 import brooklyn.management.internal.BasicSubscriptionContext
-import brooklyn.policy.wip.BufferingEnricher.*
 import brooklyn.policy.wip.BufferingEnricher
 import brooklyn.policy.wip.RangeBufferEnricher
+import brooklyn.policy.wip.BufferingEnricher.BufferChangedEvent
+import brooklyn.policy.wip.BufferingEnricher.BufferFlushedEvent
+import brooklyn.policy.wip.BufferingEnricher.BufferEvent
 
 /**
  * TODO clarify test purpose
