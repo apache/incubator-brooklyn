e6c1b8c55c9bb6f363953d5b9e6bb5ca1f1c130c
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-15 17:51:08 +0100

    Move more methods to ExecutionContext interface

--- api/src/main/java/brooklyn/management/ExecutionContext.java
+++ api/src/main/java/brooklyn/management/ExecutionContext.java
@@ -1,13 +1,24 @@
 package brooklyn.management;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 
 /**
  * This is a Brooklyn extension to the Java {@link Executor}.
  */
-public interface ExecutionContext extends Executor { 
+public interface ExecutionContext extends Executor {
     /**
      * Returns the current {@link Task} being executed by this context.
      */
     Task<?> getCurrentTask();
+
+    Set<Task<?>> getTasks();
+
+    Task<?> submit(Map<?, ?> properties, Runnable runnable);
+
+    <T> Task<T> submit(Map<?, ?> properties, Callable<T> callable);
+
+    <T> Task<T> submit(Task<T> task);
 }
