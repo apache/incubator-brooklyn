d3979b41afe3cce9af958511efac3f2c2a9b77cd
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-17 11:06:31 +0100

    Add more generic attribute sensors

--- core/src/main/java/brooklyn/entity/basic/Attributes.groovy
+++ core/src/main/java/brooklyn/entity/basic/Attributes.groovy
@@ -1,5 +1,8 @@
 package brooklyn.entity.basic
 
+import java.util.List
+
+import brooklyn.event.Sensor
 import brooklyn.event.basic.BasicAttributeSensor
 import brooklyn.event.basic.BasicConfigKey;
 
@@ -14,7 +17,7 @@ public interface Attributes {
     BasicAttributeSensor<Integer> VERSION = [ String, "version", "Version information" ]
 
     /*
-     * JMX sensors.
+     * JMX attributes.
      */
 
     BasicAttributeSensor<Integer> JMX_PORT = [ Integer, "jmx.port", "JMX port" ]
@@ -23,9 +26,16 @@ public interface Attributes {
     BasicAttributeSensor<String> JMX_PASSWORD = [ String, "jmx.password", "JMX password" ]
 
     /*
-     * Port number sensors.
+     * Port number attributes.
      */
 
+    BasicAttributeSensor<List<Integer>> PORT_NUMBERS = [ List, "port.list", "List of port numbers" ]
+    BasicAttributeSensor<List<Sensor<Integer>>> PORT_SENSORS = [ List, "port.list.sensors", "List of port number attributes" ]
+
+    BasicAttributeSensor<Integer> SSH_PORT = [ Integer, "ssh.port", "SSH port" ]
+    BasicAttributeSensor<Integer> SMTP_PORT = [ Integer, "smtp.port", "SMTP port" ]
+    BasicAttributeSensor<Integer> DNS_PORT = [ Integer, "dns.port", "DNS port" ]
     BasicAttributeSensor<Integer> HTTP_PORT = [ Integer, "http.port", "HTTP port" ]
+    BasicAttributeSensor<Integer> HTTPS_PORT = [ Integer, "https.port", "HTTP port (with SSL/TLS)" ]
     BasicAttributeSensor<Integer> AMQP_PORT = [ Integer, "amqp.port", "AMQP port" ]
 }
