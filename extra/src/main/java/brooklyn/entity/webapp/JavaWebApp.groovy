f0658bae35a6eb509550f1e2171d55d12acc11ab
danikov <dan.lobianco@cloudsoftcorp.com>
2011-07-18 11:37:39 +0100

    converted to use enrichers

--- extra/src/main/java/brooklyn/entity/webapp/JavaWebApp.groovy
+++ extra/src/main/java/brooklyn/entity/webapp/JavaWebApp.groovy
@@ -15,6 +15,9 @@ import brooklyn.event.basic.BasicConfigKey
 import brooklyn.event.basic.ConfigKey
 import brooklyn.location.Location
 import brooklyn.location.MachineLocation
+import brooklyn.policy.DeltaEnricher
+import brooklyn.policy.SimpleAveragingEnricher;
+import brooklyn.policy.SimpleTimeAveragingEnricher;
 
 /**
 * An {@link brooklyn.entity.Entity} representing a single java web application server instance.
@@ -26,11 +29,14 @@ public abstract class JavaWebApp extends JavaApp {
     public static final ConfigKey<Integer> SUGGESTED_HTTP_PORT = ConfigKeys.SUGGESTED_HTTP_PORT
 
     public static final AttributeSensor<Integer> HTTP_PORT = Attributes.HTTP_PORT
+    public static final Integer AVG_REQUESTS_PER_SECOND_PERIOD = 30*1000
 
     public static final BasicAttributeSensor<Integer> ERROR_COUNT = [ Integer, "webapp.reqs.errors", "Request errors" ]
     public static final BasicAttributeSensor<Integer> MAX_PROCESSING_TIME = [ Integer, "webpp.reqs.processing.max", "Max processing time" ]
     public static final BasicAttributeSensor<Integer> REQUEST_COUNT = [ Integer, "webapp.reqs.total", "Request count" ]
-    public static final BasicAttributeSensor<Double> REQUESTS_PER_SECOND = [ Double, "webapp.reqs.persec", "Reqs/Sec" ]
+    public static final BasicAttributeSensor<Double> REQUESTS_PER_SECOND = [ Double, "webapp.reqs.persec.last", "Reqs/Sec" ]
+    public static final BasicAttributeSensor<Double> AVG_REQUESTS_PER_SECOND = [ Double, "webapp.reqs.persec.avg.$AVG_REQUESTS_PER_SECOND_PERIOD",
+         "Average Reqs/Sec (over ${AVG_REQUESTS_PER_SECOND_PERIOD}ms)" ]
     public static final BasicAttributeSensor<Integer> TOTAL_PROCESSING_TIME = [ Integer, "webapp.reqs.processing.time", "Total processing time" ]
 
     public static final BasicAttributeSensor<String> HTTP_SERVER = [ String, "webapp.http.server", " Server name" ]
@@ -61,7 +67,11 @@ public abstract class JavaWebApp extends JavaApp {
         log.debug "started $this: httpPort {}, jmxHost {} and jmxPort {}",
                 getAttribute(HTTP_PORT), getAttribute(JMX_HOST), getAttribute(JMX_PORT)
 
-        attributePoller.addSensor(REQUESTS_PER_SECOND, { computeReqsPerSec() } as ValueProvider, 1000L)
+        // TODO Want to wire this up so doesn't go through SubscriptionManager;
+        // but that's an optimisation we'll do later.
+        addPolicy(new DeltaEnricher<Integer>(this, REQUEST_COUNT, REQUESTS_PER_SECOND))
+        addPolicy(new SimpleTimeAveragingEnricher<Double>(this, REQUESTS_PER_SECOND, AVG_REQUESTS_PER_SECOND, 
+            AVG_REQUESTS_PER_SECOND_PERIOD))
 
         waitForHttpPort()
 //        initHttpSensors()
@@ -77,27 +87,5 @@ public abstract class JavaWebApp extends JavaApp {
         getSshBasedSetup(locations.find({ it instanceof MachineLocation })).deploy(new File(file))
     }
 
-    protected double computeReqsPerSec() {
-        def curTimestamp = System.currentTimeMillis()
-        def curCount = getAttribute(REQUEST_COUNT) ?: 0
-        def prevTimestamp = tempWorkings['tmp.reqs.timestamp'] ?: 0
-        def prevCount = tempWorkings['tmp.reqs.count'] ?: 0
-        tempWorkings['tmp.reqs.timestamp'] = curTimestamp
-        tempWorkings['tmp.reqs.count'] = curCount
-        log.trace "previous data {} at {}, current {} at {}", prevCount, prevTimestamp, curCount, curTimestamp
-
-        // Calculate requests per second
-        double diff = curCount - prevCount
-        long dt = curTimestamp - prevTimestamp
-        double result
-
-        if (dt <= 0 || dt > 60*1000) {
-            result = -1;
-        } else {
-            result = ((double) 1000.0 * diff) / dt
-        }
-        log.trace "computed $result reqs/sec over $dt millis"
-        return result
-    }
 }
 
