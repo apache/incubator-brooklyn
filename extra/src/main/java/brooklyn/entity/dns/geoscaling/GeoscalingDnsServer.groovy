6407186db29374f95cdf2e79b17a54080e17807b
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-07-19 19:43:18 +0100

    ENGR-1594  First stab at 'setDestinationServers' effector for GeoscalingDnsServer entity

--- extra/src/main/java/brooklyn/entity/dns/geoscaling/GeoscalingDnsServer.groovy
+++ extra/src/main/java/brooklyn/entity/dns/geoscaling/GeoscalingDnsServer.groovy
@@ -2,7 +2,10 @@ package brooklyn.entity.dns.geoscaling
 
 import java.util.Set
 
+import brooklyn.entity.Effector
 import brooklyn.entity.basic.AbstractEntity
+import brooklyn.entity.basic.BasicParameterType
+import brooklyn.entity.basic.EffectorWithExplicitImplementation
 import brooklyn.entity.dns.ServerGeoInfo
 import brooklyn.event.AttributeSensor
 import brooklyn.event.basic.BasicConfigKey
@@ -13,20 +16,32 @@ class GeoscalingDnsServer extends AbstractEntity {
     public static BasicConfigKey<Integer> GEOSCALING_PORT = [ Integer.class, "geoscaling.port" ];
     public static BasicConfigKey<String> GEOSCALING_USERNAME = [ String.class, "geoscaling.username" ];
     public static BasicConfigKey<String> GEOSCALING_PASSWORD = [ String.class, "geoscaling.password" ];
-    public static BasicConfigKey<Integer> GEOSCALING_DOMAIN_ID = [ Integer.class, "geoscaling.domain.id" ];
+    public static BasicConfigKey<Integer> GEOSCALING_PRIMARY_DOMAIN_ID = [ Integer.class, "geoscaling.primary.domain.id" ];
     public static BasicConfigKey<Integer> GEOSCALING_SMART_SUBDOMAIN_ID = [ Integer.class, "geoscaling.smart.subdomain.id" ];
     public static BasicConfigKey<String> GEOSCALING_SMART_SUBDOMAIN_NAME = [ String.class, "geoscaling.smart.subdomain.name" ];
     
     public static AttributeSensor<Set<ServerGeoInfo>> DESTINATION_SERVERS = [ Set.class, "destination.servers" ];
     
+    public static Effector<String> SET_DESTINATION_SERVERS =
+        new EffectorWithExplicitImplementation<GeoscalingDnsServer, Void>(
+            "setDestinationServers", Void.class,
+            [ new BasicParameterType<Set>("servers", Set.class, "Set of all destination servers, including address and lat/long information") ],
+            "Reconfigures the GeoScaling account to redirect users to their nearst server from the passed set") {
+        
+        public Void invokeEffector(GeoscalingDnsServer gds, Map args) {
+            Set<ServerGeoInfo> servers = args.get("servers");
+            gds.setDestinationServers(servers);
+            return null;
+        }
+    };
+
     
-    // TODO: expose as effector
     public boolean setDestinationServers(Set<ServerGeoInfo> servers) {
         String host = getConfig(GEOSCALING_HOST);
         int port = getConfig(GEOSCALING_PORT);
         String username = getConfig(GEOSCALING_USERNAME);
         String password = getConfig(GEOSCALING_PASSWORD);
-        int primaryDomainId = getConfig(GEOSCALING_DOMAIN_ID);
+        int primaryDomainId = getConfig(GEOSCALING_PRIMARY_DOMAIN_ID);
         int smartSubdomainId = getConfig(GEOSCALING_SMART_SUBDOMAIN_ID);
         String smartSubdomainName = getConfig(GEOSCALING_SMART_SUBDOMAIN_NAME);
         
