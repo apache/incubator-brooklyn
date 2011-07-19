97332c5624b653ae1ff4b363a58a707d601d6d88
danikov <dan.lobianco@cloudsoftcorp.com>
2011-07-19 16:53:13 +0100

    documentation, removed orphan/dead code, extrapolation, and avoided a div by 0

--- extra/src/main/java/brooklyn/policy/RollingTimeWindowMeanEnricher.groovy
+++ extra/src/main/java/brooklyn/policy/RollingTimeWindowMeanEnricher.groovy
@@ -2,11 +2,29 @@ package brooklyn.policy
 
 import brooklyn.entity.Entity
 import brooklyn.event.SensorEvent
-import brooklyn.policy.basic.AbstractEnricher;
+import brooklyn.policy.basic.AbstractTransformingEnricher;
 import brooklyn.event.AttributeSensor
 
-class RollingTimeWindowMeanEnricher<T extends Number> extends AbstractEnricher {
-    // FIXME What if no values yet? Document what it will return, e.g. -1
+/**
+ * Transforms a sensor into a rolling average based on a time window- all values within the window are weighted or discarded 
+ * based on the timestamps associated with them (discards occur when a new value is added or an average is requested)
+ * 
+ * This will not extrapolate figures- it is assumed a value is valid and correct for the entire time period between it and
+ * the previous value. Normally, the average attribute is only updated when a new value arrives so it can give a fully informed
+ * average, but there is a danger of this going stale
+ * 
+ * When an average is requested, it is likely there will be a segment of the window for which there isn't a value. 
+ * Instead of extrapolating a value and providing different extrapolation techniques, instead the average is reported with 
+ * a confidence value which reflects the fraction of the time window for which the values were valid
+ * 
+ * Consumers of the average may ignore the confidence value and just use the last known average. They could multiply the returned 
+ * value by the confidence value to get a decay-type behavior as the window empties. A third alternative is to, at a certain
+ * confidence threshold, report that the average is no longer meaningful
+ * 
+ * The default average when no data has been received is 0, with a confidence of 0
+ */
+
+class RollingTimeWindowMeanEnricher<T extends Number> extends AbstractTransformingEnricher {
     
     public static class ConfidenceQualifiedNumber {
         final Number value
@@ -23,13 +41,11 @@ class RollingTimeWindowMeanEnricher<T extends Number> extends AbstractEnricher {
     ConfidenceQualifiedNumber lastAverage = [0,0]
     
     long timePeriod
-    boolean extrapolateFromLast
     
-    public RollingTimeWindowMeanEnricher(Entity producer, AttributeSensor<T> source, AttributeSensor<Double> target,
-            long timePeriod) {
+    public RollingTimeWindowMeanEnricher(Entity producer, AttributeSensor<T> source, 
+        AttributeSensor<ConfidenceQualifiedNumber> target, long timePeriod) {
         super(producer, source, target)
         this.timePeriod = timePeriod
-        this.extrapolateFromLast = extrapolateFromLast
     }
 
     public void onEvent(SensorEvent<T> event) {
@@ -40,7 +56,7 @@ class RollingTimeWindowMeanEnricher<T extends Number> extends AbstractEnricher {
         values.addLast(event.getValue())
         timestamps.addLast(eventTime)
         pruneValues(eventTime)
-        entity.setAttribute(target, getAverage(eventTime).value)
+        entity.setAttribute(target, getAverage(eventTime).value) //TODO this can potentially go stale... maybe we need to timestamp as well?
     }
     
     public ConfidenceQualifiedNumber getAverage() {
@@ -54,18 +70,20 @@ class RollingTimeWindowMeanEnricher<T extends Number> extends AbstractEnricher {
         }
         
         Double confidence = (timePeriod - (now - timestamps.last())) / timePeriod
+        
+        if(confidence == 0) {
+            return lastAverage = [lastAverage.value, 0]
+        }
+        
         Long start = now - timePeriod
         Long end
         Double weightedAverage = 0
         
         timestamps.eachWithIndex { timestamp, i ->
             end = timestamp
-            weightedAverage += ((end - start) / timePeriod) * values[i]
+            weightedAverage += ((end - start) / (confidence * timePeriod)) * values[i]
             start = timestamp
         }
-        end = now
-        
-        weightedAverage += ((end - start) / timePeriod) *  values.last()
         
         return lastAverage = [weightedAverage, confidence]
     }
