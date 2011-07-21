ee9dbd3fe1daa4350629a79fc946326a9fa0508a
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-07-21 11:06:32 +0100

    ENGR-1594  Oops, add required 'LocationUtils' class

---
+++ extra/src/main/java/brooklyn/entity/dns/geoscaling/LocationUtils.java
@@ -0,0 +1,59 @@
+package brooklyn.entity.dns.geoscaling;
+
+import java.net.InetAddress;
+
+import brooklyn.entity.Entity;
+import brooklyn.location.Location;
+import brooklyn.location.MachineLocation;
+
+public class LocationUtils {
+    
+    public static InetAddress findIpAddress(Entity e) {
+        for (Location l : e.getLocations()) {
+            InetAddress address = findIpAddress(l);
+            if (address != null) return address;
+        }
+        return null;
+    }
+    
+    public static Double findLatitude(Entity e) {
+        for (Location l : e.getLocations()) {
+            Double latitude = findLatitude(l);
+            if (latitude != null) return latitude;
+        }
+        return null;
+    }
+    
+    public static Double findLongitude(Entity e) {
+        for (Location l : e.getLocations()) {
+            Double longitude = findLongitude(l);
+            if (longitude != null) return longitude;
+        }
+        return null;
+    }
+    
+    public static InetAddress findIpAddress(Location l) {
+        if (l == null)
+            return null;
+        if (l instanceof MachineLocation)
+            return ((MachineLocation) l).getAddress();
+        return findIpAddress(l.getParentLocation());
+    }
+    
+    public static Double findLatitude(Location l) {
+        if (l == null)
+            return null;
+        if (l instanceof MachineLocation.WithCoordinates)
+            return ((MachineLocation.WithCoordinates) l).getLatitude();
+        return findLatitude(l.getParentLocation());
+    }
+    
+    public static Double findLongitude(Location l) {
+        if (l == null)
+            return null;
+        if (l instanceof MachineLocation.WithCoordinates)
+            return ((MachineLocation.WithCoordinates) l).getLongitude();
+        return findLatitude(l.getParentLocation());
+    }
+    
+}
