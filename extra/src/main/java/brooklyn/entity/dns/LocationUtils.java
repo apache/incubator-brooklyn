f6b2e470b441e864ed78f0d047c8adf19b99ada0
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-07-21 11:26:47 +0100

    ENGR-1594  Remove now-unused methods

--- extra/src/main/java/brooklyn/entity/dns/LocationUtils.java
+++ extra/src/main/java/brooklyn/entity/dns/LocationUtils.java
@@ -2,35 +2,10 @@ package brooklyn.entity.dns;
 
 import java.net.InetAddress;
 
-import brooklyn.entity.Entity;
 import brooklyn.location.Location;
 import brooklyn.location.MachineLocation;
 
 public class LocationUtils {
-    
-    public static InetAddress findIpAddress(Entity e) {
-        for (Location l : e.getLocations()) {
-            InetAddress address = findIpAddress(l);
-            if (address != null) return address;
-        }
-        return null;
-    }
-    
-    public static Double findLatitude(Entity e) {
-        for (Location l : e.getLocations()) {
-            Double latitude = findLatitude(l);
-            if (latitude != null) return latitude;
-        }
-        return null;
-    }
-    
-    public static Double findLongitude(Entity e) {
-        for (Location l : e.getLocations()) {
-            Double longitude = findLongitude(l);
-            if (longitude != null) return longitude;
-        }
-        return null;
-    }
     
     public static InetAddress findIpAddress(Location l) {
         if (l == null)
