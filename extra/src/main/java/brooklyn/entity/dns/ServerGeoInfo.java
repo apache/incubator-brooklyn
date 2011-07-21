e2414653a3f034dc63bb90638546c8383aaede4a
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-07-21 11:01:28 +0100

    ENGR-1594  Add 'fromEntity' convenience factory method

--- extra/src/main/java/brooklyn/entity/dns/ServerGeoInfo.java
+++ extra/src/main/java/brooklyn/entity/dns/ServerGeoInfo.java
@@ -1,5 +1,10 @@
 package brooklyn.entity.dns;
 
+import java.net.InetAddress;
+
+import brooklyn.entity.Entity;
+import brooklyn.entity.dns.geoscaling.LocationUtils;
+
 /**
  * Encapsulates geo-IP information for a given server.
  */
@@ -9,6 +14,18 @@ public class ServerGeoInfo {
     public final double latitude;
     public final double longitude;
 
+    
+    public static ServerGeoInfo fromEntity(Entity e) {
+        String displayName = e.getDisplayName();
+        InetAddress address = LocationUtils.findIpAddress(e);
+        Double latitude = LocationUtils.findLatitude(e);
+        Double longitude = LocationUtils.findLongitude(e);
+        
+        return new ServerGeoInfo(address.toString(), displayName,
+                (latitude == null ? 0.0 : latitude),
+                (longitude == null ? 0.0 : longitude));
+    }
+    
     
     public ServerGeoInfo(String address, String displayName, double latitude, double longitude) {
         this.address = address;
