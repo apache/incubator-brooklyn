dd00c0ba7256dcfd14b95ac675831dce4d03b61d
Aled Sage <aled@cloudsoftcorp.com>
2011-07-22 12:46:18 +0100

    MultiLocationWebAppDemo: use DynamicFabric; added aws location

--- example/src/main/java/brooklyn/example/MultiLocationWebAppDemo.groovy
+++ example/src/main/java/brooklyn/example/MultiLocationWebAppDemo.groovy
@@ -2,8 +2,10 @@ package brooklyn.example
 
 import brooklyn.entity.Entity
 import brooklyn.entity.basic.AbstractApplication
-
+import brooklyn.entity.basic.AbstractEntity
+import brooklyn.entity.basic.JavaApp
 import brooklyn.entity.group.DynamicCluster
+import brooklyn.entity.group.DynamicFabric
 import brooklyn.entity.proxy.nginx.NginxController
 import brooklyn.entity.trait.Startable
 import brooklyn.entity.webapp.JavaWebApp
@@ -11,12 +13,14 @@ import brooklyn.entity.webapp.tomcat.TomcatServer
 import brooklyn.location.Location
 import brooklyn.location.MachineProvisioningLocation
 import brooklyn.location.basic.FixedListMachineProvisioningLocation
-import brooklyn.location.basic.LocalhostMachineProvisioningLocation
 import brooklyn.location.basic.SshMachineLocation
+import brooklyn.location.basic.aws.AWSCredentialsFromEnv
+import brooklyn.location.basic.aws.AwsLocation
 import brooklyn.policy.Policy
-import brooklyn.util.internal.EntityStartUtils
+
 import com.google.common.base.Preconditions
-import brooklyn.entity.basic.AbstractEntity
+
+
 
 /**
  * The application demonstrates the following:
@@ -43,7 +47,7 @@ public class MultiLocationWebAppDemo extends AbstractApplication implements Star
         private Policy policy
 
         static {
-            URL resource = SimpleTomcatApp.class.getClassLoader().getResource(warName)
+            URL resource = MultiLocationWebAppDemo.class.getClassLoader().getResource(warName)
             Preconditions.checkState resource != null, "Unable to locate resource $warName"
             springTravelPath = resource.getPath()
         }
@@ -53,6 +57,9 @@ public class MultiLocationWebAppDemo extends AbstractApplication implements Star
 
             cluster = new DynamicCluster(newEntity: { properties ->
                 def server = new TomcatServer(properties)
+                server.setConfig(JavaApp.SUGGESTED_JMX_PORT, 32199)
+                server.setConfig(JavaWebApp.SUGGESTED_HTTP_PORT, 8080)
+                server.setConfig(TomcatServer.SUGGESTED_SHUTDOWN_PORT, 31880)
                 server.setConfig(JavaWebApp.WAR, springTravelPath)
                 return server;
             }, this)
@@ -88,54 +95,60 @@ public class MultiLocationWebAppDemo extends AbstractApplication implements Star
         }
     }
 
-    // The definition of the Monterey East location
-    private static final Collection<SshMachineLocation> MONTEREY_EAST_PUBLIC_ADDRESSES = [
-        '216.48.127.224', '216.48.127.225', // east1a/b
-        '216.48.127.226', '216.48.127.227', // east2a/b
-        '216.48.127.228', '216.48.127.229', // east3a/b
-        '216.48.127.230', '216.48.127.231', // east4a/b
-        '216.48.127.232', '216.48.127.233', // east5a/b
-        '216.48.127.234', '216.48.127.235'  // east6a/b
-    ].collect { new SshMachineLocation(address: InetAddress.getByName(it), userName: 'cdm') }
-    private MachineProvisioningLocation<SshMachineLocation> montereyEastLocation =
-        new FixedListMachineProvisioningLocation<SshMachineLocation>(machines: MONTEREY_EAST_PUBLIC_ADDRESSES)
-
-    // The definition of the Amazon European region
-    // FIXME: change this to actually be an AWS location
-    private MachineProvisioningLocation<SshMachineLocation> amazonEuropeLocation =
-        new LocalhostMachineProvisioningLocation()
-
-    // The groups in each location
-    private WebClusterEntity montereyEastGroup = new WebClusterEntity([:], this)
-    private WebClusterEntity amazonEuropeGroup = new WebClusterEntity([:], this)
-
+    MultiLocationWebAppDemo(Map props=[:], Entity owner=null) {
+        super(props, owner)
+        
+        new DynamicFabric(newEntity: { properties -> return new WebClusterEntity(properties) }, this)
+    }
+    
+    @Override
+    public void restart() {
+        throw new UnsupportedOperationException()
+    }
+    
     public static void main(String[] args) {
+        AwsLocation awsUsEastLocation = newAwsUsEastLocation()
+        FixedListMachineProvisioningLocation montereyEastLocation = newMontereyEastLocation()
+        
+        // FIXME: start the web management console here
         MultiLocationWebAppDemo app = new MultiLocationWebAppDemo()
-
-        // FIXME: start the web management console here instead of manually starting the app
-        app.start([])
+        app.start([montereyEastLocation, awsUsEastLocation])
+        
         System.in.read()
         app.stop()
     }
 
-    @Override
-    public void start(Collection<Location> locs) {
-        // FIXME: copy-and-paste of superclass method with customization
-        // The only way(?) to specifically start each group in the expected location
-        getManagementContext()
-        EntityStartUtils.startEntity(montereyEastGroup, [montereyEastLocation])
-        EntityStartUtils.startEntity(amazonEuropeGroup, [amazonEuropeLocation])
-        // deployed = true ReadOnlyPropertyException: Cannot set readonly property
+    private static AwsLocation newAwsUsEastLocation() {
+        final String REGION_NAME = "us-east-1" // "eu-west-1"
+        final String IMAGE_ID = REGION_NAME+"/"+"ami-0859bb61" // "ami-d7bb90a3"
+        final String IMAGE_OWNER = "411009282317"
+        final String SSH_PUBLIC_KEY_PATH = "/Users/aled/id_rsa.junit.pub"
+        final String SSH_PRIVATE_KEY_PATH = "/Users/aled/id_rsa.junit.private"
+        
+        AWSCredentialsFromEnv creds = new AWSCredentialsFromEnv();
+        AwsLocation result = new AwsLocation(identity:creds.getAWSAccessKeyId(), credential:creds.getAWSSecretKey(), providerLocationId:REGION_NAME)
+        result.setTagMapping([MyEntityType:[
+                imageId:IMAGE_ID,
+                providerLocationId:REGION_NAME,
+                sshPublicKey:new File(SSH_PUBLIC_KEY_PATH),
+                sshPrivateKey:new File(SSH_PRIVATE_KEY_PATH),
+            ]]) //, imageOwner:IMAGE_OWNER]])
+        return result
     }
-
-    public void stop() {
-        montereyEastGroup.stop()
-        amazonEuropeGroup.stop()
+    
+    private static FixedListMachineProvisioningLocation newMontereyEastLocation() {
+        // The definition of the Monterey East location
+        final Collection<SshMachineLocation> MONTEREY_EAST_PUBLIC_ADDRESSES = [
+                '216.48.127.224', '216.48.127.225', // east1a/b
+                '216.48.127.226', '216.48.127.227', // east2a/b
+                '216.48.127.228', '216.48.127.229', // east3a/b
+                '216.48.127.230', '216.48.127.231', // east4a/b
+                '216.48.127.232', '216.48.127.233', // east5a/b
+                '216.48.127.234', '216.48.127.235'  // east6a/b
+                ].collect { new SshMachineLocation(address: InetAddress.getByName(it), userName: 'cdm') }
+                
+        MachineProvisioningLocation<SshMachineLocation> result =
+            new FixedListMachineProvisioningLocation<SshMachineLocation>(machines: MONTEREY_EAST_PUBLIC_ADDRESSES)
+        return result
     }
-
-    public void restart() {
-        throw new UnsupportedOperationException()
-    }
-
-
 }
