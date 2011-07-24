128b01675a70dabbf14da35ddc0aad7ca11b2b6f
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-24 04:31:57 +0100

    Changes to launcher project to keep Eclipse happy

--- launcher/src/main/java/brooklyn/launcher/Activator.java
+++ launcher/src/main/java/brooklyn/launcher/Activator.java
@@ -1,16 +1,13 @@
 package brooklyn.launcher;
 
-import brooklyn.management.ManagementContext;
-import brooklyn.management.internal.LocalManagementContext;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Activator implements BundleActivator {
 
 	public void start(BundleContext context) throws Exception {
     	// TODO implement this class
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
  	}
 
 	public void stop(BundleContext context) throws Exception {
