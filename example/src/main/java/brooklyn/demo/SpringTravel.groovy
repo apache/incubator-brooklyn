f09cea28156c6fe095d9215e05c2766452ebb352
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-24 04:32:45 +0100

    ENGR-1695: Refactor demo application script into a separate package and split out the WebCluster entity

--- example/src/main/java/brooklyn/demo/SpringTravel.groovy
+++ example/src/main/java/brooklyn/demo/SpringTravel.groovy
@@ -1,4 +1,4 @@
-package brooklyn.example
+package brooklyn.demo
 
 import brooklyn.entity.basic.AbstractApplication
 import brooklyn.entity.trait.Startable
@@ -42,72 +42,7 @@ public class MultiLocationWebAppDemo extends AbstractApplication implements Star
     private static final Map MONTEREY_EAST_COORDS = [ 'latitude' : 41.10361, 'longitude' : -73.79583 ] // Hawthorne, NY
     private static final Map EDINBURGH_COORDS = [ 'latitude' : 55.94944, 'longitude' : -3.16028 ] // Edinburgh, Scotland
     
-    /**
-     * This group contains all the sub-groups and entities that go in to a single location.
-     * These are:
-     * <ul><li>a @{link DynamicCluster} of @{link JavaWebApp}s</li>
-     * <li>a cluster controller</li>
-     * <li>a @{link Policy} to resize the DynamicCluster</li></ul>
-     */
-    private static class WebClusterEntity extends AbstractEntity implements Startable {
-        private static final String springTravelPath
-        private static final String warName = "swf-booking-mvc.war"
-
-        private DynamicWebAppCluster cluster
-        private NginxController controller
-        private Policy policy
-
-        static {
-            URL resource = MultiLocationWebAppDemo.class.getClassLoader().getResource(warName)
-            Preconditions.checkState resource != null, "Unable to locate resource $warName"
-            springTravelPath = resource.getPath()
-        }
-
-        WebClusterEntity(Map props, Entity owner = null) {
-            super(props, owner)
-            def template = { Map properties ->
-                    def server = new TomcatServer(properties)
-                    server.setConfig(JavaApp.SUGGESTED_JMX_PORT, 32199)
-                    server.setConfig(JavaWebApp.SUGGESTED_HTTP_PORT, 8080)
-                    server.setConfig(TomcatServer.SUGGESTED_SHUTDOWN_PORT, 31880)
-                    server.setConfig(JavaWebApp.WAR, springTravelPath)
-                    return server;
-                }
-            cluster = new DynamicWebAppCluster(newEntity:template, this)
-            cluster.setConfig(DynamicCluster.INITIAL_SIZE, 0)
-        }
-
-        // FIXME: why am I implementing these?
-        void start(Collection<? extends Location> locations) {
-            cluster.start(locations)
-            cluster.resize(1)
-            
-            controller = new NginxController(
-                owner:this,
-                cluster:cluster,
-                domain:'cloudsoft.geopaas.org',
-                port:8000,
-                portNumberSensor:JavaWebApp.HTTP_PORT
-            )
-
-            policy = new ResizerPolicy(JavaWebApp.AVG_REQUESTS_PER_SECOND)
-            policy.setMinSize(1)
-            policy.setMaxSize(5)
-            policy.setMetricLowerBound(10)
-            policy.setMetricUpperBound(100)
-            policy.setEntity(cluster)
-
-            controller.start(locations)
-        }
-        void stop() {
-            controller.stop()
-            cluster.stop()
-        }
-        void restart() {
-            throw new UnsupportedOperationException()
-        }
-    }
-
+   
     final DynamicFabric fabric
     final DynamicGroup nginxEntities
     
