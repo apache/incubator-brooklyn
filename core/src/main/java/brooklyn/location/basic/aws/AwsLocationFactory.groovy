eb4ed75fdc5e6e226c9173d83f5c7f721aa10c60
Aled Sage <aled@cloudsoftcorp.com>
2011-07-25 15:19:35 +0100

    Locations: added streetAddress and displayName

--- core/src/main/java/brooklyn/location/basic/aws/AwsLocationFactory.groovy
+++ core/src/main/java/brooklyn/location/basic/aws/AwsLocationFactory.groovy
@@ -6,12 +6,39 @@ import org.jclouds.Constants
 
 class AwsLocationFactory {
 
+    // FIXME streetAddress is temporary, until we get lat-lon working in google maps properly
+    
     private static final Map locationSpecificConf = [
-            "us-west-1":[ providerLocationId:"us-west-1", 'latitude' : 40.0d, 'longitude' : -120.0d, iso3166:"US-CA" ], // Northern California (approx)
-            "us-east-1":[ providerLocationId:"us-east-1", 'latitude' : 38.0d, 'longitude' : -76.0d, iso3166:"US-VA" ], // Northern Virginia (approx)
-            "eu-west-1":[ providerLocationId:"eu-west-1", 'latitude' : 53.34778d, 'longitude' : -6.25972d, iso3166:"IE" ], // Dublin, Ireland
-            "ap-southeast-1":[ providerLocationId:"ap-southeast-1", 'latitude' : 0d, 'longitude' : 0d, iso3166:"SG" ],
-            "ap-northeast-1":[ providerLocationId:"ap-northeast-1", 'latitude' : 0d, 'longitude' : 0d, iso3166:"JP" ]
+            "us-west-1":[ 
+                providerLocationId:"us-west-1",
+                displayName:"AWS us-west", 
+                streetAddress:"California",
+                'latitude' : 40.0d, 'longitude' : -120.0d, 
+                iso3166:"US-CA" ], // Northern California (approx)
+            "us-east-1":[ 
+                providerLocationId:"us-east-1", 
+                displayName:"AWS us-east", 
+                streetAddress:"Virginia",
+                'latitude' : 38.0d, 'longitude' : -76.0d, 
+                iso3166:"US-VA" ], // Northern Virginia (approx)
+            "eu-west-1":[ 
+                providerLocationId:"eu-west-1", 
+                displayName:"AWS eu-west", 
+                streetAddress:"Dublin, Ireland",
+                'latitude' : 53.34778d, 'longitude' : -6.25972d, 
+                iso3166:"IE" ], // Dublin, Ireland
+            "ap-southeast-1":[ 
+                providerLocationId:"ap-southeast-1", 
+                displayName:"AWS ap-southeast", 
+                streetAddress:"Singapore",
+                'latitude' : 0d, 'longitude' : 0d, 
+                iso3166:"SG" ],
+            "ap-northeast-1":[ 
+                providerLocationId:"ap-northeast-1", 
+                streetAddress:"Tokyo, Japan",
+                displayName:"AWS ap-northeast", 
+                'latitude' : 0d, 'longitude' : 0d, 
+                iso3166:"JP" ]
             ]
     
     private final Map conf
