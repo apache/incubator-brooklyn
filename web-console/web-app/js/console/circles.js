759984f770df05a02b97ad39e6247141e1ac271e
Alistair MacLeod <alistair.macleod@cloudsoftcorp.com>
2011-07-25 18:10:57 +0100

    ENGR-1703 now draw a hardcoded circle

--- web-console/web-app/js/console/circles.js
+++ web-console/web-app/js/console/circles.js
@@ -1,14 +1,43 @@
 Brooklyn.location = (function() {
-  function init() {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var map;
+
+    function drawCircles() {
+        var lat = 55;
+        var lng = -2.5;
+
+        var circle_latlong = new google.maps.LatLng(lat, lng);
+        var circle_options = {
+            map: map,
+            center: circle_latlong,
+            clickableboolean: false,
+            fillColor: "#FF0000",
+            fillOpacity: 0.4,
+            radius: 50000, // meters
+            strokeColor: "#FF0000",
+            strokeOpacity: 1,
+            strokeWeight: 1,
+            zIndex: 1
+        };
+
+        var circle = new google.maps.Circle(circle_options);
+    }
+
+  function drawMap() {
+    var latlng = new google.maps.LatLng(55.6, -2.5);
     var myOptions = {
-      zoom: 8,
+      zoom: 2,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-        myOptions);
+      map = new google.maps.Map(document.getElementById("map_canvas"),
+                                myOptions);
   }
+
+    function init() {
+        drawMap();
+        drawCircles();
+    }
+
     return {init: init};
 })();
 
