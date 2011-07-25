01b3b5f62fefd002d6d1662b05a9835e57621c37
Alistair MacLeod <alistair.macleod@cloudsoftcorp.com>
2011-07-25 16:27:12 +0100

    ENGR-1703 Starting Circles View

---
+++ web-console/web-app/js/console/circles.js
@@ -0,0 +1,15 @@
+Brooklyn.location = (function() {
+  function init() {
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+      zoom: 8,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        myOptions);
+  }
+    return {init: init};
+})();
+
+$(document).ready(Brooklyn.location.init);
