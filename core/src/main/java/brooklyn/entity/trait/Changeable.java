cad5feb41fcb8f2c3914166599ca717a13cea7a8
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-07-26 17:18:13 +0100

    Fix typo in GROUP_SIZE sensor description

--- core/src/main/java/brooklyn/entity/trait/Changeable.java
+++ core/src/main/java/brooklyn/entity/trait/Changeable.java
@@ -8,7 +8,7 @@ import brooklyn.event.basic.BasicNotificationSensor;
  * A collection of entities that can change.
  */
 public interface Changeable {
-    BasicAttributeSensor<Integer> GROUP_SIZE = new BasicAttributeSensor<Integer>(Integer.class, "group.members.count", "Number of memebers");
+    BasicAttributeSensor<Integer> GROUP_SIZE = new BasicAttributeSensor<Integer>(Integer.class, "group.members.count", "Number of members");
     BasicNotificationSensor<Entity> MEMBER_ADDED = new BasicNotificationSensor<Entity>(Entity.class, "group.members.added", "Entity added to group members");
     BasicNotificationSensor<Entity> MEMBER_REMOVED = new BasicNotificationSensor<Entity>(Entity.class, "group.members.removed", "Entity removed from group members");
 }
