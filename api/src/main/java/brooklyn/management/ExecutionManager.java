165a4194d19bc2c35ecfca1d120dba56f6a8ae04
Andrew Donald Kennedy <grkvlt@apache.org>
2011-07-26 01:33:36 +0100

    Tidy Javadoc

--- api/src/main/java/brooklyn/management/ExecutionManager.java
+++ api/src/main/java/brooklyn/management/ExecutionManager.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Callable;
 /** 
  * This class manages the execution of a number of jobs with tags.
  * 
- * <p>
  * It is like an executor service (and it ends up delegating to one) but adds additional support
  * where jobs can be:
  * <ul>
@@ -18,6 +17,7 @@ import java.util.concurrent.Callable;
  * <li>Treated as {@link Task} instances (see below)
  * <li>Given powerful synchronization capabilities
  * </ul>
+ * <p>
  * The advantage of treating them as {@link Task} instances include: 
  * <ul>
  * <li>Richer status information
@@ -26,32 +26,39 @@ import java.util.concurrent.Callable;
  * <li>Grid and multi-machine support) 
  * </ul>
  * <p>
- * For usage instructions see {@link #submit(Map, Task)}, and for examples see the various *ExecutionTest* and *TaskTest* instances.
+ * For usage instructions see {@link #submit(Map, Task)}, and for examples see the various
+ * {@code ExecutionTest} and {@code TaskTest} instances.
  * <p>
  * It has been developed for multi-location provisioning and management to track work being
  * done by each {@link Entity}.
  */
-@SuppressWarnings("rawtypes")
 public interface ExecutionManager {
-    public Set<Task> getTasksWithTag(Object tag);
-    public Set<Task> getTasksWithAnyTag(Iterable tags);
-    public Set<Task> getTasksWithAllTags(Iterable tags);
+    public Set<Task<?>> getTasksWithTag(Object tag);
+
+    public Set<Task<?>> getTasksWithAnyTag(Iterable<String> tags);
+
+    public Set<Task<?>> getTasksWithAllTags(Iterable<String> tags);
+
     /** returns all tags known to this manager (immutable) */
     public Set<Object> getTaskTags();
+
     /** returns all tasks known to this manager (immutable) */
-    public Set<Task> getAllTasks();
+    public Set<Task<?>> getAllTasks();
 
     /** see {@link #submit(Map, Task)} */
-    public Task submit(Runnable r);
+    public Task<?> submit(Runnable r);
+
     /** see {@link #submit(Map, Task)} */
-    public Task submit(Callable r);
+    public <T> Task<T> submit(Callable<T> c);
+
     /** see {@link #submit(Map, Task)} */
-    public Task submit(Task task);
+    public <T> Task<T> submit(Task<T> task);
     
     /** see {@link #submit(Map, Task)} */
-    public Task submit(Map flags, Runnable r);
+    public Task<?> submit(Map<?, ?> flags, Runnable r);
+
     /** see {@link #submit(Map, Task)} */
-    public Task submit(Map flags, Callable r);
+    public <T> Task<T> submit(Map<?, ?> flags, Callable<T> c);
 
     /**
      * Submits the given {@link Task} associated with the given bucket.
@@ -60,7 +67,7 @@ public interface ExecutionManager {
      * <ul>
      * <li><em>tag</em> - A single object to be used as a tag for looking up the task
      * <li><em>tags</em> - A {@link Collection} of object tags each of which the task should be associated
-XXX     * <li><em>synchId</em> - A string, or {@link Collection} of strings, representing a category on which an object should own a synch lock 
+     * <li><em>synchId</em> - A string, or {@link Collection} of strings, representing a category on which an object should own a synch lock 
      * <li><em>synchObj</em> - A string, or {@link Collection} of strings, representing a category on which an object should own a synch lock 
      * <li><em>newTaskStartCallback</em> - A {@link Closure} that will be invoked just before the task starts if it starts as a result of this call
      * <li><em>newTaskEndCallback</em> - A {@link Closure} that will be invoked when the task completes if it starts as a result of this call
@@ -68,7 +75,7 @@ XXX     * <li><em>synchId</em> - A string, or {@link Collection} of strings, rep
      * Callbacks run in the task's thread, and if the callback is a closure it is passed the task for convenience. The closure can be any of the
      * following types; either a {@link Closure}, {@link Runnable} or {@link Callable}.
      */
-    public Task submit(Map flags, Task task);
+    public <T> Task<T> submit(Map<?, ?> flags, Task<T> task);
 
     //following also used, may be moved up to interface
 //    void setTaskPreprocessorForTag(Object tag, Class<? extends TaskPreprocessor> preprocessor);
