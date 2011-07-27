54ff0c5e9839bc1b292f7adbf9f12c090c222c7e
Richard Downer <richard.downer@cloudsoftcorp.com>
2011-07-27 16:33:35 +0100

    Change Demo.groovy to instantiate itself and do its work in an instance instead of a static method. Extract application creation into protected method to allow subclasses to switch the Brooklyn application

--- example/src/main/java/brooklyn/demo/Demo.groovy
+++ example/src/main/java/brooklyn/demo/Demo.groovy
@@ -3,6 +3,7 @@ package brooklyn.demo
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import brooklyn.entity.basic.AbstractApplication
 import brooklyn.launcher.WebAppRunner
 import brooklyn.location.Location
 import brooklyn.management.internal.AbstractManagementContext
@@ -13,14 +14,18 @@ public class Demo {
     public static final List<String> DEFAULT_LOCATIONS = [ Locations.LOCALHOST ]
 
     public static void main(String[] argv) {
+        Demo demoRunner = new Demo()
+        demoRunner.start(argv)
+    }
+
+    protected void start(String[] argv) {
         // Parse arguments for location ids and resolve each into a location
         List<String> ids = argv.length == 0 ? DEFAULT_LOCATIONS : Arrays.asList(argv)
         println "Starting in locations: "+ids
         List<Location> locations = Locations.getLocationsById(ids)
 
         // Initialize the Spring Travel application entity
-        SpringTravel app = new SpringTravel(name:'brooklyn-wide-area-demo',
-                                            displayName:'Brooklyn Wide-Area Spring Travel Demo Application')
+        AbstractApplication app = createApplication()
 
         // Locate the management context
         AbstractManagementContext context = app.getManagementContext()
@@ -42,5 +47,11 @@ public class Demo {
             app?.stop()
             web?.stop()
         }
+    }
+
+    protected AbstractApplication createApplication() {
+        SpringTravel app = new SpringTravel(name:'brooklyn-wide-area-demo',
+                displayName:'Brooklyn Wide-Area Spring Travel Demo Application')
+        return app
     }
 }
