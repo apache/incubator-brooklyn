2eea2657c5bfcf8b581ad213a1b281b2e1b6eca1
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-07-27 11:24:03 +0100

    Poll for changed locations, as they're not acessible at the time the member-changed listeners are invoked

--- extra/src/main/java/brooklyn/entity/dns/AbstractGeoDnsService.groovy
+++ extra/src/main/java/brooklyn/entity/dns/AbstractGeoDnsService.groovy
@@ -1,22 +1,30 @@
 package brooklyn.entity.dns
 
+import brooklyn.entity.Entity;
 import java.util.Map
 import java.util.Set
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import edu.emory.mathcs.backport.java.util.concurrent.ScheduledThreadPoolExecutor;
+
 import brooklyn.entity.Entity
+import brooklyn.entity.Group
 import brooklyn.entity.basic.AbstractEntity
 import brooklyn.entity.basic.AbstractGroup
+import brooklyn.entity.basic.DynamicGroup
 import brooklyn.entity.group.AbstractMembershipTrackingPolicy
+import brooklyn.management.internal.CollectionChangeListener
+import brooklyn.management.internal.LocalManagementContext
 
 
 abstract class AbstractGeoDnsService extends AbstractEntity {
     private static final Logger log = LoggerFactory.getLogger(AbstractGeoDnsService.class);
-//    public static AttributeSensor<Set<HostGeoInfo>> TARGET_HOSTS = [ Set.class, "target.hosts" ];
-    
-    private final Map<Entity, HostGeoInfo> targetHosts = new HashMap<Entity, HostGeoInfo>();
+    protected Group targetEntityProvider = null;
+    protected Map<Entity, HostGeoInfo> targetHosts = new HashMap<Entity, HostGeoInfo>();
     
 
     public AbstractGeoDnsService(Map properties = [:], Entity owner = null) {
@@ -24,25 +32,57 @@ abstract class AbstractGeoDnsService extends AbstractEntity {
     }
     
     // FIXME: also accept a closure in case the group doesn't exist yet
-    public void setTargetEntityProvider(AbstractGroup group) {
-        AbstractMembershipTrackingPolicy amtp = new AbstractMembershipTrackingPolicy() {
-            protected void onEntityAdded(Entity entity) { addTargetHost(entity); }
-            protected void onEntityRemoved(Entity entity) { removeTargetHost(entity); }
-        }
-        addPolicy(amtp);
-        amtp.setGroup(group);
+    public void setTargetEntityProvider(final AbstractGroup group) {
+        this.targetEntityProvider = group;
+        
+        // TODO: remove polling once locations can be determined via subscriptions
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
+            new Runnable() {
+                public void run() { refreshGroupMembership(); }
+            }, 0, 5, TimeUnit.SECONDS
+        );
     }
 
     protected abstract void reconfigureService(Set<HostGeoInfo> targetHosts);
     
+    // TODO: remove group member polling once locations can be determined via subscriptions
+    protected void refreshGroupMembership() {
+        try {
+            if (targetEntityProvider == null)
+                return;
+            if (targetEntityProvider instanceof DynamicGroup)
+                ((DynamicGroup) targetEntityProvider).rescanEntities();
+            
+            boolean changed = false; 
+            targetEntityProvider.members.each { Entity e ->
+                if (targetHosts.containsKey(e))
+                    return;
+                HostGeoInfo hgi = HostGeoInfo.fromEntity(e)
+                if (hgi == null)
+                    log.warn("Failed to derive geo information for entity $e");
+                else {
+                    targetHosts.put(e, hgi);
+                    changed = true;
+                }
+            }
+            // TODO: handle member removal
+            if (changed)
+                update();
+            
+        } catch (Exception e) {
+            log.error("Problem refreshing group membership: $e", e);
+        }
+    }
+    
     protected void addTargetHost(Entity e) {
+        log.info("Adding DNS redirection target entity: $e");
         if (targetHosts.containsKey(e)) {
-            log.warn("Ignoring already-added entity "+e);
+            log.warn("Ignoring already-added entity $e");
             return;
         }
         HostGeoInfo hgi = HostGeoInfo.fromEntity(e)
         if (hgi == null) {
-            log.warn("Failed to derive geo information for entity "+e);
+            log.warn("Failed to derive geo information for entity $e");
         } else {
             targetHosts.put(e, hgi);
             update();
@@ -56,7 +96,6 @@ abstract class AbstractGeoDnsService extends AbstractEntity {
     
     protected void update() {
         reconfigureService(new LinkedHashSet<HostGeoInfo>(targetHosts.values()));
-//        emit(TARGET_HOSTS, targetHosts);
     }
     
 }
