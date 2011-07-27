1f0c8f598b6a21bffefee37663e659f6d6528832
Aled Sage <aled@cloudsoftcorp.com>
2011-07-27 04:00:59 +0100

    Added display name for nginx-group

--- example/src/main/java/brooklyn/demo/SpringTravel.groovy
+++ example/src/main/java/brooklyn/demo/SpringTravel.groovy
@@ -60,7 +60,7 @@ public class SpringTravel extends AbstractApplication {
         
         Preconditions.checkState fabric.displayName == "Fabric"
 
-        nginxEntities = new DynamicGroup([:], this, { Entity e -> (e instanceof NginxController) })
+        nginxEntities = new DynamicGroup([displayName:"nginx group"], this, { Entity e -> (e instanceof NginxController) })
 
         geoDns = new GeoscalingDnsService(username: 'cloudsoft', password: 'cl0uds0ft',
             primaryDomainName: 'geopaas.org', smartSubdomainName: 'brooklyn',
