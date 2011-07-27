e1d65def6d456e282150ad17bb8f22a19c9b69f8
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-07-27 11:33:06 +0100

    Specify correct import

--- extra/src/main/java/brooklyn/entity/dns/AbstractGeoDnsService.groovy
+++ extra/src/main/java/brooklyn/entity/dns/AbstractGeoDnsService.groovy
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import edu.emory.mathcs.backport.java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import brooklyn.entity.Entity
 import brooklyn.entity.Group
