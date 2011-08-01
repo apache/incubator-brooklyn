cf864de0424c467ea286ba37e2d7ec8d5ac40833
Aled Sage <aled@cloudsoftcorp.com>
2011-08-01 15:36:28 +0100

    Monterey entity tests: fix imports

--- com.cloudsoftcorp.monterey.brooklyn/src/test/java/com/cloudsoftcorp/monterey/brooklyn/entity/MontereyBrooklynProvisioningTest.groovy
+++ com.cloudsoftcorp.monterey.brooklyn/src/test/java/com/cloudsoftcorp/monterey/brooklyn/entity/MontereyBrooklynProvisioningTest.groovy
@@ -27,6 +27,15 @@ import org.testng.annotations.AfterMethod
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
+import brooklyn.entity.ConfigKey
+import brooklyn.entity.Group
+import brooklyn.entity.basic.AbstractApplication
+import brooklyn.entity.basic.EntityLocal
+import brooklyn.event.AttributeSensor
+import brooklyn.location.Location
+import brooklyn.location.basic.FixedListMachineProvisioningLocation
+import brooklyn.location.basic.SshMachineLocation
+
 import com.cloudsoftcorp.monterey.CloudsoftThreadMonitoringTestFixture
 import com.cloudsoftcorp.monterey.clouds.dto.CloudAccountDto
 import com.cloudsoftcorp.monterey.clouds.dto.CloudEnvironmentDto
