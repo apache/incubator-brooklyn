2e63e8f7abbea3edeec1c477290c70c5896bd562
aplowe <adam@aplowe.com>
2011-08-02 18:25:56 +0100

    ENGR-1612 - Adding acceptance test to verify Activity tab is in place as expected

---
+++ acceptance/src/test/java/brooklyn/web/console/test/ActivityTabTest.groovy
@@ -0,0 +1,25 @@
+package brooklyn.web.console.test
+
+import org.testng.annotations.Test
+import static brooklyn.web.console.test.SeleniumTest.selenium
+import static brooklyn.web.console.test.SeleniumTest.waitFor
+import static org.testng.Assert.assertTrue
+
+public class ActivityTabTest {
+
+    @Test(groups=["Selenium1"])
+    public void effectorsTabShows() throws Exception {
+        selenium.open("/entity/");
+        selenium.click("link=Detail")
+
+        // Wait for tree to load
+        waitFor({selenium.isTextPresent("tomcat node 1a.3")});
+
+        selenium.click("link=tomcat node 1a.3")
+        selenium.click("link=Activity");
+        waitFor({selenium.isTextPresent("Activity Status")});
+        waitFor({selenium.isTextPresent("Update values")});
+        waitFor({selenium.isTextPresent("Waiting")});
+    }
+
+}
