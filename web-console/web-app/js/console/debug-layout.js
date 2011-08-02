4d2b27d204f8fbd63565b436a1800fd8db937b61
Don Tran <don.tran@cloudsoftcorp.com>
2011-08-02 12:01:04 +0100

    Removed duplicated JS

--- web-console/web-app/js/console/debug-layout.js
+++
@@ -1,5 +0,0 @@
- $(document).ready(function () {
-        //var debugLayout = $('body').layout({ applyDefaultStyles:true });
-        //var insideTabLayout = $('#insideTab').layout({ applyDefaultStyles:true });
-        $("#test-layout").layout({ applyDefaultStyles:true });
-    });
