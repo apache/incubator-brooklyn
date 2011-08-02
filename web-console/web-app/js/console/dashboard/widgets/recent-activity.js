526d376b83f6ec835f371a189e65eb394159c820
Jhey Tompkins <jhey.tompkins@gmail.com>
2011-08-02 14:47:09 +0100

    Added the file for recent activity widget. Added Small and Large widget areas to dashbaord. Map Widget as standard is now Large but can be resized by moving further down page

---
+++ web-console/web-app/js/console/dashboard/widgets/recent-activity.js
@@ -0,0 +1,40 @@
+Brooklyn.activitywidget = ( function(){
+//Code for the recent activity widget.
+
+/* INITIAL CONFIG */
+    var id = "#recent-activity-table";
+    var aoColumns = [   { "mDataProp": "entityDisplayName", "sTitle": "Entity Name", "sWidth":"16%" },
+                        { "mDataProp": "displayName", "sTitle": "Task Name", "sWidth":"17%" },
+                        { "mDataProp": "submitTimeUtc", "sTitle": "Submit time", "sWidth":"17%" },
+                        { "mDataProp": "endTimeUtc", "sTitle": "End time", "sWidth":"17%" },
+                        { "mDataProp": "currentStatus", "sTitle": "Status", "sWidth":"15%" }];
+    
+    function updateWidgetTable(json){
+        Brooklyn.tabs.getDataTable(id, ".", aoColumns, updateLog, json);
+    }
+
+    function updateLog(event){
+        alert("Logs up to date");
+    }
+
+
+    function init(){
+        //call a service to get the JSON and then update the table
+        //For now it's started.
+        //alert("recent activity obtained");
+        //getRecentActivity();
+    }
+
+    function getRecentActivity(){
+        $.getJSON("recentactivity", updateWidgetTable).error(
+            function(){$(Brooklyn.eventBus).trigger('update_failed', 'Could not obtain recent activity');}
+        );
+    }
+
+    return {
+        init: init
+    };
+
+
+})();
+$(document).ready( Brooklyn.activitywidget.init );
