9726e0ff2893996ff7586ac28f76a57f0cd5aa99
aplowe <adam@aplowe.com>
2011-08-03 17:23:07 +0100

    Using selenium 1 API

--- acceptance/src/test/java/brooklyn/web/console/test/EntityTreeTest.groovy
+++ acceptance/src/test/java/brooklyn/web/console/test/EntityTreeTest.groovy
@@ -1,22 +1,19 @@
 package brooklyn.web.console.test
 
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.firefox.FirefoxProfile
 import org.testng.annotations.Test
+import static brooklyn.web.console.test.SeleniumTest.selenium
+import static brooklyn.web.console.test.SeleniumTest.waitFor
 import static org.testng.Assert.assertTrue
 
 public class EntityTreeTest {
 
-    @Test
-    public void entityControllerShowsEntities() throws Exception {
-        FirefoxProfile profile = new FirefoxProfile()
-        //TODO There must be another way to get past the confirmation dialog, needs investigating
-        profile.setPreference("network.http.phishy-userpass-length", 255)
-        WebDriver driver = new FirefoxDriver(profile)
-        driver.get("http://admin:password@localhost:9090/detail/")
-        driver.findElementById("jstree").text.contains("tomcat node 1a.1")
-        driver.quit()
+    @Test(groups=["Selenium1"])
+    public void effectorsTabShows() throws Exception {
+        selenium.open("/detail/");
+
+        // Wait for tree to load
+        waitFor({selenium.isTextPresent("tomcat tier")});
+        assertTrue(selenium.isTextPresent("tomcat tier"));
     }
 
 }
