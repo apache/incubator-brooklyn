aa96725b4588cc4706a85993c219fa4ad682d351
Don Tran <don.tran@cloudsoftcorp.com>
2011-08-04 11:23:40 +0100

    ENGR-1796: changed time to UTC for sensors tab

--- web-console/grails-app/utils/brooklyn/web/console/entity/SensorSummary.groovy
+++ web-console/grails-app/utils/brooklyn/web/console/entity/SensorSummary.groovy
@@ -13,19 +13,26 @@ public class SensorSummary {
     public final Object value
     public final String timestamp
 
-    private static DateFormat formatter = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy ")
+    // formatter is not thread-safe; use thread-local storage
+    private static final ThreadLocal<DateFormat> formatter = new ThreadLocal<DateFormat>() {
+        @Override protected DateFormat initialValue() {
+            SimpleDateFormat result = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            result.setTimeZone(TimeZone.getTimeZone("GMT"))
+            return result
+        }
+    };
 
     public SensorSummary(Sensor sensor, Object value) {
         this.name = sensor.name
         this.description = sensor.description
         this.value = value
-        this.timestamp = formatter.format(new Date())
+        this.timestamp = formatter.get().format(new Date())
     }
 
     public SensorSummary(SensorEvent event) {
         this.name = event.sensor.name
         this.description = event.sensor.description
         this.value = event.value
-        this.timestamp = formatter.format(new Date(event.timestamp))
+        this.timestamp = formatter.get().format(new Date(event.timestamp))
     }
 }
