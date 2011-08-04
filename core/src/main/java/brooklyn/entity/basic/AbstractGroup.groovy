4c666f993063eb847c18f80eb104126f3986981b
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-08-04 18:14:11 +0100

    Fix return value for removeMember() to indicate whether members collection was changed as a result of the call

--- core/src/main/java/brooklyn/entity/basic/AbstractGroup.groovy
+++ core/src/main/java/brooklyn/entity/basic/AbstractGroup.groovy
@@ -29,13 +29,17 @@ public abstract class AbstractGroup extends AbstractEntity implements Group, Cha
 	    }
     }
  
+    /**
+     * Returns <code>true</code> if the group was changed as a result of the call.
+     */
     public boolean removeMember(Entity member) {
         synchronized (members) {
-            if (member != null && members.remove(member)) {
+            boolean changed = (member != null && members.remove(member))
+            if (changed) {
 	            emit(MEMBER_REMOVED, member)
 	            setAttribute(Changeable.GROUP_SIZE, currentSize)
 	        }
-	        member
+	        changed
         }
     }
  
