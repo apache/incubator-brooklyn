61be46e1a5fe78c66b88e9ee871958924cf1836a
Andrew Donald Kennedy <grkvlt@apache.org>
2011-08-07 14:28:19 +0100

    Javadoc

--- core/src/main/java/brooklyn/event/adapter/ValueProvider.groovy
+++ core/src/main/java/brooklyn/event/adapter/ValueProvider.groovy
@@ -1,7 +1,11 @@
 package brooklyn.event.adapter
 
+/**
+ * TODO javadoc
+ */
 public interface ValueProvider<T> {
-
+    /**
+     * This method is called by an adapter to compute a value for a {@link Sensor}.
+     */
     public T compute();
-        
 }
