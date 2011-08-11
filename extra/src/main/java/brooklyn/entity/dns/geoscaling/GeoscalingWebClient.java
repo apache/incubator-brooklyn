b079449f5e563d621031f50ebadb89046e8334e1
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2011-08-11 10:39:08 +0100

    Organised imports

--- extra/src/main/java/brooklyn/entity/dns/geoscaling/GeoscalingWebClient.java
+++ extra/src/main/java/brooklyn/entity/dns/geoscaling/GeoscalingWebClient.java
@@ -1,7 +1,6 @@
 package brooklyn.entity.dns.geoscaling;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.text.MessageFormat;
