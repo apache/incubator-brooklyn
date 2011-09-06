c8f10f3469e1d07cfb6567c9a678a25b82db16c3
Jhey Tompkins <jhey.tompkins@gmail.com>
2011-09-06 10:43:56 +0100

    Adding in forgotten General Purpose Policy

---
+++ core/src/main/java/brooklyn/policy/basic/GeneralPurposePolicy.groovy
@@ -0,0 +1,14 @@
+package brooklyn.policy.basic
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import brooklyn.policy.Policy
+
+public class GeneralPurposePolicy extends AbstractPolicy {
+
+    GeneralPurposePolicy(Map properties = [:]) {
+        super(properties)
+    }
+}
+
