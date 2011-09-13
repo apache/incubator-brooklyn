83a7feb2718d3cf85c07588b5e1c9386f1ed2d80
Jhey Tompkins <jhey.tompkins@gmail.com>
2011-09-13 13:22:57 +0100

    Removed methods from Policy interface for obtaining properties.

--- api/src/main/java/brooklyn/policy/Policy.java
+++ api/src/main/java/brooklyn/policy/Policy.java
@@ -5,38 +5,16 @@ package brooklyn.policy;
  */
 public interface Policy {
    /**
-     * A unique id for this location.
+     * A unique id for this policy.
      */
     String getId();
 
     /**
-     * Get the name assigned to this location.
+     * Get the name assigned to this policy.
      *
-     * @return the name assigned to the location.
+     * @return the name assigned to the policy.
      */
     String getName();
-    /**
-     * Returns <code>true</code> iff this location contains a property with the specified <code>key</code>. The
-     * property's value can be obtained by calling {@link #getLocationProperty}. This method only interrogates the
-     * immediate properties; the parent hierarchy is NOT searched in the event that the property is not found locally.
-     */
-    boolean hasPolicyProperty(String key);
-    
-    /**
-     * Returns the value of the property identified by the specified <code>key</code>. This method only interrogates the
-     * immediate properties; the parent hierarchy is NOT searched in the event that the property is not found locally.
-     * 
-     * NOTE: must not name this method 'getProperty' as this will clash with the 'magic' Groovy's method of the same
-     *       name, at which point everything stops working!
-     */
-    Object getPolicyProperty(String key);
-    
-    /**
-     * Like {@link #getLocationProperty}, but if the property is not defined on this location, searches recursively up
-     * the parent hierarchy until it is found, or the root is reached (when this method will return <code>null</code>).
-     */
-    Object findPolicyProperty(String key);
-
     /**
      * methods for actions which can be executed against a policy
      */
