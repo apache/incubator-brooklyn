9e090d168bebce12ded593e36caa34d870447230
Aled Sage <aled@Aled-Sages-MacBook-Pro.local>
2011-10-14 15:47:12 +0100

    ENGR-1668: added src/ to classpath in gemfire/start.sh (yuck!)

--- gemfire/start.sh
+++ gemfire/start.sh
@@ -6,4 +6,4 @@ PORT=$1
 CONFIG_FILE=$2
 ADDITIONAL_CLASSPATH=$3
 
-java -cp $SCRIPTS_DIR/bin:$SCRIPTS_DIR/lib/gemfire.jar:$SCRIPTS_DIR/lib/antlr.jar:$ADDITIONAL_CLASSPATH brooklyn.gemfire.demo.Server $PORT $CONFIG_FILE $SCRIPTS_DIR/gemfire.log $SCRIPTS_DIR/lib/gemfireLicense.zip
+java -cp $SCRIPTS_DIR/src:$SCRIPTS_DIR/bin:$SCRIPTS_DIR/lib/gemfire.jar:$SCRIPTS_DIR/lib/antlr.jar:$ADDITIONAL_CLASSPATH brooklyn.gemfire.demo.Server $PORT $CONFIG_FILE $SCRIPTS_DIR/gemfire.log $SCRIPTS_DIR/lib/gemfireLicense.zip
