def8b4e274850ce7e768628a05273dab7439fa3c
sjcorbett <sam.corbett@cloudsoftcorp.com>
2011-10-21 14:00:10 +0100

    Pom for building Gemfire and tweaks to start.sh

--- gemfire/start.sh
+++ gemfire/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 SCRIPTS_DIR=`cd \`dirname $0\` && pwd`
+TARGET="$SCRIPTS_DIR/target"
+LIB="$SCRIPTS_DIR/lib"
 
 PORT=$1
 CONFIG_FILE=$2
 ADDITIONAL_CLASSPATH=$3
 
-java -cp $SCRIPTS_DIR/src:$SCRIPTS_DIR/bin:$SCRIPTS_DIR/lib/gemfire.jar:$SCRIPTS_DIR/lib/antlr.jar:$ADDITIONAL_CLASSPATH brooklyn.gemfire.demo.Server $PORT $CONFIG_FILE $SCRIPTS_DIR/gemfire.log $SCRIPTS_DIR/lib/gemfireLicense.zip
+java -cp $SCRIPTS_DIR/src/main/java:$TARGET/classes:$LIB/gemfire.jar:$LIB/antlr.jar:$ADDITIONAL_CLASSPATH brooklyn.gemfire.demo.Server $PORT $CONFIG_FILE $TARGET/gemfire.log $LIB/gemfireLicense.zip
