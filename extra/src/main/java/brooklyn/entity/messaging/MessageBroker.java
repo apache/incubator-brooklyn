3c047aebb5f48b18b7b3ce6095959767b6c94d5b
Aled Sage <aled@Aled-Sages-MacBook-Pro.local>
2011-11-27 19:38:26 +0000

    Added MessageBroker marker interface (for use in monterey)

---
+++ extra/src/main/java/brooklyn/entity/messaging/MessageBroker.java
@@ -0,0 +1,9 @@
+package brooklyn.entity.messaging;
+
+import brooklyn.entity.Entity;
+
+/**
+ * Marker interface identifying message brokers.
+ */
+public interface MessageBroker extends Entity {
+}
