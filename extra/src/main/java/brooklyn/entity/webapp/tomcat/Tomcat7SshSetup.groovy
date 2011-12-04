6eae93fa2b9d3abc6acf1cc533586e1b2ec0a08a
Aled Sage <aled@Aled-Sages-MacBook-Pro.local>
2011-12-04 23:26:05 +0000

    TomcatServer: update tomcat version number

--- extra/src/main/java/brooklyn/entity/webapp/tomcat/Tomcat7SshSetup.groovy
+++ extra/src/main/java/brooklyn/entity/webapp/tomcat/Tomcat7SshSetup.groovy
@@ -12,7 +12,7 @@ import brooklyn.util.SshBasedJavaWebAppSetup
  */
 public class Tomcat7SshSetup extends SshBasedJavaWebAppSetup {
 	
-    public static final String DEFAULT_VERSION = "7.0.21"
+    public static final String DEFAULT_VERSION = "7.0.23"
     public static final String DEFAULT_INSTALL_DIR = DEFAULT_INSTALL_BASEDIR+"/"+"tomcat"
     public static final String DEFAULT_DEPLOY_SUBDIR = "webapps"
     public static final int DEFAULT_FIRST_HTTP_PORT = 8080
