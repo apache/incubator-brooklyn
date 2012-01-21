e2bf6dc6f7d4fcd20037320411e4ae9ae4603ed9
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2012-01-22 00:00:12 +0200

    version bumps: groovy 1.8.4, jclouds 1.3.1; and updated brooklyn refs to be consistently 0.3.0-SNAPSHOT

--- web-console/grails-app/conf/BuildConfig.groovy
+++ web-console/grails-app/conf/BuildConfig.groovy
@@ -7,18 +7,16 @@ grails.project.dependency.resolution = {
     inherits("global") {
         // disable ehcache (we don't use it); and log4j (we are embedded so have our own logger, usually;
         // with this as is it seems to delegate to our preferred logger (of container), so hurrah)
-        excludes 'ehcache', 
-                     'grails-plugin-logging', 'log4j', 
-                     'junit',
-                     'slf4j-api',
-                     'slf4j-log4j12'
-//                     'jcl-over-slf4j', 'jul-to-slf4j'
+        excludes 'ehcache' 
+
+        //have experimented excluding the following but it causes more problem that it's worth... sticking with log4j for now
+        // 'grails-plugin-logging', 'log4j', 'junit', 'slf4j-api', 'slf4j-log4j12' 'jcl-over-slf4j', 'jul-to-slf4j'
                      
         // following exclusions also recommended to resolve some sax / xml nasties
         // (which you may see if you add brooklyn-extra below and try grails run-app) ... but don't work 
         // "xml-apis", "xmlParserAPIs", "xalan"
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "info" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         grailsPlugins()
         grailsHome()
@@ -33,9 +31,9 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile 'brooklyn:brooklyn-api:0.2.0-SNAPSHOT', transitive:false 
-        compile 'brooklyn:brooklyn-core:0.2.0-SNAPSHOT', transitive:false
-        compile 'brooklyn:brooklyn-extra:0.2.0-SNAPSHOT', transitive:false
+        compile 'brooklyn:brooklyn-api:${appVersion}', transitive:false 
+        compile 'brooklyn:brooklyn-core:${appVersion}', transitive:false
+        compile 'brooklyn:brooklyn-extra:${appVersion}', transitive:false
         
         //instead of transitive:false the following might work better:
 //        excludes "org.codehaus.groovy","org.jclouds"
