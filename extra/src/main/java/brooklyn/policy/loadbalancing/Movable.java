fecdb3e25e52cdbe38d99087657d3d726489827c
Alasdair Hodge <alasdair.hodge@cloudsoftcorp.com>
2012-01-26 22:32:37 +0000

    Resolved spurious merge conflict

--- extra/src/main/java/brooklyn/policy/loadbalancing/Movable.java
+++ extra/src/main/java/brooklyn/policy/loadbalancing/Movable.java
@@ -1,25 +1,15 @@
 package brooklyn.policy.loadbalancing;
 
-<<<<<<< HEAD
 import brooklyn.entity.Entity;
 
 
 /**
  * Represents an item that can be migrated between entities.
-=======
-
-/**
- * TODO: javadoc
->>>>>>> 033c2c20b3a87953581285ec15d4dd6a7ff41e4c
  * 
  * @author splodge
  */
 public interface Movable {
     
-<<<<<<< HEAD
     public void move(Entity destination);
-=======
-    // TODO: move method?
->>>>>>> 033c2c20b3a87953581285ec15d4dd6a7ff41e4c
     
 }
