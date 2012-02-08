727ee9bd92140dd2411a1da4889ba3c6ed021cf4
Aled Sage <aled.sage@gmail.com>
2012-02-08 13:01:48 +0000

    AttributeMap: fix so can have two sensors where one's name is a suffix of the other

--- core/src/main/java/brooklyn/event/basic/AttributeMap.groovy
+++ core/src/main/java/brooklyn/event/basic/AttributeMap.groovy
@@ -23,74 +23,48 @@ public class AttributeMap implements Serializable {
      * The values are stored as nested maps, with the key being the constituent parts of the sensor.
      */
     // Note that we synchronize on the top-level map, to handle concurrent updates and and gets (ENGR-2111)
-    Map values = [:];
+    Map<Collection<String>, Object> values = [:];
     
     public AttributeMap(EntityLocal entity) {
         this.entity = entity;
     }
     
+    // TODO semantics of asMap have changed; previously returned a map-of-map-of-map etc for the values, arranged by the sensors' name parts.
+    // Now keys are Collection<String> for the name-parts of the sensor
     public Map asMap() { values }
-        
+    
     /** returns oldValue */
     public <T> T update(Collection<String> path, T newValue) {
-        Map map
-        String key
+        // TODO path must be ordered(and legal to contain duplicates like "a.b.a"; list would be better
         synchronized (values) {
-            Object val = values
-            path.each { 
-                if (!(val in Map)) {
-                    if (val!=null)
-                        log.debug "wrong type of value encountered when setting $path; expected map at $key but found $val; overwriting"
-                    val = [:]
-                    map.put(key, val)
-                }
-                map = val
-                key = it
-                val = map.get(it)
-            }
-            log.trace "putting at $path=$newValue for $entity"
-            def oldValue = map.put(key, newValue)
-            return oldValue
+            if (log.isTraceEnabled()) log.trace "setting sensor $path=$newValue for $entity"
+            return values.put(path, newValue)
         }
     }
     
     public <T> void update(Sensor<T> sensor, T newValue) {
         Preconditions.checkArgument(sensor in AttributeSensor, "AttributeMap can only update an attribute sensor's value, not %s", sensor)
-        def oldValue = update(sensor.getNameParts(), newValue)
-        log.trace "sensor {} set to {}", sensor.name, newValue
+        def oldValue = (T) update(sensor.getNameParts(), newValue)
         entity.emitInternal sensor, newValue
         oldValue
     }
     
     public Object getValue(Collection<String> path) {
+        // TODO previously this would return a map of the sub-tree if the path matched a prefix of a group of sensors, 
+        // or the leaf value if only one value. Arguably that is not required - what is/was the use-case?
+        // 
+        Preconditions.checkArgument(!path.isEmpty(), "path is empty")
         synchronized (values) {
-            return getValueRecurse(values, path)
+            return values.get(path);
         }
     }
     
     public <T> T getValue(Sensor<T> sensor) {
         synchronized (values) {
-            return getValueRecurse(values, sensor.getNameParts())
+            return values.get(sensor.getNameParts());
         }
     }
 
-    private static Object getValueRecurse(Map node, Collection path) {
-        if (node==null) return null
-        Preconditions.checkArgument(!path.isEmpty(), "field name is empty")
-        
-        def key = path[0]
-        Preconditions.checkState(key != null, "head of path is null")
-        Preconditions.checkState(key.length() > 0, "head of path is empty")
-        
-        def child = node.get(key)
-        Preconditions.checkState(child != null || path.size() > 0, "node $key is not present")
-        
-        if (path.size() > 1)
-            return getValueRecurse(child, path[1..-1])
-        else
-            return child
-    }
-    
     //ENGR-1458  interesting to use property change. if it works great.
     //if there are any issues with it consider instead just making attributesInternal private,
     //and forcing all changes to attributesInternal to go through update(AttributeSensor,...)
