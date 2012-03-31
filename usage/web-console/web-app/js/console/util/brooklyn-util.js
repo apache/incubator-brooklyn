10a5ce9bf0bc9ae4ab64efedf2adce6b54ec0f8b
sjcorbett <sam.corbett@cloudsoftcorp.com>
2012-03-31 16:29:58 +0100

    Formatting

--- usage/web-console/web-app/js/console/util/brooklyn-util.js
+++ usage/web-console/web-app/js/console/util/brooklyn-util.js
@@ -10,43 +10,39 @@ Brooklyn.util = (function(){
         $(Brooklyn.eventBus).bind("update", tab.handler.update);
     }
 
-    function typeTest(a, b) {
-        return (typeof a == typeof b)
-    }
-
     // Object equality test based on code in StackOverflow answer by Jean Vincent
     // http://stackoverflow.com/a/6713782/41195
     function testEquivalent(x, y) {
         // if both x and y are null or undefined and exactly the same
-        if ( x === y ) return true;
+        if (x === y) return true;
 
         // if they are not strictly equal, they both need to be Objects
-        if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+        if (!(x instanceof Object) || !(y instanceof Object)) return false;
 
         // they must have the exact same prototype chain, the closest we can do is
         // test there constructor.
-        if ( x.constructor !== y.constructor ) return false;
+        if (x.constructor !== y.constructor) return false;
 
-        for ( var p in x ) {
+        for (var p in x) {
             // other properties were tested using x.constructor === y.constructor
-            if ( ! x.hasOwnProperty( p ) ) continue;
+            if (!x.hasOwnProperty(p)) continue;
 
-            // allows to compare x[ p ] and y[ p ] when set to undefined
-            if ( ! y.hasOwnProperty( p ) ) return false;
+            // allows to compare x[p] and y[p] when set to undefined
+            if (!y.hasOwnProperty(p)) return false;
 
             // if they have the same strict value or identity then they are equal
-            if ( x[ p ] === y[ p ] ) continue;
+            if (x[p] === y[p]) continue;
 
             // Numbers, Strings, Functions, Booleans must be strictly equal
-            if ( typeof( x[ p ] ) !== "object" ) return false;
+            if (typeof(x[p]) !== "object") return false;
 
             // Objects and Arrays must be tested recursively
-            if ( ! testEquivalent2( x[ p ],  y[ p ] ) ) return false;
+            if (!testEquivalent2(x[p], y[p])) return false;
         }
 
-        for ( p in y ) {
-            // allows x[ p ] to be set to undefined
-            if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+        for (p in y) {
+            // allows x[p] to be set to undefined
+            if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) return false;
         }
         return true;
     }
