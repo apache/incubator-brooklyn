3665500fcba7063fdb664998531c9628cf5b291c
Andrei Savu <asavu@apache.org>
2012-04-20 12:20:31 +0300

    Test for EntityResource

---
+++ sandbox/rest/src/test/java/brooklyn/rest/resources/EntityResourceTest.java
@@ -0,0 +1,32 @@
+package brooklyn.rest.resources;
+
+import brooklyn.rest.BaseResourceTest;
+import com.sun.jersey.api.client.GenericType;
+import java.util.Set;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+
+public class EntityResourceTest extends BaseResourceTest {
+
+  @Override
+  protected void setUpResources() throws Exception {
+    addResource(new EntityResource());
+  }
+
+  @Test
+  public void testListAllEntities() {
+    Set<String> entities = client().resource("/entities")
+        .get(new GenericType<Set<String>>() {
+        });
+    assertTrue(entities.size() > 0);
+  }
+
+  @Test
+  public void testFilterListOfEntitiesByName() {
+    Set<String> entities = client().resource("/entities")
+        .queryParam("name", "redis").get(new GenericType<Set<String>>() {
+        });
+    assertEquals(entities.size(), 3);
+  }
+}
