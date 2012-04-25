fda4d3fd173074e8f15ac6c5a1b9435f885cca3f
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2012-04-25 12:10:21 +0100

    Fix environment variable propagation for SshSensorAdapter

--- core/src/main/java/brooklyn/event/adapter/SshSensorAdapter.groovy
+++ core/src/main/java/brooklyn/event/adapter/SshSensorAdapter.groovy
@@ -5,10 +5,12 @@ import java.util.Map
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import com.google.common.base.Preconditions
+
 import brooklyn.event.Sensor
 import brooklyn.location.basic.SshMachineLocation;
 import brooklyn.util.flags.FlagUtils
-import brooklyn.util.internal.StringEscapeUtils
+import brooklyn.util.flags.SetFromFlag
 
 
 /**
@@ -18,32 +20,44 @@ public class SshSensorAdapter extends AbstractSensorAdapter {
 
 	public static final Logger log = LoggerFactory.getLogger(SshSensorAdapter.class)
 
-	protected final SshPollHelper poller = new SshPollHelper(this);
-	protected final Map env=[:]
+	@SetFromFlag("env")
+	Map env
+	@SetFromFlag("command")
+	String command
+
+	protected final SshPollHelper poller = new SshPollHelper(this)
 	protected final SshMachineLocation location;
 
-	protected String command
+	public SshSensorAdapter(Map flags=[:], SshMachineLocation location) {
+		super(flags)
+		this.location = location
+        if (!env) env = [:]
+    }
 
-	public SshSensorAdapter(Map flags=[:], SshMachineLocation location, env=[:]) {
-		super(flags);
-		this.location = location;
-        this.env << env
-	}
+	protected boolean isConnected() { isActivated() && poller != null && poller.getLastWasSuccessful() }
 
-	protected boolean isConnected() { isActivated() && poller!=null && poller.getLastWasSuccessful() }
-	
-	/** returns a new adapter, registered, with the given acommand and additional environment */ 
-	public SshSensorAdapter command(String command, Map cmdEnv=[:]) {
-		def newFlags = FlagUtils.getFieldsWithValues(this)
-		def newAdapter = new SshSensorAdapter(newFlags, location, env)
-		newAdapter.command = command
+    /** returns a new adapter, registered, with the given command and optional additional environment */
+    public SshSensorAdapter command(String command, Map cmdEnv=[:]) {
+        def newFlags = FlagUtils.getFieldsWithValues(this)
+        def newAdapter = new SshSensorAdapter(newFlags, location)
+        newAdapter.command = command
         newAdapter.env << cmdEnv
-		if (registry) return registry.register(newAdapter);
-		return newAdapter;
-	}
+        if (registry) return registry.register(newAdapter);
+        return newAdapter;
+    }
+
+    /** returns a new adapter, registered, with the same command and additional environment */
+    public SshSensorAdapter env(Map cmdEnv) {
+        def newFlags = FlagUtils.getFieldsWithValues(this)
+        def newAdapter = new SshSensorAdapter(newFlags, location)
+        newAdapter.env << cmdEnv
+        if (registry) return registry.register(newAdapter)
+        return newAdapter;
+    }
 
 	/** closure will run in an {@link SshResultContext}, default value is {@code stdout} */
 	public void poll(Sensor s, Closure c={it}) {
+        Preconditions.checkNotNull(command, "command")
 		poller.addSensor(s, c);
 	}
 
