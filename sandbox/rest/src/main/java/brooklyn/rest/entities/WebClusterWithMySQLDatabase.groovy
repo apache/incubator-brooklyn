36bc691657261ac97e6a13960711b858193d591c
Andrei Savu <asavu@apache.org>
2012-04-27 13:13:52 +0300

    Added a web cluster entity with all the wiring already done and exposed entity configuration keys

---
+++ sandbox/rest/src/main/java/brooklyn/rest/entities/WebClusterWithMySQLDatabase.groovy
@@ -0,0 +1,63 @@
+package brooklyn.rest.entities
+
+import static brooklyn.event.basic.DependentConfiguration.valueWhenAttributeReady
+
+import brooklyn.entity.basic.AbstractApplication
+import brooklyn.entity.webapp.DynamicWebAppCluster
+import brooklyn.policy.ResizerPolicy
+import brooklyn.entity.database.mysql.MySqlNode
+import brooklyn.entity.basic.UsesJava
+import brooklyn.entity.webapp.ControlledDynamicWebAppCluster
+import brooklyn.event.basic.BasicConfigKey
+import brooklyn.util.flags.SetFromFlag
+
+class WebClusterWithMySQLDatabase extends AbstractApplication {
+
+    @SetFromFlag("war_url")
+    public static final BasicConfigKey<String> WAR_PATH =
+        [String, "rest.webcluster.war.url", "", "classpath://hello-world-webapp.war"]
+
+    @SetFromFlag("db_username")
+    public static final BasicConfigKey<String> DB_USERNAME =
+        [String, "rest.webcluster.db.username", "", "brooklyn"]
+
+    @SetFromFlag("db_password")
+    public static final BasicConfigKey<String> DB_PASSWORD =
+        [String, "rest.webcluster.db.password", "", "br00k11n"]
+
+    @SetFromFlag("db_setup_url")
+    public static final BasicConfigKey<String> DB_SETUP_SQL_URL =
+        [String, "rest.webcluster.db.setup.url", "", "classpath://visitors-creation-script.sql"]
+
+    @SetFromFlag("http_port")
+    public static final BasicConfigKey<String> HTTP_PORT =
+        [String, "rest.webcluster.http.port", "", "8080+"]
+
+    // TODO configs for resize policy range and resize policy metric range
+
+    public WebClusterWithMySQLDatabase(Map props = [:]) {
+        super(props)
+    }
+
+    public String makeJdbcUrl(String dbUrl) {
+        //jdbc:mysql://192.168.1.2:3306/visitors?user=brooklyn&password=br00k11n
+        "jdbc:" + dbUrl + "visitors" + "?" +
+                "user=" + getConfig(DB_USERNAME) + "\\&" +
+                "password=" + getConfig(DB_PASSWORD)
+    }
+
+    ControlledDynamicWebAppCluster web = new ControlledDynamicWebAppCluster(this, war: getConfig(WAR_PATH));
+    MySqlNode mysql = new MySqlNode(this, creationScriptUrl: getConfig(DB_SETUP_SQL_URL));
+
+    {
+        web.factory.configure(
+                httpPort: getConfig(HTTP_PORT),
+                (UsesJava.JAVA_OPTIONS):
+                        ["brooklyn.example.db.url": valueWhenAttributeReady(mysql, MySqlNode.MYSQL_URL, this.&makeJdbcUrl)]);
+
+        web.cluster.addPolicy(new
+        ResizerPolicy(DynamicWebAppCluster.AVERAGE_REQUESTS_PER_SECOND).
+                setSizeRange(1, 5).
+                setMetricRange(10, 100));
+    }
+}
--- sandbox/rest/src/main/java/brooklyn/rest/resources/EntityResource.java
+++ sandbox/rest/src/main/java/brooklyn/rest/resources/EntityResource.java
@@ -1,23 +1,32 @@
 package brooklyn.rest.resources;
 
 import brooklyn.entity.basic.AbstractEntity;
+import brooklyn.entity.basic.EntityLocal;
 import brooklyn.entity.trait.Startable;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableSet;
 import static com.google.common.collect.Iterables.filter;
 import static com.google.common.collect.Iterables.transform;
+import com.google.common.collect.Maps;
 import static com.google.common.collect.Sets.newHashSet;
 import com.yammer.dropwizard.logging.Log;
 import com.yammer.metrics.annotation.Timed;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nullable;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.reflections.Reflections;
 
 @Path("/v1/entities")
@@ -57,7 +66,6 @@ public class EntityResource {
   }
 
   @GET
-  @Timed
   public Iterable<String> list(
       final @QueryParam("name") @DefaultValue("") String name
   ) {
@@ -73,4 +81,23 @@ public class EntityResource {
       });
     }
   }
+
+  @GET
+  @Path("{entity}")
+  public Set<String> get(@PathParam("entity") String entityName) throws Exception {
+    try {
+      Class<EntityLocal> clazz = (Class<EntityLocal>) Class.forName(entityName);
+      Constructor constructor = clazz.getConstructor(new Class[]{Map.class});
+
+      EntityLocal instance = (EntityLocal) constructor.newInstance(Maps.newHashMap());
+      return instance.getConfigKeys().keySet();
+
+    } catch (ClassNotFoundException e) {
+      throw new WebApplicationException(Response.Status.NOT_FOUND);
+
+    } catch (NoSuchMethodException e) {
+      throw new WebApplicationException(Response.Status.NOT_FOUND);
+    }
+  }
+
 }
