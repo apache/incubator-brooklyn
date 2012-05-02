dc0bf55756ee56272de5b46c5ea4f8c9ce5da69d
Aled Sage <aled.sage@gmail.com>
2012-05-02 09:43:52 +0100

    Issue 46: refactor aggregating enricher, particularly to improve aggregation of group members

--- core/src/main/java/brooklyn/enricher/basic/AbstractAggregatingEnricher.groovy
+++ core/src/main/java/brooklyn/enricher/basic/AbstractAggregatingEnricher.groovy
@@ -1,23 +1,26 @@
 package brooklyn.enricher.basic
 
-import java.util.List
-import java.util.Map
-import java.util.Map.Entry
-
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import brooklyn.entity.Entity
+import brooklyn.entity.Group
+import brooklyn.entity.basic.EntityLocal
+import brooklyn.entity.trait.Changeable
+import brooklyn.event.Sensor
+import brooklyn.event.SensorEvent
+import brooklyn.event.SensorEventListener
+
+import com.google.common.base.Predicate
+import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableMap
 
-import brooklyn.entity.Entity
-import brooklyn.entity.basic.EntityLocal
-import brooklyn.event.Sensor
 
 /**
  * AggregatingEnrichers implicitly subscribes to the same sensor<S> on all entities inside an
  * {@link Group} and should emit an aggregate<T> on the target sensor
  */
-public abstract class AbstractAggregatingEnricher<S,T> extends AbstractEnricher {
+public abstract class AbstractAggregatingEnricher<S,T> extends AbstractEnricher implements SensorEventListener<S> {
     
     private static final Logger LOG = LoggerFactory.getLogger(AbstractAggregatingEnricher.class)
     
@@ -25,42 +28,98 @@ public abstract class AbstractAggregatingEnricher<S,T> extends AbstractEnricher 
     protected Sensor<T> target
     protected S defaultValue
 
+    Set<Entity> producers
+    List<Entity> hardCodedProducers
+    boolean allMembers
+    Predicate<Entity> filter
+    
     /**
      * Users of values should either on it synchronize when iterating over its entries or use
      * copyOfValues to obtain an immutable copy of the map.
      */
     // We use a synchronizedMap over a ConcurrentHashMap for entities that store null values.
-    protected Map<Entity, S> values = Collections.synchronizedMap(new LinkedHashMap<Entity, S>())
+    protected final Map<Entity, S> values = Collections.synchronizedMap(new LinkedHashMap<Entity, S>())
     
-    public AbstractAggregatingEnricher(List<Entity> producers, Sensor<S> source, Sensor<T> target, S defaultValue=null) {
-        for (Entity producer : producers) { values.put(producer, defaultValue) }
+    public AbstractAggregatingEnricher(Map<String,?> flags, Sensor<S> source, Sensor<T> target, S defaultValue=null) {
         this.source = source
         this.target = target
         this.defaultValue = defaultValue
+        hardCodedProducers = flags.producers ?: []
+        allMembers = flags.allMembers ?: false
+        filter = (flags.filter in Closure) ? (flags.filter as Predicate) : (flags.filter ?: Predicates.alwaysTrue())
     }
-    
+
+    /**
+     * @deprecated Instead use AbstractAggregatingEnricher(source, target, defaultValue, producers:producers, allMembers:true)
+     */
+    @Deprecated 
+    public AbstractAggregatingEnricher(List<Entity> producers, Sensor<S> source, Sensor<T> target, S defaultValue=null) {
+        this(source, target, defaultValue, producers:producers, allMembers:true)
+    }
+
     public void addProducer(Entity producer) {
-        LOG.debug "$this linked ($producer, $source) to $target"
-        values.put(producer, defaultValue)
+        if (LOG.isDebugEnabled()) LOG.debug "$this linked ($producer, $source) to $target"
+        values.put(producer, producer.getAttribute(source) ?: defaultValue)
         subscribe(producer, source, this)
+        onUpdated()
     }
     
+    // TODO If producer removed but then get (queued) event from it after this method returns,  
     public S removeProducer(Entity producer) {
-        LOG.debug "$this unlinked ($producer, $source) from $target"
+        if (LOG.isDebugEnabled()) LOG.debug "$this unlinked ($producer, $source) from $target"
         unsubscribe(producer)
         values.remove(producer)
+        onUpdated()
+    }
+    
+    @Override
+    public void onEvent(SensorEvent<S> event) {
+        Entity e = event.getSource();
+        synchronized (values) {
+            if (values.containsKey(e)) {
+                values.put(e, event.getValue())
+            } else {
+                if (LOG.isDebugEnabled()) LOG.debug("$this received event for unknown producer ($e); presumably that producer has recently been removed");
+            }
+        }
+        onUpdated();
+    }
+
+    /**
+     * Called whenever the values for the set of producers changes (e.g. on an event, or on a member added/removed).
+     * Defaults to no-op
+     */
+    protected void onUpdated() {
+        // no-op
+    }
+    
+    @Override
+    public void setEntity(EntityLocal entity) {
+        super.setEntity(entity)
+        
+        for (Entity producer : hardCodedProducers) {
+            if (filter.apply(producer)) {
+                addProducer(producer)
+            }
+        }
+        
+        if (allMembers) {
+            subscribe(entity, Changeable.MEMBER_ADDED, { if (filter.apply(it.value)) addProducer(it.value) } as SensorEventListener)
+            subscribe(entity, Changeable.MEMBER_REMOVED, { removeProducer(it.value) } as SensorEventListener)
+            
+            if (entity instanceof Group) {
+                for (Entity member : ((Group)entity).getMembers()) {
+                    if (filter.apply(member)) {
+                        addProducer(member)
+                    }
+                }
+            }
+        }
     }
     
     protected Map<Entity, S> copyOfValues() {
         synchronized (values) {
             return ImmutableMap.copyOf(values)
-        }
-    }
-    
-    public void setEntity(EntityLocal entity) {
-        super.setEntity(entity)
-        for (Entry<Entity, S> entry : copyOfValues()) {
-            subscribe(entry.getKey(), source, this)
         }
     }
 }
--- core/src/test/java/brooklyn/enricher/CustomAggregatingEnricherTest.groovy
+++ core/src/test/java/brooklyn/enricher/CustomAggregatingEnricherTest.groovy
@@ -2,19 +2,26 @@ package brooklyn.enricher
 
 import static org.testng.Assert.*
 
+import org.testng.annotations.AfterMethod
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
-import brooklyn.enricher.CustomAggregatingEnricher;
+import com.google.common.base.Function
+
 import brooklyn.entity.LocallyManagedEntity
 import brooklyn.entity.basic.AbstractApplication
+import brooklyn.entity.basic.AbstractGroup
 import brooklyn.entity.basic.EntityLocal
 import brooklyn.event.AttributeSensor
 import brooklyn.event.basic.BasicAttributeSensor
-import brooklyn.event.basic.BasicSensor
+import brooklyn.location.basic.SimulatedLocation
+import brooklyn.test.TestUtils
 
 class CustomAggregatingEnricherTest {
 
+    private static final long TIMEOUT_MS = 10*1000
+    private static final long SHORT_WAIT_MS = 250
+    
     AbstractApplication app
 
     EntityLocal producer
@@ -27,28 +34,34 @@ class CustomAggregatingEnricherTest {
         producer = new LocallyManagedEntity(owner:app)
         intSensor = new BasicAttributeSensor<Integer>(Integer.class, "int sensor")
         target = new BasicAttributeSensor<Integer>(Integer.class, "target sensor")
+        
+        app.start([new SimulatedLocation()])
+    }
+    
+    @AfterMethod(alwaysRun=true)
+    public void after() {
+        app?.stop()
     }
     
     @Test
     public void testEnrichersWithNoProducers() {
-        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>getSummingEnricher(
-                [], intSensor, target)
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(intSensor, target)
         producer.addEnricher(cae)
         assertEquals cae.getAggregate(), 0
     }
 
     @Test
     public void testSummingEnricherWhenNoSensorValuesYet() {
-        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>getSummingEnricher([producer],
-                intSensor, target)
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(
+                intSensor, target, producers:[producer])
         producer.addEnricher(cae)
         assertEquals cae.getAggregate(), 0
     }
 
     @Test
     public void testSummingEnricherWhenNullSensorValue() {
-        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>getSummingEnricher([producer],
-                intSensor, target)
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(
+                intSensor, target, producers:[producer])
         producer.addEnricher(cae)
         cae.onEvent(intSensor.newEvent(producer, null))
         assertEquals cae.getAggregate(), 0
@@ -56,8 +69,8 @@ class CustomAggregatingEnricherTest {
     
     @Test
     public void testSingleProducerSum() {
-        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>getSummingEnricher([producer],
-                intSensor, target)
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(
+                intSensor, target, producers:[producer])
         producer.addEnricher(cae)
         assertEquals cae.getAggregate(), 0
         cae.onEvent(intSensor.newEvent(producer, 1))
@@ -70,8 +83,8 @@ class CustomAggregatingEnricherTest {
                 [owner: app] as LocallyManagedEntity,
                 [owner: app] as LocallyManagedEntity,
                 [owner: app] as LocallyManagedEntity]
-        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>getSummingEnricher(producers,
-            intSensor, target)
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(
+            intSensor, target, producers:producers)
         
         producer.addEnricher(cae)
         assertEquals cae.getAggregate(), 0
@@ -88,8 +101,8 @@ class CustomAggregatingEnricherTest {
     public void testAveragingEnricherWhenNoSensorValuesYet() {
         List<LocallyManagedEntity> producers = [
                 [owner: app] as LocallyManagedEntity]
-        CustomAggregatingEnricher<Double> cae = CustomAggregatingEnricher.<Double>getAveragingEnricher(producers,
-            intSensor, new BasicAttributeSensor<Double>(Double.class, "target sensor"))
+        CustomAggregatingEnricher<Double> cae = CustomAggregatingEnricher.<Double>newAveragingEnricher(
+                intSensor, new BasicAttributeSensor<Double>(Double.class, "target sensor"), producers:producers)
         producer.addEnricher(cae)
         assertEquals cae.getAggregate(), 0d
     }
@@ -98,8 +111,8 @@ class CustomAggregatingEnricherTest {
     public void testAveragingEnricherWhenNullSensorValue() {
         List<LocallyManagedEntity> producers = [
                 [owner: app] as LocallyManagedEntity]
-        CustomAggregatingEnricher<Double> cae = CustomAggregatingEnricher.<Double>getAveragingEnricher(producers,
-            intSensor, new BasicAttributeSensor<Double>(Double.class, "target sensor"))
+        CustomAggregatingEnricher<Double> cae = CustomAggregatingEnricher.<Double>newAveragingEnricher(
+                intSensor, new BasicAttributeSensor<Double>(Double.class, "target sensor"), producers:producers)
         producer.addEnricher(cae)
         
         cae.onEvent(intSensor.newEvent(producers[0], null))
@@ -112,8 +125,8 @@ class CustomAggregatingEnricherTest {
                 [owner: app] as LocallyManagedEntity,
                 [owner: app] as LocallyManagedEntity,
                 [owner: app] as LocallyManagedEntity]
-        CustomAggregatingEnricher<Double> cae = CustomAggregatingEnricher.<Double>getAveragingEnricher(producers,
-            intSensor, new BasicAttributeSensor<Double>(Double.class, "target sensor"))
+        CustomAggregatingEnricher<Double> cae = CustomAggregatingEnricher.<Double>newAveragingEnricher(
+                intSensor, new BasicAttributeSensor<Double>(Double.class, "target sensor"), producers:producers)
         
         producer.addEnricher(cae)
         
@@ -137,8 +150,8 @@ class CustomAggregatingEnricherTest {
         LocallyManagedEntity p1 = [owner: app] 
         LocallyManagedEntity p2 = [owner: app]
         
-        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>getSummingEnricher([p1],
-                intSensor, target)
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(
+                intSensor, target, producers:[p1])
 
         producer.addEnricher(cae)
         assertEquals cae.getAggregate(), 0
@@ -154,6 +167,100 @@ class CustomAggregatingEnricherTest {
         
         cae.removeProducer(p2)
         assertEquals cae.getAggregate(), 1
+    }
+    
+    @Test
+    public void testAggregatesNewMembersOfGroup() {
+        AbstractGroup group = new AbstractGroup(owner:app) {}
+        LocallyManagedEntity p1 = [owner: app] 
+        LocallyManagedEntity p2 = [owner: app]
         
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(intSensor, target, allMembers:true)
+        group.addEnricher(cae)
+        
+        assertEquals cae.getAggregate(), 0
+
+        group.addMember(p1)
+        p1.setAttribute(intSensor, 1)
+        TestUtils.executeUntilSucceeds(timeout:TIMEOUT_MS) {
+            assertEquals cae.getAggregate(), 1
+        }
+        
+        group.addMember(p2)
+        p2.setAttribute(intSensor, 2)
+        TestUtils.executeUntilSucceeds(timeout:TIMEOUT_MS) {
+            assertEquals cae.getAggregate(), 3
+        }
+        
+        group.removeMember(p2)
+        TestUtils.executeUntilSucceeds(timeout:TIMEOUT_MS) {
+            assertEquals cae.getAggregate(), 1
+        }
+    }
+    
+    @Test
+    public void testAggregatesExistingMembersOfGroup() {
+        AbstractGroup group = new AbstractGroup(owner:app) {}
+        LocallyManagedEntity p1 = [owner: app] 
+        LocallyManagedEntity p2 = [owner: app]
+        group.addMember(p1)
+        group.addMember(p2)
+        p1.setAttribute(intSensor, 1)
+        
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(intSensor, target, allMembers:true)
+        group.addEnricher(cae)
+        
+        assertEquals cae.getAggregate(), 1
+
+        p2.setAttribute(intSensor, 2)
+        TestUtils.executeUntilSucceeds(timeout:TIMEOUT_MS) {
+            assertEquals cae.getAggregate(), 3
+        }
+        
+        group.removeMember(p2)
+        TestUtils.executeUntilSucceeds(timeout:TIMEOUT_MS) {
+            assertEquals cae.getAggregate(), 1
+        }
+    }
+    
+    @Test
+    public void testAppliesFilterWhenAggregatingMembersOfGroup() {
+        AbstractGroup group = new AbstractGroup(owner:app) {}
+        LocallyManagedEntity p1 = [owner: app] 
+        LocallyManagedEntity p2 = [owner: app]
+        LocallyManagedEntity p3 = [owner: app]
+        group.addMember(p1)
+        group.addMember(p2)
+        p1.setAttribute(intSensor, 1)
+        p2.setAttribute(intSensor, 2)
+        p3.setAttribute(intSensor, 4)
+        
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newSummingEnricher(intSensor, target, allMembers:true, filter:{it == p1})
+        group.addEnricher(cae)
+        
+        assertEquals cae.getAggregate(), 1
+        
+        group.addMember(p3)
+        TestUtils.assertSucceedsContinually(timeout:SHORT_WAIT_MS) {
+            assertEquals cae.getAggregate(), 1
+        }
+    }
+    
+    @Test
+    public void testCustomAggregatingFunction() {
+        LocallyManagedEntity p1 = [owner: app] 
+        Function<Collection<Integer>,Integer> aggregator = { Collection c -> 
+            int result = 0; c.each { result += it*it }; return result;
+        } as Function
+         
+        CustomAggregatingEnricher<Integer> cae = CustomAggregatingEnricher.<Integer>newEnricher(
+                intSensor, target, aggregator, 0, producers:[p1])
+
+        producer.addEnricher(cae)
+        assertEquals cae.getAggregate(), 0
+        
+        // Event by producer
+        cae.onEvent(intSensor.newEvent(p1, 2))
+        assertEquals cae.getAggregate(), 4
     }
 }
