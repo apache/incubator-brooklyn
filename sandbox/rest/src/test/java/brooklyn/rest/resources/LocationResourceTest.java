044bb4804f6d608d38f870e6a5f51cbc6092e8d3
Andrei Savu <asavu@apache.org>
2012-05-09 02:37:43 +0300

    A location can be configured in any way, as needed

--- sandbox/rest/src/test/java/brooklyn/rest/resources/LocationResourceTest.java
+++ sandbox/rest/src/test/java/brooklyn/rest/resources/LocationResourceTest.java
@@ -1,17 +1,20 @@
 package brooklyn.rest.resources;
 
 import brooklyn.rest.BaseResourceTest;
-import brooklyn.rest.api.Location;
+import brooklyn.rest.api.LocationSpec;
 import brooklyn.rest.api.LocationSummary;
 import brooklyn.rest.core.LocationStore;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import java.net.URI;
+import java.util.Map;
 import java.util.Set;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNull;
 import org.testng.annotations.Test;
 
@@ -44,11 +47,18 @@ public class LocationResourceTest extends BaseResourceTest {
 
   @Test
   public void testAddNewLocation() {
+    Map<String, String> expectedConfig = ImmutableMap.of(
+        "identity", "identity",
+        "credential", "credential",
+        "location", "us-east-1");
     ClientResponse response = client().resource("/v1/locations")
-        .post(ClientResponse.class, new Location("aws-ec2", "identity", "credential", "us-east-1"));
+        .post(ClientResponse.class, new LocationSpec("aws-ec2", expectedConfig));
 
     LocationSummary location = client().resource(response.getLocation()).get(LocationSummary.class);
     assertThat(location.getProvider(), is("aws-ec2"));
+
+    assertThat(location.getConfig().get("identity"), is("identity"));
+    assertFalse(location.getConfig().containsKey("credential"));
   }
 
   @Test(dependsOnMethods = {"testAddNewLocation"})
