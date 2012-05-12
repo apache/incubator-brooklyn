c6f03eb1c22bfa8b5535a6b2497fe7889be2f2d4
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2012-05-12 17:53:46 +0100

    Improved interface and documentation for start-stop driver and script runner

--- core/src/main/java/brooklyn/entity/basic/lifecycle/StartStopDriver.java
+++ core/src/main/java/brooklyn/entity/basic/lifecycle/StartStopDriver.java
@@ -1,18 +1,34 @@
 package brooklyn.entity.basic.lifecycle;
 
+import brooklyn.entity.basic.EntityLocal;
+import brooklyn.location.Location;
 
-/** In many cases it is cleaner to store entity lifecycle effectors (and sometimes other implementations) in a class to 
+/**
+ * Delegate for {@link Entity} lifecycle control.
+ *
+ * In many cases it is cleaner to store entity lifecycle effectors (and sometimes other implementations) in a class to 
  * which the entity delegates.  Classes implementing this interface provide this delegate, often inheriting utilities
  * specific to a particular transport (e.g. ssh) shared among many different entities.
  * <p>
  * In this way, it is also possible for entities to cleanly support multiple mechanisms for start/stop and other methods. 
- **/
+ */
 public interface StartStopDriver {
 
-	public boolean isRunning();
+    /** The entity whose components we are controlling. */
+    EntityLocal getEntity();
+
+    /** The location the entity is running in. */
+    Location getLocation();
+
+    /** Whether the entity components have started. */
+    boolean isRunning();
 	
-	public void start();
-	public void stop();
-	public void restart();
-		
+	/** @see Startable#start(Collection) */
+	void start();
+
+	/** @see Startable#stop() */
+	void stop();
+
+	/** @see Startable#restart() */
+	void restart();
 }
