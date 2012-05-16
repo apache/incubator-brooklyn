6c94017eeddf85e5f0ede333356fac0f8d564f61
Cosmin Dumitrache <cosmin.dumitrache@cloudsoftcorp.com>
2012-05-16 12:50:53 +0100

    Refactored the Launch command code to be more clean and testable.

--- usage/cli/src/main/java/brooklyn/cli/Main.java
+++ usage/cli/src/main/java/brooklyn/cli/Main.java
@@ -4,9 +4,11 @@ import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyShell;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import org.iq80.cli.Cli;
 import org.iq80.cli.Cli.CliBuilder;
@@ -23,15 +25,18 @@ import brooklyn.location.Location;
 import brooklyn.location.basic.LocationRegistry;
 import brooklyn.util.ResourceUtils;
 
+import com.google.common.annotations.Beta;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Iterables;
 
 public class Main {
 
     private static final StringBuilder launchHelp = new StringBuilder();
     
+    @SuppressWarnings({ "rawtypes" })
     public static void main(String...args) {
-        @SuppressWarnings("unchecked")
-        CliBuilder<Runnable> builder = Cli.buildCli("brooklyn", Runnable.class)
+        @SuppressWarnings({ "unchecked" })
+        CliBuilder<Callable> builder = Cli.buildCli("brooklyn", Callable.class)
                 .withDescription("Brooklyn Management Service")
                 .withDefaultCommand(Help.class)
                 .withCommands(
@@ -39,20 +44,23 @@ public class Main {
                         Launch.class
                 );
 
-        Cli<Runnable> brooklynParser = builder.build();
+        Cli<Callable> brooklynParser = builder.build();
         Help.help(brooklynParser.getMetadata(), Arrays.asList("launch"), launchHelp);
 
         try {
-            Runnable command = brooklynParser.parse(args); 
-            command.run();
+            Callable<?> command = brooklynParser.parse(args); 
+            command.call();
         } catch (ParseException pe) {
             System.err.println("Parse error: " + pe.getMessage());
             System.err.println(launchHelp.toString());
             System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Execution error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public static abstract class BrooklynCommand implements Runnable {
+    public static abstract class BrooklynCommand implements Callable<Void> {
         @Option(type = OptionType.GLOBAL, name = { "-v", "--verbose" }, description = "Verbose mode")
         public boolean verbose = false;
 
@@ -66,8 +74,9 @@ public class Main {
                 description = "The Application to start. For example my.AppName or file://my/AppName.groovy or classpath://my/AppName.groovy")
         public String app;
 
+        @Beta
         @Option(name = { "-s", "--script" }, title = "script URI",
-                description = "URI for a Groovy script to parse and load.")
+                description = "EXPERIMENTAL. URI for a Groovy script to parse and load. This script will run before starting the app.")
         public String script = null;
         
         @Option(name = { "-l", "--location", "--locations" }, title = "location list",
@@ -83,7 +92,8 @@ public class Main {
         public boolean noConsole = false;
 
         @Override
-        public void run() {
+        public Void call() throws Exception {
+            
             if (verbose) {
                 System.out.println("Launching brooklyn app: "+app+" in "+Iterables.toString(locations));
             }
@@ -92,33 +102,17 @@ public class Main {
             ClassLoader parent = utils.getLoader();
             GroovyClassLoader loader = new GroovyClassLoader(parent);
             
-            Class<?> appClass;
-            AbstractApplication application = null;
+            // Get an instance of the brooklyn app
+            AbstractApplication application = loadApplicationFromClasspathOrParse(utils, loader, app);
             
-            try {
-                try {
-                    appClass = loader.loadClass(app, true, false);
-                } catch (ClassNotFoundException cnfe) { // Not a class on the classpath
-                    String content = utils.getResourceAsString(app);
-                    appClass = loader.parseClass(content);
-                }
-                
-                Constructor<?> constructor = appClass.getConstructor();
-                application = (AbstractApplication) constructor.newInstance();
-                
-                if (script != null) {
-                    String content = utils.getResourceAsString(script);
-                    GroovyShell shell = new GroovyShell(loader);
-                    shell.evaluate(content);
-                }
-            } catch (Exception e) {
-                System.err.println("Unexpected exception: " + e.getMessage());
-                System.err.println(launchHelp);
-                System.exit(1);
+            //First, run a setup script if the user has provided one
+            if (script != null) {
+                String content = utils.getResourceAsString(script);
+                GroovyShell shell = new GroovyShell(loader);
+                shell.evaluate(content);
             }
             
             // Figure out the brooklyn location(s) where to launch the application
-            //Iterable<String> parsedLocations = Splitter.on(",").split(locations);
             List<Location> brooklynLocations = new LocationRegistry().getLocationsById(locations);
             
             // Start the application
@@ -128,6 +122,28 @@ public class Main {
             if (verbose) {
                 Entities.dumpInfo(application);
             }
+            
+            return null;
         }
+        
+        /**
+         * Helper method that gets an instance of a brooklyn application
+         */
+        @VisibleForTesting
+        AbstractApplication loadApplicationFromClasspathOrParse(ResourceUtils utils, GroovyClassLoader loader, String app) 
+                throws SecurityException, NoSuchMethodException, IllegalArgumentException, InstantiationException, 
+                IllegalAccessException, InvocationTargetException {
+            Class<?> appClass;
+            try {
+                appClass = loader.loadClass(app, true, false);
+            } catch (ClassNotFoundException cnfe) { // Not a class on the classpath
+                String content = utils.getResourceAsString(app);
+                appClass = loader.parseClass(content);
+            }
+            
+            Constructor<?> constructor = appClass.getConstructor();
+            return (AbstractApplication) constructor.newInstance();
+        }
+        
     }
 }
