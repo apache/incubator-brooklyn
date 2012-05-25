6744458737d22e1213257e11d1b919b87d33fd9c
Aled Sage <aled.sage@gmail.com>
2012-05-26 00:08:07 +0100

    Enricher classes: convert form groovy to java

---
+++ core/src/main/java/brooklyn/enricher/CustomAggregatingEnricher.java
@@ -0,0 +1,230 @@
+package brooklyn.enricher;
+
+import groovy.lang.Closure;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import brooklyn.enricher.basic.AbstractAggregatingEnricher;
+import brooklyn.entity.Entity;
+import brooklyn.event.AttributeSensor;
+import brooklyn.event.SensorEventListener;
+import brooklyn.util.GroovyJavaMethods;
+import brooklyn.util.flags.TypeCoercions;
+
+import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Subscribes to events from producers with a sensor of type T, aggregates them with the 
+ * provided closure and emits the result on the target sensor V.
+ * @param <T>
+ */
+public class CustomAggregatingEnricher<S,T> extends AbstractAggregatingEnricher<S,T> implements SensorEventListener<S> {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(CustomAggregatingEnricher.class);
+    
+    protected final Function<Collection<S>, T> aggregator;
+    
+    /**
+     * The valid keys for the flags are:
+     * - producers: a collection of entities to be aggregated
+     * - allMembers: indicates that should track members of the entity that the aggregator is associated with,
+     *               to aggregate across all those members.
+     * - filter:     a Predicate or Closure, indicating which entities to include
+     * 
+     * @param flags
+     * @param source
+     * @param target
+     * @param aggregator   Aggregates a collection of values, to return a single value for the target sensor
+     * @param defaultValue Default value to populate the collection given to aggregator, defaults to null
+     */
+    public CustomAggregatingEnricher(Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target,
+            Function<Collection<S>, T> aggregator, S defaultValue) {
+        super(flags, source, target, defaultValue);
+        this.aggregator = aggregator;
+    }
+    
+    public CustomAggregatingEnricher(Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target,
+            Function<Collection<S>, T> aggregator) {
+        this(flags, source, target, aggregator, null);
+    }
+    
+    public CustomAggregatingEnricher(AttributeSensor<S> source, AttributeSensor<T> target,
+            Function<Collection<S>, T> aggregator, S defaultValue) {
+        this(Collections.<String,Object>emptyMap(), source, target, aggregator, defaultValue);
+    }
+    
+    public CustomAggregatingEnricher(AttributeSensor<S> source, AttributeSensor<T> target,
+            Function<Collection<S>, T> aggregator) {
+        this(Collections.<String,Object>emptyMap(), source, target, aggregator, null);
+    }
+
+    /**
+     * @param flags
+     * @param source
+     * @param target
+     * @param aggregator   Should take a collection of values and return a single, aggregate value
+     * @param defaultValue
+     * 
+     * @see #CustomAggregatingEnricher(Map<String,?>, AttributeSensor<S>, AttributeSensor<T> target, Function<Collection<S>, T> aggregator, S defaultValue)
+     */
+    public CustomAggregatingEnricher(Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target,
+            Closure aggregator, S defaultValue) {
+        this(flags, source, target, GroovyJavaMethods.<Collection<S>, T>functionFromClosure(aggregator), defaultValue);
+    }
+
+    public CustomAggregatingEnricher(Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator) {
+        this(flags, source, target, aggregator, null);
+    }
+
+    public CustomAggregatingEnricher(AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator, S defaultValue) {
+        this(Collections.<String,Object>emptyMap(), source, target, aggregator, defaultValue);
+    }
+
+    public CustomAggregatingEnricher(AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator) {
+        this(Collections.<String,Object>emptyMap(), source, target, aggregator, null);
+    }
+
+    /**
+     * @deprecated will be deleted in 0.5. Use CustomAggregatingEnricher(source, target, aggregator, deafultValue, producers:producer)
+     */
+    public CustomAggregatingEnricher(List<Entity> producer, AttributeSensor<S> source, AttributeSensor<T> target, 
+            Closure aggregator, S defaultValue) {
+        this(producer, source, target, GroovyJavaMethods.<Collection<S>, T>functionFromClosure(aggregator), defaultValue);
+    }
+
+    @Deprecated
+    public CustomAggregatingEnricher(List<Entity> producer, AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator) {
+        this(producer, source, target, aggregator, null);
+    }
+
+    /**
+     * @deprecated will be deleted in 0.5. Use CustomAggregatingEnricher(source, target, aggregator, deafultValue, producers:producer)
+     */
+    @Deprecated
+    public CustomAggregatingEnricher(List<Entity> producer, AttributeSensor<S> source, AttributeSensor<T> target,
+            Function<Collection<S>, T> aggregator, S defaultValue) {
+        super(producer, source, target, defaultValue);
+        this.aggregator = aggregator;
+    }
+
+    @Deprecated
+    public CustomAggregatingEnricher(List<Entity> producer, AttributeSensor<S> source, AttributeSensor<T> target, Function<Collection<S>, T> aggregator) {
+        this(producer, source, target, aggregator, (S) null);
+    }
+
+    @Override
+    public void onUpdated() {
+        try {
+            entity.setAttribute(target, getAggregate());
+        } catch (Throwable t) {
+            LOG.warn("Error calculating and setting aggregate for enricher "+this, t);
+            throw Throwables.propagate(t);
+        }
+    }
+    
+    public T getAggregate() {
+        synchronized (values) {
+            return (T) aggregator.apply(values.values());
+        }
+    }
+
+    // FIXME Clean up explosion of overloading, caused by groovy-equivalent default vals...
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator, S defaultVal) {
+        return newEnricher(flags, source, target, aggregator, defaultVal);
+    }
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator) {
+        return newEnricher(flags, source, target, aggregator, null);
+    }
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator, S defaultVal) {
+        return newEnricher(Collections.<String,Object>emptyMap(), source, target, aggregator, defaultVal);
+    }
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            AttributeSensor<S> source, AttributeSensor<T> target, Closure aggregator) {
+        return newEnricher(Collections.<String,Object>emptyMap(), source, target, aggregator, null);
+    }
+    
+    
+    // FIXME Clean up explosion of overloading, caused by groovy-equivalent default vals...
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target, Function<Collection<S>, T> aggregator, S defaultVal) {
+        return new CustomAggregatingEnricher<S,T>(flags, source, target, aggregator, defaultVal);
+    }
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            Map<String,?> flags, AttributeSensor<S> source, AttributeSensor<T> target, Function<Collection<S>, T> aggregator) {
+        return newEnricher(flags, source, target, aggregator, null);
+    }
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            AttributeSensor<S> source, AttributeSensor<T> target, Function<Collection<S>, T> aggregator, S defaultVal) {
+        return newEnricher(Collections.<String,Object>emptyMap(), source, target, aggregator, defaultVal);
+    }
+    public static <S,T> CustomAggregatingEnricher<S,T> newEnricher(
+            AttributeSensor<S> source, AttributeSensor<T> target, Function<Collection<S>, T> aggregator) {
+        return newEnricher(Collections.<String,Object>emptyMap(), source, target, aggregator, null);
+    }
+    
+    public static <N extends Number, T extends Number> CustomAggregatingEnricher<N,T> newSummingEnricher(
+            Map<String,?> flags, AttributeSensor<N> source, final AttributeSensor<T> target) {
+        
+        Function<Collection<N>, T> aggregator = new Function<Collection<N>, T>() {
+            @Override public T apply(Collection<N> vals) {
+                Object result = (vals == null || vals.isEmpty()) ? 0 : sum(vals);
+                return TypeCoercions.castPrimitive(result, target.getType());
+            }
+        };
+        return new CustomAggregatingEnricher<N,T>(flags, source, target, aggregator);
+    }
+    public static <N extends Number> CustomAggregatingEnricher<N,N> newSummingEnricher(
+            AttributeSensor<N> source, AttributeSensor<N> target) {
+        return newSummingEnricher(Collections.<String,Object>emptyMap(), source, target);
+    }
+
+    /**
+     * @deprecated will be deleted in 0.5. Use newAveragingEnricher(source, target, producers:producer, allMembers:true)
+     */
+    @Deprecated
+    public static <N extends Number> CustomAggregatingEnricher<N,N> getSummingEnricher(
+            List<Entity> producer, AttributeSensor<N> source, AttributeSensor<N> target) {
+        return newSummingEnricher(ImmutableMap.of("producers", producer, "allMembers", true), source, target);
+    }
+
+    public static <N extends Number> CustomAggregatingEnricher<N,Double> newAveragingEnricher(
+            Map<String,?> flags, AttributeSensor<N> source, AttributeSensor<Double> target) {
+        
+        Function<Collection<N>, Double> aggregator = new Function<Collection<N>, Double>() {
+            @Override public Double apply(Collection<N> vals) {
+                return (vals == null || vals.isEmpty()) ? 0d : ((Double) sum(vals) / vals.size());
+            }
+        };
+        return new CustomAggregatingEnricher<N,Double>(flags, source, target, aggregator);
+    }
+    public static <N extends Number> CustomAggregatingEnricher<N,Double> newAveragingEnricher(
+            AttributeSensor<N> source, AttributeSensor<Double> target) {
+        return newAveragingEnricher(Collections.<String,Object>emptyMap(), source, target);
+    }
+
+    /**
+     * @deprecated will be deleted in 0.5. Use newAveragingEnricher(source, target, producers:producer, allMembers:true)
+     */
+    @Deprecated
+    public static <N extends Number> CustomAggregatingEnricher<N,Double> getAveragingEnricher(
+            List<Entity> producer, AttributeSensor<N> source, AttributeSensor<Double> target) {
+        return newAveragingEnricher(ImmutableMap.of("producers", producer, "allMembers", true), source, target);
+    }
+    
+    private static <N extends Number> double sum(Iterable<N> vals) {
+        double result = 0d;
+        for (N val : vals) result += (val != null) ? val.doubleValue() : 0d;
+        return result;
+    }
+}
---
+++ core/src/main/java/brooklyn/enricher/basic/SensorPropagatingEnricher.java
@@ -0,0 +1,89 @@
+package brooklyn.enricher.basic;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import brooklyn.entity.Entity;
+import brooklyn.entity.basic.AbstractEntity;
+import brooklyn.entity.basic.EntityLocal;
+import brooklyn.event.AttributeSensor;
+import brooklyn.event.Sensor;
+import brooklyn.event.SensorEvent;
+import brooklyn.event.SensorEventListener;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+/** an enricher policy which just listens for the target sensor(s) on a child entity and passes it up */
+public class SensorPropagatingEnricher extends AbstractEnricher implements SensorEventListener<Object> {
+    
+    public static final Logger log = LoggerFactory.getLogger(SensorPropagatingEnricher.class);
+        
+    /** the entity to listen to */
+    private final Entity source;
+    /** the sensors to listen to */
+    private final Set<Sensor<?>> sensors = Sets.newLinkedHashSet();
+    
+    public static SensorPropagatingEnricher newInstanceListeningToAllSensors(Entity source) {
+        return newInstanceListeningToAllSensorsBut(source);
+    }
+    public static SensorPropagatingEnricher newInstanceListeningToAllSensorsBut(Entity source, Sensor<?>... excludes) {
+        Set<Sensor<?>> excluded = ImmutableSet.copyOf(excludes);
+        Set<Sensor<?>> includes = Sets.newLinkedHashSet();
+        
+        for (Sensor<?> it : source.getEntityClass().getSensors()) {
+            if (!excluded.contains(it)) includes.add(it);
+        }
+        return new SensorPropagatingEnricher(source, includes);
+    }
+
+    public static SensorPropagatingEnricher newInstanceListeningTo(Entity source, Sensor<?>... includes) {
+        return new SensorPropagatingEnricher(source, includes);
+    }
+
+    public SensorPropagatingEnricher(Entity source, Sensor<?>... sensors) {
+        this(source, Arrays.asList(sensors));
+    }
+    public SensorPropagatingEnricher(Entity source, Collection<Sensor<?>> sensors) {
+        this.source = source;
+        this.sensors.addAll(sensors);
+    }
+    
+    public void setEntity(EntityLocal entity) {
+        super.setEntity(entity);
+        for (Sensor<?> s: sensors) {
+            subscribe(source, s, this);
+        }
+    }
+    
+    public void onEvent(SensorEvent event) {
+        if (log.isTraceEnabled()) log.trace("policy {} got {}, propagating via {}", new Object[] {this, event, entity});
+        //just propagate upwards
+        if (event.getSensor() instanceof AttributeSensor) {
+            entity.setAttribute((AttributeSensor)event.getSensor(), event.getValue());
+        } else {
+            entity.emit(event.getSensor(), event.getValue());
+        }       
+    }
+    
+    /** useful post-addition to emit current values */
+    public void emitAllAttributes() {
+        for (Sensor s: sensors) {
+            if (s instanceof AttributeSensor) {
+                Object v = source.getAttribute((AttributeSensor)s);
+                if (v!=null) entity.setAttribute((AttributeSensor)s, v);
+            }
+        }
+    }
+
+    /** convenience, to be called by the host */
+    public SensorPropagatingEnricher addToEntityAndEmitAll(AbstractEntity host) {
+        host.addEnricher(this);
+        emitAllAttributes();
+        return this;
+    }    
+}
