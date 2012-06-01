443cc713b33a69e44f85c40ee6b888fb98c00691
Cosmin Dumitrache <cosmin.dumitrache@cloudsoftcorp.com>
2012-06-01 15:38:36 +0100

    Fix failing MySQL download caused by invalid URL

--- software/database/src/main/java/brooklyn/entity/database/mysql/MySqlNode.groovy
+++ software/database/src/main/java/brooklyn/entity/database/mysql/MySqlNode.groovy
@@ -14,8 +14,9 @@ import brooklyn.util.flags.SetFromFlag
 
 public class MySqlNode extends SoftwareProcessEntity {
 
+    // NOTE MySQL changes the minor version number of their GA release frequently, check for latest version if install fails
     @SetFromFlag("version")
-    public static final BasicConfigKey<String> SUGGESTED_VERSION = [ SoftwareProcessEntity.SUGGESTED_VERSION, "5.5.24" ]
+    public static final BasicConfigKey<String> SUGGESTED_VERSION = [ SoftwareProcessEntity.SUGGESTED_VERSION, "5.5.25" ]
 
     @SetFromFlag("port")
     public static final PortAttributeSensorAndConfigKey MYSQL_PORT = [ "mysql.port", "MySQL port", PortRanges.fromString("3306, 13306+") ]
@@ -30,7 +31,8 @@ public class MySqlNode extends SoftwareProcessEntity {
 	@SetFromFlag("mirrorUrl")
 	public static final BasicConfigKey<String> MIRROR_URL = [ String, "mysql.install.mirror.url", "URL of mirror", 
 //		"http://mysql.mirrors.pair.com/"   // Pennsylvania
-		"http://gd.tuwien.ac.at/db/mysql/"
+//		"http://gd.tuwien.ac.at/db/mysql/"
+        "http://www.mirrorservice.org/sites/ftp.mysql.com/" //UK mirror service
 		 ]
 
     public static final BasicAttributeSensor<String> MYSQL_URL = [ String, "mysql.url", "URL to access mysql (e.g. mysql://localhost:3306/)" ]
