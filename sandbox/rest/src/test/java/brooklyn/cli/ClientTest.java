4a427489014f41d48b057c7714320bc1e0009730
Cosmin Dumitrache <cosmin.dumitrache@cloudsoftcorp.com>
2012-07-10 17:07:12 +0100

    Add a unit test class for the new cli client

---
+++ sandbox/rest/src/test/java/brooklyn/cli/ClientTest.java
@@ -0,0 +1,22 @@
+package brooklyn.cli;
+
+import brooklyn.rest.BrooklynService;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ClientTest {
+
+    @BeforeClass
+    public void testSetUp() {}
+
+    @AfterClass
+    public void testTearDown() {}
+
+    @Test
+    public void testCatalogEntitiesCommand() {
+
+    }
+
+
+}
