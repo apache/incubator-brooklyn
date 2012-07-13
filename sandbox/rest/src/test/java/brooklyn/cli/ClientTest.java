fc8236fca961bcc3e98e01b3d596d0fb62f276ae
Cosmin Dumitrache <cosmin.dumitrache@cloudsoftcorp.com>
2012-07-13 18:16:50 +0100

    Basic template for testCatalogEntitiesCommand

--- sandbox/rest/src/test/java/brooklyn/cli/ClientTest.java
+++ sandbox/rest/src/test/java/brooklyn/cli/ClientTest.java
@@ -1,21 +1,36 @@
 package brooklyn.cli;
 
 import brooklyn.rest.BrooklynService;
+import org.junit.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ClientTest {
 
+    BrooklynService brooklynServer;
+    Client brooklynClient;
+
     @BeforeClass
-    public void testSetUp() {}
+    public void testSetUp() throws Exception {
+        // Start the REST server
+        brooklynServer = BrooklynService.newBrooklynService();
+        String[] args = {"server","config.sample.yml"};
+        brooklynServer.runAsync(args);
+    }
 
     @AfterClass
-    public void testTearDown() {}
+    public void testTearDown() {
+        // Kill the REST server
+        brooklynServer = null;
+    }
 
-    @Test
+    @Test(enabled = true)
     public void testCatalogEntitiesCommand() {
-
+        System.out.println("testCatalogEntitiesCommand");
+        String[] args = {"catalog-entities"};
+        Client.main(args);
+        //TODO: check that client output matches what we expect (need to make the client more testable)
     }
 
 
