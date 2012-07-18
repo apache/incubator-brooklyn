a53f0821aeef18ebf3627d443d08d84e0692b25c
Cosmin Dumitrache <cosmin.dumitrache@cloudsoftcorp.com>
2012-07-18 18:12:08 +0100

    Add VersionResource to server

---
+++ sandbox/rest/src/main/java/brooklyn/rest/resources/VersionResource.java
@@ -0,0 +1,19 @@
+package brooklyn.rest.resources;
+
+import com.wordnik.swagger.core.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/version")
+@Api(value = "/v1/version", description = "Get brooklyn version")
+@Produces(MediaType.APPLICATION_JSON)
+public class VersionResource extends BaseResource {
+
+  @GET
+  @Path("/")
+  @ApiOperation(value = "Get brooklyn version", responseClass = "String", multiValueResponse = false)
+  public String getVersion() {
+    return "VERSION_STRING_STUB";
+  }
+
+}
