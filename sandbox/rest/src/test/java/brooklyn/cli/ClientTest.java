3b7937da0a8259b6de46f3c10ff345e1dc354ddb
Cosmin Dumitrache <cosmin.dumitrache@cloudsoftcorp.com>
2012-07-18 14:47:24 +0100

    Add "create app" test + improvements to tests

--- sandbox/rest/src/test/java/brooklyn/cli/ClientTest.java
+++ sandbox/rest/src/test/java/brooklyn/cli/ClientTest.java
@@ -1,19 +1,28 @@
 package brooklyn.cli;
 
 import brooklyn.rest.BrooklynService;
+import brooklyn.rest.core.ApplicationManager;
+import com.google.common.collect.Iterables;
+import com.yammer.dropwizard.logging.Log;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.net.URL;
 
+import static org.testng.Assert.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 public class ClientTest {
 
-    BrooklynService brooklynServer;
-    Client brooklynClient;
+    private final static Log LOG = Log.forClass(ClientTest.class);
+
+    private BrooklynService brooklynServer;
+    private ApplicationManager applicationManager;
+    private Client brooklynClient;
 
     private ByteArrayOutputStream outBytes;
     private PrintStream out;
@@ -29,40 +38,61 @@ public class ClientTest {
     }
 
     @BeforeClass
-    public void testSetUp() throws Exception {
+    public void setUp() throws Exception {
         // Start the REST server
         brooklynServer = BrooklynService.newBrooklynService();
+
+        //TODO Create a temporary file for config.sample.yml, so can run in jenkins?
+        //URL configUrl = getClass().getClassLoader().getResource("config/config.sample.yml");
         String[] args = {"server","config.sample.yml"};
         brooklynServer.runAsync(args);
-        // Rewire the output/error stream of the client
+        applicationManager = brooklynServer.getApplicationManager();
+    }
+
+    @BeforeMethod
+    public void setUpBeforeMethod() throws Exception {
+        // Set i/o streams
         outBytes = new ByteArrayOutputStream();
         out = new PrintStream(outBytes);
         errBytes = new ByteArrayOutputStream();
         err = new PrintStream(errBytes);
+        // Create a client instance
         brooklynClient = new Client(out,err);
     }
 
     @AfterClass
-    public void testTearDown() {
+    public void tearDown() throws Exception {
         // Kill the REST server and client instance
-        brooklynServer = null;
-        brooklynClient = null;
-        // Reset i/o streams
-        outBytes = null;
-        out = null;
-        errBytes = null;
-        err = null;
+        brooklynServer.stop();
+    }
+
+    @Test(enabled = false)
+    public void testCatalogEntitiesCommand() throws Exception {
+        try {
+            // Run the catalog-entities commandb
+            String[] args = {"catalog-entities"};
+            brooklynClient.run(args);
+            // Check that output matches what we expect
+            assertThat(standardOut(), containsString("brooklyn.entity.basic.BasicEntity"));
+        } catch (Exception e) {
+            LOG.error("\nstdout="+standardOut()+"\nstderr="+standardErr()+"\n", e);
+            throw e;
+        }
     }
 
     @Test(enabled = true)
-    public void testCatalogEntitiesCommand() throws Exception {
-        // Run the catalog-entities command
-        String[] args = {"catalog-entities"};
-        brooklynClient.run(args);
-        // Check that output matches what we expect
-        assertThat(standardOut(), containsString("brooklyn.entity.webapp.jboss.JBoss6Server"));
-
+    public void testDeployCreatesApp() throws Exception {
+        try {
+            // Run the deploy command
+            String[] args = {"deploy","--format","class", "brooklyn.cli.ExampleApp"};
+            brooklynClient.run(args);
+            // We should only have 1 app in the server's registry
+            assertEquals(applicationManager.registry().size(), 1);
+            // The name of that app should match what we have provided in the deploy command
+            assertEquals(Iterables.getOnlyElement(applicationManager.registry().keySet()), ExampleApp.class.getName());
+        } catch (Exception e) {
+            LOG.error("stdout="+standardOut()+"; stderr="+standardErr(), e);
+            throw e;
+        }
     }
-
-
 }
