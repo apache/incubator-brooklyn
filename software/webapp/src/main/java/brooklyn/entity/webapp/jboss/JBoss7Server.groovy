8806fe0f275c28d1a4bdb134f8433108c74c2c20
Peter Veentjer <alarmnummer@gmail.com>
2012-07-23 09:52:36 +0300

    Partial conversion of JBoss7Server Groovy->Java

--- software/webapp/src/main/java/brooklyn/entity/webapp/jboss/JBoss7Server.groovy
+++ software/webapp/src/main/java/brooklyn/entity/webapp/jboss/JBoss7Server.groovy
@@ -1,84 +1,77 @@
 package brooklyn.entity.webapp.jboss
 
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.with
+import brooklyn.util.MutableMap
+import groovy.time.TimeDuration
 
-import java.util.Map
-import java.util.concurrent.TimeUnit
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-import brooklyn.entity.Entity
-import brooklyn.entity.basic.BasicConfigurableEntityFactory
-import brooklyn.entity.basic.SoftwareProcessEntity
-import brooklyn.entity.webapp.JavaWebAppService
-import brooklyn.entity.webapp.JavaWebAppSoftwareProcess
-import brooklyn.event.adapter.HttpSensorAdapter
-import brooklyn.event.basic.BasicAttributeSensor
-import brooklyn.event.basic.BasicAttributeSensorAndConfigKey
-import brooklyn.event.basic.BasicConfigKey
-import brooklyn.event.basic.PortAttributeSensorAndConfigKey
-import brooklyn.location.basic.SshMachineLocation
-import brooklyn.util.flags.SetFromFlag
+import brooklyn.entity.Entity;
+import brooklyn.entity.basic.SoftwareProcessEntity;
+import brooklyn.entity.webapp.JavaWebAppService;
+import brooklyn.entity.webapp.JavaWebAppSoftwareProcess;
+import brooklyn.event.adapter.HttpSensorAdapter;
+import brooklyn.event.basic.BasicAttributeSensor ;
+import brooklyn.event.basic.BasicAttributeSensorAndConfigKey;
+import brooklyn.event.basic.BasicConfigKey;
+import brooklyn.event.basic.PortAttributeSensorAndConfigKey;
+import brooklyn.location.basic.SshMachineLocation;
+import brooklyn.util.flags.SetFromFlag;
 
 public class JBoss7Server extends JavaWebAppSoftwareProcess implements JavaWebAppService {
 
-	public static final Logger log = LoggerFactory.getLogger(JBoss7Server.class)
-	
+	public static final Logger log = LoggerFactory.getLogger(JBoss7Server.class);
+
     @SetFromFlag("version")
     public static final BasicConfigKey<String> SUGGESTED_VERSION =
-            [ SoftwareProcessEntity.SUGGESTED_VERSION, "7.1.1.Final" ]
+        new BasicConfigKey<String>(SoftwareProcessEntity.SUGGESTED_VERSION, "7.1.1.Final");
 
     @SetFromFlag("managementPort")
-    public static final PortAttributeSensorAndConfigKey MANAGEMENT_PORT = 
-            [ "webapp.jboss.managementPort", "Management port", "9990+" ]
+    public static final PortAttributeSensorAndConfigKey MANAGEMENT_PORT =
+            new PortAttributeSensorAndConfigKey("webapp.jboss.managementPort", "Management port", "9990+");
 
     @SetFromFlag("managementNativePort")
-    public static final PortAttributeSensorAndConfigKey MANAGEMENT_NATIVE_PORT = 
-            [ "webapp.jboss.managementNativePort", "Management native port", "10999+" ]
+    public static final PortAttributeSensorAndConfigKey MANAGEMENT_NATIVE_PORT =
+            new PortAttributeSensorAndConfigKey("webapp.jboss.managementNativePort", "Management native port", "10999+");
 
     @SetFromFlag("portIncrement")
     public static final BasicAttributeSensorAndConfigKey PORT_INCREMENT =
-            [ Integer, "webapp.jboss.portIncrement", "Port increment, for all ports in config file", 0 ]
+            new BasicAttributeSensorAndConfigKey(Integer.class, "webapp.jboss.portIncrement", "Port increment, for all ports in config file", 0);
 
     public static final BasicAttributeSensor<Integer> MANAGEMENT_STATUS =
-            [ Integer, "webapp.jboss.managementStatus", "HTTP response code for the management server" ]
+            new BasicAttributeSensor<Integer>(Integer.class, "webapp.jboss.managementStatus", "HTTP response code for the management server");
 
-    public JBoss7Server(Map flags=[:], Entity owner=null) {
-        super(flags, owner)
+    public JBoss7Server(Map flags){
+        this(flags, null);
     }
 
-	@Override	
-	protected void connectSensors() {
-		super.connectSensors();
+    public JBoss7Server(Map flags, Entity owner) {
+        super(flags, owner);
+    }
 
-		def host = getAttribute(HOSTNAME)
-		def port = getAttribute(MANAGEMENT_PORT) + getAttribute(PORT_INCREMENT)
-		def http = sensorRegistry.register(
-			new HttpSensorAdapter("http://$host:$port/management/subsystem/web/connector/http/read-resource", 
-                period: 200*TimeUnit.MILLISECONDS).
-				vars("include-runtime":true) )
-		
-		with(http) {
-			poll(MANAGEMENT_STATUS, { responseCode })
-			poll(SERVICE_UP, { responseCode==200 })
+    @Override
+    protected void connectSensors() {
+        super.connectSensors();
 
-			poll(REQUEST_COUNT) { json.requestCount }
-			poll(ERROR_COUNT) { json.errorCount }
-			poll(TOTAL_PROCESSING_TIME) { json.processingTime }
-			poll(MAX_PROCESSING_TIME) { json.maxTime }
-			poll(BYTES_RECEIVED) { json.bytesReceived }
-			poll(BYTES_SENT, { json.bytesSent })
-		}
-	}
+        String host = getAttribute(HOSTNAME);
+        int port = getAttribute(MANAGEMENT_PORT) + getAttribute(PORT_INCREMENT);
+        HttpSensorAdapter http = new HttpSensorAdapter(
+                MutableMap.of("period",new TimeDuration(0,0,0,200)),
+                "http://$host:$port/management/subsystem/web/connector/http/read-resource",
+        );
+        http = http.vars(MutableMap.of("include-runtime",true)) ;
+        http = sensorRegistry.register(http) ;
+        http.poll(MANAGEMENT_STATUS, { responseCode }) ;
+        http.poll(SERVICE_UP, { responseCode==200 }) ;
+        http.poll(REQUEST_COUNT) { json.requestCount } ;
+        http.poll(ERROR_COUNT) { json.errorCount };
+        http.poll(TOTAL_PROCESSING_TIME) { json.processingTime } ;
+        http.poll(MAX_PROCESSING_TIME) { json.maxTime } ;
+        http.poll(BYTES_RECEIVED) { json.bytesReceived };
+        http.poll(BYTES_SENT, { json.bytesSent }) ;
+    }
 
     public JBoss7SshDriver newDriver(SshMachineLocation machine) {
-        return new JBoss7SshDriver(this, machine)
-    }
-}
-
-public class JBoss7ServerFactory extends BasicConfigurableEntityFactory<JBoss7Server> {
-    public JBoss7ServerFactory(Map flags=[:]) {
-        super(flags, JBoss7Server)
+        return new JBoss7SshDriver(this, machine);
     }
 }
---
+++ software/webapp/src/main/java/brooklyn/entity/webapp/jboss/JBoss7SshDriver.java
@@ -0,0 +1,152 @@
+package brooklyn.entity.webapp.jboss;
+
+import brooklyn.entity.basic.SoftwareProcessEntity;
+import brooklyn.entity.basic.lifecycle.CommonCommands;
+import brooklyn.entity.webapp.JavaWebAppSshDriver;
+import brooklyn.location.basic.SshMachineLocation;
+import brooklyn.util.MutableMap;
+import brooklyn.util.NetworkUtils;
+import com.google.common.base.Preconditions;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.String.format;
+
+
+class JBoss7SshDriver extends JavaWebAppSshDriver {
+
+    /*
+      * TODO
+      * - security for stats access (see below)
+      * - expose log file location, or even support accessing them dynamically
+      * - more configurability of config files, java memory, etc
+      */
+
+    public static final String SERVER_TYPE = "standalone";
+    private static final String CONFIG_FILE = "standalone-brooklyn.xml";
+
+    public JBoss7SshDriver(JBoss7Server entity, SshMachineLocation machine) {
+        super(entity, machine);
+    }
+
+    protected String getLogFileLocation() {
+        return String.format("%s/%s/log/server.log", getRunDir(), SERVER_TYPE);
+    }
+
+    protected String getDeploySubdir() {
+        return String.format("%s/deployments", SERVER_TYPE);
+    }
+
+    protected Integer getManagementPort() {
+        return entity.getAttribute(JBoss7Server.MANAGEMENT_PORT);
+    }
+
+    protected Integer getManagementNativePort() {
+        return entity.getAttribute(JBoss7Server.MANAGEMENT_NATIVE_PORT);
+    }
+
+    protected Integer getPortIncrement() {
+        return (Integer) entity.getAttribute(JBoss7Server.PORT_INCREMENT);
+    }
+
+    public void install() {
+        String url = format("http://download.jboss.org/jbossas/7.1/jboss-as-%s/jboss-as-%s.tar.gz", getVersion(), getVersion());
+        String saveAs = format("jboss-as-distribution-%s.tar.gz", getVersion());
+
+        List<String> commands = new LinkedList<String>();
+        commands.addAll(CommonCommands.downloadUrlAs(url, getEntityVersionLabel("/"), saveAs));
+        commands.add(CommonCommands.INSTALL_TAR);
+        commands.add("tar xzfv " + saveAs);
+
+        newScript(INSTALLING).
+                failOnNonZeroResultCode().
+                body.append(commands).execute();
+    }
+
+    /**
+     * AS7 config notes and TODOs:
+     * We're using the http management interface on port managementPort
+     * We're not using any JMX.
+     * - AS 7 simply doesn't boot with Sun JMX enabled (https://issues.jboss.org/browse/JBAS-7427)
+     * - 7.1 onwards uses Remoting 3, which we haven't configured
+     * We're completely disabling security on the management interface.
+     * - In the future we probably want to use the as7/bin/add-user.sh script using config keys for user and password
+     * - Or we could create our own security realm and use that.
+     * We disable the root welcome page, since we can't deploy our own root otherwise
+     * We bind all interfaces to entity.hostname, rather than 127.0.0.1.
+     */
+    @Override
+    public void customize() {
+        Map ports = MutableMap.of("httpPort", getHttpPort(), "managementPort", getManagementPort(), "managementNativePort",
+                getManagementNativePort());
+
+        NetworkUtils.checkPortsValid(ports);
+        String hostname = entity.getAttribute(SoftwareProcessEntity.HOSTNAME);
+        Preconditions.checkNotNull(hostname, "AS 7 entity must set hostname otherwise server will only be visible on localhost");
+        newScript(CUSTOMIZING).
+                body.append(
+                format("cp -r %s/jboss-as-%s/%s . || exit $!", getInstallDir(), getVersion(), SERVER_TYPE),
+                format("cd %s/%s/configuration/", getRunDir(), SERVER_TYPE),
+                format("cp standalone.xml %s", CONFIG_FILE),
+                format("sed -i.bk 's/8080/%s/' %s", getHttpPort(), CONFIG_FILE),
+                format("sed -i.bk 's/9990/%s/' %s", getManagementPort(), CONFIG_FILE),
+                format("sed -i.bk 's/9999/%s/' %s", getManagementNativePort(), CONFIG_FILE),
+                format("sed -i.bk 's/port-offset:0/port-offset:%s/' %s", getPortIncrement(), CONFIG_FILE),
+                format("sed -i.bk 's/enable-welcome-root=\"true\"/enable-welcome-root=\"false\"/' %s", CONFIG_FILE),
+
+                // Disable Management security (!) by deleting the security-realm attribute
+                format("sed -i.bk 's/http-interface security-realm=\"ManagementRealm\"/http-interface/' %s", CONFIG_FILE),
+
+                // Increase deployment timeout to ten minutes
+                format("sed -i.bk 's/\\(path=\"deployments\"\\)/\\1 deployment-timeout=\"600\"/' %s", CONFIG_FILE),
+
+                // Bind interfaces to entity hostname
+                format("sed -i.bk 's/\\(inet-address value=.*\\)127.0.0.1/\\1%s/' %s", getHostname(), CONFIG_FILE)
+        ).execute();
+
+        ((JBoss7Server) entity).deployInitialWars();
+    }
+
+    @Override
+    public void launch() {
+        Map flags = MutableMap.of("usePidFile", false);
+        newScript(flags, LAUNCHING).
+                body.append(
+                "export LAUNCH_JBOSS_IN_BACKGROUND=true",
+                format("export JBOSS_HOME=%s/jboss-as-%s", getInstallDir(), getVersion()),
+                format("export JBOSS_PIDFILE=%s/%s", getRunDir(), PID_FILENAME),
+                format("%s/jboss-as-%s/bin/%s.sh ", getInstallDir(), getVersion(), SERVER_TYPE) +
+                        format("--server-config %s ", CONFIG_FILE) +
+                        format("-Djboss.server.base.dir=%s/%s ", getRunDir(), SERVER_TYPE) +
+                        format("\"-Djboss.server.base.url=file://%s/%s\" ", getRunDir(), SERVER_TYPE) +
+                        "-Djava.net.preferIPv4Stack=true " +
+                        "-Djava.net.preferIPv6Addresses=false " +
+                        format(" >> %s/console 2>&1 </dev/null &", getRunDir())
+        ).execute();
+    }
+
+
+    @Override
+    public boolean isRunning() {
+        Map flags = MutableMap.of("usePidFile", true);
+        return newScript(flags, CHECK_RUNNING).execute() == 0;
+    }
+
+    @Override
+    public void stop() {
+        Map flags = MutableMap.of("usePidFile", true);
+        newScript(flags, STOPPING).execute();
+    }
+
+    @Override
+    protected List<String> getCustomJavaConfigOptions() {
+        List<String> options = new LinkedList<String>();
+        options.addAll(super.getCustomJavaConfigOptions());
+        options.add("-Xms200m");
+        options.add("-Xmx800m");
+        options.add("-XX:MaxPermSize=400m");
+        return options;
+    }
+}
