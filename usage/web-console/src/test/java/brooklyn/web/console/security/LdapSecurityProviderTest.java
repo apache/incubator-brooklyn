1380f6e4a45c2e018e37e1f9c8d68a87e291847c
Peter Veentjer <alarmnummer@gmail.com>
2012-10-01 17:54:02 +0300

    Added the ldap authentication & integration test

--- usage/web-console/src/test/java/brooklyn/web/console/security/LdapSecurityProviderTest.java
+++ usage/web-console/src/test/java/brooklyn/web/console/security/LdapSecurityProviderTest.java
@@ -1,50 +1,72 @@
 package brooklyn.web.console.security;
 
 
-import org.apache.directory.server.core.DefaultDirectoryService;
-import org.apache.directory.server.core.api.partition.Partition;
+import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
+import org.opends.server.extensions.ConfigFileHandler;
+import org.opends.server.types.DirectoryEnvironmentConfig;
+import org.opends.server.util.EmbeddedUtils;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class LdapSecurityProviderTest {
 
-    private DefaultDirectoryService service;
+    public static final String LDAP_REALM = "dc=example,dc=com";
+    public static final String LDAP_URL = "ldap://127.0.0.1:1389";
 
-    @BeforeClass
-    public void beforeClass()throws Exception{
-    // Initialize the LDAP service
-        service = new DefaultDirectoryService();
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() throws Exception {
+        String targetRootDir = Files.createTempDir().getAbsolutePath();
+        String originalServerRootDir = getFile("ldap");
+        initOpendsDirectory(originalServerRootDir, targetRootDir);
 
-        // Disable the ChangeLog system
-        service.getChangeLog().setEnabled( false );
+        DirectoryEnvironmentConfig config = new DirectoryEnvironmentConfig();
+        config.setConfigClass(ConfigFileHandler.class);
+        config.setServerRoot(new File(targetRootDir));
 
-        // Create a new partition named 'apache'.
-        //Partition apachePartition = addPartition( "apache", "dc=apache,dc=org" );
+        EmbeddedUtils.startServer(config);
+    }
 
-        // Index some attributes on the apache partition
-        //addIndex( apachePartition, "objectClass", "ou", "uid" );
+    @AfterClass(alwaysRun = true)
+    public void afterClass() throws Exception {
+        EmbeddedUtils.stopServer("org.opends.server.tools.StopDS", org.opends.messages.Message.EMPTY);
+    }
 
-        // And start the service
-        service.startup();
+    private static void initOpendsDirectory(String originalRootDir,
+                                            String targetRootDir) throws IOException {
+        File workingDirectory = new File(targetRootDir);
+        FileUtils.copyDirectory(new File(originalRootDir), new File(targetRootDir));
 
-        // Inject the apache root entry if it does not already exist
-        //if ( !service.getAdminSession().exists( apachePartition.getSuffixDn() ) )
-        //{
-        //    LdapDN dnApache = new LdapDN( "dc=Apache,dc=Org" );
-        //    ServerEntry entryApache = service.newEntry( dnApache );
-        //    entryApache.add( "objectClass", "top", "domain", "extensibleObject" );
-        //    entryApache.add( "dc", "Apache" );
-        //    service.getAdminSession().add( entryApache );
-        //}
+        // create missing directories
+        // db backend, logs, locks
+        String[] subDirectories = {"db", "ldif", "locks", "logs"};
+        for (String s : subDirectories) {
+            new File(workingDirectory, s).mkdir();
+        }
+    }
 
+    private String getFile(String file) {
+        return new File(getClass().getResource("/" + file).getFile()).getAbsolutePath();
     }
 
     @Test
-    public void test() {
-        LdapSecurityProvider provider = new LdapSecurityProvider("foo","bar");
-        boolean success = provider.authenticate(null,"user","password");
+    public void whenAuthenticated() {
+        LdapSecurityProvider provider = new LdapSecurityProvider(LDAP_URL, LDAP_REALM);
+        boolean success = provider.authenticate(null, "peter", "password");
         assertTrue(success);
+    }
+
+    @Test
+    public void whenNotAuthenticated() {
+        LdapSecurityProvider provider = new LdapSecurityProvider(LDAP_URL, LDAP_REALM);
+        boolean success = provider.authenticate(null, "peter", "badpassword");
+        assertFalse(success);
     }
 }
