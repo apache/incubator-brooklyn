419def29a1429a75aeda296d681841f60a20773f
Peter Veentjer <peter.veentjer@cloudsoft.com>
2012-10-09 17:04:31 +0300

    Upgraded tomcat to 7.0.32 (since 7.0.29 can't be downloaded anymore)

--- software/webapp/src/main/java/brooklyn/entity/webapp/tomcat/TomcatServer.java
+++ software/webapp/src/main/java/brooklyn/entity/webapp/tomcat/TomcatServer.java
@@ -33,7 +33,7 @@ public class TomcatServer extends JavaWebAppSoftwareProcess implements JavaWebAp
 
     @SetFromFlag("version")
     public static final BasicConfigKey<String> SUGGESTED_VERSION =
-            new BasicConfigKey<String>(SoftwareProcessEntity.SUGGESTED_VERSION, "7.0.29");
+            new BasicConfigKey<String>(SoftwareProcessEntity.SUGGESTED_VERSION, "7.0.32");
 
     /**
      * Tomcat insists on having a port you can connect to for the sole purpose of shutting it down.
