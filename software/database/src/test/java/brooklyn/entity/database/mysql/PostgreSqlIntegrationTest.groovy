d71aca01fade56c209c8ca6a5bb0c66bf5ce2ca1
Adam Lowe <adam.p.lowe@gmail.com>
2012-11-27 23:42:29 +0000

    Adding PostgreSQL integration test and adjusting MySQL integration test to share code

---
+++ software/database/src/test/java/brooklyn/entity/database/mysql/PostgreSqlIntegrationTest.groovy
@@ -0,0 +1,79 @@
+package brooklyn.entity.database.mysql;
+
+
+import brooklyn.location.basic.LocalhostMachineProvisioningLocation
+import brooklyn.test.entity.TestApplication
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+
+import java.sql.Connection
+import java.sql.Date
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.sql.Statement
+import brooklyn.entity.database.postgresql.PostgreSqlNode
+
+/**
+ * Runs the popular Vogella MySQL tutorial against PostgreSQL
+ * from
+ * http://www.vogella.de/articles/MySQLJava/article.html
+ */
+public class PostgreSqlIntegrationTest {
+
+    public static final Logger log = LoggerFactory.getLogger(PostgreSqlIntegrationTest.class);
+
+    @BeforeMethod(groups = ["Integration"])
+    public void ensureNoInstance() {
+    }
+
+    @AfterMethod(groups = ["Integration"])
+    public void ensureShutDown() {
+    }
+
+    //from http://www.vogella.de/articles/MySQLJava/article.html
+    public static final String CREATION_SCRIPT = """
+CREATE USER sqluser WITH PASSWORD 'sqluserpw'; 
+create database feedback;
+
+\\c feedback;
+
+CREATE TABLE COMMENTS (
+        id INT8 NOT NULL, 
+        MYUSER VARCHAR(30) NOT NULL,
+        EMAIL VARCHAR(30), 
+        WEBPAGE VARCHAR(100) NOT NULL, 
+        DATUM DATE NOT NULL, 
+        SUMMARY VARCHAR(40) NOT NULL,
+        COMMENTS VARCHAR(400) NOT NULL,
+        PRIMARY KEY (ID)
+    );
+
+GRANT ALL ON comments TO sqluser;
+
+INSERT INTO COMMENTS values (1, 'lars', 'myemail@gmail.com','http://www.vogella.de', '2009-09-14 10:33:11', 'Summary','My first comment' );
+""";
+
+    @Test(groups = ["Integration"])
+    public void runIt() {
+        TestApplication tapp = new TestApplication(name: "PostgreSqlIntegrationTest");
+        PostgreSqlNode pgsql = new PostgreSqlNode(tapp, creationScriptContents: CREATION_SCRIPT);
+
+        try {
+            tapp.start([new LocalhostMachineProvisioningLocation()]);
+
+            log.info("PostgreSql started");
+
+            new VogellaExampleAccess().readDataBase("org.postgresql.Driver", "postgresql", "localhost", pgsql.getPort());
+
+            log.info("Ran vogella PostgreSql example -- SUCCESS");
+        } finally {
+            pgsql.stop();
+            tapp.destroy();
+        }
+    }
+}
---
+++ software/database/src/test/java/brooklyn/entity/database/mysql/VogellaExampleAccess.java
@@ -0,0 +1,125 @@
+package brooklyn.entity.database.mysql;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Slightly altered version of the popular Vogella MySQL tutorial
+ * http://www.vogella.de/articles/MySQLJava/article.html
+ */
+public class VogellaExampleAccess {
+    public static final Logger log = LoggerFactory.getLogger(VogellaExampleAccess.class);
+
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    public void readDataBase(String driverClass, String type, String host, int port) throws Exception {
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName(driverClass);
+            // Setup the connection with the DB
+            connect = DriverManager.getConnection(
+                    "jdbc:" + type + "://" + host + ":" + port + "/feedback?"
+                            + "user=sqluser&password=sqluserpw");
+
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement.executeQuery("select * from COMMENTS");
+            writeResultSet(resultSet);
+
+            // PreparedStatements can use variables and are more efficient
+            preparedStatement = connect.prepareStatement("insert into  COMMENTS values (?, ?, ?, ?, ? , ?, ?)");
+            // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
+            // Parameters start with 1
+            preparedStatement.setInt(1, 2);
+            preparedStatement.setString(2, "Test");
+            preparedStatement.setString(3, "TestEmail");
+            preparedStatement.setString(4, "TestWebpage");
+            preparedStatement.setDate(5, new java.sql.Date(2009, 12, 11));
+            preparedStatement.setString(6, "TestSummary");
+            preparedStatement.setString(7, "TestComment");
+            preparedStatement.executeUpdate();
+
+            preparedStatement = connect.prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from COMMENTS");
+            resultSet = preparedStatement.executeQuery();
+            writeResultSet(resultSet);
+
+            // Remove again the insert comment
+            preparedStatement = connect
+                    .prepareStatement("delete from COMMENTS where myuser= ? ; ");
+            preparedStatement.setString(1, "Test");
+            preparedStatement.executeUpdate();
+
+            resultSet = statement.executeQuery("select * from COMMENTS");
+            writeMetaData(resultSet);
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+
+    }
+
+    private void writeMetaData(ResultSet resultSet) throws SQLException {
+        //  Now get some metadata from the database
+        // Result set get the result of the SQL query
+
+        log.info("The columns in the table are: ");
+
+        log.info("Table: " + resultSet.getMetaData().getTableName(1));
+        for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+            log.info("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+        }
+    }
+
+    private void writeResultSet(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+        while (resultSet.next()) {
+            // It is possible to get the columns via name
+            // also possible to get the columns via the column number
+            // which starts at 1
+            // e.g. resultSet.getSTring(2);
+            String user = resultSet.getString("myuser");
+            String website = resultSet.getString("webpage");
+            String summary = resultSet.getString("summary");
+            Date date = resultSet.getDate("datum");
+            String comment = resultSet.getString("comments");
+            log.info("User: " + user);
+            log.info("Website: " + website);
+            log.info("Summary: " + summary);
+            log.info("Date: " + date);
+            log.info("Comment: " + comment);
+        }
+    }
+
+    // You need to close the resultSet
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}    
