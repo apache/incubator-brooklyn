e776aa560730ec950247f348d683deb2c094bdf8
Aled Sage <aled.sage@gmail.com>
2012-12-20 15:23:50 +0000

    SSH Feed for Java

---
+++ core/src/test/java/brooklyn/event/feed/ssh/SshFeedIntegrationTest.java
@@ -0,0 +1,118 @@
+package brooklyn.event.feed.ssh;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import brooklyn.entity.basic.Entities;
+import brooklyn.entity.basic.EntityLocal;
+import brooklyn.event.basic.BasicAttributeSensor;
+import brooklyn.event.feed.function.FunctionFeedTest;
+import brooklyn.location.basic.LocalhostMachineProvisioningLocation;
+import brooklyn.location.basic.SshMachineLocation;
+import brooklyn.test.EntityTestUtils;
+import brooklyn.test.TestUtils;
+import brooklyn.test.entity.TestApplication;
+import brooklyn.test.entity.TestEntity;
+import brooklyn.util.MutableMap;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Closeables;
+
+public class SshFeedIntegrationTest {
+
+    final static BasicAttributeSensor<String> SENSOR_STRING = new BasicAttributeSensor<String>(String.class, "aString", "");
+    final static BasicAttributeSensor<Integer> SENSOR_INT = new BasicAttributeSensor<Integer>(Integer.class, "aLong", "");
+
+    private LocalhostMachineProvisioningLocation loc;
+    private SshMachineLocation machine;
+    private TestApplication app;
+    private EntityLocal entity;
+    private SshFeed feed;
+    
+    @BeforeMethod(alwaysRun=true)
+    public void setUp() throws Exception {
+        loc = new LocalhostMachineProvisioningLocation();
+        machine = loc.obtain();
+        app = new TestApplication();        
+        entity = new TestEntity(app);
+        Entities.startManagement(app);
+        app.start(ImmutableList.of(loc));
+    }
+
+    @AfterMethod(alwaysRun=true)
+    public void tearDown() throws Exception {
+        if (feed != null) feed.stop();
+        if (app != null) Entities.destroy(app);
+        if (loc != null) Closeables.closeQuietly(loc);
+    }
+    
+    @Test(groups="Integration")
+    public void testReturnsSshExitStatus() throws Exception {
+        feed = SshFeed.builder()
+                .entity(entity)
+                .machine(machine)
+                .poll(new SshPollConfig<Integer>(SENSOR_INT)
+                        .command("exit 123")
+                        .onSuccess(SshValueFunctions.exitStatus()))
+                .build();
+
+        EntityTestUtils.assertAttributeEqualsEventually(entity, SENSOR_INT, 123);
+    }
+    
+    @Test(groups="Integration")
+    public void testReturnsSshStdout() throws Exception {
+        feed = SshFeed.builder()
+                .entity(entity)
+                .machine(machine)
+                .poll(new SshPollConfig<String>(SENSOR_STRING)
+                        .command("echo hello")
+                        .onSuccess(SshValueFunctions.stdout()))
+                .build();
+        
+        TestUtils.executeUntilSucceeds(MutableMap.of(), new Runnable() {
+            public void run() {
+                String val = entity.getAttribute(SENSOR_STRING);
+                assertTrue(val != null && val.contains("hello"), "val="+val);
+            }});
+    }
+
+    @Test(groups="Integration")
+    public void testReturnsSshStderr() throws Exception {
+        final String cmd = "thiscommanddoesnotexist";
+        
+        feed = SshFeed.builder()
+                .entity(entity)
+                .machine(machine)
+                .poll(new SshPollConfig<String>(SENSOR_STRING)
+                        .command(cmd)
+                        .onSuccess(SshValueFunctions.stderr()))
+                .build();
+        
+        TestUtils.executeUntilSucceeds(MutableMap.of(), new Runnable() {
+            public void run() {
+                String val = entity.getAttribute(SENSOR_STRING);
+                assertTrue(val != null && val.contains(cmd), "val="+val);
+            }});
+    }
+    
+    @Test(groups="Integration")
+    public void testFailsOnNonZeroWhenConfigured() throws Exception {
+        feed = SshFeed.builder()
+                .entity(entity)
+                .machine(machine)
+                .poll(new SshPollConfig<String>(SENSOR_STRING)
+                        .command("exit 123")
+                        .failOnNonZeroResultCode(true)
+                        .onError(new FunctionFeedTest.ToStringFunction()))
+                .build();
+        
+        TestUtils.executeUntilSucceeds(MutableMap.of(), new Runnable() {
+            public void run() {
+                String val = entity.getAttribute(SENSOR_STRING);
+                assertTrue(val != null && val.contains("Exit status 123"), "val="+val);
+            }});
+    }
+}
