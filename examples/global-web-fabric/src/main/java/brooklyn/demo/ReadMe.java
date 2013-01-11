25e3553135d9ad687dea67752adfe6fbfeba9f59
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-01-11 16:43:38 +0000

    created bogus empty java files for examples, so that javadoc artifact is created, and build is not rejected by maven central!

---
+++ examples/global-web-fabric/src/main/java/brooklyn/demo/ReadMe.java
@@ -0,0 +1,10 @@
+package brooklyn.demo;
+
+public class ReadMe {
+
+/* This class exists only so that javadoc is generated and artifacts can be staged at sonatype for deployment to maven central.
+ * See: https://docs.sonatype.org/display/Repository/Sonatype+OSS+Maven+Repository+Usage+Guide
+ *
+ * (The actual examples are in other classes in this package / project.) */
+
+}
