f42489af13413e8b3bd32404ac1c2aa76fa49ec5
Aled Sage <aled.sage@gmail.com>
2013-01-24 23:53:14 +0000

    SshTool impl that doing system calls to ssh/scp
    - workaround for Tectia: if connecting to a Tectia server,
      the sshj doesn't work (see https://github.com/shikhar/sshj/issues/54)

---
+++ core/src/test/java/brooklyn/util/internal/ssh/cli/SshCliToolPerformanceTest.java
@@ -0,0 +1,26 @@
+package brooklyn.util.internal.ssh.cli;
+
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import brooklyn.util.internal.ssh.SshTool;
+import brooklyn.util.internal.ssh.SshToolPerformanceTest;
+
+/**
+ * Test the performance of different variants of invoking the sshj tool.
+ * 
+ * Intended for human-invocation and inspection, to see which parts are most expensive.
+ */
+public class SshCliToolPerformanceTest extends SshToolPerformanceTest {
+
+    @Override
+    protected SshTool newSshTool(Map<String,?> flags) {
+        return new SshCliTool(flags);
+    }
+    
+    // Need to have at least one test method here (rather than just inherited) for eclipse to recognize it
+    @Test(enabled = false)
+    public void testDummy() throws Exception {
+    }
+}
