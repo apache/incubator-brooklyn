2a8132a1d371dee3c2cf7a17717ea88e7be3f65b
Aled Sage <aled.sage@gmail.com>
2013-01-29 12:37:39 +0000

    convert ElasticJavaWebAppService from groovy to java

--- software/webapp/src/main/java/brooklyn/entity/webapp/ElasticJavaWebAppService.java
+++ software/webapp/src/main/java/brooklyn/entity/webapp/ElasticJavaWebAppService.java
@@ -1,12 +1,14 @@
-package brooklyn.entity.webapp
+package brooklyn.entity.webapp;
 
-import brooklyn.entity.Entity
-import brooklyn.entity.basic.AbstractConfigurableEntityFactory
-import brooklyn.entity.basic.ConfigurableEntityFactory
-import brooklyn.entity.basic.EntityFactoryForLocation
-import brooklyn.entity.trait.Startable
-import brooklyn.location.Location
-import brooklyn.location.MachineProvisioningLocation
+import java.util.Map;
+
+import brooklyn.entity.Entity;
+import brooklyn.entity.basic.AbstractConfigurableEntityFactory;
+import brooklyn.entity.basic.ConfigurableEntityFactory;
+import brooklyn.entity.basic.EntityFactoryForLocation;
+import brooklyn.entity.trait.Startable;
+import brooklyn.location.Location;
+import brooklyn.location.MachineProvisioningLocation;
 
 public interface ElasticJavaWebAppService extends JavaWebAppService, Startable {
 
@@ -18,15 +20,15 @@ public interface ElasticJavaWebAppService extends JavaWebAppService, Startable {
     implements EntityFactoryForLocation<ElasticJavaWebAppService> {
 
         public ElasticJavaWebAppService newEntity2(Map flags, Entity parent) {
-            new ControlledDynamicWebAppClusterImpl(flags, parent);
+            return new ControlledDynamicWebAppClusterImpl(flags, parent);
         }
 
         public ConfigurableEntityFactory<ElasticJavaWebAppService> newFactoryForLocation(Location l) {
-            if (l in ElasticJavaWebAppServiceAwareLocation) {
+            if (l instanceof ElasticJavaWebAppServiceAwareLocation) {
                 return ((ElasticJavaWebAppServiceAwareLocation)l).newWebClusterFactory().configure(config);
             }
             //optional, fail fast if location not supported
-            if (!(l in MachineProvisioningLocation))
+            if (!(l instanceof MachineProvisioningLocation))
                 throw new UnsupportedOperationException("cannot create this entity in location "+l);
             return this;
         }
