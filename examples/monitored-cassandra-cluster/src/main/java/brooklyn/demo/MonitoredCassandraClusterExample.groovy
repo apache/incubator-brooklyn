bfec1a29629a095d91ab3752a6d26f2b650eb8c6
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-01-30 07:33:20 +0000

    Use CassandraCluster in demo application

--- examples/monitored-cassandra-cluster/src/main/java/brooklyn/demo/MonitoredCassandraClusterExample.groovy
+++ examples/monitored-cassandra-cluster/src/main/java/brooklyn/demo/MonitoredCassandraClusterExample.groovy
@@ -3,14 +3,15 @@ package brooklyn.demo
 import java.util.List
 
 import brooklyn.entity.basic.AbstractApplication
-import brooklyn.entity.nosql.cassandra.CassandraNode
+import brooklyn.entity.nosql.cassandra.CassandraCluster
 import brooklyn.location.Location
 
 /** Cassandra Application */
 public class MonitoredCassandraClusterExample extends AbstractApplication {
 
-    CassandraNode server = new CassandraNode(this,
+    CassandraCluster cluster = new CassandraCluster(this,
             clusterName:'CassandraDemo',
+            initialSize:2,
             jmxPort:'11099+', rmiPort:'9001+', thriftPort:'9160+')
 
 }
