a7fc2628003a412b5cdaeb14b3e4a45458a251f8
Aled Sage <aled.sage@gmail.com>
2013-01-31 14:48:29 +0000

    Makes SubscriptionTracker thread-safe

--- core/src/main/java/brooklyn/management/internal/SubscriptionTracker.java
+++ core/src/main/java/brooklyn/management/internal/SubscriptionTracker.java
@@ -1,7 +1,6 @@
 package brooklyn.management.internal;
 
 import java.util.Collection;
-import java.util.Collections;
 
 import brooklyn.entity.Entity;
 import brooklyn.entity.Group;
@@ -11,6 +10,7 @@ import brooklyn.management.SubscriptionContext;
 import brooklyn.management.SubscriptionHandle;
 
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.SetMultimap;
 
 /**
@@ -19,32 +19,48 @@ import com.google.common.collect.SetMultimap;
  */
 public class SubscriptionTracker {
 
-    protected transient SubscriptionContext subscription;
+    // This class is thread-safe. All modifications to subscriptions are synchronized on the 
+    // "subscriptions" field. However, calls to alien code (i.e. context.subscribe etc) is
+    // done without holding the lock.
+    //
+    // If two threads do subscribe() and unsubscribeAll() concurrently, then it's non-derministic
+    // whether the subscription will be in place at the end (but that's unavoidable). However, it
+    // is guaranteed that the internal state of the SubscriptionTracker will be consistent: if
+    // the "subscriptions" includes the new subscription then that subscription will really exist,
+    // and vice versa.
     
-    private SetMultimap<Entity, SubscriptionHandle> subscriptions = HashMultimap.create();
+    protected SubscriptionContext context;
+    
+    private final SetMultimap<Entity, SubscriptionHandle> subscriptions = HashMultimap.create();
 
     public SubscriptionTracker(SubscriptionContext subscriptionContext) {
-        this.subscription = subscriptionContext;
+        this.context = subscriptionContext;
     }
     
     /** @see SubscriptionContext#subscribe(Entity, Sensor, SensorEventListener) */
     public <T> SubscriptionHandle subscribe(Entity producer, Sensor<T> sensor, SensorEventListener<? super T> listener) {
-        SubscriptionHandle handle = subscription.subscribe(producer, sensor, listener);
-        subscriptions.put(producer, handle);
+        SubscriptionHandle handle = context.subscribe(producer, sensor, listener);
+        synchronized (subscriptions) {
+            subscriptions.put(producer, handle);
+        }
         return handle;
     }
     
     /** @see SubscriptionContext#subscribeToChildren(Entity, Sensor, SensorEventListener) */
     public <T> SubscriptionHandle subscribeToChildren(Entity parent, Sensor<T> sensor, SensorEventListener<? super T> listener) {
-        SubscriptionHandle handle = subscription.subscribeToChildren(parent, sensor, listener);
-        subscriptions.put(parent, handle);
+        SubscriptionHandle handle = context.subscribeToChildren(parent, sensor, listener);
+        synchronized (subscriptions) {
+            subscriptions.put(parent, handle);
+        }
         return handle;
     }
 
     /** @see SubscriptionContext#subscribeToMembers(Entity, Sensor, SensorEventListener) */
     public <T> SubscriptionHandle subscribeToMembers(Group parent, Sensor<T> sensor, SensorEventListener<? super T> listener) {
-        SubscriptionHandle handle = subscription.subscribeToMembers(parent, sensor, listener);
-        subscriptions.put(parent, handle);
+        SubscriptionHandle handle = context.subscribeToMembers(parent, sensor, listener);
+        synchronized (subscriptions) {
+            subscriptions.put(parent, handle);
+        }
         return handle;
     }    
 
@@ -54,10 +70,13 @@ public class SubscriptionTracker {
      * @see SubscriptionContext#unsubscribe(SubscriptionHandle)
      */
     public boolean unsubscribe(Entity producer) {
-        Collection<SubscriptionHandle> handles = subscriptions.removeAll(producer);
+        Collection<SubscriptionHandle> handles;
+        synchronized (subscriptions) {
+            handles = subscriptions.removeAll(producer);
+        }
         if (handles != null) {
             for (SubscriptionHandle handle : handles) {
-                subscription.unsubscribe(handle);
+                context.unsubscribe(handle);
             }
             return true;
         }
@@ -70,20 +89,29 @@ public class SubscriptionTracker {
     * @see SubscriptionContext#unsubscribe(SubscriptionHandle)
     */
    public boolean unsubscribe(Entity producer, SubscriptionHandle handle) {
-       subscriptions.remove(producer, handle);
-       return subscription.unsubscribe(handle);
+       synchronized (subscriptions) {
+           subscriptions.remove(producer, handle);
+       }
+       return context.unsubscribe(handle);
    }
 
     /**
     * @return an ordered list of all subscription handles
     */
    public Collection<SubscriptionHandle> getAllSubscriptions() {
-       return Collections.unmodifiableCollection(subscriptions.values());
+       synchronized (subscriptions) {
+           return ImmutableList.copyOf(subscriptions.values());
+       }
    }
    
    public void unsubscribeAll() {
-       for (SubscriptionHandle s: subscriptions.values())
-           subscription.unsubscribe(s);
-       subscriptions.clear();
+       Collection<SubscriptionHandle> subscriptionsSnapshot;
+       synchronized (subscriptions) {
+           subscriptionsSnapshot = ImmutableList.copyOf(subscriptions.values());
+           subscriptions.clear();
+       }
+       for (SubscriptionHandle s: subscriptionsSnapshot) {
+           context.unsubscribe(s);
+       }
    }
 }
