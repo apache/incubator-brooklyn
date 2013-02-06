3ce3bc6d5231ac61d1e9b6df6bd80cecfc8d54d7
Aled Sage <aled.sage@gmail.com>
2013-02-06 16:54:42 +0000

    CLI: accept an entity with `--app`
    - automatically wraps it in an app

--- usage/cli/src/test/java/brooklyn/cli/CliTest.java
+++ usage/cli/src/test/java/brooklyn/cli/CliTest.java
@@ -1,11 +1,11 @@
 package brooklyn.cli;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+import groovy.lang.GroovyClassLoader;
 
 import java.util.Collection;
-
-import groovy.lang.GroovyClassLoader;
 
 import org.iq80.cli.Cli;
 import org.iq80.cli.ParseException;
@@ -14,45 +14,77 @@ import org.testng.annotations.Test;
 import brooklyn.cli.Main.BrooklynCommand;
 import brooklyn.cli.Main.HelpCommand;
 import brooklyn.cli.Main.LaunchCommand;
+import brooklyn.entity.Entity;
 import brooklyn.entity.basic.AbstractApplication;
+import brooklyn.entity.basic.AbstractEntity;
 import brooklyn.entity.basic.ApplicationBuilder;
 import brooklyn.entity.basic.Entities;
+import brooklyn.entity.basic.StartableApplication;
+import brooklyn.entity.proxying.EntityProxy;
+import brooklyn.entity.proxying.ImplementedBy;
+import brooklyn.entity.trait.Startable;
 import brooklyn.location.Location;
 import brooklyn.location.basic.SimulatedLocation;
 import brooklyn.util.ResourceUtils;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 
 public class CliTest {
 
     @Test
     public void testLoadApplicationFromClasspath() throws Exception {
-        LaunchCommand launchCommand = new Main.LaunchCommand();
-        ResourceUtils resourceUtils = new ResourceUtils(this);
-        GroovyClassLoader loader = new GroovyClassLoader(CliTest.class.getClassLoader());
         String appName = ExampleApp.class.getName();
-        Object app = launchCommand.loadApplicationFromClasspathOrParse(resourceUtils, loader, appName);
+        Object app = loadApplicationFromClasspathOrParse(appName);
         assertTrue(app instanceof ExampleApp, "app="+app);
     }
 
     @Test
     public void testLoadApplicationBuilderFromClasspath() throws Exception {
-        LaunchCommand launchCommand = new Main.LaunchCommand();
-        ResourceUtils resourceUtils = new ResourceUtils(this);
-        GroovyClassLoader loader = new GroovyClassLoader(CliTest.class.getClassLoader());
         String appName = ExampleAppBuilder.class.getName();
-        Object appBuilder = launchCommand.loadApplicationFromClasspathOrParse(resourceUtils, loader, appName);
+        Object appBuilder = loadApplicationFromClasspathOrParse(appName);
         assertTrue(appBuilder instanceof ExampleAppBuilder, "app="+appBuilder);
     }
 
     @Test
+    public void testLoadEntityFromClasspath() throws Exception {
+        String entityName = ExampleEntity.class.getName();
+        Object appBuilder = loadApplicationFromClasspathOrParse(entityName);
+        assertTrue(appBuilder instanceof ApplicationBuilder, "app="+appBuilder);
+        
+        StartableApplication app = ((ApplicationBuilder)appBuilder).manage();
+        Collection<Entity> entities = app.getChildren();
+        assertEquals(entities.size(), 1, "entities="+entities);
+        assertTrue(Iterables.getOnlyElement(entities) instanceof ExampleEntity, "entities="+entities);
+        assertTrue(Iterables.getOnlyElement(entities) instanceof EntityProxy, "entities="+entities);
+    }
+
+    @Deprecated // Tests deprecated approach of using impl directly
+    @Test
+    public void testLoadEntityImplFromClasspath() throws Exception {
+        String entityName = ExampleEntityImpl.class.getName();
+        Object appBuilder = loadApplicationFromClasspathOrParse(entityName);
+        assertTrue(appBuilder instanceof ApplicationBuilder, "app="+appBuilder);
+        
+        StartableApplication app = ((ApplicationBuilder)appBuilder).manage();
+        Collection<Entity> entities = app.getChildren();
+        assertEquals(entities.size(), 1, "entities="+entities);
+        assertEquals(Iterables.getOnlyElement(entities).getEntityType().getName(), ExampleEntityImpl.class.getCanonicalName(), "entities="+entities);
+        assertTrue(Iterables.getOnlyElement(entities) instanceof EntityProxy, "entities="+entities);
+    }
+
+    @Test
     public void testLoadApplicationByParsingFile() throws Exception {
+        String appName = "ExampleAppInFile.groovy"; // file found in src/test/resources (contains empty app)
+        Object app = loadApplicationFromClasspathOrParse(appName);
+        assertTrue(app.getClass().getName().equals("ExampleAppInFile"), "app="+app);
+    }
+    
+    private Object loadApplicationFromClasspathOrParse(String appName) throws Exception {
         LaunchCommand launchCommand = new Main.LaunchCommand();
         ResourceUtils resourceUtils = new ResourceUtils(this);
         GroovyClassLoader loader = new GroovyClassLoader(CliTest.class.getClassLoader());
-        String appName = "ExampleAppInFile.groovy"; // file found in src/test/resources (contains empty app)
-        Object app = launchCommand.loadApplicationFromClasspathOrParse(resourceUtils, loader, appName);
-        assertTrue(app.getClass().getName().equals("ExampleAppInFile"), "app="+app);
+        return launchCommand.loadApplicationFromClasspathOrParse(resourceUtils, loader, appName);
     }
     
     @Test
@@ -129,6 +161,25 @@ public class CliTest {
         }
     }
     
+    // An empty entity to be used for testing
+    @ImplementedBy(ExampleEntityImpl.class)
+    public static interface ExampleEntity extends Entity, Startable {
+    }
+
+    @SuppressWarnings("serial")
+    public static class ExampleEntityImpl extends AbstractEntity implements ExampleEntity {
+        volatile boolean running;
+        
+        @Override public void start(Collection<? extends Location> locations) {
+            running = true;
+        }
+        @Override public void stop() {
+            running = false;
+        }
+        @Override public void restart() {
+        }
+    }
+
     // An empty app builder to be used for testing
     public static class ExampleAppBuilder extends ApplicationBuilder {
         @Override protected void doBuild() {
