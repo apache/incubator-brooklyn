1f4adfb100d90d10609c1fb036085b89b24dab36
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-02-07 17:03:38 +0000

    fix so that ApplicationBuilder @Catalog entities can be deployed

--- usage/rest/src/main/java/brooklyn/rest/util/BrooklynRestResourceUtils.java
+++ usage/rest/src/main/java/brooklyn/rest/util/BrooklynRestResourceUtils.java
@@ -22,6 +22,7 @@ import brooklyn.entity.Application;
 import brooklyn.entity.Entity;
 import brooklyn.entity.basic.AbstractApplication;
 import brooklyn.entity.basic.AbstractEntity;
+import brooklyn.entity.basic.ApplicationBuilder;
 import brooklyn.entity.basic.Entities;
 import brooklyn.entity.basic.EntityInternal;
 import brooklyn.entity.basic.EntityLocal;
@@ -164,8 +165,10 @@ public class BrooklynRestResourceUtils {
             throw Throwables.propagate(e);
         }
 
-        log.info("REST placing '{}' under management as {}", spec.getName(), instance);
-        Entities.startManagement(instance, mgmt);
+        if (!Entities.isManaged(instance)) {
+            log.info("REST placing '{}' under management as {}", spec.getName(), instance);
+            Entities.startManagement(instance, mgmt);
+        }
         
         return instance;
     }
@@ -222,7 +225,10 @@ public class BrooklynRestResourceUtils {
     private EntityLocal tryInstantiateEntity(Constructor<?>[] constructors, Class<?>[] classes, Object[] objects) throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
         for (Constructor<?> c: constructors) {
             if (Arrays.equals(c.getParameterTypes(), classes)) {
-                return (EntityLocal) c.newInstance(objects);
+                Object o = c.newInstance(objects);
+                if (o instanceof EntityLocal) return (EntityLocal)o;
+                if (o instanceof ApplicationBuilder) 
+                    return (EntityLocal) ((ApplicationBuilder)o).manage(mgmt);
             }
         }
         return null;
