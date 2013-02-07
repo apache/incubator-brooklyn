fdc83692c320f891503d9320d3ae5d1ac20b4d5f
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-02-07 17:05:17 +0000

    make it easier to subclass JcloudsLocation, and to define sub-locations e.g. for subnet-specific locations (ConfigBag holds the set up, and the plan is that it can be used to simplify some of the passing of config / usedConfig everywhere)

---
+++ core/src/main/java/brooklyn/util/config/ConfigBag.java
@@ -0,0 +1,40 @@
+package brooklyn.util.config;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class ConfigBag {
+
+    private Map config = new LinkedHashMap();
+    private Map unusedConfig = new LinkedHashMap();
+
+    /** current values for all entries which have put into the map */
+    public Map getAllConfig() {
+        return Collections.unmodifiableMap(config);
+    }
+
+    /** current values for all entries which have not yet been used */
+    public Map getUnusedConfig() {
+        return Collections.unmodifiableMap(unusedConfig);
+    }
+    
+    public ConfigBag putAll(Map flags) {
+        for (Object eo: flags.entrySet()) {
+            Map.Entry e = (Map.Entry)eo;
+            boolean isNew = !config.containsKey(e.getKey());
+            boolean isUsed = !isNew && unusedConfig.containsKey(e.getKey());
+            config.put(e.getKey(), e.getValue());
+            if (isNew || !isUsed) 
+                unusedConfig.put(e.getKey(), e.getValue());
+            //if (!isNew && !isUsed) log.debug("updating config value which has already been used");
+        }
+        return this;
+    }
+    
+    public void markUsed(Object key) {
+        unusedConfig.remove(key);
+    }
+    
+}
