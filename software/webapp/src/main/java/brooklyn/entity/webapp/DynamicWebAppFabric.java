c83642889c7b69070b6613bba8a55e89b03224df
Aled Sage <aled.sage@gmail.com>
2013-02-21 10:33:28 +0000

    Adds DynamicWebAppFabric

---
+++ software/webapp/src/main/java/brooklyn/entity/webapp/DynamicWebAppFabric.java
@@ -0,0 +1,34 @@
+package brooklyn.entity.webapp;
+
+import brooklyn.entity.group.DynamicFabric;
+import brooklyn.entity.proxying.ImplementedBy;
+import brooklyn.event.AttributeSensor;
+import brooklyn.event.basic.BasicAttributeSensor;
+
+/**
+ * DynamicWebAppFabric provide a fabric of clusters, aggregating the entity attributes.  Currently totals and averages:
+ * <ul>
+ *   <li>Entity request counts</li>
+ *   <li>Entity error counts</li>
+ *   <li>Requests per second</li>
+ *   <li>Entity processing time</li>
+ * </ul>
+ */
+@ImplementedBy(DynamicWebAppFabricImpl.class)
+public interface DynamicWebAppFabric extends DynamicFabric, WebAppService {
+
+    public static final AttributeSensor<Double> REQUEST_COUNT_PER_NODE = new BasicAttributeSensor<Double>(
+            Double.class, "webapp.reqs.total.perNode", "Fabric entity request average");
+
+    public static final AttributeSensor<Integer> ERROR_COUNT_PER_NODE = new BasicAttributeSensor<Integer>(
+            Integer.class, "webapp.reqs.errors.perNode", "Fabric entity request error average");
+
+    public static final AttributeSensor<Double> REQUESTS_PER_SECOND_LAST_PER_NODE = new BasicAttributeSensor<Double>(
+            Double.class, "webapp.reqs.perSec.last.perNode", "Reqs/sec (last datapoint) averaged over all nodes");
+
+    public static final AttributeSensor<Double> REQUESTS_PER_SECOND_IN_WINDOW_PER_NODE = new BasicAttributeSensor<Double>(
+            Double.class, "webapp.reqs.perSec.windowed.perNode", "Reqs/sec (over time window) averaged over all nodes");
+
+    public static final AttributeSensor<Integer> TOTAL_PROCESSING_TIME_PER_NODE = new BasicAttributeSensor<Integer>(
+            Integer.class, "webapp.reqs.processingTime.perNode", "Total processing time per node");
+}
