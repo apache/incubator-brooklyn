483d16d2a313ca39b48e3b02669412cebc839ca8
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-03-01 13:55:00 +0000

    Deprecate old server factories and change javadoc to note any WebAppService can be used in clusters

--- software/webapp/src/main/java/brooklyn/entity/webapp/jboss/JBoss7ServerFactory.java
+++ software/webapp/src/main/java/brooklyn/entity/webapp/jboss/JBoss7ServerFactory.java
@@ -1,11 +1,14 @@
 package brooklyn.entity.webapp.jboss;
 
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import brooklyn.entity.basic.BasicConfigurableEntityFactory;
 
+/**
+ * @deprecated since 0.5; instead use new BasicConfigurableEntityFactory(JBoss7ServerImpl.class)
+ */
+@Deprecated
 public class JBoss7ServerFactory extends BasicConfigurableEntityFactory<JBoss7Server> {
     public JBoss7ServerFactory(){
         this(new LinkedHashMap());
