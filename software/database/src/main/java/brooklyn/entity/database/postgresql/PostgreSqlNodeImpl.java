1f181f8b57eb09c11eb9fdb57bd16c2e71c773c5
Adam Lowe <adam.p.lowe@gmail.com>
2013-03-08 16:45:29 +0000

    PostgreSql: adjusting to use the correct HOSTNAME attribute in DB_URL and adding SERVICE_UP check in SshMachineLocations

--- software/database/src/main/java/brooklyn/entity/database/postgresql/PostgreSqlNodeImpl.java
+++ software/database/src/main/java/brooklyn/entity/database/postgresql/PostgreSqlNodeImpl.java
@@ -1,18 +1,56 @@
 package brooklyn.entity.database.postgresql;
 
 import brooklyn.entity.basic.SoftwareProcessImpl;
+import brooklyn.event.feed.ssh.SshFeed;
+import brooklyn.event.feed.ssh.SshPollConfig;
+import brooklyn.event.feed.ssh.SshPollValue;
+import brooklyn.location.Location;
+import brooklyn.location.basic.SshMachineLocation;
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
+import com.google.common.collect.Iterables;
 
 public class PostgreSqlNodeImpl extends SoftwareProcessImpl implements PostgreSqlNode {
+
+    private SshFeed feed;
 
     public Class getDriverInterface() {
         return PostgreSqlDriver.class;
     }
-
+    @Override
+    public PostgreSqlDriver getDriver() {
+        return (PostgreSqlDriver) super.getDriver();
+    }
+    
     @Override
     protected void connectSensors() {
         super.connectSensors();
+        setAttribute(DB_URL, String.format("postgresql://%s:%s/", getAttribute(HOSTNAME), getAttribute(POSTGRESQL_PORT)));
 
-        setAttribute(DB_URL, "postgresql://" + getLocalHostname() + ":" + getAttribute(POSTGRESQL_PORT) + "/");
-        setAttribute(SERVICE_UP, true);  // TODO poll for status, and activity
+        Location machine = Iterables.get(getLocations(), 0, null);
+
+        if (machine instanceof SshMachineLocation) {
+            String cmd = getDriver().getStatusCmd();
+
+            feed = SshFeed.builder()
+                    .entity(this)
+                    .machine((SshMachineLocation)machine)
+                    .poll(new SshPollConfig<Boolean>(SERVICE_UP)
+                            .command(cmd)
+                            .onSuccess(new Function<SshPollValue, Boolean>() {
+                                public Boolean apply(SshPollValue input) {
+                                    return (input.getExitStatus() == 0);
+                                }})
+                            .onError(Functions.constant(false)))
+                    .build();
+        } else {
+            LOG.warn("Location(s) %s not an ssh-machine location, so not polling for status; setting serviceUp immediately", getLocations());
+            setAttribute(SERVICE_UP, true);
+        }
+    }
+
+    @Override
+    protected void disconnectSensors() {
+        if (feed != null) feed.stop();
     }
 }
