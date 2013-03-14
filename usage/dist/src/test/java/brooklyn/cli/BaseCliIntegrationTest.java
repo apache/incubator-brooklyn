2f70e01f7950e6ce94c21bf892dc72f2b7f9478c
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-03-14 00:11:45 +0000

    Created separate base class for CLI integration tests to lower code duplication

---
+++ usage/dist/src/test/java/brooklyn/cli/BaseCliIntegrationTest.java
@@ -0,0 +1,168 @@
+package brooklyn.cli;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import brooklyn.entity.Application;
+import brooklyn.entity.basic.AbstractApplication;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Command line interface test support.
+ */
+public class BaseCliIntegrationTest {
+
+    // FIXME this should not be hardcoded; needed to use the local code for Main
+    private static final String BROOKLYN_BIN_PATH = "./target/brooklyn-dist/bin/brooklyn";
+    private static final String BROOKLYN_CLASSPATH = "./target/test-classes/:./target/classes/";
+
+    private ExecutorService executor;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setup() {
+        executor = Executors.newCachedThreadPool();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+        executor.shutdownNow();
+    }
+
+    /** Invoke the brooklyn script with arguments. */
+    public Process startBrooklyn(String...argv) throws Throwable {
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.environment().remove("BROOKLYN_HOME");
+        pb.environment().put("BROOKLYN_CLASSPATH", BROOKLYN_CLASSPATH);
+        pb.command(Lists.asList(BROOKLYN_BIN_PATH, argv).toArray(new String[0]));
+        return pb.start();
+    }
+
+    public void testBrooklyn(Process brooklyn, BrooklynCliTest test, int expectedExit) throws Throwable {
+        testBrooklyn(brooklyn, test, expectedExit, false);
+    }
+
+    /** Tests the operation of the Brooklyn CLI. */
+    public void testBrooklyn(Process brooklyn, BrooklynCliTest test, int expectedExit, boolean stop) throws Throwable {
+        try {
+            Future<Integer> future = executor.submit(test);
+
+            // Send CR to stop if required
+            if (stop) {
+                OutputStream out = brooklyn.getOutputStream();
+                out.write('\n');
+                out.flush();
+            }
+
+            int exitStatus = future.get(10, TimeUnit.SECONDS);
+
+            // Check error code from process
+            assertEquals(exitStatus, expectedExit, "Command returned wrong status");
+        } catch (TimeoutException te) {
+            fail("Timed out waiting for process to complete");
+        } catch (ExecutionException ee) {
+            if (ee.getCause() instanceof AssertionError) {
+                throw ee.getCause();
+            } else throw ee;
+        } finally {
+            brooklyn.destroy();
+        }
+    }
+
+    /** A {@link Callable} that encapsulates Brooklyn CLI test logic. */
+    public static abstract class BrooklynCliTest implements Callable<Integer> {
+        private final Process brooklyn;
+
+        private String consoleOutput;
+        private String consoleError;
+
+        public BrooklynCliTest(Process brooklyn) {
+            this.brooklyn = brooklyn;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            // Wait for initial output
+            Thread.sleep(5000l);
+
+            // Get the console output of running that command
+            consoleOutput = convertStreamToString(brooklyn.getInputStream());
+            consoleError = convertStreamToString(brooklyn.getErrorStream());
+
+            // Check if the output looks as expected
+            checkConsole();
+
+            // Return exit status on completion
+            return brooklyn.waitFor();
+        }
+
+        /** Perform test assertions on console output and error streams. */
+        public abstract void checkConsole();
+
+        private String convertStreamToString(InputStream is) {
+            try {
+                return new Scanner(is).useDelimiter("\\A").next();
+            } catch (NoSuchElementException e) {
+                return "";
+            }
+        }
+
+        protected void assertConsoleOutput(String...expected) {
+            for (String e : expected) {
+                assertTrue(consoleOutput.contains(e), "Execution output not logged; output=" + consoleOutput);
+            }
+        }
+
+        protected void assertNoConsoleOutput(String...expected) {
+            for (String e : expected) {
+                assertFalse(consoleOutput.contains(e), "Execution output logged; output=" + consoleOutput);
+            }
+        }
+
+        protected void assertConsoleError(String...expected) {
+            for (String e : expected) {
+                assertTrue(consoleError.contains(e), "Execution error not logged; error=" + consoleError);
+            }
+        }
+
+        protected void assertNoConsoleError(String...expected) {
+            for (String e : expected) {
+                assertFalse(consoleError.contains(e), "Execution error logged; error=" + consoleError);
+            }
+        }
+
+        protected void assertConsoleOutputEmpty() {
+            assertTrue(consoleOutput.isEmpty(), "Output present; output=" + consoleOutput);
+        }
+
+        protected void assertConsoleErrorEmpty() {
+            assertTrue(consoleError.isEmpty(), "Error present; error=" + consoleError);
+        }
+    };
+
+    /** An empty {@link Application} for testing. */
+    @SuppressWarnings("serial")
+    public static class TestApplication extends AbstractApplication {
+        public TestApplication() {
+            // Empty, for testing
+        }
+    }
+
+}
--- usage/dist/src/test/java/brooklyn/cli/CliIntegrationTest.java
+++ usage/dist/src/test/java/brooklyn/cli/CliIntegrationTest.java
@@ -1,108 +1,33 @@
 package brooklyn.cli;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.fail;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import brooklyn.entity.Application;
-import brooklyn.entity.basic.AbstractApplication;
 
 /**
  * Test the command line interface operation.
  */
-public class CliIntegrationTest {
-
-    // FIXME this should not be hardcoded; needed to use the local code for Main
-    private static final String BROOKLYN_BIN_PATH = "./target/brooklyn-dist/bin/brooklyn";
-    private static final String BROOKLYN_CLASSPATH = "./target/test-classes/:./target/classes/";
-
-    private ExecutorService executor;
-
-    @BeforeMethod(alwaysRun = true)
-    public void setup() {
-        executor = Executors.newCachedThreadPool();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void teardown() {
-        executor.shutdownNow();
-    }
-
-    // Helper function used in testing
-    private String convertStreamToString(InputStream is) {
-        try {
-            return new Scanner(is).useDelimiter("\\A").next();
-        } catch (NoSuchElementException e) {
-            return "";
-        }
-    }
+public class CliIntegrationTest extends BaseCliIntegrationTest {
 
     /**
      * Checks if running {@code brooklyn help} produces the expected output.
      */
     @Test(groups = "Integration")
     public void testLaunchCliHelp() throws Throwable {
-        // Invoke the brooklyn script with the "help" argument
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.environment().remove("BROOKLYN_HOME");
-        pb.environment().put("BROOKLYN_CLASSPATH", BROOKLYN_CLASSPATH);
-        pb.command(BROOKLYN_BIN_PATH, "help");
-        final Process brooklyn = pb.start();
- 
-        Callable<Integer> cli = new Callable<Integer>() {
+        final Process brooklyn = startBrooklyn("help");
+
+        BrooklynCliTest test = new BrooklynCliTest(brooklyn) {
             @Override
-            public Integer call() throws Exception {
-                // Get the console output of running that command
-                String consoleOutput = convertStreamToString(brooklyn.getInputStream());
-                String consoleError = convertStreamToString(brooklyn.getErrorStream());
-
-                // Check if the output looks as expected for the help command
-                assertTrue(consoleOutput.contains("usage: brooklyn"), "Usage info not present; output=" + consoleOutput);
-                assertTrue(consoleOutput.contains("The most commonly used brooklyn commands are:"), "List of common commands not present; output=" + consoleOutput);
-                assertTrue(consoleOutput.contains("help     Display help for available commands")
-                        && consoleOutput.contains("info     Display information about brooklyn")
-                        && consoleOutput.contains("launch   Starts a brooklyn application"), "List of common commands present; output=" + consoleOutput);
-                assertTrue(consoleOutput.contains("See 'brooklyn help <command>' for more information on a specific command."),
-                        "Implemented commands not listed; output=" + consoleOutput);
-                assertTrue(consoleError.isEmpty(), "Output present; error=" + consoleError);
-
-                // Return exit status on completion
-                return brooklyn.waitFor();
+            public void checkConsole() {
+                assertConsoleOutput("usage: brooklyn"); // Usage info not present
+                assertConsoleOutput("The most commonly used brooklyn commands are:");
+                assertConsoleOutput("help     Display help for available commands",
+                                    "info     Display information about brooklyn",
+                                    "launch   Starts a brooklyn application"); // List of common commands not present
+                assertConsoleOutput("See 'brooklyn help <command>' for more information on a specific command.");
+                assertConsoleErrorEmpty();
             }
         };
 
-        try {
-            Future<Integer> future = executor.submit(cli);
-            int exitStatus = future.get(10, TimeUnit.SECONDS);
-
-            // Check error code from process is 0
-            assertEquals(exitStatus, 0, "Command terminated with error status");
-        } catch (TimeoutException te) {
-            fail("Timed out waiting for process to complete");
-        } catch (ExecutionException ee) {
-            if (ee.getCause() instanceof AssertionError) {
-                throw ee.getCause();
-            } else throw ee;
-        } finally {
-            brooklyn.destroy();
-        }
+        testBrooklyn(brooklyn, test, 0);
     }
 
     /**
@@ -110,103 +35,39 @@ public class CliIntegrationTest {
      */
     @Test(groups = "Integration")
     public void testLaunchCliApp() throws Throwable {
-        // Invoke the brooklyn script with the launch command
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.environment().remove("BROOKLYN_HOME");
-        pb.environment().put("BROOKLYN_CLASSPATH", BROOKLYN_CLASSPATH);
-        pb.command(BROOKLYN_BIN_PATH, "--verbose", "launch", "--stopOnKeyPress", "--app", "brooklyn.cli.CliIntegrationTest$TestApplication", "--location", "localhost", "--noConsole");
-        final Process brooklyn = pb.start();
- 
-        Callable<Integer> cli = new Callable<Integer>() {
+        final Process brooklyn = startBrooklyn("--verbose", "launch", "--stopOnKeyPress", "--app", "brooklyn.cli.BaseCliIntegrationTest$TestApplication", "--location", "localhost", "--noConsole");
+
+        BrooklynCliTest test = new BrooklynCliTest(brooklyn) {
             @Override
-            public Integer call() throws Exception {
-                // Get the console output of running that command
-                Thread.sleep(5000L);
-                String consoleOutput = convertStreamToString(brooklyn.getInputStream());
-                String consoleError = convertStreamToString(brooklyn.getErrorStream());
-
-                // Check if the output looks as expected for the launch command
-                assertTrue(consoleOutput.contains("Launching Brooklyn web console management"), "Launch message not output; output=" + consoleOutput);
-                assertFalse(consoleOutput.contains("Initiating Jersey application"), "Web console started; output=" + consoleOutput);
-                assertTrue(consoleOutput.contains("Started application TestApplication"), "Application not started; output=" + consoleOutput);
-                assertTrue(consoleOutput.contains("Server started. Press return to stop."), "Server started message not output; output=" + consoleOutput);
-                assertTrue(consoleError.isEmpty(), "Output present; error=" + consoleError);
-
-                // Return exit status on completion
-                return brooklyn.waitFor();
+            public void checkConsole() {
+                assertConsoleOutput("Launching Brooklyn web console management"); // Launch message not output
+                assertNoConsoleOutput("Initiating Jersey application"); // Web console started
+                assertConsoleOutput("Started application TestApplication"); // Application not started
+                assertConsoleOutput("Server started. Press return to stop."); // Server started message not output
+                assertConsoleErrorEmpty();
             }
         };
 
-        try {
-            Future<Integer> future = executor.submit(cli);
-
-            // Send CR to stop
-            OutputStream out = brooklyn.getOutputStream();
-            out.write('\n');
-            out.flush();
-
-            int exitStatus = future.get(30, TimeUnit.SECONDS);
-
-            // Check error code from process is 0
-            assertEquals(exitStatus, 0, "Command terminated with error status");
-        } catch (TimeoutException te) {
-            fail("Timed out waiting for process to complete");
-        } catch (ExecutionException ee) {
-            if (ee.getCause() instanceof AssertionError) {
-                throw ee.getCause();
-            } else throw ee;
-        } finally {
-            brooklyn.destroy();
-        }
+        testBrooklyn(brooklyn, test, 0, true);
     }
 
     /**
-     * Checks if a correct error and help message is given if using incorrect params.
+     * Checks if a correct error and help message is given if using incorrect param.
      */
     @Test(groups = "Integration")
     public void testLaunchCliAppParamError() throws Throwable {
-        // Invoke the brooklyn script with incorrect arguments
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.environment().remove("BROOKLYN_HOME");
-        pb.environment().put("BROOKLYN_CLASSPATH", BROOKLYN_CLASSPATH);
-        pb.command(BROOKLYN_BIN_PATH, "launch", "nothing", "--app");
-        final Process brooklyn = pb.start();
+        final Process brooklyn = startBrooklyn("launch", "nothing", "--app");
 
-        Callable<Integer> cli = new Callable<Integer>() {
+        BrooklynCliTest test = new BrooklynCliTest(brooklyn) {
             @Override
-            public Integer call() throws Exception {
-                // Get the console output of running that command
-                String consoleOutput = convertStreamToString(brooklyn.getInputStream());
-                String consoleError = convertStreamToString(brooklyn.getErrorStream());
-
-                // Check if the output looks as expected
-                assertTrue(consoleError.contains("Parse error: Required values for option 'application class or file' not provided"), "Parse error not reported; error=" + consoleError);
-                assertTrue(consoleError.contains("NAME")
-                        && consoleError.contains("SYNOPSIS")
-                        && consoleError.contains("OPTIONS")
-                        && consoleError.contains("COMMANDS"), "Usage info not printed; error=" + consoleError);
-                assertTrue(consoleOutput.isEmpty(), "Output present; output=" + consoleOutput);
-
-                // Return exit status on completion
-                return brooklyn.waitFor();
+            public void checkConsole() {
+                assertConsoleError("Parse error: Required values for option 'application class or file' not provided");
+                assertConsoleError("NAME", "SYNOPSIS", "OPTIONS", "COMMANDS");
+                assertConsoleOutputEmpty();
             }
         };
 
-        try {
-            Future<Integer> future = executor.submit(cli);
-            int exitStatus = future.get(10, TimeUnit.SECONDS);
-
-            // Check error code from process
-            assertEquals(exitStatus, 1, "Command returned wrong status");
-        } catch (TimeoutException te) {
-            fail("Timed out waiting for process to complete");
-        } catch (ExecutionException ee) {
-            if (ee.getCause() instanceof AssertionError) {
-                throw ee.getCause();
-            } else throw ee;
-        } finally {
-            brooklyn.destroy();
-        }
+        testBrooklyn(brooklyn, test, 1);
     }
 
     /**
@@ -214,48 +75,18 @@ public class CliIntegrationTest {
      */
     @Test(groups = "Integration")
     public void testLaunchCliAppCommandError() throws Throwable {
-        // Invoke the brooklyn script with incorrect arguments
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.environment().remove("BROOKLYN_HOME");
-        pb.environment().put("BROOKLYN_CLASSPATH", BROOKLYN_CLASSPATH);
-        pb.command(BROOKLYN_BIN_PATH, "biscuit");
-        final Process brooklyn = pb.start();
+        final Process brooklyn = startBrooklyn("biscuit");
 
-        Callable<Integer> cli = new Callable<Integer>() {
+        BrooklynCliTest test = new BrooklynCliTest(brooklyn) {
             @Override
-            public Integer call() throws Exception {
-                // Get the console output of running that command
-                String consoleOutput = convertStreamToString(brooklyn.getInputStream());
-                String consoleError = convertStreamToString(brooklyn.getErrorStream());
-
-                // Check if the output looks as expected
-                assertTrue(consoleError.contains("Parse error: No command specified"), "Parse error not reported; error=" + consoleError);
-                assertTrue(consoleError.contains("NAME")
-                        && consoleError.contains("SYNOPSIS")
-                        && consoleError.contains("OPTIONS")
-                        && consoleError.contains("COMMANDS"), "Usage info not printed; error=" + consoleError);
-                assertTrue(consoleOutput.isEmpty(), "Output present; output=" + consoleOutput);
-
-                // Return exit status on completion
-                return brooklyn.waitFor();
+            public void checkConsole() {
+                assertConsoleError("Parse error: No command specified");
+                assertConsoleError("NAME", "SYNOPSIS", "OPTIONS", "COMMANDS");
+                assertConsoleOutputEmpty();
             }
         };
 
-        try {
-            Future<Integer> future = executor.submit(cli);
-            int exitStatus = future.get(10, TimeUnit.SECONDS);
-
-            // Check error code from process
-            assertEquals(exitStatus, 1, "Command returned wrong status");
-        } catch (TimeoutException te) {
-            fail("Timed out waiting for process to complete");
-        } catch (ExecutionException ee) {
-            if (ee.getCause() instanceof AssertionError) {
-                throw ee.getCause();
-            } else throw ee;
-        } finally {
-            brooklyn.destroy();
-        }
+        testBrooklyn(brooklyn, test, 1);
     }
 
     /**
@@ -263,52 +94,17 @@ public class CliIntegrationTest {
      */
     @Test(groups = "Integration")
     public void testLaunchCliAppLaunchError() throws Throwable {
-        // Invoke the brooklyn script with incorrect arguments
-        ProcessBuilder pb = new ProcessBuilder();
-        pb.environment().remove("BROOKLYN_HOME");
-        pb.environment().put("BROOKLYN_CLASSPATH", BROOKLYN_CLASSPATH);
-        pb.command(BROOKLYN_BIN_PATH, "launch", "--app", "org.eample.DoesNotExist", "--location", "nowhere");
-        final Process brooklyn = pb.start();
+        final Process brooklyn = startBrooklyn("launch", "--app", "org.eample.DoesNotExist", "--location", "nowhere");
 
-        Callable<Integer> cli = new Callable<Integer>() {
+        BrooklynCliTest test = new BrooklynCliTest(brooklyn) {
             @Override
-            public Integer call() throws Exception {
-                // Get the console output of running that command
-                String consoleOutput = convertStreamToString(brooklyn.getInputStream());
-                String consoleError = convertStreamToString(brooklyn.getErrorStream());
-
-                // Check if the output looks as expected
-                assertTrue(consoleOutput.contains("ERROR Execution error: brooklyn.util.ResourceUtils.getResourceFromUrl"), "Execution error not logged; output=" + consoleOutput);
-                assertTrue(consoleError.contains("Execution error: Error getting resource for LaunchCommand"), "Execution error not reported; error=" + consoleError);
-
-                // Return exit status on completion
-                return brooklyn.waitFor();
+            public void checkConsole() {
+                assertConsoleOutput("ERROR Execution error: brooklyn.util.ResourceUtils.getResourceFromUrl");
+                assertConsoleError("Execution error: Error getting resource for LaunchCommand");
             }
         };
 
-        try {
-            Future<Integer> future = executor.submit(cli);
-            int exitStatus = future.get(10, TimeUnit.SECONDS);
-
-            // Check error code from process
-            assertEquals(exitStatus, 2, "Command returned wrong status");
-        } catch (TimeoutException te) {
-            fail("Timed out waiting for process to complete");
-        } catch (ExecutionException ee) {
-            if (ee.getCause() instanceof AssertionError) {
-                throw ee.getCause();
-            } else throw ee;
-        } finally {
-            brooklyn.destroy();
-        }
-    }
-
-    /** An empty {@link Application} for testing. */
-    @SuppressWarnings("serial")
-    public static class TestApplication extends AbstractApplication {
-        public TestApplication() {
-            // Empty, for testing
-        }
+        testBrooklyn(brooklyn, test, 2);
     }
 
 }
