36366784cabdf614b5dd7798f4d1d8cf2f61e6c4
Aled Sage <aled.sage@gmail.com>
2013-03-26 16:56:12 +0000

    Convert DynamicWebAppClusterImpl from groovy to java

---
+++ software/webapp/src/main/java/brooklyn/entity/webapp/DynamicWebAppClusterImpl.java
@@ -0,0 +1,107 @@
+package brooklyn.entity.webapp;
+
+import java.util.List;
+import java.util.Map;
+
+import brooklyn.enricher.CustomAggregatingEnricher;
+import brooklyn.entity.Entity;
+import brooklyn.entity.group.DynamicCluster;
+import brooklyn.entity.group.DynamicClusterImpl;
+import brooklyn.event.AttributeSensor;
+import brooklyn.event.SensorEvent;
+import brooklyn.event.SensorEventListener;
+import brooklyn.util.MutableMap;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * DynamicWebAppClusters provide cluster-wide aggregates of entity attributes.  Currently totals and averages:
+ * <ul>
+ *   <li>Entity request counts</li>
+ *   <li>Entity error counts</li>
+ *   <li>Requests per second</li>
+ *   <li>Entity processing time</li>
+ * </ul>
+ */
+public class DynamicWebAppClusterImpl extends DynamicClusterImpl implements DynamicWebAppCluster {
+
+    /**
+     * Instantiate a new DynamicWebAppCluster.  Parameters as per {@link DynamicCluster#DynamicCluster()}
+     */
+    public DynamicWebAppClusterImpl() {
+        super();
+    }
+    public DynamicWebAppClusterImpl(Map properties) {
+        super(properties, null);
+    }
+    public DynamicWebAppClusterImpl(Entity parent) {
+        super(MutableMap.of(), parent);
+    }
+    public DynamicWebAppClusterImpl(Map properties, Entity parent) {
+        super(properties, parent);
+    }
+    
+    @Override
+    public void onManagementBecomingMaster() {
+        // Enricher attribute setup.  A way of automatically discovering these (but avoiding
+        // averaging things like HTTP port and response codes) would be neat.
+        List<? extends List<? extends AttributeSensor<? extends Number>>> summingEnricherSetup = ImmutableList.of(
+                ImmutableList.of(REQUEST_COUNT, REQUEST_COUNT),
+                ImmutableList.of(ERROR_COUNT, ERROR_COUNT),
+                ImmutableList.of(REQUESTS_PER_SECOND_LAST, REQUESTS_PER_SECOND_LAST),
+                ImmutableList.of(REQUESTS_PER_SECOND_IN_WINDOW, REQUESTS_PER_SECOND_IN_WINDOW),
+                ImmutableList.of(TOTAL_PROCESSING_TIME, TOTAL_PROCESSING_TIME)
+        );
+        
+        List<? extends List<? extends AttributeSensor<? extends Number>>> averagingEnricherSetup = ImmutableList.of(
+                ImmutableList.of(REQUEST_COUNT, REQUEST_COUNT_PER_NODE),
+                ImmutableList.of(ERROR_COUNT, ERROR_COUNT_PER_NODE),
+                ImmutableList.of(REQUESTS_PER_SECOND_LAST, REQUESTS_PER_SECOND_LAST_PER_NODE),
+                ImmutableList.of(REQUESTS_PER_SECOND_IN_WINDOW, REQUESTS_PER_SECOND_IN_WINDOW_PER_NODE),
+                ImmutableList.of(TOTAL_PROCESSING_TIME, TOTAL_PROCESSING_TIME_PER_NODE)
+        );
+        
+        for (List<? extends AttributeSensor<? extends Number>> es : summingEnricherSetup) {
+            AttributeSensor<? extends Number> t = es.get(0);
+            AttributeSensor<? extends Number> total = es.get(1);
+            CustomAggregatingEnricher<?,?> totaller = CustomAggregatingEnricher.newSummingEnricher(MutableMap.of("allMembers", true), t, total);
+            addEnricher(totaller);
+        }
+        
+        for (List<? extends AttributeSensor<? extends Number>> es : averagingEnricherSetup) {
+            AttributeSensor<Number> t = (AttributeSensor<Number>) es.get(0);
+            AttributeSensor<Double> average = (AttributeSensor<Double>) es.get(1);
+            CustomAggregatingEnricher<?,?> averager = CustomAggregatingEnricher.newAveragingEnricher(MutableMap.of("allMembers", true), t, average);
+            addEnricher(averager);
+        }
+
+        subscribeToMembers(this, SERVICE_UP, new SensorEventListener<Boolean>() {
+            @Override public void onEvent(SensorEvent<Boolean> event) {
+                setAttribute(SERVICE_UP, calculateServiceUp());
+            }
+        });
+    }
+
+        
+    public synchronized boolean addMember(Entity member) {
+        boolean result = super.addMember(member);
+        setAttribute(SERVICE_UP, calculateServiceUp());
+        return result;
+    }
+    
+    @Override
+    public synchronized boolean removeMember(Entity member) {
+        boolean result = super.removeMember(member);
+        setAttribute(SERVICE_UP, calculateServiceUp());
+        return result;
+    }
+
+    @Override    
+    protected boolean calculateServiceUp() {
+        boolean up = false;
+        for (Entity member : getMembers()) {
+            if (Boolean.TRUE.equals(member.getAttribute(SERVICE_UP))) up = true;
+        }
+        return up;
+    }
+}
