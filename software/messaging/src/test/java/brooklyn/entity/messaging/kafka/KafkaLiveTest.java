0236deceede33a6659b9959b40b37a144749bfb8
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-04-03 12:37:09 +0100

    Added EC2 live test for KafkaCluster

--- software/messaging/src/test/java/brooklyn/entity/messaging/kafka/KafkaLiveTest.java
+++ software/messaging/src/test/java/brooklyn/entity/messaging/kafka/KafkaLiveTest.java
@@ -15,17 +15,50 @@
  */
 package brooklyn.entity.messaging.kafka;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.concurrent.Callable;
+
 import brooklyn.entity.AbstractEc2LiveTest;
+import brooklyn.entity.basic.Entities;
+import brooklyn.entity.proxying.EntitySpecs;
+import brooklyn.entity.trait.Startable;
 import brooklyn.location.Location;
+import brooklyn.test.Asserts;
+import brooklyn.util.MutableMap;
+
+import com.google.common.collect.ImmutableList;
 
 public class KafkaLiveTest extends AbstractEc2LiveTest {
 
     /**
-     * Test Kafka cluster operation.
+     * Test that can install, start and use a Kafka cluster with two brokers.
      */
     @Override
     protected void doTest(Location loc) throws Exception {
-        throw new UnsupportedOperationException();
+        final KafkaCluster cluster = app.createAndManageChild(EntitySpecs.spec(KafkaCluster.class)
+                .configure("startTimeout", 300) // 5 minutes
+                .configure("initialSize", 2));
+        app.start(ImmutableList.of(loc));
+
+        Asserts.succeedsEventually(MutableMap.of("timeout", 300000l), new Callable<Void>() {
+            @Override
+            public Void call() {
+                assertTrue(cluster.getAttribute(Startable.SERVICE_UP));
+                assertTrue(cluster.getZookeeper().getAttribute(Startable.SERVICE_UP));
+                assertEquals(cluster.getCurrentSize().intValue(), 2);
+                return null;
+            }
+        });
+
+        Entities.dumpInfo(cluster);
+
+        KafkaSupport support = new KafkaSupport(cluster);
+
+        support.sendMessage("brooklyn", "TEST_MESSAGE");
+        String message = support.getMessage("brooklyn");
+        assertEquals(message, "TEST_MESSAGE");
     }
 
 }
