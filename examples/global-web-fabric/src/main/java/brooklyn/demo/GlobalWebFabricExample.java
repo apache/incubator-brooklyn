8209d574c63a55fd91616175c2c384ac8b87fc45
Peter Veentjer <alarmnummer@gmail.com>
2013-04-19 14:49:20 +0300

    Removal of all Cloud Foundry references in the code and in the poms

--- examples/global-web-fabric/src/main/java/brooklyn/demo/GlobalWebFabricExample.java
+++ examples/global-web-fabric/src/main/java/brooklyn/demo/GlobalWebFabricExample.java
@@ -10,15 +10,12 @@ import org.slf4j.LoggerFactory;
 
 import brooklyn.config.StringConfigMap;
 import brooklyn.entity.basic.AbstractApplication;
-import brooklyn.entity.basic.Attributes;
 import brooklyn.entity.basic.Entities;
 import brooklyn.entity.dns.geoscaling.GeoscalingDnsService;
 import brooklyn.entity.group.DynamicFabric;
 import brooklyn.entity.proxy.AbstractController;
 import brooklyn.entity.proxying.EntitySpecs;
 import brooklyn.entity.webapp.ElasticJavaWebAppService;
-import brooklyn.event.basic.DependentConfiguration;
-import brooklyn.extras.cloudfoundry.CloudFoundryJavaWebAppCluster;
 import brooklyn.launcher.BrooklynLauncher;
 import brooklyn.location.basic.PortRanges;
 import brooklyn.util.CommandLineUtil;
@@ -36,9 +33,7 @@ public class GlobalWebFabricExample extends AbstractApplication {
     static final List<String> DEFAULT_LOCATIONS = ImmutableList.of(
             "aws-ec2:eu-west-1",
             "aws-ec2:ap-southeast-1",
-            "aws-ec2:us-west-1" 
-//            "cloudfoundry:https://api.aws.af.cm/",
-        );
+            "aws-ec2:us-west-1" );
 
     @Override
     public void init() {
@@ -59,11 +54,7 @@ public class GlobalWebFabricExample extends AbstractApplication {
                 .configure(ElasticJavaWebAppService.ROOT_WAR, WAR_PATH)
                 
                 //load-balancer instances must run on 80 to work with GeoDNS (default is 8000)
-                .configure(AbstractController.PROXY_HTTP_PORT, PortRanges.fromInteger(80))
-                
-                //CloudFoundry requires to be told what URL it should listen to, which is chosen by the GeoDNS service
-                .configure(CloudFoundryJavaWebAppCluster.HOSTNAME_TO_USE_FOR_URL,
-                        DependentConfiguration.attributeWhenReady(geoDns, Attributes.HOSTNAME)));
+                .configure(AbstractController.PROXY_HTTP_PORT, PortRanges.fromInteger(80)));
 
         //tell GeoDNS what to monitor
         geoDns.setTargetEntityProvider(webFabric);
