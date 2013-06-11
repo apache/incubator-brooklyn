bc26620ebb39b9ca16d6c3524d5883d58a829cfa
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2013-06-11 15:45:11 +0100

    Test brooklyn-utils.roundIfNumberToDecimalPlaces and remove unused Util function

--- usage/jsgui/src/main/webapp/assets/js/libs/brooklyn-utils.js
+++ usage/jsgui/src/main/webapp/assets/js/libs/brooklyn-utils.js
@@ -15,21 +15,6 @@ define([
         }
     };
 
-    // TODO: Unused. Necessary?
-    function count_occurrences(string, subString, allowOverlapping) {
-        string+=""; subString+="";
-        if(subString.length<=0) return string.length+1;
-
-        var n=0, pos=0;
-        var step=(allowOverlapping)?(1):(subString.length);
-
-        while(true){
-            pos=string.indexOf(subString,pos);
-            if(pos>=0){ n++; pos+=step; } else break;
-        }
-        return(n);
-    }
-
     // TODO: Also rename
     /** preps data for output */
     Util.prep = function (s) {
@@ -42,7 +27,7 @@ define([
     }
 
     Util.roundIfNumberToNumDecimalPlaces = function (v, mantissa) {
-        if (typeof v !== 'number')
+        if (!_.isNumber(v) || mantissa < 0)
             return v;
 
         if (isWholeNumber(v))
---
+++ usage/jsgui/src/test/javascript/specs/brooklyn-utils-spec.js
@@ -0,0 +1,53 @@
+define([
+    'brooklyn-utils'
+], function (Util) {
+
+    describe('Rounding numbers', function () {
+
+        var round = Util.roundIfNumberToNumDecimalPlaces;
+
+        it("should round in the correct direction", function() {
+            // unchanged
+            expect(round(1, 2)).toBe(1);
+            expect(round(1.1, 1)).toBe(1.1);
+            expect(round(1.9, 1)).toBe(1.9);
+            expect(round(1.123123, 6)).toBe(1.123123);
+            expect(round(-22.222, 3)).toBe(-22.222);
+
+            // up
+            expect(round(1.9, 0)).toBe(2);
+            expect(round(1.5, 0)).toBe(2);
+            expect(round(1.49, 1)).toBe(1.5);
+
+            // down
+            expect(round(1.01, 1)).toBe(1.0);
+            expect(round(1.49, 0)).toBe(1);
+            expect(round(1.249, 1)).toBe(1.2);
+            expect(round(1.0000000000000000000001, 0)).toBe(1);
+        });
+
+        it("should round negative numbers correctly", function() {
+            // up
+            expect(round(-10, 0)).toBe(-10);
+            expect(round(-10.49999, 0)).toBe(-10);
+
+            // down
+            expect(round(-10.5, 0)).toBe(-11);
+            expect(round(-10.50001, 0)).toBe(-11);
+            expect(round(-10.49999, 1)).toBe(-10.5);
+        });
+
+        it("should ignore non-numeric values", function() {
+            expect(round("xyz", 1)).toBe("xyz");
+            expect(round("2.4", 0)).toBe("2.4");
+            expect(round({a: 2}, 0)).toEqual({a: 2});
+        });
+
+        it("should ignore negative mantissas", function() {
+            expect(round(10.5, -1)).toBe(10.5);
+            expect(round(100, -1)).toBe(100);
+            expect(round(0, -1)).toBe(0);
+        });
+
+    });
+});
