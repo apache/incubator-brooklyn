49652bf6a1e1cc02a0b6dde0957b08ddfb82e038
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-06-14 12:16:48 +0100

    neat Boolean, Double, String, and Integer sensors

--- core/src/main/java/brooklyn/event/basic/BasicAttributeSensor.java
+++ core/src/main/java/brooklyn/event/basic/BasicAttributeSensor.java
@@ -5,6 +5,7 @@ import brooklyn.event.AttributeSensor;
 /**
  * A {@link Sensor} describing an attribute change.
  */
+@SuppressWarnings("serial")
 public class BasicAttributeSensor<T> extends BasicSensor<T> implements AttributeSensor<T> {
     private static final long serialVersionUID = -2493209215974820300L;
 
@@ -15,4 +16,45 @@ public class BasicAttributeSensor<T> extends BasicSensor<T> implements Attribute
     public BasicAttributeSensor(Class<T> type, String name, String description) {
         super(type, name, description);
     }
+    
+    public static class StringAttributeSensor extends BasicAttributeSensor<String> {
+        public StringAttributeSensor(String name) {
+            super(String.class, name);
+        }
+        
+        public StringAttributeSensor(String name, String description) {
+            super(String.class, name, description);
+        }
+    }
+    
+    public static class DoubleAttributeSensor extends BasicAttributeSensor<Double> {
+        public DoubleAttributeSensor(String name) {
+            super(Double.class, name);
+        }
+        
+        public DoubleAttributeSensor(String name, String description) {
+            super(Double.class, name, description);
+        }
+    }
+
+    public static class IntegerAttributeSensor extends BasicAttributeSensor<Integer> {
+        public IntegerAttributeSensor(String name) {
+            super(Integer.class, name);
+        }
+        
+        public IntegerAttributeSensor(String name, String description) {
+            super(Integer.class, name, description);
+        }
+    }
+
+    public static class BooleanAttributeSensor extends BasicAttributeSensor<Boolean> {
+        public BooleanAttributeSensor(String name) {
+            super(Boolean.class, name);
+        }
+        
+        public BooleanAttributeSensor(String name, String description) {
+            super(Boolean.class, name, description);
+        }
+    }
+
 }
