5c5feb17be868cd9818363b90bd5660963e7549d
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2013-07-01 18:29:49 +0100

    Remove unnecessary callPeriodically in entity-policies.
    The callback in initialize() means showPolicyConfig() is called so often
    that this callback is constantly overridden and never gets run.

--- usage/jsgui/src/main/webapp/assets/js/view/entity-policies.js
+++ usage/jsgui/src/main/webapp/assets/js/view/entity-policies.js
@@ -34,6 +34,7 @@ define([
             }, 3000);
             that.refresh();
         },
+
         refresh:function() {
             var that = this;
             that.render();
@@ -41,6 +42,7 @@ define([
                 that.render();
             }});
         },
+
         render:function () {
             var that = this,
                 $tbody = this.$('#policies-table tbody').empty();
@@ -70,12 +72,15 @@ define([
             }
             return that;
         },
+
         toggleFilterEmpty:function() {
             ViewUtils.toggleFilterEmpty($('#policy-config-table'), 2);
         },
+
         refreshPolicyConfigNow:function () {
             this.refreshPolicyConfig(this);  
         },
+
         rowClick:function(evt) {
             var row = $(evt.currentTarget).closest("tr"),
                 id = row.attr("id"),
@@ -99,6 +104,7 @@ define([
                 }});
             }
         },
+
         showPolicyConfig:function (activePolicyId) {
             var that = this;
             if (activePolicyId != null && that.activePolicy != activePolicyId) {
@@ -132,10 +138,8 @@ define([
                 }
             }
             that.refreshPolicyConfig(that);
-            that.callPeriodically("entity-policy-config", function() {
-                that.refreshPolicyConfig(that);
-            }, 3000);
         },
+
         refreshPolicyConfig:function (that) {
             var $table = that.$('#policy-config-table').dataTable(),
                 $rows = that.$("tr.policy-config-row");
@@ -150,6 +154,7 @@ define([
             });
             $table.dataTable().fnStandingRedraw();
         },
+
         showPolicyConfigModal:function (evt) {
             // get the model that we need to show, create its view and show it
             var cid = $(evt.currentTarget).attr("id");
@@ -160,6 +165,7 @@ define([
             });
             this._modal.render().$el.modal('show');
         },
+
         callStart:function(event) { this.doPost(event, "start"); },
         callStop:function(event) { this.doPost(event, "stop"); },
         callDestroy:function(event) { this.doPost(event, "destroy"); },
@@ -174,6 +180,8 @@ define([
                 }
             });
         }
+
     });
+
     return EntityPoliciesView;
 });
