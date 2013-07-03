4d53c5d577a7bd77377e29f3c5d0b42e75877461
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-07-03 20:48:12 +0100

    Update resolv.conf on managed machines and handle server startup better

--- software/network/src/main/java/brooklyn/entity/network/bind/BindDnsServerImpl.java
+++ software/network/src/main/java/brooklyn/entity/network/bind/BindDnsServerImpl.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.net.InetAddress;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,7 @@ import brooklyn.entity.basic.DynamicGroup;
 import brooklyn.entity.basic.SoftwareProcessImpl;
 import brooklyn.entity.group.AbstractMembershipTrackingPolicy;
 import brooklyn.entity.proxying.EntitySpecs;
+import brooklyn.entity.trait.Startable;
 import brooklyn.location.Location;
 import brooklyn.location.basic.SshMachineLocation;
 import brooklyn.util.collections.MutableMap;
@@ -27,6 +29,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 
 /**
@@ -40,6 +43,7 @@ public class BindDnsServerImpl extends SoftwareProcessImpl implements BindDnsSer
     private DynamicGroup entities;
     private AbstractMembershipTrackingPolicy policy;
     private Multimap<Location, Entity> entityLocations = HashMultimap.create();
+    private ConcurrentMap<String, String> addressMappings = Maps.newConcurrentMap();
     protected List<InetAddress> addresses = Lists.newArrayList();
 
     public BindDnsServerImpl() {
@@ -69,10 +73,8 @@ public class BindDnsServerImpl extends SoftwareProcessImpl implements BindDnsSer
         return BindDnsServerDriver.class;
     }
 
-    public List<Location> getEntityLocations() {
-        synchronized (mutex) {
-            return ImmutableList.copyOf(entityLocations.keySet());
-        }
+    public Map<String, String> getAddressMappings() {
+        return addressMappings;
     }
 
     @Override
@@ -104,12 +106,14 @@ public class BindDnsServerImpl extends SoftwareProcessImpl implements BindDnsSer
             @Override
             protected void onEntityRemoved(Entity member) { removed(member); }
         };
+
         addPolicy(policy);
         policy.setGroup(entities);
+    }
 
-        for (Entity each : entities.getMembers()) {
-            added(each);
-        }
+    @Override
+    public void postStart() {
+        update();
     }
 
     public void added(Entity member) {
@@ -118,11 +122,16 @@ public class BindDnsServerImpl extends SoftwareProcessImpl implements BindDnsSer
             String hostname = member.getAttribute(getConfig(HOSTNAME_SENSOR));
             if (location.isPresent() && Strings.isNonBlank(hostname)) {
                 SshMachineLocation machine = (SshMachineLocation) location.get();
+                String address = machine.getAddress().getHostAddress();
                 if (!entityLocations.containsKey(machine)) {
                     entityLocations.put(machine, member);
-                    update(machine);
+                    addressMappings.putIfAbsent(address, hostname);
+                    if (getAttribute(Startable.SERVICE_UP)) {
+                        update();
+                    }
+                    configure(machine);
+                    LOG.info("{} added at location {} with name {}", new Object[] { member, machine, hostname });
                 }
-                LOG.info("{} added at location {} with name {}", new Object[] { member, machine, hostname });
             } else {
                 LOG.warn("added({}) called but entity not ready", member);
             }
@@ -132,22 +141,32 @@ public class BindDnsServerImpl extends SoftwareProcessImpl implements BindDnsSer
     public void removed(Entity member) {
         synchronized (mutex) {
             Optional<Location> location = Iterables.tryFind(member.getLocations(), Predicates.instanceOf(SshMachineLocation.class));
-            entityLocations.remove(location, member);
-            update((SshMachineLocation) location.get());
+            SshMachineLocation machine = (SshMachineLocation) location.get();
+            entityLocations.remove(machine, member);
+            if (!entityLocations.containsKey(machine)) {
+                addressMappings.remove(machine.getAddress().getHostAddress());
+            }
+            update();
         }
     }
 
-    public void update(SshMachineLocation machine) {
-        synchronized (mutex) {
-            String[] templateList = new String[] { "domain.zone", "named.conf" };
-            for (String fileName : templateList) {
-                String contents = ((BindDnsServerSshDriver) getDriver()).processTemplate("classpath://brooklyn/entity/network/bind/" + fileName);
-                machine.copyTo(new ByteArrayInputStream(contents.getBytes()), "/tmp/" + fileName);
-                machine.execScript("update bind config", ImmutableList.of(CommonCommands.sudo("cp /tmp/" + fileName + " /var/named/" + fileName)));
-            }
-
-            // Restart BIND
-            machine.execCommands("configuring BIND", ImmutableList.of(CommonCommands.sudo("service bind restart")));
+    public void update() {
+        Optional<Location> location = Iterables.tryFind(getLocations(), Predicates.instanceOf(SshMachineLocation.class));
+        SshMachineLocation machine = (SshMachineLocation) location.get();
+        String[] templateList = new String[] { "domain.zone", "named.conf" };
+        for (String fileName : templateList) {
+            String contents = ((BindDnsServerSshDriver) getDriver()).processTemplate("classpath://brooklyn/entity/network/bind/" + fileName);
+            machine.copyTo(new ByteArrayInputStream(contents.getBytes()), "/tmp/" + fileName);
+            machine.execScript("update bind config", ImmutableList.of(CommonCommands.sudo("cp /tmp/" + fileName + " /var/named/" + fileName)));
         }
+
+        // Restart BIND
+        machine.execCommands("configuring BIND", ImmutableList.of(CommonCommands.sudo("service bind restart")));
+    }
+
+    public void configure(SshMachineLocation machine) {
+        String contents = ((BindDnsServerSshDriver) getDriver()).processTemplate("classpath://brooklyn/entity/network/bind/resolv.conf");
+        machine.copyTo(new ByteArrayInputStream(contents.getBytes()), "/tmp/resolv.conf");
+        machine.execScript("update resolver config", ImmutableList.of(CommonCommands.sudo("cp /tmp/resolv.conf /etc/resolv.conf")));
     }
 }
