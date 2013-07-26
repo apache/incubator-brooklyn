e83fa9cbcf39953e61c05cf8c7082303ad1c5197
Peter Veentjer <alarmnummer@gmail.com>
2013-07-26 14:14:20 +0300

    EntityId has serialVersionUid Made EntitySpec and LocationSpec serializable since they need to go over the line.

--- core/src/main/java/brooklyn/internal/storage/impl/hazelcast/EntityId.java
+++ core/src/main/java/brooklyn/internal/storage/impl/hazelcast/EntityId.java
@@ -3,6 +3,9 @@ package brooklyn.internal.storage.impl.hazelcast;
 import java.io.Serializable;
 
 class EntityId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private final String id;
 
     EntityId(String id) {
