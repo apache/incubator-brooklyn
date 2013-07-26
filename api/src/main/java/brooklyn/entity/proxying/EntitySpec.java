1b1e8a1d27473e5ce9105b9fa3d673a9b1200cfb
Peter Veentjer <alarmnummer@gmail.com>
2013-07-26 14:14:20 +0300

    EntityId has serialVersionUid Made EntitySpec and LocationSpec serializable since they need to go over the line.

--- api/src/main/java/brooklyn/entity/proxying/EntitySpec.java
+++ api/src/main/java/brooklyn/entity/proxying/EntitySpec.java
@@ -1,5 +1,6 @@
 package brooklyn.entity.proxying;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -22,7 +23,9 @@ import brooklyn.policy.Policy;
  * 
  * @author aled
  */
-public interface EntitySpec<T extends Entity> {
+public interface EntitySpec<T extends Entity> extends Serializable {
+
+    long serialVersionUID = 1L;
 
     /**
      * @return The type of the entity
