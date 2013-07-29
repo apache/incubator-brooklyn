5deb51debd44294dca006cf15c06f4381b199d17
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-07-29 10:22:39 +0100

    correct spelling, add javadoc, for EffectorParam

--- core/src/main/java/brooklyn/entity/annotation/EffectorParam.java
+++ core/src/main/java/brooklyn/entity/annotation/EffectorParam.java
@@ -18,7 +18,7 @@ public @interface EffectorParam {
     String defaultValue() default MAGIC_STRING_MEANING_NULL;
     boolean nullable() default true;
     
-    // Cannot use null as a default (e.g. for defaultValue); therefore define a magic string to mean that
-    // so can tell when no-one has set it.
-    public static final String MAGIC_STRING_MEANING_NULL = "null default value; do not missuse! 3U=Hhfkr8wuov]WO";
+    /** Cannot use null as a default (e.g. for defaultValue); therefore define a magic string to mean that
+    /* so can tell when no-one has set it. */
+    public static final String MAGIC_STRING_MEANING_NULL = "null default value; do not mis-use! 3U=Hhfkr8wuov]WO";
 }
