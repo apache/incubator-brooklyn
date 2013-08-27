8d5db47c7dfa77611bfb32d57d395140b2caa857
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-08-27 14:04:06 +0100

    geoscaling now publishes ROOT_URL, but only when clusters are available, so listening to it instead of other sensors should let callers know when it is safe to do a DNS lookup without causing a failure to be cached; nginx and postgres tidies around the need for sudo (no longer required for nginx)

--- software/webapp/src/main/java/brooklyn/entity/dns/geoscaling/GeoscalingDnsService.java
+++ software/webapp/src/main/java/brooklyn/entity/dns/geoscaling/GeoscalingDnsService.java
@@ -1,7 +1,10 @@
 package brooklyn.entity.dns.geoscaling;
 
+import brooklyn.config.ConfigKey;
 import brooklyn.entity.dns.AbstractGeoDnsService;
 import brooklyn.entity.proxying.ImplementedBy;
+import brooklyn.entity.webapp.WebAppServiceConstants;
+import brooklyn.event.AttributeSensor;
 import brooklyn.event.basic.BasicAttributeSensor;
 import brooklyn.event.basic.BasicConfigKey;
 import brooklyn.util.flags.SetFromFlag;
@@ -10,25 +13,27 @@ import brooklyn.util.flags.SetFromFlag;
 public interface GeoscalingDnsService extends AbstractGeoDnsService {
     
     @SetFromFlag("randomizeSubdomainName")
-    public static final BasicConfigKey<Boolean> RANDOMIZE_SUBDOMAIN_NAME = new BasicConfigKey<Boolean>(
+    public static final ConfigKey<Boolean> RANDOMIZE_SUBDOMAIN_NAME = new BasicConfigKey<Boolean>(
             Boolean.class, "randomize.subdomain.name");
     @SetFromFlag("username")
-    public static final BasicConfigKey<String> GEOSCALING_USERNAME = new BasicConfigKey<String>(
+    public static final ConfigKey<String> GEOSCALING_USERNAME = new BasicConfigKey<String>(
             String.class, "geoscaling.username");
     @SetFromFlag("password")
-    public static final BasicConfigKey<String> GEOSCALING_PASSWORD = new BasicConfigKey<String>(
+    public static final ConfigKey<String> GEOSCALING_PASSWORD = new BasicConfigKey<String>(
             String.class, "geoscaling.password");
     @SetFromFlag("primaryDomainName")
-    public static final BasicConfigKey<String> GEOSCALING_PRIMARY_DOMAIN_NAME = new BasicConfigKey<String>(
+    public static final ConfigKey<String> GEOSCALING_PRIMARY_DOMAIN_NAME = new BasicConfigKey<String>(
             String.class, "geoscaling.primary.domain.name");
     @SetFromFlag("smartSubdomainName")
-    public static final BasicConfigKey<String> GEOSCALING_SMART_SUBDOMAIN_NAME = new BasicConfigKey<String>(
+    public static final ConfigKey<String> GEOSCALING_SMART_SUBDOMAIN_NAME = new BasicConfigKey<String>(
             String.class, "geoscaling.smart.subdomain.name");
     
-    public static final BasicAttributeSensor<String> GEOSCALING_ACCOUNT = new BasicAttributeSensor<String>(
+    public static final AttributeSensor<String> GEOSCALING_ACCOUNT = new BasicAttributeSensor<String>(
             String.class, "geoscaling.account", "Active user account for the GeoScaling.com service");
-    public static final BasicAttributeSensor<String> MANAGED_DOMAIN = new BasicAttributeSensor<String>(
-            String.class, "geoscaling.managed.domain", "Fully qualified domain name that will be geo-redirected");
+    public static final AttributeSensor<String> ROOT_URL = WebAppServiceConstants.ROOT_URL;
+    public static final AttributeSensor<String> MANAGED_DOMAIN = new BasicAttributeSensor<String>(
+            String.class, "geoscaling.managed.domain", "Fully qualified domain name that will be geo-redirected; " +
+            		"this will be the same as "+ROOT_URL.getName()+" but the latter will only be set when the domain has active targets");
     
     public void applyConfig();
     
