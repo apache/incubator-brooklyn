feddb9cec3c4fc940a6b47dbf81417f29e38c14f
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-09-16 15:23:36 +0100

    new-style lozenge tree view

--- usage/jsgui/src/main/webapp/assets/js/view/application-tree.js
+++ usage/jsgui/src/main/webapp/assets/js/view/application-tree.js
@@ -17,7 +17,8 @@ define([
         template: treeViewTemplate,
 
         events:{
-            'click .name.entity':'displayEntity'
+            'click span.entity_tree_node .tree-change':'treeChange',
+            'click span.entity_tree_node':'displayEntity'
         },
 
         initialize:function () {
@@ -39,10 +40,10 @@ define([
                 that.$el.append(_.template(TreeEmptyHtml))
             } else {
                 that.$el.append(
-                        '<div class="navbar_main_wrapper treelist cssninja">'+
-                        '<div id="tree-list" class="navbar_main treelist">'+
-                        '<ol class="tree applications"/>');
-                var node = $('ol.tree.applications', that.$el);
+                        '<div class="navbar_main_wrapper treeloz">'+
+                        '<div id="tree-list" class="navbar_main treeloz">'+
+                        '<div class="lozenge-app-tree-wrapper">');
+                var node = $('div.lozenge-app-tree-wrapper', that.$el);
                 
                 this.collection.each(function (app) {
                     node.append(that.buildTree(app))
@@ -76,59 +77,96 @@ define([
                 $template = $(this.template({
                     id:application.get("id"),
                     type:"application",
+                    hasChildren: application.hasChildren(),
                     parentApp:application.get("id"),
-                    displayName:application.get("name")
+                    displayName:application.get("name"),
+                    depth: 0
                 })),
-                treeFromEntity = function (entity) {
+                treeFromEntity = function (entity, depth) {
                     var $entityTpl
 
                     if (entity.hasChildren()) {
                         $entityTpl = $(that.template({
                             id:entity.get("id"),
                             type:"entity",
+                            hasChildren: true,
                             parentApp:application.get("id"),
-                            displayName:entity.getDisplayName()
+                            displayName:entity.getDisplayName(),
+                            depth: depth
                         }))
-                        var $parentTpl = $entityTpl.find("ol.tree")
+                        var $parentTpl = $entityTpl.find("#children")
                         _.each(entity.get("children"), function (childEntity) {
-                            $parentTpl.append(treeFromEntity(new AppTree.Model(childEntity)))
+                            $parentTpl.append(treeFromEntity(new AppTree.Model(childEntity), depth+1))
                         })
                     } else {
                         $entityTpl = $(that.template({
                             id:entity.get("id"),
                             type:"leaf",
+                            hasChildren: false,
                             parentApp:application.get("id"),
-                            displayName:entity.getDisplayName()
+                            displayName:entity.getDisplayName(),
+                            depth: depth
                         }))
                     }
                     return $entityTpl
                 }
 
             // start rendering from initial children of the application
-            var $tree = $template.find("ol.tree")
+            var $tree = $template.find("#children")
             _.each(application.get("children"), function (entity) {
-                $tree.append(treeFromEntity(new AppTree.Model(entity)))
+                $tree.append(treeFromEntity(new AppTree.Model(entity), 1))
             })
             $('a', $tree).click(function(e) { e.preventDefault(); })
+            
+            // show the "light-popup" (expand / expand all / etc) menu
+            // if user hovers for 500ms. surprising there is no option for this.
+            var hoverTimer;
+            $('.light-popup', $template).parent().parent().hover(
+                    function (parent) {
+                        if (hoverTimer!=null) {
+                            clearTimeout(hoverTimer);
+                            hoverTimer = null;
+                        }
+                        hoverTimer = setTimeout(function() {
+                            var menu = $(parent.currentTarget).find('.light-popup')
+                            menu.show()
+                        }, 500);
+                    },
+                    function (parent) {
+                        if (hoverTimer!=null) {
+                            clearTimeout(hoverTimer);
+                            hoverTimer = null;
+                        }
+                        var menu = $(parent.currentTarget).find('.light-popup')
+                        menu.hide()
+                        $('.light-popup').hide()
+                    })
 
             return $template
         },
 
         displayEntity: function(event) {
             event.preventDefault();
-            var entityId = $(event.currentTarget).attr("id"),
+            var nodeSpan = $(event.currentTarget)
+            var nodeA = $(event.currentTarget).children('a').first()
+            var entityId = nodeSpan.attr("id"),
                 stateId = entityId,
-                href = event.target.getAttribute('href'),
+                href = nodeA.attr('href'),
                 tab = (this.detailsView)
                     ? this.detailsView.$el.find(".tab-pane.active").attr("id")
                     : undefined;
-            if (tab) {
-                href = href+"/"+tab
-                stateId = entityId+"/"+tab
-                this.preselectTab(tab)
+            if (href) {
+                if (tab) {
+                    href = href+"/"+tab
+                    stateId = entityId+"/"+tab
+                    this.preselectTab(tab)
+                }
+                window.history.pushState(stateId, "", href)
+                this.displayEntityId(entityId, $(event.currentTarget).data("parent-app"));
+            } else {
+                log("no a.href in clicked target")
+                log(nodeSpan)
             }
-            window.history.pushState(stateId, "", href)
-        	this.displayEntityId(entityId, $(event.currentTarget).data("parent-app"));
         },
 
         displayEntityId:function (id, appName) {
@@ -171,6 +209,53 @@ define([
             $("div#details").html(notFoundTemplate({"id": id}));
         },
 
+        treeChange: function(event) {
+            log("changing")
+            log(event)
+            var $target = $(event.currentTarget);
+            var $treeBox = $target.closest('.tree-box');
+            if ($target.hasClass('tr-expand')) {
+                this.showChildrenOf($treeBox, false)
+            } else if ($target.hasClass('tr-expand-all')) {
+                this.showChildrenOf($treeBox, true)
+            } else if ($target.hasClass('tr-collapse')) {
+                this.hideChildrenOf($treeBox, false)
+            } else if ($target.hasClass('tr-collapse-all')) {
+                this.hideChildrenOf($treeBox, true)
+            } else {
+                // default - toggle
+                if ($treeBox.children('#children').is(':visible')) {
+                    this.hideChildrenOf($treeBox, false)
+                } else {
+                    this.showChildrenOf($treeBox, false)
+                }
+            }
+            // hide the popup menu
+            $('.light-popup').hide()
+            // don't let other events interfere
+            return false
+        },
+        hideChildrenOf: function($treeBox, recurse) {
+            var that = this;
+            $treeBox.children('#children').slideUp(300)
+            $treeBox.children('.tree-node').find('.tree-node-state').removeClass('icon-chevron-down').addClass('icon-chevron-right')
+            if (recurse) {
+                $treeBox.children('#children').children().each(function (index, childBox) {
+                    that.hideChildrenOf($(childBox), recurse)
+                })
+            }
+        },
+        showChildrenOf: function($treeBox, recurse) {
+            var that = this;
+            $treeBox.children('#children').slideDown(300)
+            $treeBox.children('.tree-node').find('.tree-node-state').removeClass('icon-chevron-right').addClass('icon-chevron-down')            
+            if (recurse) {
+                $treeBox.children('#children').children().each(function (index, childBox) {
+                    that.showChildrenOf($(childBox), recurse)
+                })
+            }
+        },
+        
         /**
          * Causes the tab with the given name to be selected automatically when
          * the view is next rendered.
@@ -205,7 +290,9 @@ define([
         	else id = this.selectedEntityId
         	$("span.entity_tree_node").removeClass("active")
         	if (id) {
-        		$("span.entity_tree_node#"+id).addClass("active")
+        	    var $selectedNode = $("span.entity_tree_node#"+id);
+        		this.showChildrenOf($selectedNode.parents('#app-tree .tree-box'), false)
+        		$selectedNode.addClass("active")
         	}
         }
     })
