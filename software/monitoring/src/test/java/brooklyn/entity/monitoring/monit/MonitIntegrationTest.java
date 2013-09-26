02afc92bc6ffca3b6ef34bcab0ed143895ef022c
Martin Harris <github@nakomis.com>
2013-09-26 14:53:29 +0100

    Added integration test for using monit to monitor MySql

--- software/monitoring/src/test/java/brooklyn/entity/monitoring/monit/MonitIntegrationTest.java
+++ software/monitoring/src/test/java/brooklyn/entity/monitoring/monit/MonitIntegrationTest.java
@@ -1,16 +1,22 @@
 package brooklyn.entity.monitoring.monit;
 
+import javax.validation.constraints.AssertTrue;
+
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 import brooklyn.config.BrooklynProperties;
 import brooklyn.entity.basic.ApplicationBuilder;
 import brooklyn.entity.basic.Entities;
+import brooklyn.entity.basic.SameServerEntity;
+import brooklyn.entity.database.mysql.MySqlNode;
 import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.location.basic.LocalhostMachineProvisioningLocation;
 import brooklyn.management.ManagementContext;
@@ -41,11 +47,32 @@ public class MonitIntegrationTest {
     }
     
     @Test(groups = "Integration")
-    public void test_localhost() {
+    public void test_localhost() throws InterruptedException {
         MonitNode monitNode = app.createAndManageChild(EntitySpec.create(MonitNode.class)
             .configure(MonitNode.CONTROL_FILE_URL, "classpath:///brooklyn/entity/monitoring/monit/monit.monitrc"));
         LocalhostMachineProvisioningLocation location = new LocalhostMachineProvisioningLocation();
         app.start(ImmutableSet.of(location));
         LOG.info("Monit started");
+        boolean started = false;
+        for (int i = 1; i < 10; i++) {
+            if (monitNode.getAttribute(MonitNode.MONIT_TARGET_PROCESS_STATUS).equals("Running")) {
+                started = true;
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        Assert.assertTrue("Expected monit target process to return status 'Running' within 10 seconds", started);
+    }
+    
+    @Test(groups = "Integration")
+    public void test_monitorMySql() {
+        SameServerEntity sameServerEntity = app.createAndManageChild(EntitySpec.create(SameServerEntity.class));
+        LocalhostMachineProvisioningLocation location = new LocalhostMachineProvisioningLocation();
+        MySqlNode mySqlNode = sameServerEntity.addChild(EntitySpec.create(MySqlNode.class));
+        MonitNode monitNode = sameServerEntity.addChild(EntitySpec.create(MonitNode.class)
+            .configure(MonitNode.CONTROL_FILE_URL, "classpath:///brooklyn/entity/monitoring/monit/monitmysql.monitrc")
+            .configure(MonitNode.CONTROL_FILE_SUBSTITUTIONS, ImmutableMap.<String, Object>of("targetPidFile", "")));
+        app.start(ImmutableSet.of(location));
+        LOG.info("Monit and MySQL started");
     }
 }
