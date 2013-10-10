bca362ae4a84d3453ef3ea41350a34b0a12e8679
Aled Sage <aled.sage@gmail.com>
2013-10-10 10:48:19 +0100

    Adds EmptySoftwareProcess to software/base

---
+++ software/base/src/main/java/brooklyn/entity/basic/EmptySoftwareProcessSshDriver.java
@@ -0,0 +1,38 @@
+package brooklyn.entity.basic;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import brooklyn.location.basic.SshMachineLocation;
+
+
+public class EmptySoftwareProcessSshDriver extends AbstractSoftwareProcessSshDriver implements EmptySoftwareProcessDriver {
+
+    private final AtomicBoolean running = new AtomicBoolean();
+
+    public EmptySoftwareProcessSshDriver(EmptySoftwareProcessImpl entity, SshMachineLocation machine) {
+        super(entity, machine);
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running.get();
+    }
+
+    @Override
+    public void install() {
+    }
+
+    @Override
+    public void customize() {
+    }
+
+    @Override
+    public void launch() {
+        running.set(true);
+    }
+    
+    @Override
+    public void stop() {
+        running.set(false);
+    }
+}
