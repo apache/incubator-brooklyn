9e08f757e38bf4e1056f2bbe9895625772cbfd79
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-10-24 15:40:35 +0100

    Tidy up Cassandra fabric and cluster interfaces

--- software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraFabric.java
+++ software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraFabric.java
@@ -3,49 +3,52 @@
  */
 package brooklyn.entity.nosql.cassandra;
 
-import java.util.Map;
 import java.util.Set;
 
 import brooklyn.catalog.Catalog;
 import brooklyn.config.ConfigKey;
 import brooklyn.entity.Entity;
+import brooklyn.entity.annotation.Effector;
 import brooklyn.entity.basic.ConfigKeys;
+import brooklyn.entity.basic.MethodEffector;
 import brooklyn.entity.group.DynamicFabric;
 import brooklyn.entity.proxying.ImplementedBy;
 import brooklyn.event.AttributeSensor;
-import brooklyn.event.basic.Sensors;
 
 import com.google.common.collect.Multimap;
 
 /**
  * A fabric of {@link CassandraNode}s, which forms a cluster spanning multiple locations.
- * 
- * Each "CassandraCluster" child instance is actually just a part of the whole cluster. It consists of the
+ * <p>
+ * Each {@link CassandraCluster} child instance is actually just a part of the whole cluster. It consists of the
  * nodes in that single location (which normally corresponds to a "datacenter" in Cassandra terminology).
  */
-@Catalog(name="Apache Cassandra Database Fabric", description="Cassandra is a highly scalable, eventually consistent, distributed, structured key-value store which provides a ColumnFamily-based data model richer than typical key/value systems", iconUrl="classpath:///cassandra-logo.jpeg")
+@Catalog(name="Apache Cassandra Database Fabric", description="Cassandra is a highly scalable, eventually " +
+        "consistent, distributed, structured key-value store which provides a ColumnFamily-based data model " +
+        "richer than typical key/value systems", iconUrl="classpath:///cassandra-logo.jpeg")
 @ImplementedBy(CassandraFabricImpl.class)
 public interface CassandraFabric extends DynamicFabric {
-    
+
     ConfigKey<Integer> INITIAL_QUORUM_SIZE = ConfigKeys.newIntegerConfigKey(
             "fabric.initial.quorumSize",
-            "Initial fabric quorum size - number of initial nodes that must have been successfully started to report success (if < 0, then use value of INITIAL_SIZE)", 
+            "Initial fabric quorum size - number of initial nodes that must have been successfully started " +
+            "to report success (if less than 0, then use value of INITIAL_SIZE)",
             -1);
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    AttributeSensor<Multimap<String,Entity>> DATACENTER_USAGE = (AttributeSensor)Sensors.newSensor(Map.class, "cassandra.cluster.datacenters", "Current set of datacenters in use, with nodes in each");
+    AttributeSensor<Multimap<String,Entity>> DATACENTER_USAGE = CassandraCluster.DATACENTER_USAGE;
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    AttributeSensor<Set<String>> DATACENTERS = (AttributeSensor)Sensors.newSensor(Set.class, "cassandra.cluster.datacenters", "Current set of datacenters in use");
+    AttributeSensor<Set<String>> DATACENTERS = CassandraCluster.DATACENTERS;
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    AttributeSensor<Set<Entity>> CURRENT_SEEDS = (AttributeSensor)Sensors.newSensor(Set.class, "cassandra.cluster.seeds.current", "Current set of seeds to use to bootstrap the cluster");
-    
-    AttributeSensor<Boolean> HAS_PUBLISHED_SEEDS = Sensors.newBooleanSensor("cassandra.cluster.seeds.hasPublished", "Whether we have published any seeds");
-    
-    AttributeSensor<String> HOSTNAME = Sensors.newStringSensor("cassandra.cluster.hostname", "Hostname to connect to cluster with");
+    AttributeSensor<Set<Entity>> CURRENT_SEEDS = CassandraCluster.CURRENT_SEEDS;
 
-    AttributeSensor<Integer> THRIFT_PORT = Sensors.newIntegerSensor("cassandra.cluster.thrift.port", "Cassandra Thrift RPC port to connect to cluster with");
+    AttributeSensor<Boolean> HAS_PUBLISHED_SEEDS = CassandraCluster.HAS_PUBLISHED_SEEDS;
 
+    AttributeSensor<String> HOSTNAME = CassandraCluster.HOSTNAME;
+
+    AttributeSensor<Integer> THRIFT_PORT = CassandraCluster.THRIFT_PORT;
+
+    MethodEffector<Void> UPDATE = new MethodEffector<Void>(CassandraFabric.class, "update");
+
+    @Effector(description="Updates the cluster members")
     void update();
 }
