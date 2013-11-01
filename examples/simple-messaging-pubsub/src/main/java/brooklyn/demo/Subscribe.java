0aa1996f90643ccbee688dfefade930aa37088d0
Martin Harris <github@nakomis.com>
2013-11-01 16:49:08 +0000

    Added message to the example message subscriber to clarify why it run, then quit

--- examples/simple-messaging-pubsub/src/main/java/brooklyn/demo/Subscribe.java
+++ examples/simple-messaging-pubsub/src/main/java/brooklyn/demo/Subscribe.java
@@ -15,7 +15,9 @@ import com.google.common.base.Throwables;
 /** Receives messages from a queue on a Qpid broker at a given URL. */
 public class Subscribe {
     public static final String QUEUE = "'amq.direct'/'testQueue'; { node: { type: queue } }";
-
+    private static final long MESSAGE_TIMEOUT_MILLIS = 15000L;
+    private static final int MESSAGE_COUNT = 100;
+    
     public static void main(String...argv) throws Exception {
         Preconditions.checkElementIndex(0, argv.length, "Must specify broker URL");
         String url = argv[0];
@@ -30,15 +32,16 @@ public class Subscribe {
         connection.start();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
+        System.out.printf("Waiting up to %s milliseconds to receive %s messages\n", MESSAGE_TIMEOUT_MILLIS, MESSAGE_COUNT);
         try {
             // Create a producer for the queue
             Queue destination = session.createQueue(QUEUE);
             MessageConsumer messageConsumer = session.createConsumer(destination);
 
             // Try and receive 100 messages
-            int n = 100;
+            int n = MESSAGE_COUNT;
             do {
-                TextMessage msg = (TextMessage) messageConsumer.receive(15000L);
+                TextMessage msg = (TextMessage) messageConsumer.receive(MESSAGE_TIMEOUT_MILLIS);
                 if (msg == null) break;
                 System.out.printf("Got message: '%s'\n", msg.getText());
             } while (n --> 0);
