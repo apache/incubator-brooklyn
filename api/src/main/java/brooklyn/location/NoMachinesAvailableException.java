3e15e2ad22fad8b2f1cc51c0b6e27bbadbe40dcc
Aled Sage <aled.sage@gmail.com>
2013-11-19 14:42:33 +0000

    Tidy deprecated NoMachineAvailableException constructors

--- api/src/main/java/brooklyn/location/NoMachinesAvailableException.java
+++ api/src/main/java/brooklyn/location/NoMachinesAvailableException.java
@@ -7,64 +7,11 @@ package brooklyn.location;
 public class NoMachinesAvailableException extends Exception {
     private static final long serialVersionUID = 1079817235289265761L;
     
-    @Deprecated
-    private Location location;
-
-    /** @deprecated since 0.5; always include a helpful message! */
-    public NoMachinesAvailableException() {
-    }
-
     public NoMachinesAvailableException(String s) {
         super(s);
     }
 
     public NoMachinesAvailableException(String s, Throwable throwable) {
         super(s, throwable);
-    }
-
-    /** @deprecated since 0.5; always include a helpful message! */
-    public NoMachinesAvailableException(Throwable throwable) {
-        super(throwable);
-    }
-
-    /**
-     * @deprecated since 0.5; don't include Location - just use {@link #NoMachinesAvailableException(String)}
-     */
-    public NoMachinesAvailableException(Location location) {
-        super("No machines available in "+location.toString());
-        this.location = location;
-    }
-
-    /**
-     * @deprecated since 0.5; don't include Location - just use {@link #NoMachinesAvailableException(String)}
-     */
-    public NoMachinesAvailableException(Location location, String s) {
-        super(s);
-        this.location = location;
-    }
-    
-    /**
-     * @deprecated since 0.5; don't include Location - just use {@link #NoMachinesAvailableException(String, Throwable)}
-     */
-    public NoMachinesAvailableException(Location location, String s, Throwable throwable) {
-        super(s, throwable);
-        this.location = location;
-    }
-
-    /**
-     * @deprecated since 0.5; don't include Location - just use {@link #NoMachinesAvailableException(String, Throwable)}
-     */
-    public NoMachinesAvailableException(Location location, Throwable throwable) {
-        super("No machines available in "+location.toString(), throwable);
-        this.location = location;
-    }
-
-    /**
-     * @deprecated since 0.5; the catcher can know which location had no machines based on which
-     *             location they asked for; will remove this so that the exception is definitely 
-     *             serializable.
-     */
-    public Location getLocation() {
-        return location;
     }
 }
