938f3eb896140b2287ef759ca6d701c0848d4456
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-12-10 16:36:36 +0000

    Added PostgreSqlNode icon for catalog entry

--- software/database/src/main/java/brooklyn/entity/database/postgresql/PostgreSqlNode.java
+++ software/database/src/main/java/brooklyn/entity/database/postgresql/PostgreSqlNode.java
@@ -39,7 +39,7 @@ import brooklyn.util.flags.SetFromFlag;
  * <li>You will also need to enable passwordless sudo.
  * </ul>
  */
-@Catalog(name="PostgreSQL Node", description="PostgreSQL is an object-relational database management system (ORDBMS)", iconUrl="classpath:///postgresql-logo.jpeg")
+@Catalog(name="PostgreSQL Node", description="PostgreSQL is an object-relational database management system (ORDBMS)", iconUrl="classpath:///postgresql-logo-200px.png")
 @ImplementedBy(PostgreSqlNodeImpl.class)
 public interface PostgreSqlNode extends SoftwareProcess, HasShortName, DatastoreCommon {
 
