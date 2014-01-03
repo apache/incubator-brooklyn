033fb17a9c645eeea8e2cf79b98db9c60e3aeff1
Richard Downer <richard.downer@cloudsoftcorp.com>
2014-01-03 12:42:40 +0000

    Detect JRE location using registry; other fixes
    A default install of the Oracle JRE will not set PATH or JAVA_HOME. This change scans the registry for JRE installs to determine the correct one to use, meaning there is no longer a requirement for the Brooklyn user to manually set either JAVA_HOME or PATH to be able to run brooklyn.

--- usage/dist/src/main/dist/bin/brooklyn.ps1
+++ usage/dist/src/main/dist/bin/brooklyn.ps1
@@ -16,12 +16,67 @@ if ( $env:BROOKLYN_HOME -eq $null ) {
     $BROOKLYN_HOME = $env:BROOKLYN_HOME
 }
 
-# set up the classpath
+# Discover the location of Java.
+# Use JAVA_HOME environment variable, if available;
+# else, search registry for Java installations;
+# else, check the path;
+# else fail.
+$bin = [System.IO.Path]::Combine("bin", "java.exe")
+if ( $env:JAVA_HOME -ne $null ) {
+    $javahome = $env:JAVA_HOME
+    $javabin = [System.IO.Path]::Combine($javahome, $bin)
+}
+if ( $javabin -eq $null ) {
+    $reglocations = ( 'HKLM:\SOFTWARE\JavaSoft\Java Runtime Environment',
+                      'HKLM:\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment' )
+    $jres = @{}
+    foreach ($loc in $reglocations) {
+        $item = Get-Item $loc -ErrorAction SilentlyContinue
+        if ($item -eq $null) { continue }
+        foreach ($key in Get-ChildItem $loc) {
+            $version = $key.PSChildName
+            $jrehome = $key.GetValue("JavaHome")
+            $jres.Add($version, $jrehome)
+        }
+    }
+    # TODO - this does a simple sort on the registry key name (the JRE version). This is not ideal - better would be
+    # to understand semantic versioning, filter out incompatible JREs (1.5 and earlier), prefer known good JREs (1.6
+    # or 1.7) and pick the highest patchlevel.
+    $last = ( $jres.Keys | Sort-Object | Select-Object -Last 1 )
+    if ( $last -ne $null ) {
+        $javahome = $jres.Get_Item($last)
+        $javabin = [System.IO.Path]::Combine($javahome, $bin)
+    }
+}
+if ( $javabin -eq $null ) {
+    $where = Get-Command "java.exe" -ErrorAction SilentlyContinue
+    if ( $where -ne $null ) {
+        $javabin = $where.Definition
+        $bindir = [System.IO.Path]::GetDirectoryName($javabin)
+        $javahome = [System.IO.Path]::GetDirectoryName($bindir)
+    }
+}
+
+if ( $javabin -eq $null ) {
+    throw "Unable to locate a Java installation. Please set JAVA_HOME or PATH environment variables."
+} elseif ( $( Get-Item $javabin -ErrorAction SilentlyContinue ) -eq $null ) {
+    throw "java.exe does not exist where specified. Please check JAVA_HOME or PATH environment variables."
+}
+
+﻿# set up the classpath
 
 $cp = Get-ChildItem ${BROOKLYN_HOME}\conf | Select-Object -ExpandProperty FullName
-$cp += Get-ChildItem ${BROOKLYN_HOME}\patch | Select-Object -ExpandProperty FullName
-$cp += Get-ChildItem ${BROOKLYN_HOME}\lib | Select-Object -ExpandProperty FullName
-$cp += Get-ChildItem ${BROOKLYN_HOME}\dropin | Select-Object -ExpandProperty FullName
+
+if ( Test-Path ${BROOKLYN_HOME}\patch ) {
+    $cp += Get-ChildItem ${BROOKLYN_HOME}\patch | Select-Object -ExpandProperty FullName
+}
+
+$cp += Get-ChildItem ${BROOKLYN_HOME}\lib\brooklyn | Select-Object -ExpandProperty FullName
+
+if ( Test-Path ${BROOKLYN_HOME}\dropin ) {
+    $cp += Get-ChildItem ${BROOKLYN_HOME}\dropin | Select-Object -ExpandProperty FullName
+}
+
 $INITIAL_CLASSPATH = $cp -join ';'
 
 # specify additional CP args in BROOKLYN_CLASSPATH
@@ -57,7 +112,7 @@ $javaargs += "brooklyn.cli.Main"
 $javaargs += $args
 
 # start Brooklyn
-$process = Start-Process -FilePath "java" -ArgumentList $javaargs -NoNewWindow -PassThru
+$process = Start-Process -FilePath $javabin -ArgumentList $javaargs -NoNewWindow -PassThru
 
 # save PID
 Set-Content -Path ${BROOKLYN_HOME}\pid_java -Value $($process.Id)
