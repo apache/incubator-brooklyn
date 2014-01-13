51d057ef2405bb763b587f750c53a245cc121044
Aled Sage <aled.sage@gmail.com>
2014-01-13 23:44:54 +0000

    Delete deprecated code in MutableSet/List

--- utils/common/src/main/java/brooklyn/util/collections/MutableSet.java
+++ utils/common/src/main/java/brooklyn/util/collections/MutableSet.java
@@ -92,15 +92,6 @@ public class MutableSet<V> extends LinkedHashSet<V> {
             return this;
         }
 
-        /** @deprecated since 0.6.0 ambiguous with {@link #addAll(Iterable)}; 
-         * use {@link #add(Object, Object, Object...)} */ 
-        public Builder<V> addAll(V... values) {
-            for (V v : values) {
-                result.add(v);
-            }
-            return this;
-        }
-
         public Builder<V> removeAll(Iterable<? extends V> iterable) {
             if (iterable instanceof Collection) {
                 result.removeAll((Collection<? extends V>) iterable);
@@ -108,15 +99,6 @@ public class MutableSet<V> extends LinkedHashSet<V> {
                 for (V v : iterable) {
                     result.remove(v);
                 }
-            }
-            return this;
-        }
-        
-        /** @deprecated since 0.6.0 ambiguous with {@link #removeAll(Iterable)}; 
-         * use <code>removeAll(Arrays.asList(Object, Object, Object...))</code> */ 
-        public Builder<V> removeAll(V... values) {
-            for (V v : values) {
-                result.remove(v);
             }
             return this;
         }
