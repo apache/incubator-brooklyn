c65276eb8eafb6aeae7451978ae6fa530ae2c16e
Martin Harris <github@nakomis.com>
2014-01-16 17:01:46 +0000

    Added support for accessing static fields (e.g. AttributeSensors) from YAML

--- usage/camp/src/main/java/io/brooklyn/camp/brooklyn/spi/dsl/methods/BrooklynDslCommon.java
+++ usage/camp/src/main/java/io/brooklyn/camp/brooklyn/spi/dsl/methods/BrooklynDslCommon.java
@@ -1,9 +1,13 @@
 package io.brooklyn.camp.brooklyn.spi.dsl.methods;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 import io.brooklyn.camp.brooklyn.spi.dsl.BrooklynDslDeferredSupplier;
 import io.brooklyn.camp.brooklyn.spi.dsl.DslUtils;
 import brooklyn.event.basic.DependentConfiguration;
 import brooklyn.management.Task;
+import brooklyn.util.exceptions.Exceptions;
 
 /** static import functions which can be used in `$brooklyn:xxx` contexts */
 public class BrooklynDslCommon {
@@ -30,4 +34,16 @@ public class BrooklynDslCommon {
         };
     }
 
+    /** returns any public static fields such as config keys or attribute sensors */
+    public static Object getStaticField(String clazzName, String fieldName) {
+        try {
+            Class<?> clazz = Class.forName(clazzName);
+            Field field = clazz.getField(fieldName);
+            if (!Modifier.isStatic(field.getModifiers()))
+                throw new IllegalArgumentException("Cannot get static field: field " + fieldName + " in class " + clazzName + " is not static");
+            return field.get(null);
+        } catch (Exception e) {
+            throw Exceptions.propagate(e);
+        }
+    }
 }
