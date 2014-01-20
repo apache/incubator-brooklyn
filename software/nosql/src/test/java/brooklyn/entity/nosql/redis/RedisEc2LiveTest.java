56a26f90b5076219cc0cb272bb2850728522a1cc
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2014-01-20 17:45:58 +0000

    Upgrade RedisStore to version 2.8.4.

--- software/nosql/src/test/java/brooklyn/entity/nosql/redis/RedisEc2LiveTest.java
+++ software/nosql/src/test/java/brooklyn/entity/nosql/redis/RedisEc2LiveTest.java
@@ -1,5 +1,7 @@
 package brooklyn.entity.nosql.redis;
 
+import javax.annotation.Nullable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -9,6 +11,7 @@ import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.location.Location;
 import brooklyn.test.EntityTestUtils;
 
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
@@ -16,12 +19,6 @@ public class RedisEc2LiveTest extends AbstractEc2LiveTest {
 
     @SuppressWarnings("unused")
     private static final Logger LOG = LoggerFactory.getLogger(RedisEc2LiveTest.class);
-
-    @Test(groups = {"Live"})
-    public void test_CentOS_6_3() throws Exception {
-        // Image: {id=us-east-1/ami-7d7bfc14, providerId=ami-7d7bfc14, name=RightImage_CentOS_6.3_x64_v5.8.8.5, location={scope=REGION, id=us-east-1, description=us-east-1, parent=aws-ec2, iso3166Codes=[US-VA]}, os={family=centos, arch=paravirtual, version=6.0, description=rightscale-us-east/RightImage_CentOS_6.3_x64_v5.8.8.5.manifest.xml, is64Bit=true}, description=rightscale-us-east/RightImage_CentOS_6.3_x64_v5.8.8.5.manifest.xml, version=5.8.8.5, status=AVAILABLE[available], loginUser=root, userMetadata={owner=411009282317, rootDeviceType=instance-store, virtualizationType=paravirtual, hypervisor=xen}}
-        runTest(ImmutableMap.of("imageId", "us-east-1/ami-7d7bfc14", "hardwareId", SMALL_HARDWARE_ID));
-    }
 
     @Override
     protected void doTest(Location loc) throws Exception {
@@ -35,6 +32,13 @@ public class RedisEc2LiveTest extends AbstractEc2LiveTest {
         } finally {
             redis.stop();
         }
+
+        // Confirm sensors are valid
+        EntityTestUtils.assertPredicateEventuallyTrue(redis, new Predicate<RedisStore>() {
+            @Override public boolean apply(@Nullable RedisStore input) {
+                return input != null && input.getAttribute(RedisStore.UPTIME) > 0;
+            }
+        });
     }
 
     @Test(enabled=false)
