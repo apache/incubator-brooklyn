1429439e32cd55d6f119fac34d82ce34fda2fb03
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-01-29 09:39:26 -0500

    support for starting cassandra nodes sequentially, and info on where the unreliable starts occur; also switch JMX to use 7199 as that's what cassandra nodetool expects

--- software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraNode.java
+++ software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraNode.java
@@ -62,6 +62,11 @@ public interface CassandraNode extends DatastoreMixins.DatastoreCommon, Software
     @SetFromFlag("nativePort")
     PortAttributeSensorAndConfigKey NATIVE_TRANSPORT_PORT = new PortAttributeSensorAndConfigKey("cassandra.native.port", "Cassandra Native Transport port", PortRanges.fromString("9042+"));
 
+    @SetFromFlag("rmiRegistryPort")
+    // cassandra nodetool and others want 7199 - not required, but useful
+    PortAttributeSensorAndConfigKey RMI_REGISTRY_PORT = new PortAttributeSensorAndConfigKey(UsesJmx.RMI_REGISTRY_PORT, 
+        PortRanges.fromInteger(7199));
+
     @SetFromFlag("customSnitchJarUrl")
     ConfigKey<String> CUSTOM_SNITCH_JAR_URL = ConfigKeys.newStringConfigKey("cassandra.config.customSnitchUrl", 
             "URL for a jar file to be uploaded (e.g. \"classpath://brooklyn/entity/nosql/cassandra/cassandra-multicloud-snitch.jar\"); defaults to null which means nothing to upload", 
