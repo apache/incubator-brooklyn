eea556470b2f268d6e20bf7836ceb360e05782fb
Martin Harris <github@nakomis.com>
2014-02-11 18:27:27 +0000

    Run start effectors async

--- software/nosql/src/main/java/brooklyn/entity/nosql/mongodb/sharding/MongoDBShardedDeploymentImpl.java
+++ software/nosql/src/main/java/brooklyn/entity/nosql/mongodb/sharding/MongoDBShardedDeploymentImpl.java
@@ -3,11 +3,18 @@ package brooklyn.entity.nosql.mongodb.sharding;
 import static brooklyn.event.basic.DependentConfiguration.attributeWhenReady;
 
 import java.util.Collection;
+import java.util.concurrent.ExecutionException;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 import brooklyn.entity.basic.AbstractEntity;
+import brooklyn.entity.basic.Entities;
 import brooklyn.entity.group.DynamicCluster;
 import brooklyn.entity.proxying.EntitySpec;
+import brooklyn.entity.trait.Startable;
 import brooklyn.location.Location;
+import brooklyn.util.exceptions.Exceptions;
 
 public class MongoDBShardedDeploymentImpl extends AbstractEntity implements MongoDBShardedDeployment {
     private MongoDBRouterCluster routers;
@@ -27,10 +34,16 @@ public class MongoDBShardedDeploymentImpl extends AbstractEntity implements Mong
 
     @Override
     public void start(Collection<? extends Location> locations) {
-        configServers.start(locations);
-        routers.start(locations);
-        shards.start(locations);
-        // FIXME: Check if service is up
+        try {
+            Entities.invokeEffectorList(this, ImmutableList.of(configServers, routers, shards), Startable.START, ImmutableMap.of("locations", locations))
+                .get();
+            // wait for everything to start, then add the sharded servers
+        } catch (InterruptedException e) {
+            throw Exceptions.propagate(e);
+        } catch (ExecutionException e) {
+            throw Exceptions.propagate(e);
+        }
+        // FIXME: Check if service is up, call checkSensors
         setAttribute(SERVICE_UP, true);
     }
 
