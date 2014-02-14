1d1c2b6bbc105dd1e2eb9ecb15d51490ec9e87dc
Martin Harris <github@nakomis.com>
2014-02-14 15:08:24 +0000

    Added tests for launching YAML after reloading brooklyn properties

---
+++ usage/camp/src/test/java/io/brooklyn/camp/brooklyn/ReloadBrooklynPropertiesTest.java
@@ -0,0 +1,74 @@
+package io.brooklyn.camp.brooklyn;
+
+import io.brooklyn.camp.CampPlatform;
+import io.brooklyn.camp.spi.Assembly;
+import io.brooklyn.camp.spi.AssemblyTemplate;
+
+import java.io.Reader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import brooklyn.entity.Entity;
+import brooklyn.entity.basic.Entities;
+import brooklyn.entity.trait.Startable;
+import brooklyn.management.ManagementContext;
+import brooklyn.test.Asserts;
+import brooklyn.util.ResourceUtils;
+import brooklyn.util.exceptions.Exceptions;
+import brooklyn.util.stream.Streams;
+
+public class ReloadBrooklynPropertiesTest {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(ReloadBrooklynPropertiesTest.class);
+    
+    private ManagementContext brooklynMgmt;
+    
+    @BeforeMethod(alwaysRun=true)
+    public void setup() {
+        brooklynMgmt = new BrooklynCampPlatformLauncherNoServer().launch().getBrooklynMgmt();
+    }
+    
+    @AfterMethod(alwaysRun=true)
+    public void teardown() {
+        if (brooklynMgmt!=null) Entities.destroyAll(brooklynMgmt);
+    }
+    
+    @Test
+    public void testReloadBrooklynPropertiesNonDeploy() {
+        CampPlatform platform = brooklynMgmt.getConfig().getConfig(BrooklynCampConstants.CAMP_PLATFORM);
+        Assert.assertNotNull(platform);
+        brooklynMgmt.reloadBrooklynProperties();
+        CampPlatform reloadedPlatform = brooklynMgmt.getConfig().getConfig(BrooklynCampConstants.CAMP_PLATFORM);
+        Assert.assertEquals(reloadedPlatform, platform);
+    }
+    
+    @Test
+    public void testReloadBrooklynPropertiesDeploy() {
+        brooklynMgmt.reloadBrooklynProperties();
+        CampPlatform reloadedPlatform = brooklynMgmt.getConfig().getConfig(BrooklynCampConstants.CAMP_PLATFORM);
+        Assert.assertNotNull(reloadedPlatform);
+        Reader input = Streams.reader(new ResourceUtils(this).getResourceFromUrl("test-entity-basic-template.yaml"));
+        AssemblyTemplate template = reloadedPlatform.pdp().registerDeploymentPlan(input);
+        try {
+            Assembly assembly = template.getInstantiator().newInstance().instantiate(template, reloadedPlatform);
+            LOG.info("Test - created " + assembly);
+            final Entity app = brooklynMgmt.getEntityManager().getEntity(assembly.getId());
+            LOG.info("App - " + app);
+            Assert.assertEquals(app.getDisplayName(), "test-entity-basic-template");
+            Asserts.succeedsEventually(new Runnable() {
+                @Override
+                public void run() {
+                    Assert.assertTrue(app.getAttribute(Startable.SERVICE_UP));
+                }
+            });
+        } catch (Exception e) {
+            LOG.warn("Unable to instantiate " + template + " (rethrowing): " + e);
+            throw Exceptions.propagate(e);
+        }
+    }
+}
