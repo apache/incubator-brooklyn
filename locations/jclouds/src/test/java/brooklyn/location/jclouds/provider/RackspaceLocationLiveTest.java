d0d013c8386cf1200e51fe713fca94707882ae8c
Aled Sage <aled.sage@gmail.com>
2014-02-24 16:16:37 +0000

    Delete RackspaceUkLocationWithImageNameLiveTest
    - moved one useful(isn) test into RackspaceLocationLiveTest

--- locations/jclouds/src/test/java/brooklyn/location/jclouds/provider/RackspaceLocationLiveTest.java
+++ locations/jclouds/src/test/java/brooklyn/location/jclouds/provider/RackspaceLocationLiveTest.java
@@ -1,14 +1,28 @@
 package brooklyn.location.jclouds.provider;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import brooklyn.location.basic.SshMachineLocation;
+import brooklyn.location.jclouds.JcloudsLocation;
+import brooklyn.location.jclouds.JcloudsSshMachineLocation;
+import brooklyn.util.collections.MutableMap;
+
 public class RackspaceLocationLiveTest extends AbstractJcloudsLocationTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RackspaceLocationLiveTest.class);
 
     private static final String PROVIDER = "rackspace-cloudservers-uk";
     private static final String REGION_NAME = null;
     private static final String IMAGE_ID = "LON/f70ed7c7-b42e-4d77-83d8-40fa29825b85"; // CentOS 6.4
-    private static final String IMAGE_NAME_PATTERN = "Ubuntu 13.04";
+    private static final String IMAGE_NAME_PATTERN = "CentOS 6.4";
     private static final String IMAGE_OWNER = null;
 
     public RackspaceLocationLiveTest() {
@@ -37,7 +51,15 @@ public class RackspaceLocationLiveTest extends AbstractJcloudsLocationTest {
         return new Object[0][0];
     }
 
-    @Test(enabled = false)
-    public void noop() { /* just exists to let testNG IDE run the test */ }
+    @Test(groups = "Live")
+    public void testVmMetadata() {
+        loc = (JcloudsLocation) ctx.getLocationRegistry().resolve(PROVIDER + (REGION_NAME == null ? "" : ":" + REGION_NAME));
+        SshMachineLocation machine = obtainMachine(MutableMap.of("imageId", IMAGE_ID, "userMetadata", MutableMap.of("mykey", "myval"), JcloudsLocation.MACHINE_CREATE_ATTEMPTS, 2));
 
+        LOG.info("Provisioned {} vm {}; checking metadata and if ssh'able", PROVIDER, machine);
+
+        Map<String,String> userMetadata = ((JcloudsSshMachineLocation)machine).getNode().getUserMetadata();
+        assertEquals(userMetadata.get("mykey"), "myval", "metadata="+userMetadata);
+        assertTrue(machine.isSshable());
+    }
 }
