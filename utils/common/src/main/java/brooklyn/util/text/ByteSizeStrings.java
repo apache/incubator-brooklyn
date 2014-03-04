eec338953f3fa56583e15e1c97d999cff763688e
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-03-04 22:30:50 +0000

    Changes based on review comments
    - Added a builder and deprecated setter methods
    - More comprehensive Javadoc comments
    - Implemented Function and Formattable interfaces
    - Improved test coverage

--- utils/common/src/main/java/brooklyn/util/text/ByteSizeStrings.java
+++ utils/common/src/main/java/brooklyn/util/text/ByteSizeStrings.java
@@ -1,40 +1,275 @@
+/*
+ * Copyright 2009-2014 by Cloudsoft Corporation Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package brooklyn.util.text;
 
-public class ByteSizeStrings {
+import java.util.Formattable;
+import java.util.FormattableFlags;
+import java.util.Formatter;
 
-    /** Formats byte sizes suitable for Java {@code -Xms} arguments. */
-    public static final ByteSizeStrings java() {
-        ByteSizeStrings strings = new ByteSizeStrings();
-        strings.setBytesPerMetricUnit(1024);
-        strings.setPrecision(0);
-        strings.setMaxLen(6);
-        strings.setSuffixBytes("");
-        strings.setSuffixKilo("k");
-        strings.setSuffixMega("m");
-        strings.setSuffixGiga("g");
-        return strings;
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
+
+/**
+ * A formatter to pretty-print numeric values representing sizes in byes.
+ * <p>
+ * The {@link ByteSizeStrings#builder()} presents a fluent interface to create
+ * various configurations of formatting. The defaults produce metric units in
+ * multiples of 1000 bytes at a precision of three significant figures. This is
+ * the way disk space is normally measured, for example {@literal 128.1GB}.
+ * <p>
+ * Alternatively the {@link ByteSizeStrings#iso()} convenience method produces
+ * ISO standard units in multiples of 1024 bytes, with the same precision as the
+ * metric output. This is how RAM is normally measured, for example {@literal 12.4MiB}
+ * or {@literal 1.04GiB}.
+ * <p>
+ * Finally, the {@link ByteSizeStrings#java()} convenience method will produce
+ * strings suitable for use with a Java command line, as part of the {@code -Xms}
+ * or {@code -Xmx} options. These output integer values only, so values up to
+ * 10GB will be reported in MB to preserve accuracy. For size values over 1000GB,
+ * the output will still be formatted as GB but rounded to a mutiple of 1000.
+ * <p>
+ * The class is immutable and thread safe once built and a single instance of
+ * the three pre-defined configurations is created and returned buy the methods
+ * described above.
+ *
+ * @see Strings#makeSizeString(long)
+ * @see Strings#makeISOSizeString(long)
+ * @see Strings#makeJavaSizeString(long)
+ */
+public class ByteSizeStrings implements Function<Long, String> {
+
+    /**
+     * Configures and builds a {@link ByteSizeStrings} formatter.
+     */
+    public static class Builder {
+
+        private String suffixBytes = "B";
+        private String suffixKilo = "kB";
+        private String suffixMega = "MB";
+        private String suffixGiga = "GB";
+        private String suffixTera = "TB";
+        private int bytesPerMetricUnit = 1000;
+        private int maxLen = 4;
+        private int precision = 3;
+        private int lowerLimit = 1;
+
+        /**
+         * The suffix to use when printing bytes.
+         */
+        public Builder suffixBytes(String suffixBytes) { this.suffixBytes = suffixBytes; return this; }
+
+        /**
+         * The suffix to use when printing Kilobytes.
+         */
+        public Builder suffixKilo(String suffixKilo) { this.suffixKilo = suffixKilo; return this; }
+
+        /**
+         * The suffix to use when printing Megabytes.
+         */
+        public Builder suffixMega(String suffixMega) { this.suffixMega = suffixMega; return this; }
+
+        /**
+         * The suffix to use when printing Gigabytes.
+         */
+        public Builder suffixGiga(String suffixGiga) { this.suffixGiga = suffixGiga; return this; }
+
+        /**
+         * The suffix to use when printing Terabytes.
+         */
+        public Builder suffixTera(String suffixTera) { this.suffixTera = suffixTera; return this; }
+
+        /**
+         * The number of bytes per metric usnit, usually either 1000 or 1024.
+         * <p>
+         * Used to determine when to use the next suffix string.
+         */
+        public Builder bytesPerMetricUnit(int bytesPerMetricUnit) { this.bytesPerMetricUnit = bytesPerMetricUnit; return this; }
+
+        /**
+         * The maximum length of the printed number.
+         *
+         * @see Strings#makeRealString(double, int, int, int, double, boolean)
+         */
+        public Builder maxLen(int maxLen) { this.maxLen = maxLen; return this; }
+
+        /**
+         * The number of digits accuracy desired in the printed number.
+         *
+         * @see Strings#makeRealString(double, int, int, int, double, boolean)
+         */
+        public Builder precision(int precision) { this.precision = precision; return this; }
+
+        /**
+         * Prints using a lower suffix until the size is greater than this limit multiplied
+         * by bytes per metric unit, when the next highest suffix will be used.§
+         * <p>
+         * If this has the value 5 then sizes up to 5000 will be printed as bytes, and over 5000
+         * as Kilobytes.
+         */
+        public Builder lowerLimit(int lowerLimit) { this.lowerLimit = lowerLimit; return this; }
+
+        /**
+         * Returns an immutable {@link ByteSizeStrings} formatter using the builder configuration.
+         */
+        public ByteSizeStrings build() {
+            return new ByteSizeStrings(suffixBytes, suffixKilo, suffixMega, suffixGiga,
+                    suffixTera, bytesPerMetricUnit, maxLen, precision, lowerLimit);
+        }
+
     }
 
-    /** Formats byte sizes using ISO standard binary multiples of 1024. */
-    public static ByteSizeStrings iso() {
-        ByteSizeStrings strings = new ByteSizeStrings();
-        strings.setBytesPerMetricUnit(1024);
-        strings.setSuffixBytes("B");
-        strings.setSuffixKilo("KiB");
-        strings.setSuffixMega("MiB");
-        strings.setSuffixGiga("GiB");
-        return strings;
+    /**
+     * Returns a builder for a {@link ByteSizeStrings} formatter.
+     */
+    public static Builder builder() { return new Builder(); }
+
+    /**
+     * Format byte sizes suitable for Java {@code -Xms} arguments.
+     */
+    public static final ByteSizeStrings java() { return JAVA; }
+
+    private static final ByteSizeStrings JAVA = ByteSizeStrings.builder()
+                .suffixBytes("")
+                .suffixKilo("k")
+                .suffixMega("m")
+                .suffixGiga("g")
+                .suffixTera("000g") // Java has no Tera suffix
+                .bytesPerMetricUnit(1024)
+                .maxLen(6)
+                .precision(0)
+                .lowerLimit(10)
+                .build();
+
+    /**
+     * Formats byte sizes using ISO standard suffixes and binary multiples of 1024
+     */
+    public static ByteSizeStrings iso() { return ISO; }
+
+    private static ByteSizeStrings ISO = ByteSizeStrings.builder()
+                .suffixBytes("B")
+                .suffixKilo("KiB")
+                .suffixMega("MiB")
+                .suffixGiga("GiB")
+                .suffixTera("TiB")
+                .bytesPerMetricUnit(1024)
+                .build();
+
+    /**
+     * Default byte size formatter using metric multiples of 1000.
+     */
+    public static ByteSizeStrings metric() { return METRIC; }
+
+    private static ByteSizeStrings METRIC = ByteSizeStrings.builder().build();
+
+    private String suffixBytes;
+    private String suffixKilo;
+    private String suffixMega;
+    private String suffixGiga;
+    private String suffixTera;
+    private int bytesPerMetricUnit;
+    private int maxLen;
+    private int precision;
+    private int lowerLimit;
+
+    /**
+     * For use by the {@link Builder} only.
+     */
+    private ByteSizeStrings(String suffixBytes, String suffixKilo, String suffixMega, String suffixGiga,
+            String suffixTera, int bytesPerMetricUnit, int maxLen, int precision, int lowerLimit) {
+        this.suffixBytes = suffixBytes;
+        this.suffixKilo = suffixKilo;
+        this.suffixMega = suffixMega;
+        this.suffixGiga = suffixGiga;
+        this.suffixTera = suffixTera;
+        this.bytesPerMetricUnit = bytesPerMetricUnit;
+        this.maxLen = maxLen;
+        this.precision = precision;
+        this.lowerLimit = lowerLimit;
     }
 
-    private String suffixBytes = "B";
-    private String suffixKilo = "kB";
-    private String suffixMega = "MB";
-    private String suffixGiga = "GB";
-    private int bytesPerMetricUnit = 1000;
-    private int maxLen = 4;
-    private int precision = 3;
+    /** @deprecated Use {@link ByteSizeStrings#builder()} */
+    @Deprecated
+    public ByteSizeStrings() { }
 
+    /** @deprecated Use {@link ByteSizeStrings.Builder#suffixBytes(String)} */
+    @Deprecated
+    public void setSuffixBytes(String suffixBytes) {
+        this.suffixBytes = suffixBytes;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#suffixKilo(String)} */
+    @Deprecated
+    public void setSuffixKilo(String suffixKilo) {
+        this.suffixKilo = suffixKilo;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#suffixMega(String)} */
+    @Deprecated
+    public void setSuffixMega(String suffixMega) {
+        this.suffixMega = suffixMega;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#suffixGiga(String)} */
+    @Deprecated
+    public void setSuffixGiga(String suffixGiga) {
+        this.suffixGiga = suffixGiga;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#suffixTera(String)} */
+    @Deprecated
+    public void setSuffixTera(String suffixTera) {
+        this.suffixTera = suffixTera;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#bytesPerMetricUnit(int)} */
+    @Deprecated
+    public void setBytesPerMetricUnit(int bytesPerMetricUnit) {
+        this.bytesPerMetricUnit = bytesPerMetricUnit;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#maxLen(int)} */
+    @Deprecated
+    public void setMaxLen(int maxLen) {
+        this.maxLen = maxLen;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#precision(int)} */
+    @Deprecated
+    public void setPrecision(int precision) {
+        this.precision = precision;
+    }
+
+    /** @deprecated Use {@link ByteSizeStrings.Builder#lowerLimit(int)} */
+    @Deprecated
+    public void setLowerLimit(int lowerLimit) {
+        this.lowerLimit = lowerLimit;
+    }
+
+    /**
+     * Format the {@literal size} bytes as a String.
+     */
     public String makeSizeString(long size) {
+        return makeSizeString(size, precision);
+    }
+
+    /**
+     * Format the {@literal size} bytes as a String with the given precision.
+     */
+    public String makeSizeString(long size, int precision) {
         long t = size;
         if (t==0) return "0"+suffixBytes;
         if (t<0) return "-"+makeSizeString(-t);
@@ -44,37 +279,55 @@ public class ByteSizeStrings {
         t = t/bytesPerMetricUnit;
         long mb = t%bytesPerMetricUnit;
         t = t/bytesPerMetricUnit;
-        long gb = t;
+        long gb = t%bytesPerMetricUnit;
+        t = t/bytesPerMetricUnit;
+        long tb = t;
 
-        if (gb>2)
-            return Strings.makeRealString(gb + (1.0*mb/bytesPerMetricUnit), maxLen, precision, 0) + suffixGiga;
-        if (mb>2)
+        if (tb>lowerLimit)
+            return Strings.makeRealString(tb + (1.0*gb/bytesPerMetricUnit), -1, precision, 0) + suffixTera;
+        if (gb>lowerLimit)
+            return Strings.makeRealString((tb*bytesPerMetricUnit) + gb + (1.0*mb/bytesPerMetricUnit), maxLen, precision, 0) + suffixGiga;
+        if (mb>lowerLimit)
             return Strings.makeRealString((gb*bytesPerMetricUnit) + mb + (1.0*kb/bytesPerMetricUnit), maxLen, precision, 0) + suffixMega;
-        if (kb>2)
+        if (kb>lowerLimit)
             return Strings.makeRealString((mb*bytesPerMetricUnit) + kb + (1.0*b/bytesPerMetricUnit), maxLen, precision, 0) + suffixKilo;
         return (kb*bytesPerMetricUnit) + b + suffixBytes;
     }
 
-    public void setBytesPerMetricUnit(int bytesPerMetricUnit) {
-        this.bytesPerMetricUnit = bytesPerMetricUnit;
+    /**
+     * Returns a {@link Formattable} object that can be used with {@link String#format(String, Object...)}.
+     * <p>
+     * When used as the argument for a {@literal %s} format string element, the {@literal bytes} value
+     * will be formatted using the current {@link ByteSizeStrings} values, or if the alternative
+     * flag is set (using the {@literal %#s} format string) it will use the {@link ByteSizeStrings#metric()}
+     * formatter. Finally, the precision of the formatted value can be adjusted using format string
+     * argumenbts like {@literal %.6s}.
+     *
+     * @see http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax
+     */
+    public Formattable formatted(final long bytes) {
+        return new Formattable() {
+            @Override
+            public void formatTo(Formatter formatter, int flags, int width, int precision) {
+                boolean alternate = (flags & FormattableFlags.ALTERNATE) == FormattableFlags.ALTERNATE;
+                ByteSizeStrings strings = alternate ? ByteSizeStrings.metric() : ByteSizeStrings.this;
+                if (precision != -1) {
+                    formatter.format("%s", strings.makeSizeString(bytes, precision));
+                } else {
+                    formatter.format("%s", strings.makeSizeString(bytes));
+                }
+            }
+        };
     }
-    public void setMaxLen(int maxLen) {
-        this.maxLen = maxLen;
-    }
-    public void setPrecision(int precision) {
-        this.precision = precision;
-    }
-    public void setSuffixBytes(String suffixBytes) {
-        this.suffixBytes = suffixBytes;
-    }
-    public void setSuffixGiga(String suffixGiga) {
-        this.suffixGiga = suffixGiga;
-    }
-    public void setSuffixKilo(String suffixKilo) {
-        this.suffixKilo = suffixKilo;
-    }
-    public void setSuffixMega(String suffixMega) {
-        this.suffixMega = suffixMega;
+
+    /**
+     * A {@link Function} implementation that formats its input using the current {@link ByteSizeStrings} values.
+     */
+    @Override
+    @Nullable
+    public String apply(@Nullable Long input) {
+        if (input == null) return null;
+        return makeSizeString(input);
     }
 
 }
--- utils/common/src/test/java/brooklyn/util/text/ByteSizeStringsTest.java
+++ utils/common/src/test/java/brooklyn/util/text/ByteSizeStringsTest.java
@@ -1,48 +1,125 @@
 /*
- * Copyright (c) 2009-2013 Cloudsoft Corporation Ltd.
+ * Copyright 2009-2014 by Cloudsoft Corporation Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package brooklyn.util.text;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.testng.annotations.Test;
 
+import com.google.common.collect.Iterables;
+
+/**
+ * Test the different {@link ByteSizeStrings} formatting options using a list of byte sizes.
+ */
 @Test
 public class ByteSizeStringsTest {
 
     public void testSizeString() {
+        assertEquals(Strings.makeSizeString(-1), "-1B");
         assertEquals(Strings.makeSizeString(0), "0B");
         assertEquals(Strings.makeSizeString(999), "999B");
+        assertEquals(Strings.makeSizeString(1024), "1024B");
         assertEquals(Strings.makeSizeString(1234), "1234B");
-        assertEquals(Strings.makeSizeString(2345), "2345B");
+        assertEquals(Strings.makeSizeString(2345), "2.34kB");
+        assertEquals(Strings.makeSizeString(4096), "4.10kB");
         assertEquals(Strings.makeSizeString(4567), "4.57kB");
         assertEquals(Strings.makeSizeString(65535), "65.5kB");
-        assertEquals(Strings.makeSizeString(23456789), "23.5MB");
+        assertEquals(Strings.makeSizeString(23456789L), "23.5MB");
         assertEquals(Strings.makeSizeString(23456789012L), "23.5GB");
-        assertEquals(Strings.makeSizeString(23456789012345L), "2.35E4GB");
+        assertEquals(Strings.makeSizeString(23456789012345L), "23.5TB");
+        assertEquals(Strings.makeSizeString(Long.MAX_VALUE), "9223372TB");
     }
 
     public void testJavaSizeString() {
+        assertEquals(ByteSizeStrings.java().makeSizeString(-1), "-1");
         assertEquals(ByteSizeStrings.java().makeSizeString(0), "0");
         assertEquals(ByteSizeStrings.java().makeSizeString(999), "999");
+        assertEquals(ByteSizeStrings.java().makeSizeString(1024), "1024");
         assertEquals(ByteSizeStrings.java().makeSizeString(1234), "1234");
         assertEquals(ByteSizeStrings.java().makeSizeString(2345), "2345");
-        assertEquals(ByteSizeStrings.java().makeSizeString(4567), "4k");
+        assertEquals(ByteSizeStrings.java().makeSizeString(4096), "4096");
+        assertEquals(ByteSizeStrings.java().makeSizeString(4567), "4567");
+        assertEquals(ByteSizeStrings.java().makeSizeString(6789), "6789");
         assertEquals(ByteSizeStrings.java().makeSizeString(65535), "64k");
-        assertEquals(ByteSizeStrings.java().makeSizeString(23456789), "22m");
+        assertEquals(ByteSizeStrings.java().makeSizeString(23456789L), "22m");
         assertEquals(ByteSizeStrings.java().makeSizeString(23456789012L), "22g");
-        assertEquals(ByteSizeStrings.java().makeSizeString(23456789012345L), "21846g");
+        assertEquals(ByteSizeStrings.java().makeSizeString(23456789012345L), "21000g");
+        assertEquals(ByteSizeStrings.java().makeSizeString(Long.MAX_VALUE), "8388608000g");
     }
 
     public void testISOSizeString() {
+        assertEquals(ByteSizeStrings.iso().makeSizeString(-1), "-1B");
         assertEquals(ByteSizeStrings.iso().makeSizeString(0), "0B");
         assertEquals(ByteSizeStrings.iso().makeSizeString(999), "999B");
         assertEquals(ByteSizeStrings.iso().makeSizeString(1024), "1024B");
-        assertEquals(ByteSizeStrings.iso().makeSizeString(2048), "2048B");
+        assertEquals(ByteSizeStrings.iso().makeSizeString(1234), "1234B");
+        assertEquals(ByteSizeStrings.iso().makeSizeString(2345), "2.29KiB");
         assertEquals(ByteSizeStrings.iso().makeSizeString(4096), "4KiB");
+        assertEquals(ByteSizeStrings.iso().makeSizeString(4567), "4.46KiB");
+        assertEquals(ByteSizeStrings.iso().makeSizeString(6789), "6.63KiB");
         assertEquals(ByteSizeStrings.iso().makeSizeString(65535), "64.0KiB");
-        assertEquals(ByteSizeStrings.iso().makeSizeString(23456789), "22.4MiB");
+        assertEquals(ByteSizeStrings.iso().makeSizeString(23456789L), "22.4MiB");
         assertEquals(ByteSizeStrings.iso().makeSizeString(23456789012L), "21.8GiB");
-        assertEquals(ByteSizeStrings.iso().makeSizeString(23456789012345L), "2.18E4GiB");
+        assertEquals(ByteSizeStrings.iso().makeSizeString(23456789012345L), "21.3TiB");
+        assertEquals(ByteSizeStrings.iso().makeSizeString(Long.MAX_VALUE), "8388608TiB");
+    }
+
+    public void testBuilder() {
+        ByteSizeStrings strings = ByteSizeStrings.builder()
+                .bytesPerMetricUnit(1024)
+                .precision(4)
+                .lowerLimit(5)
+                .maxLen(4)
+                .suffixBytes("b")
+                .suffixKilo("kb")
+                .suffixMega("Mb")
+                .suffixGiga("Gb")
+                .suffixTera("Tb")
+                .build();
+
+        assertEquals(strings.makeSizeString(-1), "-1b");
+        assertEquals(strings.makeSizeString(0), "0b");
+        assertEquals(strings.makeSizeString(999), "999b");
+        assertEquals(strings.makeSizeString(1024), "1024b");
+        assertEquals(strings.makeSizeString(1234), "1234b");
+        assertEquals(strings.makeSizeString(2345), "2345b");
+        assertEquals(strings.makeSizeString(4096), "4096b");
+        assertEquals(strings.makeSizeString(4567), "4567b");
+        assertEquals(strings.makeSizeString(6789), "6.630kb");
+        assertEquals(strings.makeSizeString(65535), "64.00kb");
+        assertEquals(strings.makeSizeString(23456789L), "22.37Mb");
+        assertEquals(strings.makeSizeString(23456789012L), "21.85Gb");
+        assertEquals(strings.makeSizeString(23456789012345L), "21.33Tb");
+        assertEquals(strings.makeSizeString(Long.MAX_VALUE), "8388608Tb");
+    }
+
+    public void testFormatter() {
+        ByteSizeStrings iso = ByteSizeStrings.iso();
+        assertEquals(String.format("%s", iso.formatted(23456789L)), "22.4MiB");
+        assertEquals(String.format("%.6s", iso.formatted(23456789L)), "22.3701MiB");
+        assertEquals(String.format("%#s", iso.formatted(23456789L)), "23.5MB");
+    }
+
+    public void testFunction() {
+        ByteSizeStrings iso = ByteSizeStrings.iso();
+        Iterable<String> bytes = Iterables.transform(Arrays.asList(23456789L, 23456789012L, 23456789012345L), iso);
+        assertEquals(Iterables.get(bytes, 0), "22.4MiB");
+        assertEquals(Iterables.get(bytes, 1), "21.8GiB");
+        assertEquals(Iterables.get(bytes, 2), "21.3TiB");
     }
 }
