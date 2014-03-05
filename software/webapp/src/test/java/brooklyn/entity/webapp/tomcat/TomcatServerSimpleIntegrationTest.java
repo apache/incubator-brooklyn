ed5d6155ce2defea797dfcd4b089b6144016dfa4
Aled Sage <aled.sage@gmail.com>
2014-03-05 23:38:58 +0000

    Fix TomcatServerSimpleIntegrationTest.teraDown
    - so stops management context

--- software/webapp/src/test/java/brooklyn/entity/webapp/tomcat/TomcatServerSimpleIntegrationTest.java
+++ software/webapp/src/test/java/brooklyn/entity/webapp/tomcat/TomcatServerSimpleIntegrationTest.java
@@ -54,7 +54,7 @@ public class TomcatServerSimpleIntegrationTest {
  
     @AfterMethod(alwaysRun=true)
     public void tearDown() throws Exception {
-        if (app != null) Entities.destroy(app);
+        if (app != null) Entities.destroyAll(app.getManagementContext());
     }
     
     @Test(groups="Integration")
