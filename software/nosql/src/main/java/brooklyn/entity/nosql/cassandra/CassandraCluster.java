4035248bc7045fcd97eb92416529bad42da30464
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-03-05 15:47:48 +0000

    Updated methods in Cassandra interfaces

--- software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraCluster.java
+++ software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraCluster.java
@@ -2,8 +2,9 @@ package brooklyn.entity.nosql.cassandra;
 
 import brooklyn.entity.proxying.ImplementedBy;
 
-/** @deprecated since 0.7.0; use {@link CassandraDatacenter} which is equivalent -- 
- * but a less ambiguous name as "Cluster" in Cassandra corresponds to what Brooklyn terms a "Fabric".
+/**
+ * @deprecated since 0.7.0; use {@link CassandraDatacenter} which is equivalent but has
+ * a less ambiguous name; <em>Cluster</em> in Cassandra corresponds to what Brooklyn terms a <em>Fabric</em>.
  */
 @Deprecated
 @ImplementedBy(CassandraClusterImpl.class)
--- software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraClusterImpl.java
+++ software/nosql/src/main/java/brooklyn/entity/nosql/cassandra/CassandraClusterImpl.java
@@ -1,7 +1,9 @@
 package brooklyn.entity.nosql.cassandra;
 
-/** @deprecated since 0.7.0; use {@link CassandraDatacenter} which is equivalent -- 
- * but a less ambiguous name as "Cluster" in Cassandra corresponds to what Brooklyn terms a "Fabric".
+/**
+ * @deprecated since 0.7.0; use {@link CassandraDatacenter} which is equivalent but has
+ * a less ambiguous name; <em>Cluster</em> in Cassandra corresponds to what Brooklyn terms a <em>Fabric</em>.
  */
+@Deprecated
 public class CassandraClusterImpl extends CassandraDatacenterImpl implements CassandraCluster {
 }
