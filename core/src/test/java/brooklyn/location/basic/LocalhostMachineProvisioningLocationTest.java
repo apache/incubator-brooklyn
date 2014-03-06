53ff3772afbaaae7d50ceeb40e4b10085c957b1a
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-03-06 09:41:12 +0000

    update LocalhostMachineProvisioningLocationTest to use location specs

--- core/src/test/java/brooklyn/location/basic/LocalhostMachineProvisioningLocationTest.java
+++ core/src/test/java/brooklyn/location/basic/LocalhostMachineProvisioningLocationTest.java
@@ -6,23 +6,44 @@ import static org.testng.Assert.fail;
 
 import java.net.ServerSocket;
 
+import org.junit.AfterClass;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import brooklyn.entity.basic.Entities;
+import brooklyn.location.LocationSpec;
 import brooklyn.location.NoMachinesAvailableException;
 import brooklyn.location.PortRange;
-import brooklyn.util.collections.MutableMap;
+import brooklyn.management.internal.LocalManagementContext;
 import brooklyn.util.net.Networking;
 
 public class LocalhostMachineProvisioningLocationTest {
     
-    protected LocalhostMachineProvisioningLocation newWithAddress(String address) {
-        return new LocalhostMachineProvisioningLocation(MutableMap.of("address", address));
+    private LocalManagementContext mgmt;
+
+    @BeforeClass
+    protected void setup() {
+        mgmt = new LocalManagementContext();
+    }
+    
+    @AfterClass
+    protected void teardown() {
+        Entities.destroyAll(mgmt);
+    }
+    
+    protected LocalhostMachineProvisioningLocation newLocalhostProvisioner() {
+        return mgmt.getLocationManager().createLocation(LocationSpec.create(LocalhostMachineProvisioningLocation.class));
+    }
+    
+    protected LocalhostMachineProvisioningLocation newLocalhostProvisionerWithAddress(String address) {
+        return mgmt.getLocationManager().createLocation(LocationSpec.create(LocalhostMachineProvisioningLocation.class)
+            .configure("address", address));
     }
     
     @Test
     public void defaultInvocationCanProvisionALocalhostInstance() throws Exception {
-        LocalhostMachineProvisioningLocation provisioner = new LocalhostMachineProvisioningLocation();
+        LocalhostMachineProvisioningLocation provisioner = mgmt.getLocationManager().createLocation(LocationSpec.create(LocalhostMachineProvisioningLocation.class));
         SshMachineLocation machine = provisioner.obtain();
         assertNotNull(machine);
         assertEquals(machine.address, Networking.getLocalHost());
@@ -30,18 +51,19 @@ public class LocalhostMachineProvisioningLocationTest {
 
     @Test
     public void testUsesLocationNameProvided() throws Exception {
-        LocalhostMachineProvisioningLocation provisioner = newWithAddress("localhost");
+        LocalhostMachineProvisioningLocation provisioner = newLocalhostProvisionerWithAddress("localhost");
         assertEquals(((SshMachineLocation)provisioner.obtain()).getAddress().getHostName(), "localhost");
 
-        LocalhostMachineProvisioningLocation provisioner2 = newWithAddress("1.2.3.4");
+        LocalhostMachineProvisioningLocation provisioner2 = newLocalhostProvisionerWithAddress("1.2.3.4");
         assertEquals(((SshMachineLocation)provisioner2.obtain()).getAddress().getHostName(), "1.2.3.4");
         
-        LocalhostMachineProvisioningLocation provisioner3 = newWithAddress("127.0.0.1");
+        LocalhostMachineProvisioningLocation provisioner3 = newLocalhostProvisionerWithAddress("127.0.0.1");
         assertEquals(((SshMachineLocation)provisioner3.obtain()).getAddress().getHostName(), "127.0.0.1");
     }
     
     public void provisionWithASpecificNumberOfInstances() throws NoMachinesAvailableException {
-        LocalhostMachineProvisioningLocation provisioner = new LocalhostMachineProvisioningLocation(MutableMap.of("count", 2));
+        LocalhostMachineProvisioningLocation provisioner = mgmt.getLocationManager().createLocation(LocationSpec.create(LocalhostMachineProvisioningLocation.class)
+            .configure("count", 2));
 
         // first machine
         SshMachineLocation first = provisioner.obtain();
@@ -64,7 +86,7 @@ public class LocalhostMachineProvisioningLocationTest {
     
     @Test
     public void obtainTwoAddressesInRangeThenDontObtain() throws Exception {
-        LocalhostMachineProvisioningLocation p = new LocalhostMachineProvisioningLocation();
+        LocalhostMachineProvisioningLocation p = newLocalhostProvisioner();
         SshMachineLocation m = p.obtain();
         int start = 48311;
         PortRange r = PortRanges.fromString(""+start+"-"+(start+1));
@@ -91,7 +113,7 @@ public class LocalhostMachineProvisioningLocationTest {
     
     @Test
     public void obtainLowNumberedPortsAutomatically() throws Exception {
-        LocalhostMachineProvisioningLocation p = new LocalhostMachineProvisioningLocation();
+        LocalhostMachineProvisioningLocation p = newLocalhostProvisioner();
         SshMachineLocation m = p.obtain();
         int start = 983;  //random rarely used port, not that it matters
         try {
@@ -105,7 +127,7 @@ public class LocalhostMachineProvisioningLocationTest {
 
     @Test
     public void obtainPortFailsIfInUse() throws Exception {
-        LocalhostMachineProvisioningLocation p = new LocalhostMachineProvisioningLocation();
+        LocalhostMachineProvisioningLocation p = newLocalhostProvisioner();
         SshMachineLocation m = p.obtain();
         int start = 48311;
         PortRange r = PortRanges.fromString(""+start+"-"+(start+1));
