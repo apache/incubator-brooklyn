c9f2365dbed81c64469841b48e486fa9b5981979
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-03-10 15:06:04 +0000

    run javaagents as daemon threads so that they don't prevent apps from terminating

--- utils/jmx/jmxrmi-agent/src/main/java/brooklyn/util/jmx/jmxrmi/JmxRmiAgent.java
+++ utils/jmx/jmxrmi-agent/src/main/java/brooklyn/util/jmx/jmxrmi/JmxRmiAgent.java
@@ -64,7 +64,23 @@ public class JmxRmiAgent {
         doMain(agentArgs);
     }
     
-    public static void doMain(String agentArgs) {
+    public static void doMain(final String agentArgs) {
+        // taken from JmxmpAgent in sister project
+        
+        // do the work in a daemon thread so that if the main class terminates abnormally,
+        // such that shutdown hooks aren't called, we don't keep the application running
+        // (e.g. if the app is compiled with java7 then run with java6, with a java6 agent here;
+        // that causes the agent to launch, the main to fail, but the process to keep going)
+        Thread t = new Thread() {
+            public void run() {
+                doMainForeground(agentArgs);
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+    }
+
+    public static void doMainForeground(String agentArgs) {
         final JMXConnectorServer connector = new JmxRmiAgent().startServer(System.getProperties());
         if (connector != null) {
             Runtime.getRuntime().addShutdownHook(new Thread("jmxmp-agent-shutdownHookThread") {
