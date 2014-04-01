7ad82a45e9e77b9f3a2b746be9cf7ceef609f734
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-04-01 13:54:32 +0100

    Added GROUP_MEMBERS attribute to AbstractGroup

--- core/src/main/java/brooklyn/entity/trait/Changeable.java
+++ core/src/main/java/brooklyn/entity/trait/Changeable.java
@@ -9,7 +9,9 @@ import brooklyn.event.basic.Sensors;
  * A collection of entities that can change.
  */
 public interface Changeable {
+
     AttributeSensor<Integer> GROUP_SIZE = Sensors.newIntegerSensor("group.members.count", "Number of members");
+
     BasicNotificationSensor<Entity> MEMBER_ADDED = new BasicNotificationSensor<Entity>(Entity.class, "group.members.added", "Entity added to group members");
     BasicNotificationSensor<Entity> MEMBER_REMOVED = new BasicNotificationSensor<Entity>(Entity.class, "group.members.removed", "Entity removed from group members");
 }
