3eb3b463637da52a2aaafe8f251b3225f2ef7091
ZaidM <zaid.mohsin@cloudsoftcorp.com>
2014-04-10 14:42:38 +0100

    added RiakClusterExample

--- examples/simple-nosql-cluster/src/main/java/brooklyn/demo/RiakClusterExample.java
+++ examples/simple-nosql-cluster/src/main/java/brooklyn/demo/RiakClusterExample.java
@@ -1,7 +1,16 @@
 package brooklyn.demo;
 
-/**
- * Created by zaid.mohsin on 09/04/2014.
- */
-public class RiakClusterExample {
+import brooklyn.entity.basic.ApplicationBuilder;
+import brooklyn.entity.nosql.riak.RiakCluster;
+import brooklyn.entity.nosql.riak.RiakNode;
+import brooklyn.entity.proxying.EntitySpec;
+
+public class RiakClusterExample extends ApplicationBuilder {
+
+    protected void doBuild() {
+
+        addChild(EntitySpec.create(RiakCluster.class)
+                .configure(RiakCluster.INITIAL_SIZE, 3)
+                .configure(RiakCluster.MEMBER_SPEC, EntitySpec.create(RiakNode.class)));
+    }
 }
