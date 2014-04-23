264f71b3517ed387e0f4893fbc31b5ccffcb3d13
Aled Sage <aled.sage@gmail.com>
2014-04-23 22:19:27 +0100

    Improve HttpLatencyDetectorTest

--- policy/src/test/java/brooklyn/enricher/HttpLatencyDetectorTest.java
+++ policy/src/test/java/brooklyn/enricher/HttpLatencyDetectorTest.java
@@ -1,5 +1,6 @@
 package brooklyn.enricher;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
@@ -14,33 +15,44 @@ import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.event.AttributeSensor;
 import brooklyn.event.basic.Sensors;
 import brooklyn.location.basic.LocalhostMachineProvisioningLocation;
-import brooklyn.test.TestUtils;
+import brooklyn.test.EntityTestUtils;
 import brooklyn.test.entity.TestApplication;
 import brooklyn.test.entity.TestEntity;
 import brooklyn.util.collections.MutableMap;
+import brooklyn.util.http.BetterMockWebServer;
 
-import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableList;
+import com.google.mockwebserver.MockResponse;
 
 public class HttpLatencyDetectorTest {
 
     private static final Logger log = LoggerFactory.getLogger(HttpLatencyDetectorTest.class);
     public static final AttributeSensor<String> TEST_URL = Sensors.newStringSensor( "test.url");
     
+    private BetterMockWebServer server;
     private LocalhostMachineProvisioningLocation loc;
     private TestApplication app;
     private TestEntity entity;
-
+    private URL baseUrl;
+    
     @BeforeMethod(alwaysRun=true)
     public void setUp() throws Exception {
         loc = new LocalhostMachineProvisioningLocation();
         app = ApplicationBuilder.newManagedApp(TestApplication.class);
         entity = app.createAndManageChild(EntitySpec.create(TestEntity.class));
         app.start(ImmutableList.of(loc));
+        
+        server = BetterMockWebServer.newInstanceLocalhost();
+        for (int i = 0; i < 100; i++) {
+            server.enqueue(new MockResponse().setResponseCode(200).addHeader("content-type: application/json").setBody("{\"foo\":\"myfoo\"}"));
+        }
+        server.play();
+        baseUrl = server.getUrl("/");
     }
 
     @AfterMethod(alwaysRun=true)
     public void tearDown() throws Exception {
+        if (server != null) server.shutdown();
         if (app != null) Entities.destroyAll(app.getManagementContext());
     }
     
@@ -52,19 +64,18 @@ public class HttpLatencyDetectorTest {
                 rollup(500, TimeUnit.MILLISECONDS).
                 period(100, TimeUnit.MILLISECONDS).
                 build());
+        
         // nothing until url is set
-        TestUtils.assertContinuallyFromJava(MutableMap.of("timeout", 200), 
-                Entities.attributeSupplier(entity, HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_MOST_RECENT), 
-                Predicates.equalTo(null));
-        entity.setAttribute(TEST_URL, "http://www.google.com");
-        TestUtils.assertEventually(MutableMap.of("timeout", 10000), 
-                Entities.attributeSupplier(entity, HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_MOST_RECENT), 
-                Predicates.notNull());
+        EntityTestUtils.assertAttributeEqualsContinually(
+                MutableMap.of("timeout", 200), 
+                entity, HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_MOST_RECENT, null);
+        
+        // gets value after url is set, and gets rolling average
+        entity.setAttribute(TEST_URL, baseUrl.toString());
+        EntityTestUtils.assertAttributeEventuallyNonNull(entity, HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_MOST_RECENT); 
+        EntityTestUtils.assertAttributeEventuallyNonNull(entity, HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_IN_WINDOW); 
+
         log.info("Latency to "+entity.getAttribute(TEST_URL)+" is "+entity.getAttribute(HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_MOST_RECENT));
-        TestUtils.assertEventually(MutableMap.of("timeout", 10000), 
-                Entities.attributeSupplier(entity, HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_IN_WINDOW), 
-                Predicates.notNull());
         log.info("Mean latency to "+entity.getAttribute(TEST_URL)+" is "+entity.getAttribute(HttpLatencyDetector.REQUEST_LATENCY_IN_SECONDS_IN_WINDOW));
     }
-    
 }
