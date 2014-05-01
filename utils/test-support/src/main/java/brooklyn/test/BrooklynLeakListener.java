eaef99a3827afaad26ef2345c6f3e8bb207c2dce
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-05-01 10:48:59 +0100

    improve test leak logging

--- utils/test-support/src/main/java/brooklyn/test/BrooklynLeakListener.java
+++ utils/test-support/src/main/java/brooklyn/test/BrooklynLeakListener.java
@@ -14,25 +14,13 @@ public class BrooklynLeakListener extends TestListenerAdapter {
     @Override
     public void onStart(ITestContext testContext) {
         super.onStart(testContext);
-        TEST_RESOURCE_LOG.info("BrooklynLeakListener.onStart attempting to terminate all extant ManagementContexts: "
-                + "name=" + testContext.getName()
-                + "; includedGroups="+Arrays.toString(testContext.getIncludedGroups())
-                + "; excludedGroups="+Arrays.toString(testContext.getExcludedGroups())
-                + "; suiteName="+testContext.getSuite().getName()
-                + "; outDir="+testContext.getOutputDirectory());
-        tryTerminateAll();
+        tryTerminateAll("BrooklynLeakListener.onStart", testContext);
     }
     
     @Override
     public void onFinish(ITestContext testContext) {
         super.onFinish(testContext);
-        TEST_RESOURCE_LOG.info("BrooklynLeakListener.onFinish attempting to terminate all extant ManagementContexts: "
-                + "name=" + testContext.getName()
-                + "; includedGroups="+Arrays.toString(testContext.getIncludedGroups())
-                + "; excludedGroups="+Arrays.toString(testContext.getExcludedGroups())
-                + "; suiteName="+testContext.getSuite().getName()
-                + "; outDir="+testContext.getOutputDirectory());
-        tryTerminateAll();
+        tryTerminateAll("BrooklynLeakListener.onFinish", testContext);
     }
     
     /**
@@ -41,16 +29,43 @@ public class BrooklynLeakListener extends TestListenerAdapter {
      * It does this reflectively because the listener is executed for all projects, included those that don't
      * depend on brooklyn-core, so LocalManagementContext may not be on the classpath.
      */
-    private void tryTerminateAll() {
+    private void tryTerminateAll(String context, ITestContext testContext) {
         String clazzName = "brooklyn.management.internal.LocalManagementContext";
+        String message;
+        int level;
         try {
             Class<?> clazz = BrooklynLeakListener.class.getClassLoader().loadClass(clazzName);
             clazz.getMethod("logAll", new Class[] {Logger.class}).invoke(null, TEST_RESOURCE_LOG);
-            clazz.getMethod("terminateAll").invoke(null);
+            Integer count = (Integer)clazz.getMethod("terminateAll").invoke(null);
+            if (count>0) {
+                level = 0;
+                message = ""+count+" ManagementContexts terminated";
+            } else if (count<0) {
+                level = 1;
+                message = ""+(-count)+" ManagementContexts left dangling";
+            } else {
+                level = -1;
+                message = ""+count+" ManagementContexts terminated";
+            }
         } catch (ClassNotFoundException e) {
-            TEST_RESOURCE_LOG.info("Class {} not found in testng listener, so not attempting to terminate all extant ManagementContexts; continuing", clazzName);
+            TEST_RESOURCE_LOG.debug("Class {} not found in testng listener, so not attempting to terminate all extant ManagementContexts; continuing", clazzName);
+            level = 0;
+            message = "no "+clazzName+" available, so skipped";
         } catch (Throwable e) {
             TEST_RESOURCE_LOG.error("ERROR in testng listener, attempting to terminate all extant ManagementContexts", e);
+            level = 1;
+            message = "ERROR: "+e;
         }
+        
+        String logMessage = context+" attempting to terminate all extant ManagementContexts: "
+            + "name=" + testContext.getName()
+            + "; includedGroups="+Arrays.toString(testContext.getIncludedGroups())
+            + "; excludedGroups="+Arrays.toString(testContext.getExcludedGroups())
+            + "; suiteName="+testContext.getSuite().getName()
+            + "; outDir="+testContext.getOutputDirectory()
+            + ": "+message;
+        if (level<0) TEST_RESOURCE_LOG.debug(logMessage);
+        else if (level>0) TEST_RESOURCE_LOG.warn(logMessage);
+        else TEST_RESOURCE_LOG.info(logMessage);
     }
 }
