f5626be3260bf48e0bda0d1851fe10ccfce22bcf
Aled Sage <aled.sage@gmail.com>
2014-05-27 12:39:19 +0100

    Rebind policy fixes

---
+++ core/src/test/java/brooklyn/entity/rebind/RebindEnricherTest.java
@@ -0,0 +1,186 @@
+package brooklyn.entity.rebind;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import brooklyn.config.ConfigKey;
+import brooklyn.enricher.Enrichers;
+import brooklyn.enricher.basic.AbstractEnricher;
+import brooklyn.entity.Entity;
+import brooklyn.entity.basic.ConfigKeys;
+import brooklyn.entity.basic.EntityInternal;
+import brooklyn.entity.group.DynamicCluster;
+import brooklyn.entity.proxying.EntitySpec;
+import brooklyn.event.AttributeSensor;
+import brooklyn.event.basic.Sensors;
+import brooklyn.policy.EnricherSpec;
+import brooklyn.test.EntityTestUtils;
+import brooklyn.test.entity.TestApplication;
+import brooklyn.test.entity.TestEntity;
+import brooklyn.util.flags.SetFromFlag;
+import brooklyn.util.text.StringFunctions;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+
+public class RebindEnricherTest extends RebindTestFixtureWithApp {
+
+    public static AttributeSensor<String> METRIC1 = Sensors.newStringSensor("RebindEnricherTest.metric1");
+    public static AttributeSensor<String> METRIC2 = Sensors.newStringSensor("RebindEnricherTest.metric2");
+    
+    private DynamicCluster origCluster;
+    private TestEntity origEntity;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        origCluster = origApp.createAndManageChild(EntitySpec.create(DynamicCluster.class).configure("memberSpec", EntitySpec.create(TestEntity.class)));
+        origEntity = origApp.createAndManageChild(EntitySpec.create(TestEntity.class));
+    }
+    
+    @Test
+    public void testPropagatingEnricher() throws Exception {
+        origApp.addEnricher(Enrichers.builder()
+                .propagating(METRIC1)
+                .from(origEntity)
+                .build());
+        
+        TestApplication newApp = rebind();
+        TestEntity newEntity = (TestEntity) Iterables.find(newApp.getChildren(), Predicates.instanceOf(TestEntity.class));
+
+        newEntity.setAttribute(METRIC1, "myval");
+        EntityTestUtils.assertAttributeEqualsEventually(newApp, METRIC1, "myval");
+    }
+
+    @Test
+    public void testPropagatingAllEnricher() throws Exception {
+        origApp.addEnricher(Enrichers.builder()
+                .propagatingAll()
+                .from(origEntity)
+                .build());
+        
+        TestApplication newApp = rebind();
+        TestEntity newEntity = (TestEntity) Iterables.find(newApp.getChildren(), Predicates.instanceOf(TestEntity.class));
+
+        newEntity.setAttribute(METRIC1, "myval");
+        EntityTestUtils.assertAttributeEqualsEventually(newApp, METRIC1, "myval");
+    }
+
+    @Test
+    public void testPropagatingAsEnricher() throws Exception {
+        origApp.addEnricher(Enrichers.builder()
+                .propagating(ImmutableMap.of(METRIC1, METRIC2))
+                .from(origEntity)
+                .build());
+        
+        TestApplication newApp = rebind();
+        TestEntity newEntity = (TestEntity) Iterables.find(newApp.getChildren(), Predicates.instanceOf(TestEntity.class));
+
+        newEntity.setAttribute(METRIC1, "myval");
+        EntityTestUtils.assertAttributeEqualsEventually(newApp, METRIC2, "myval");
+    }
+
+    @Test
+    public void testCombiningEnricher() throws Exception {
+        origApp.addEnricher(Enrichers.builder()
+                .combining(METRIC1, METRIC2)
+                .from(origEntity)
+                .computing(StringFunctions.joiner(","))
+                .publishing(METRIC2)
+                .build());
+        
+        TestApplication newApp = rebind();
+        TestEntity newEntity = (TestEntity) Iterables.find(newApp.getChildren(), Predicates.instanceOf(TestEntity.class));
+
+        newEntity.setAttribute(METRIC1, "myval");
+        newEntity.setAttribute(METRIC2, "myval2");
+        EntityTestUtils.assertAttributeEventually(newApp, METRIC2, Predicates.or(Predicates.equalTo("myval,myval2"), Predicates.equalTo("myval2,myval")));
+    }
+
+    @Test
+    public void testAggregatingMembersEnricher() throws Exception {
+        origCluster.resize(2);
+        
+        origApp.addEnricher(Enrichers.builder()
+                .aggregating(METRIC1)
+                .from(origCluster)
+                .fromMembers()
+                .computing(StringFunctions.joiner(","))
+                .publishing(METRIC2)
+                .build());
+        
+        TestApplication newApp = rebind();
+        DynamicCluster newCluster = (DynamicCluster) Iterables.find(newApp.getChildren(), Predicates.instanceOf(DynamicCluster.class));
+
+        int i = 0;
+        for (Entity member : newCluster.getMembers()) {
+            ((EntityInternal)member).setAttribute(METRIC1, "myval"+(i++));
+        }
+        EntityTestUtils.assertAttributeEventually(newApp, METRIC2, Predicates.or(Predicates.equalTo("myval1,myval2"), Predicates.equalTo("myval2,myval1")));
+    }
+    
+    @Test
+    public void testRestoresConfig() throws Exception {
+        origApp.addEnricher(EnricherSpec.create(MyEnricher.class)
+                .configure(MyEnricher.MY_CONFIG_WITH_SETFROMFLAG_NO_SHORT_NAME, "myVal for with setFromFlag noShortName")
+                .configure(MyEnricher.MY_CONFIG_WITH_SETFROMFLAG_WITH_SHORT_NAME, "myVal for setFromFlag withShortName")
+                .configure(MyEnricher.MY_CONFIG_WITHOUT_SETFROMFLAG, "myVal for witout setFromFlag"));
+
+        newApp = (TestApplication) rebind();
+        MyEnricher newEnricher = (MyEnricher) Iterables.getOnlyElement(newApp.getEnrichers());
+        
+        assertEquals(newEnricher.getConfig(MyEnricher.MY_CONFIG_WITH_SETFROMFLAG_NO_SHORT_NAME), "myVal for with setFromFlag noShortName");
+        assertEquals(newEnricher.getConfig(MyEnricher.MY_CONFIG_WITH_SETFROMFLAG_WITH_SHORT_NAME), "myVal for setFromFlag withShortName");
+        assertEquals(newEnricher.getConfig(MyEnricher.MY_CONFIG_WITHOUT_SETFROMFLAG), "myVal for witout setFromFlag");
+    }
+
+    public static class MyEnricher extends AbstractEnricher {
+        public static final ConfigKey<String> MY_CONFIG = ConfigKeys.newStringConfigKey("myconfigkey");
+        
+        @SetFromFlag
+        public static final ConfigKey<String> MY_CONFIG_WITH_SETFROMFLAG_NO_SHORT_NAME = ConfigKeys.newStringConfigKey("myconfig.withSetfromflag.noShortName");
+
+        @SetFromFlag("myConfigWithSetFromFlagWithShortName")
+        public static final ConfigKey<String> MY_CONFIG_WITH_SETFROMFLAG_WITH_SHORT_NAME = ConfigKeys.newStringConfigKey("myconfig.withSetfromflag.withShortName");
+
+        public static final ConfigKey<String> MY_CONFIG_WITHOUT_SETFROMFLAG = ConfigKeys.newStringConfigKey("myconfig.withoutSetfromflag");
+
+        @SetFromFlag
+        String myfield;
+
+        @SuppressWarnings("unused")
+        private final Object dummy = new Object(); // so not serializable
+        
+        public volatile boolean initCalled;
+        public volatile boolean rebindCalled;
+        
+        public MyEnricher() {
+        }
+        
+        public MyEnricher(Map<?,?> flags) {
+            super(flags);
+        }
+        
+        @Override
+        public void init() {
+            super.init();
+            initCalled = true;
+        }
+        
+        // TODO When AbstractEnricher declares rebind; @Override
+        public void rebind() {
+            // TODO super.rebind();
+            rebindCalled = true;
+        }
+    }
+    
+    public static class MyEnricherWithoutNoArgConstructor extends MyEnricher {
+        public MyEnricherWithoutNoArgConstructor(Map<?,?> flags) {
+            super(flags);
+        }
+    }
+}
--- core/src/test/java/brooklyn/entity/rebind/RebindPolicyTest.java
+++ core/src/test/java/brooklyn/entity/rebind/RebindPolicyTest.java
@@ -1,13 +1,16 @@
 package brooklyn.entity.rebind;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
-import java.util.Collection;
 import java.util.Map;
 
 import org.testng.annotations.Test;
 
-import brooklyn.policy.Policy;
+import brooklyn.config.ConfigKey;
+import brooklyn.entity.basic.ConfigKeys;
+import brooklyn.policy.PolicySpec;
 import brooklyn.policy.basic.AbstractPolicy;
 import brooklyn.test.entity.TestApplication;
 import brooklyn.util.collections.MutableMap;
@@ -26,29 +29,102 @@ public class RebindPolicyTest extends RebindTestFixtureWithApp {
      * See RebindManagerImpl.CheckpointingChangeListener.onChanged(Entity) and
      * MementosGenerator.newEntityMementoBuilder()
      */
-    @Test(enabled=false)
-    public void testRestoresSimplePolicy() throws Exception {
-        MyPolicy origPolicy = new MyPolicy(MutableMap.of("myfield", "myval"));
+    
+    @Test
+    public void testRestoresSimplePolicyFromConstructor() throws Exception {
+        MyPolicy origPolicy = new MyPolicy(MutableMap.of("myfield", "myFieldVal", "myconfigkey", "myConfigVal"));
         origApp.addPolicy(origPolicy);
+        runRestoresSimplePolicy();
+    }
+
+    @Test
+    public void testRestoresDeprecatedPolicyFromConstructorWithoutNoArgs() throws Exception {
+        MyPolicyWithoutNoArgConstructor origPolicy = new MyPolicyWithoutNoArgConstructor(MutableMap.of("myfield", "myFieldVal", "myconfigkey", "myConfigVal"));
+        origApp.addPolicy(origPolicy);
+        runRestoresSimplePolicy();
+    }
+
+    @Test
+    public void testRestoresSimplePolicyFromPolicySpec() throws Exception {
+        origApp.addPolicy(PolicySpec.create(MyPolicy.class)
+                .configure("myfield", "myFieldVal")
+                .configure(MyPolicy.MY_CONFIG, "myConfigVal"));
+        runRestoresSimplePolicy();
+    }
+    
+    protected void runRestoresSimplePolicy() throws Exception {
+        MyPolicy origPolicy = (MyPolicy) Iterables.getOnlyElement(origApp.getPolicies());
+        assertTrue(origPolicy.isRunning());
+        assertTrue(origPolicy.initCalled);
+        assertFalse(origPolicy.rebindCalled);
         
         TestApplication newApp = rebind();
-        Collection<Policy> policies = newApp.getPolicies();
-        MyPolicy newPolicy = (MyPolicy) Iterables.get(policies, 0);
+        MyPolicy newPolicy = (MyPolicy) Iterables.getOnlyElement(newApp.getPolicies());
         
-        assertEquals(newPolicy.myfield, origPolicy.myfield);
+        assertEquals(newPolicy.myfield, "myFieldVal");
+        assertEquals(newPolicy.getConfig(MyPolicy.MY_CONFIG), "myConfigVal");
+        assertTrue(newPolicy.isRunning());
+        assertFalse(newPolicy.initCalled);
+        assertTrue(newPolicy.rebindCalled);
+    }
+
+    @Test
+    public void testRestoresConfig() throws Exception {
+        origApp.addPolicy(PolicySpec.create(MyPolicy.class)
+                .configure(MyPolicy.MY_CONFIG_WITH_SETFROMFLAG_NO_SHORT_NAME, "myVal for with setFromFlag noShortName")
+                .configure(MyPolicy.MY_CONFIG_WITH_SETFROMFLAG_WITH_SHORT_NAME, "myVal for setFromFlag withShortName")
+                .configure(MyPolicy.MY_CONFIG_WITHOUT_SETFROMFLAG, "myVal for witout setFromFlag"));
+
+        newApp = (TestApplication) rebind();
+        MyPolicy newPolicy = (MyPolicy) Iterables.getOnlyElement(newApp.getPolicies());
+        
+        assertEquals(newPolicy.getConfig(MyPolicy.MY_CONFIG_WITH_SETFROMFLAG_NO_SHORT_NAME), "myVal for with setFromFlag noShortName");
+        assertEquals(newPolicy.getConfig(MyPolicy.MY_CONFIG_WITH_SETFROMFLAG_WITH_SHORT_NAME), "myVal for setFromFlag withShortName");
+        assertEquals(newPolicy.getConfig(MyPolicy.MY_CONFIG_WITHOUT_SETFROMFLAG), "myVal for witout setFromFlag");
     }
 
     public static class MyPolicy extends AbstractPolicy {
+        public static final ConfigKey<String> MY_CONFIG = ConfigKeys.newStringConfigKey("myconfigkey");
+        
+        @SetFromFlag
+        public static final ConfigKey<String> MY_CONFIG_WITH_SETFROMFLAG_NO_SHORT_NAME = ConfigKeys.newStringConfigKey("myconfig.withSetfromflag.noShortName");
+
+        @SetFromFlag("myConfigWithSetFromFlagWithShortName")
+        public static final ConfigKey<String> MY_CONFIG_WITH_SETFROMFLAG_WITH_SHORT_NAME = ConfigKeys.newStringConfigKey("myconfig.withSetfromflag.withShortName");
+
+        public static final ConfigKey<String> MY_CONFIG_WITHOUT_SETFROMFLAG = ConfigKeys.newStringConfigKey("myconfig.withoutSetfromflag");
+
         @SetFromFlag
         String myfield;
 
         @SuppressWarnings("unused")
         private final Object dummy = new Object(); // so not serializable
         
+        public transient volatile boolean initCalled;
+        public transient volatile boolean rebindCalled;
+        
         public MyPolicy() {
         }
         
         public MyPolicy(Map<?,?> flags) {
+            super(flags);
+        }
+        
+        @Override
+        public void init() {
+            super.init();
+            initCalled = true;
+        }
+        
+        // TODO When AbstractPolicy declares rebind; @Override
+        public void rebind() {
+            // TODO super.rebind();
+            rebindCalled = true;
+        }
+    }
+    
+    public static class MyPolicyWithoutNoArgConstructor extends MyPolicy {
+        public MyPolicyWithoutNoArgConstructor(Map<?,?> flags) {
             super(flags);
         }
     }
