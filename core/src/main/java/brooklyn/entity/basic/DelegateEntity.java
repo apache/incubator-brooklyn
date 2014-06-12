a9fa54e1708131361e5dd3fcce43b88c7eddc3af
Andrew Donald Kennedy <grkvlt@apache.org>
2014-06-12 19:07:37 +0100

    Move renderer hint function to delegate interface

--- core/src/main/java/brooklyn/entity/basic/DelegateEntity.java
+++ core/src/main/java/brooklyn/entity/basic/DelegateEntity.java
@@ -1,10 +1,13 @@
 package brooklyn.entity.basic;
 
-import brooklyn.config.ConfigKey;
+import com.google.common.base.Function;
+
+import brooklyn.config.render.RendererHints;
 import brooklyn.entity.Entity;
 import brooklyn.entity.Group;
 import brooklyn.entity.proxying.ImplementedBy;
 import brooklyn.event.AttributeSensor;
+import brooklyn.event.basic.AttributeSensorAndConfigKey;
 import brooklyn.event.basic.Sensors;
 
 /**
@@ -13,8 +16,31 @@ import brooklyn.event.basic.Sensors;
 @ImplementedBy(DelegateEntityImpl.class)
 public interface DelegateEntity extends Entity {
 
-    ConfigKey<Entity> DELEGATE_ENTITY = ConfigKeys.newConfigKey(Entity.class, "delegate.entity", "The delegate entity");
+    AttributeSensorAndConfigKey<Entity, Entity> DELEGATE_ENTITY = ConfigKeys.newSensorAndConfigKey(Entity.class, "delegate.entity", "The delegate entity");
 
-    AttributeSensor<Entity> DELEGATE_ENTITY_LINK = Sensors.newSensor(Entity.class, "webapp.url", "The delegate entity link");
+    AttributeSensor<String> DELEGATE_ENTITY_LINK = Sensors.newStringSensor("webapp.url", "The delegate entity link");
+
+    public static class EntityUrl {
+ 
+        private static final Function<Object, String> entityUrlFunction = new Function<Object, String>() {
+            @Override
+            public String apply(Object input) {
+                if (input instanceof Entity) {
+                    Entity entity = (Entity) input;
+                    String url = String.format("#/v1/applications/%s/entities/%s", entity.getApplicationId(), entity.getId());
+                    return url;
+                } else {
+                    return null;
+                }
+            }
+        };
+
+        public static Function<Object, String> entityUrl() { return entityUrlFunction; }
+
+        static {
+            RendererHints.register(DELEGATE_ENTITY, new RendererHints.NamedActionWithUrl("Open", entityUrl()));
+            RendererHints.register(DELEGATE_ENTITY_LINK, new RendererHints.NamedActionWithUrl("Open"));
+        }
+    }
 
 }
--- core/src/main/java/brooklyn/entity/basic/DelegateEntityImpl.java
+++ core/src/main/java/brooklyn/entity/basic/DelegateEntityImpl.java
@@ -1,10 +1,8 @@
 package brooklyn.entity.basic;
 
-import brooklyn.config.render.RendererHints;
 import brooklyn.enricher.Enrichers;
 import brooklyn.entity.Entity;
 
-import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 
 public class DelegateEntityImpl extends AbstractEntity implements DelegateEntity {
@@ -13,27 +11,16 @@ public class DelegateEntityImpl extends AbstractEntity implements DelegateEntity
     public void init() {
     	Entity delegate = getConfig(DELEGATE_ENTITY);
     	Preconditions.checkNotNull(delegate, "delegate");
+
+    	// Propagate all sensors from the delegate entity
     	addEnricher(Enrichers.builder()
     			.propagatingAll()
     			.from(delegate)
     			.build());
-    	setAttribute(DELEGATE_ENTITY_LINK, delegate);
-    }
 
-	static {
-		RendererHints.register(DELEGATE_ENTITY_LINK, new RendererHints.NamedActionWithUrl("Open",
-				new Function<Object, String>() {
-					@Override
-					public String apply(Object input) {
-						if (input instanceof Entity) {
-							Entity entity = (Entity) input;
-							String url = String.format("#/v1/applications/%s/entities/%s", entity.getApplicationId(), entity.getId());
-							return url;
-						} else {
-							return null;
-						}
-					}
-				}));
-	}
+    	// Publish the entity as an attribute for linking
+    	setAttribute(DELEGATE_ENTITY, delegate);
+    	setAttribute(DELEGATE_ENTITY_LINK, EntityUrl.entityUrl().apply(delegate));
+    }
 }
 
