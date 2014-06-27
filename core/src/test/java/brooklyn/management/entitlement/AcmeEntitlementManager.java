a9970bffd3651f8da712855ae0e032778db318a4
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-06-27 12:34:19 +0100

    fix entitlements failing test

--- core/src/test/java/brooklyn/management/entitlement/AcmeEntitlementManager.java
+++ core/src/test/java/brooklyn/management/entitlement/AcmeEntitlementManager.java
@@ -4,6 +4,7 @@ class AcmeEntitlementManager extends PerUserEntitlementManagerWithDefault {
 
     public AcmeEntitlementManager() {
         super(Entitlements.root());
+        super.addUser("hacker", Entitlements.minimal());
         super.addUser("bob", Entitlements.readOnly());
         super.addUser("alice", Entitlements.root());
     }
