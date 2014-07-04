67201742bcee79c88d9cecdbc276088a1cfcfed4
Martin Harris <github@nakomis.com>
2014-07-04 12:15:06 +0100

    Moved #!/bin/bash to the start of bash scripts (added where missing

--- sandbox/examples/src/main/scripts/create-instance.sh
+++ sandbox/examples/src/main/scripts/create-instance.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -16,7 +17,6 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-#!/bin/bash
 # Creates an instance in each of the given region.
 # Must supply the ami to use,
 # the region,
