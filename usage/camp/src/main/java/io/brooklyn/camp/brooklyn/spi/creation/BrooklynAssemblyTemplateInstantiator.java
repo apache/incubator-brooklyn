02137ef3724eb90b4b8d1608c06d4b83578b14c7
Aled Sage <aled.sage@gmail.com>
2014-07-08 10:02:04 +0100

    CAMP catalog handling: delete unused code

--- usage/camp/src/main/java/io/brooklyn/camp/brooklyn/spi/creation/BrooklynAssemblyTemplateInstantiator.java
+++ usage/camp/src/main/java/io/brooklyn/camp/brooklyn/spi/creation/BrooklynAssemblyTemplateInstantiator.java
@@ -9,8 +9,6 @@ import io.brooklyn.camp.spi.instantiate.AssemblyTemplateInstantiator;
 
 import java.io.Reader;
 import java.io.StringReader;
-import java.lang.reflect.Constructor;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -23,20 +21,14 @@ import brooklyn.camp.brooklyn.api.HasBrooklynManagementContext;
 import brooklyn.catalog.BrooklynCatalog;
 import brooklyn.catalog.CatalogItem;
 import brooklyn.catalog.internal.BasicBrooklynCatalog.BrooklynLoaderTracker;
-import brooklyn.config.ConfigKey;
 import brooklyn.entity.Application;
 import brooklyn.entity.Entity;
-import brooklyn.entity.basic.ApplicationBuilder;
-import brooklyn.entity.basic.BasicApplication;
 import brooklyn.entity.basic.BasicApplicationImpl;
 import brooklyn.entity.basic.Entities;
-import brooklyn.entity.basic.EntityInternal;
 import brooklyn.entity.basic.EntityLocal;
-import brooklyn.entity.basic.EntityTypes;
 import brooklyn.entity.basic.StartableApplication;
 import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.entity.trait.Startable;
-import brooklyn.location.Location;
 import brooklyn.management.ManagementContext;
 import brooklyn.management.Task;
 import brooklyn.management.classloading.BrooklynClassLoadingContext;
@@ -45,11 +37,9 @@ import brooklyn.util.collections.MutableList;
 import brooklyn.util.collections.MutableMap;
 import brooklyn.util.exceptions.Exceptions;
 import brooklyn.util.flags.TypeCoercions;
-import brooklyn.util.text.Strings;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 public class BrooklynAssemblyTemplateInstantiator implements AssemblyTemplateSpecInstantiator {
 
@@ -100,18 +90,10 @@ public class BrooklynAssemblyTemplateInstantiator implements AssemblyTemplateSpe
         BrooklynClassLoadingContext loader;
         if (item!=null) {
             loader = item.newClassLoadingContext(mgmt);
-            // TODO legacy path - currently item is always null because template.id is a random String,
-            // and pretty sure that is the desired behaviour; we now (Jul 2014) automatically promote
-            // so fine for users always to put the catalog registeredType in the services block;
-            // if we did want to support users supplying an `id' that would be available not via the above
-            // but via template.getCustomAttributes().get("id");
-            
-            // FIXME No longer called; delete:
-            // return createApplicationFromCatalog(platform, item, template, requireSpec);
         } else {
             loader = JavaBrooklynClassLoadingContext.newDefault(mgmt);
         }
-        return new AppOrSpec(createApplicationFromNonCatalogCampTemplate(template, platform, loader));
+        return new AppOrSpec(createApplicationFromCampTemplate(template, platform, loader));
     }
     
     private static class AppOrSpec {
@@ -142,86 +124,6 @@ public class BrooklynAssemblyTemplateInstantiator implements AssemblyTemplateSpe
         }
     }
     
-    protected AppOrSpec createApplicationFromCatalog(CampPlatform platform, CatalogItem<?,?> item, AssemblyTemplate template, boolean requireSpec) {
-        ManagementContext mgmt = getBrooklynManagementContext(platform);
-
-        if (!template.getApplicationComponentTemplates().isEmpty() ||
-                !template.getPlatformComponentTemplates().isEmpty())
-            log.warn("CAMP AssemblyTemplate was not empty when creating from catalog spec; ignoring templates declared within it " +
-                    "("+template+")");
-
-        // name (and description) -- not prescribed by camp spec (cf discussion with gil)
-        String name = template.getName();
-                
-        String type = item.getJavaType();
-
-        // Load the class; first try to use the appropriate catalog item; but then allow anything that is on the classpath
-        final Class<? extends Entity> clazz;
-        if (Strings.isEmpty(type)) {
-            clazz = BasicApplication.class;
-        } else {
-            clazz = item.newClassLoadingContext(mgmt).loadClass(type, Entity.class);
-        }
-        
-        try {
-            if (ApplicationBuilder.class.isAssignableFrom(clazz)) {
-                if (requireSpec) {
-                    // TODO we could enable this, by returning the spec from the ApplicationBuilder;
-                    // note that we would have to set it up so that ApplicationBuilder.doBuild is called from an
-                    // entity initializer set on the spec, and remove the doBuild call from ApplicationBuilder.manage.
-                    // (this could also allow ApplicationBuilder instances to be used from catalog)
-                    throw new IllegalStateException("ApplicationBuilder items cannot be used when specs have to be created");
-                }
-                Constructor<?> constructor = clazz.getConstructor();
-                ApplicationBuilder appBuilder = (ApplicationBuilder) constructor.newInstance();
-                // for builder, we (1) can't get a spec (so discourage use of Builder?),
-                // and (2) we have to manually extract key bits of the template
-                
-                if (!Strings.isEmpty(name)) appBuilder.appDisplayName(name);
-        
-                // TODO use resolver's configureEntitySpec instead
-                final Map<?,?> configO = (Map<?,?>) template.getCustomAttributes().get("brooklyn.config");
-
-                log.info("CAMP placing '{}' under management", appBuilder);
-                appBuilder.configure( convertFlagsToKeys(appBuilder.getType(), configO) );
-                Application instance = appBuilder.manage(mgmt);
-                
-                applyLocations(mgmt, template, instance);
-
-                return new AppOrSpec(instance);
-                
-            } else if (Application.class.isAssignableFrom(clazz)) {
-                // TODO use resolver's configureEntitySpec instead
-                final Map<?,?> configO = (Map<?,?>) template.getCustomAttributes().get("brooklyn.config");
-                
-                @SuppressWarnings("unchecked")
-                brooklyn.entity.proxying.EntitySpec<? extends Application> coreSpec = toCoreEntitySpec((Class<? extends Application>)clazz, name, configO);
-                applyLocations(mgmt, template, coreSpec);
-                
-                return new AppOrSpec(coreSpec);
-                
-            } else {
-                throw new IllegalArgumentException("Class "+clazz+" must extend one of ApplicationBuilder or Application");
-            }
-            
-        } catch (Exception e) {
-            log.error("CAMP failed to create application: "+e, e);
-            throw Exceptions.propagate(e);
-        }
-    }
-
-    private void applyLocations(ManagementContext mgmt, AssemblyTemplate template, final Application instance) {
-        List<Location> locations = new BrooklynYamlLocationResolver(mgmt).resolveLocations(template.getCustomAttributes(), false);
-        if (locations!=null)
-            ((EntityInternal)instance).addLocations(locations);
-    }
-
-    private void applyLocations(ManagementContext mgmt, AssemblyTemplate template, final EntitySpec<?> spec) {
-        List<Location> locations = new BrooklynYamlLocationResolver(mgmt).resolveLocations(template.getCustomAttributes(), false);
-        if (locations!=null)
-            spec.locations(locations);
-    }
-
     private ManagementContext getBrooklynManagementContext(CampPlatform platform) {
         return ((HasBrooklynManagementContext)platform).getBrooklynManagementContext();
     }
@@ -233,47 +135,8 @@ public class BrooklynAssemblyTemplateInstantiator implements AssemblyTemplateSpe
             MutableMap.of("locations", MutableList.of()));
     }
 
-    // TODO this is an exact copy of BrooklynRestResoureUtils; make available to both somehow? (or even better, avoid somehow)
-    private static Map<?,?> convertFlagsToKeys(Class<? extends Entity> javaType, Map<?, ?> config) {
-        if (config==null || config.isEmpty() || javaType==null) return config;
-
-        Map<String, ConfigKey<?>> configKeys = EntityTypes.getDefinedConfigKeys(javaType);
-        Map<Object,Object> result = new LinkedHashMap<Object,Object>();
-        for (Map.Entry<?,?> entry: config.entrySet()) {
-            log.debug("Setting key {} to {} for CAMP creation of {}", new Object[] { entry.getKey(), entry.getValue(), javaType});
-            Object key = configKeys.get(entry.getKey());
-            if (key==null) {
-                log.warn("Unrecognised config key {} passed to {}; will be treated as flag (and likely ignored)", entry.getKey(), javaType);
-                key = entry.getKey();
-            }
-            result.put(key, entry.getValue());
-        }
-        return result;
-    }
-
-    // TODO exact copy of BRRU, as above
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    private <T extends Entity> EntitySpec<? extends T> toCoreEntitySpec(Class<? extends T> clazz, String name, Map<?,?> configO) {
-        Map<?, ?> config = (configO == null) ? Maps.newLinkedHashMap() : Maps.newLinkedHashMap(configO);
-        
-        EntitySpec result;
-        if (clazz.isInterface()) {
-            result = EntitySpec.create(clazz);
-        } else {
-            // If this is a concrete class, particularly for an Application class, we want the proxy
-            // to expose all interfaces it implements.
-            Class interfaceclazz = (Application.class.isAssignableFrom(clazz)) ? Application.class : Entity.class;
-            Class<?>[] additionalInterfaceClazzes = clazz.getInterfaces();
-            result = EntitySpec.create(interfaceclazz).impl(clazz).additionalInterfaces(additionalInterfaceClazzes);
-        }
-        
-        if (!Strings.isEmpty(name)) result.displayName(name);
-        result.configure( convertFlagsToKeys(result.getImplementation(), config) );
-        return result;
-    }
-
     @SuppressWarnings("unchecked")
-    protected EntitySpec<? extends Application> createApplicationFromNonCatalogCampTemplate(AssemblyTemplate template, CampPlatform platform, BrooklynClassLoadingContext loader) {
+    protected EntitySpec<? extends Application> createApplicationFromCampTemplate(AssemblyTemplate template, CampPlatform platform, BrooklynClassLoadingContext loader) {
         // AssemblyTemplates created via PDP, _specifying_ then entities to put in
 
         BrooklynComponentTemplateResolver resolver = BrooklynComponentTemplateResolver.Factory.newInstance(
@@ -403,5 +266,4 @@ public class BrooklynAssemblyTemplateInstantiator implements AssemblyTemplateSpe
             }
         }
     }
-
 }
