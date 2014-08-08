63fe4b3abaabc64293d72e47e28ea7d5694ab692
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-08-08 02:05:22 -0400

    prefer 64-bit images (it makes sense, and without this it can sometimes pick a 32-bit os w a 64-bit hw, e.g. minRam 4096 in AWS VA, gives ami c68150ae on m3.large); also include the new ubuntu lts (14.04) as a slightly preferred; and a bit more jclouds template logging (previously we did not log the templateBuilder)

--- locations/jclouds/src/main/java/brooklyn/location/jclouds/JcloudsLocation.java
+++ locations/jclouds/src/main/java/brooklyn/location/jclouds/JcloudsLocation.java
@@ -998,7 +998,7 @@ public class JcloudsLocation extends AbstractCloudMachineProvisioningLocation im
         if (templateBuilder==null) {
             templateBuilder = new PortableTemplateBuilder<PortableTemplateBuilder<?>>();
         } else {
-            LOG.debug("jclouds using templateBuilder {} as base for provisioning in {} for {}", new Object[] {
+            LOG.debug("jclouds using templateBuilder {} as custom base for provisioning in {} for {}", new Object[] {
                     templateBuilder, this, config.getDescription()});
         }
         if (templateBuilder instanceof PortableTemplateBuilder<?>) {
@@ -1040,12 +1040,15 @@ public class JcloudsLocation extends AbstractCloudMachineProvisioningLocation im
             customizer.customize(this, computeService, templateBuilder);
         }
         
+        LOG.debug("jclouds using templateBuilder {} for provisioning in {} for {}", new Object[] {
+            templateBuilder, this, config.getDescription()});
+        
         // Finally try to build the template
         Template template;
         try {
             template = templateBuilder.build();
             if (template==null) throw new NullPointerException("No template found (templateBuilder.build returned null)");
-            LOG.debug(""+this+" got template "+template+" (image "+template.getImage()+")");
+            LOG.debug("jclouds found template "+template+" (image "+template.getImage()+") for provisioning in "+this+" for "+config.getDescription());
             if (template.getImage()==null) throw new NullPointerException("Template does not contain an image (templateBuilder.build returned invalid template)");
         } catch (AuthorizationException e) {
             LOG.warn("Error resolving template: not authorized (rethrowing: "+e+")");
