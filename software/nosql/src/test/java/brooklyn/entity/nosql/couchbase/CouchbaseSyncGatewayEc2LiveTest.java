f23ee27b877642d794f5a209aac8f1597b59ceb8
Martin Harris <github@nakomis.com>
2014-08-20 11:06:16 +0100

    Adds apache header to Ec2LiveTest, adds 'Live' group to tests

--- software/nosql/src/test/java/brooklyn/entity/nosql/couchbase/CouchbaseSyncGatewayEc2LiveTest.java
+++ software/nosql/src/test/java/brooklyn/entity/nosql/couchbase/CouchbaseSyncGatewayEc2LiveTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package brooklyn.entity.nosql.couchbase;
 
 import java.util.List;
@@ -15,7 +33,6 @@ import brooklyn.test.EntityTestUtils;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
-@Test
 public class CouchbaseSyncGatewayEc2LiveTest extends AbstractEc2LiveTest {
 
     @Override
@@ -57,17 +74,19 @@ public class CouchbaseSyncGatewayEc2LiveTest extends AbstractEc2LiveTest {
     
     
     // Supported operating systems
-    
+    @Test(groups = {"Live"})
     @Override
     public void test_Ubuntu_12_0() throws Exception {
         super.test_Ubuntu_12_0();
     }
     
+    @Test(groups = {"Live"})
     @Override
     public void test_Red_Hat_Enterprise_Linux_6() throws Exception {
         super.test_Red_Hat_Enterprise_Linux_6();
     }
     
+    @Test(groups = {"Live"})
     @Override
     public void test_CentOS_6_3() throws Exception {
         super.test_CentOS_6_3();
@@ -75,6 +94,7 @@ public class CouchbaseSyncGatewayEc2LiveTest extends AbstractEc2LiveTest {
     
     // Unsupported operating systems
     
+    @Test(groups = {"Live"})
     @Override
     public void test_CentOS_5_6() throws Exception {
         // Unsupported
@@ -92,16 +112,19 @@ public class CouchbaseSyncGatewayEc2LiveTest extends AbstractEc2LiveTest {
         //        rpmlib(PayloadIsXz) <= 5.2-1 is needed by couchbase-server-2.5.1-1083.x86_64
     }
     
+    @Test(groups = {"Live"})
     @Override
     public void test_Debian_6() throws Exception {
         // Unsupported
     }
     
+    @Test(groups = {"Live"})
     @Override
     public void test_Debian_7_2() throws Exception {
         // Unsupported
     }
     
+    @Test(groups = {"Live"})
     @Override
     public void test_Ubuntu_10_0() throws Exception {
         // Unsupported
