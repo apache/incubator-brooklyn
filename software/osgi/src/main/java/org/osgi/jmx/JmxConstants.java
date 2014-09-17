fb88d088942ded88734c21aed0ab4cfb7a69ac85
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-09-17 13:14:22 +0100

    address 3rd party java source inclusions, for BROOKLYN-35

--- software/osgi/src/main/java/org/osgi/jmx/JmxConstants.java
+++ software/osgi/src/main/java/org/osgi/jmx/JmxConstants.java
@@ -1,22 +1,4 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-/*
  * Copyright (c) OSGi Alliance (2009, 2010). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,8 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.osgi.jmx;
+
+/*
+ * BROOKLYN NOTE: This is a verbatim copy of the original in org.osgi.enterprise-4.2.0, 
+ * reproduced here to avoid pulling in the rest of that bundle and its chain of dependencies.
+ * The only difference is this comment and the suppress warnings.
+ */
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -52,11 +39,8 @@ import javax.management.openmbean.TabularType;
  * 
  * @version $Revision: 8512 $
  * @Immutable
- * 
- * ALED/BROOKLYN: This is a verbatim copy of from org.osgi.jmx.JmxConstants, to avoid  
- * pulling in the dependency org.osgi.enterprise-4.2.0, and its chain of dependencies.
- * The only difference is this comment.
  */
+@SuppressWarnings("rawtypes")
 public class JmxConstants {
 
     /*
