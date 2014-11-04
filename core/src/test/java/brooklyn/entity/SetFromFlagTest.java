aa6421bd24c3d47c13e1c4472004c9ab85fe5c62
Aled Sage <aled.sage@gmail.com>
2014-11-04 14:36:46 +0000

    Convert SetFromFlagTest from groovy to java

--- core/src/test/java/brooklyn/entity/SetFromFlagTest.java
+++ core/src/test/java/brooklyn/entity/SetFromFlagTest.java
@@ -18,114 +18,115 @@
  */
 package brooklyn.entity;
 
-import static org.testng.Assert.*
+import static org.testng.Assert.assertEquals;
 
-import java.util.Map
+import java.util.Map;
 
-import org.testng.annotations.Test
+import org.testng.annotations.Test;
 
-import brooklyn.entity.basic.AbstractEntity
-import brooklyn.location.PortRange
+import brooklyn.entity.basic.AbstractEntity;
+import brooklyn.location.PortRange;
 import brooklyn.location.basic.PortRanges;
-import brooklyn.util.flags.SetFromFlag
+import brooklyn.util.collections.MutableMap;
+import brooklyn.util.flags.SetFromFlag;
 
 public class SetFromFlagTest {
 
     @Test
     public void testSetFromFlagUsingFieldName() {
-        MyEntity entity = new MyEntity(str1:"myval")
-        assertEquals(entity.str1, "myval")
+        MyEntity entity = new MyEntity(MutableMap.of("str1", "myval"));
+        assertEquals(entity.str1, "myval");
     }
     
     @Test
     public void testSetFromFlagUsingOverridenName() {
-        MyEntity entity = new MyEntity(altStr2:"myval")
-        assertEquals(entity.str2, "myval")
+        MyEntity entity = new MyEntity(MutableMap.of("altStr2", "myval"));
+        assertEquals(entity.str2, "myval");
     }
     
     @Test
     public void testSetFromFlagWhenNoDefaultIsNull() {
-        MyEntity entity = new MyEntity()
-        assertEquals(entity.str1, null)
+        MyEntity entity = new MyEntity();
+        assertEquals(entity.str1, null);
     }
     
     @Test
     public void testSetFromFlagUsesDefault() {
-        MyEntity entity = new MyEntity()
-        assertEquals(entity.str3, "default str3")
+        MyEntity entity = new MyEntity();
+        assertEquals(entity.str3, "default str3");
     }
     
     @Test
     public void testSetFromFlagOverridingDefault() {
-        MyEntity entity = new MyEntity(str3:"overridden str3")
-        assertEquals(entity.str3, "overridden str3")
+        MyEntity entity = new MyEntity(MutableMap.of("str3", "overridden str3"));
+        assertEquals(entity.str3, "overridden str3");
     }
 
     @Test
     public void testSetFromFlagCastsPrimitives() {
-        MyEntity entity = new MyEntity(double1:1f)
-        assertEquals(entity.double1, 1d)
+        MyEntity entity = new MyEntity(MutableMap.of("double1", 1f));
+        assertEquals(entity.double1, 1d);
     }
 
     @Test
     public void testSetFromFlagCastsDefault() {
-        MyEntity entity = new MyEntity()
-        assertEquals(entity.byte1, (byte)1)
-        assertEquals(entity.short1, (short)2)
-        assertEquals(entity.int1, 3)
-        assertEquals(entity.long1, 4l)
-        assertEquals(entity.float1, 5f)
-        assertEquals(entity.double1, 6d)
-         assertEquals(entity.char1, 'a' as char)
-        assertEquals(entity.bool1, true)
+        MyEntity entity = new MyEntity();
+        assertEquals(entity.byte1, (byte)1);
+        assertEquals(entity.short1, (short)2);
+        assertEquals(entity.int1, 3);
+        assertEquals(entity.long1, 4l);
+        assertEquals(entity.float1, 5f);
+        assertEquals(entity.double1, 6d);
+         assertEquals(entity.char1, 'a');
+        assertEquals(entity.bool1, true);
         
-        assertEquals(entity.byte2, Byte.valueOf((byte)1))
-        assertEquals(entity.short2, Short.valueOf((short)2))
-        assertEquals(entity.int2, Integer.valueOf(3))
-        assertEquals(entity.long2, Long.valueOf(4l))
-        assertEquals(entity.float2, Float.valueOf(5f))
-        assertEquals(entity.double2, Double.valueOf(6d))
-        assertEquals(entity.char2, Character.valueOf('a' as char))
-        assertEquals(entity.bool2, Boolean.TRUE)
+        assertEquals(entity.byte2, Byte.valueOf((byte)1));
+        assertEquals(entity.short2, Short.valueOf((short)2));
+        assertEquals(entity.int2, Integer.valueOf(3));
+        assertEquals(entity.long2, Long.valueOf(4l));
+        assertEquals(entity.float2, Float.valueOf(5f));
+        assertEquals(entity.double2, Double.valueOf(6d));
+        assertEquals(entity.char2, Character.valueOf('a'));
+        assertEquals(entity.bool2, Boolean.TRUE);
     }
     
     @Test
     public void testSetFromFlagCoercesDefaultToPortRange() {
-        MyEntity entity = new MyEntity()
-        assertEquals(entity.portRange1, PortRanges.fromInteger(1234))
+        MyEntity entity = new MyEntity();
+        assertEquals(entity.portRange1, PortRanges.fromInteger(1234));
     }
     
     @Test
     public void testSetFromFlagCoercesStringValueToPortRange() {
-        MyEntity entity = new MyEntity(portRange1:"1-3")
-        assertEquals(entity.portRange1, new PortRanges.LinearPortRange(1, 3))
+        MyEntity entity = new MyEntity(MutableMap.of("portRange1", "1-3"));
+        assertEquals(entity.portRange1, new PortRanges.LinearPortRange(1, 3));
     }
     
     @Test
     public void testSetFromFlagCoercesStringValueToInt() {
-        MyEntity entity = new MyEntity(int1:"123")
-        assertEquals(entity.int1, 123)
+        MyEntity entity = new MyEntity(MutableMap.of("int1", "123"));
+        assertEquals(entity.int1, 123);
     }
 
     @Test
     public void testSetIconUrl() {
-        MyEntity entity = new MyEntity(iconUrl:"/img/myicon.gif")
-        assertEquals(entity.getIconUrl(), "/img/myicon.gif")
+        MyEntity entity = new MyEntity(MutableMap.of("iconUrl", "/img/myicon.gif"));
+        assertEquals(entity.getIconUrl(), "/img/myicon.gif");
     }
 
     @Test(expectedExceptions=IllegalArgumentException.class)
-    public void testFailsFastOnInvalidCoercion() {
-        MyEntity entity = new MyEntity(int1:"thisisnotanint")
+    public void testFailsFastOnInvalidCoercion() {;
+        new MyEntity(MutableMap.of("int1", "thisisnotanint"));
     }
     
     // Fails because configure being called from inside constructor; so field is set after configure called
     @Test(enabled=false) 
     public void testSetFromFlagWithFieldThatIsExplicitySet() {
-        MyEntity entity = new MyEntity(str4:"myval")
-        assertEquals(entity.str4, "myval")
+        MyEntity entity = new MyEntity(MutableMap.of("str4", "myval"));
+        assertEquals(entity.str4, "myval");
         
-        MyEntity entity2 = new MyEntity()
-        assertEquals(entity2.str4, "explicit str4")
+        MyEntity entity2 = new MyEntity();
+        assertEquals(entity2.str4, "explicit str4");
     }
     
     private static class MyEntity extends AbstractEntity {
@@ -193,8 +194,20 @@ public class SetFromFlagTest {
         @SetFromFlag(defaultVal="true")
         Boolean bool2;
 
-        MyEntity(Map flags=[:], Entity parent=null) {
-            super(flags, parent)
+        MyEntity() {
+            super(MutableMap.of(), null);
+        }
+        
+        MyEntity(Map flags) {
+            super(flags, null);
+        }
+        
+        MyEntity(Entity parent) {
+            super(MutableMap.of(), parent);
+        }
+        
+        MyEntity(Map flags, Entity parent) {
+            super(flags, parent);
         }
     }
 }
