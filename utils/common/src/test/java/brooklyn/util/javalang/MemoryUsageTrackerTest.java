eb57557146b371ad0745d6d1c271a3ecd33a6c6b
Aled Sage <aled.sage@gmail.com>
2014-11-13 10:56:17 +0000

    Fix MemoryUsageTrackerTest
    - was non-deterministic (asserting memory usage immediately, before
      listener had been called by GC)

--- utils/common/src/test/java/brooklyn/util/javalang/MemoryUsageTrackerTest.java
+++ utils/common/src/test/java/brooklyn/util/javalang/MemoryUsageTrackerTest.java
@@ -20,35 +20,57 @@ package brooklyn.util.javalang;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import brooklyn.test.Asserts;
 import brooklyn.util.collections.MutableList;
 import brooklyn.util.guava.Maybe;
 import brooklyn.util.text.Strings;
 
 public class MemoryUsageTrackerTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemoryUsageTrackerTest.class);
+
     @Test(groups="Integration")
     public void testBigUsage() {
+        final int ALLOCATION_CHUNK_SIZE = 10*1000*1000; // 10MB
+        
+        // Don't just use runtime.maxMemory()*2; javadoc says:
+        //     If there is no inherent limit then the value java.lang.Long.MAX_VALUE will be returned.
+        // Therefore cap at 10GB.
+        final long MAX_MEMORY_CAP = 10*1024*1024*1024L; // 10GB
+        final long maxMemory = Math.min(Runtime.getRuntime().maxMemory(), MAX_MEMORY_CAP);
+        
         List<Maybe<byte[]>> references = MutableList.of();
         long created = 0;
-        while (created < 2*Runtime.getRuntime().maxMemory()) {
-            byte d[] = new byte[1000000];
+        while (created < 2*maxMemory) {
+            byte d[] = new byte[ALLOCATION_CHUNK_SIZE];
             references.add(Maybe.soft(d));
             MemoryUsageTracker.SOFT_REFERENCES.track(d, d.length);
             created += d.length;
             
-            System.out.println("created "+Strings.makeSizeString(created) +
-                " ... in use: "+Strings.makeSizeString(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())+" / " +
-                Strings.makeSizeString(Runtime.getRuntime().totalMemory()) +
-                " ... reclaimable: "+Strings.makeSizeString(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed()) +
-                " ... live refs: "+Strings.makeSizeString(sizeOfActiveReferences(references)));
+            long totalMemory = Runtime.getRuntime().totalMemory();
+            long freeMemory = Runtime.getRuntime().freeMemory();
             
-            assertLessThan(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed(), Runtime.getRuntime().maxMemory());
-            assertLessThan(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed(), Runtime.getRuntime().totalMemory());
-            assertLessThan(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed(), Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+            LOG.info("created "+Strings.makeSizeString(created) +
+                " ... in use: "+Strings.makeSizeString(totalMemory - freeMemory)+" / " +
+                Strings.makeSizeString(totalMemory) +
+                " ... reclaimable: "+Strings.makeSizeString(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed()) +
+                " ... live refs: "+Strings.makeSizeString(sizeOfActiveReferences(references)) +
+                " ... maxMem="+maxMemory+"; totalMem="+totalMemory+"; usedMem="+(totalMemory-freeMemory));
         }
+        
+        Asserts.succeedsEventually(new Runnable() {
+            public void run() {
+                long totalMemory = Runtime.getRuntime().totalMemory();
+                long freeMemory = Runtime.getRuntime().freeMemory();
+                assertLessThan(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed(), maxMemory);
+                assertLessThan(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed(), totalMemory);
+                assertLessThan(MemoryUsageTracker.SOFT_REFERENCES.getBytesUsed(), totalMemory - freeMemory);
+            }});
     }
 
     private long sizeOfActiveReferences(List<Maybe<byte[]>> references) {
