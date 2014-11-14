253583e8cc68c8edf8bee5c725497bd393b8be02
Svetoslav Neykov <svetoslav.neykov@cloudsoftcorp.com>
2014-11-15 00:13:22 +0200

    Catalog item comparator - implement custom
    The NaturalOrderComparator leads to unexpected sort order (i.e 0.0.0_SNAPSHOT and 0.0.1-SNAPSHOT-20141111114709760). OSGi's Version class and other semver java implementations are too strict.

--- core/src/main/java/brooklyn/catalog/internal/CatalogItemComparator.java
+++ core/src/main/java/brooklyn/catalog/internal/CatalogItemComparator.java
@@ -21,7 +21,6 @@ package brooklyn.catalog.internal;
 import java.util.Comparator;
 
 import brooklyn.catalog.CatalogItem;
-import brooklyn.util.text.NaturalOrderComparator;
 
 /**
  * When using the comparator to sort - first using symbolicName
@@ -38,17 +37,71 @@ public class CatalogItemComparator implements Comparator<CatalogItem<?, ?>> {
         if (symbolicNameComparison != 0) {
             return symbolicNameComparison;
         } else {
-            String v1 = o1.getVersion().toUpperCase();
-            String v2 = o2.getVersion().toUpperCase();
-            boolean isV1Snapshot = v1.contains(SNAPSHOT);
-            boolean isV2Snapshot = v2.contains(SNAPSHOT);
+            String v1 = o1.getVersion();
+            String v2 = o2.getVersion();
+
+            boolean isV1Snapshot = v1.toUpperCase().contains(SNAPSHOT);
+            boolean isV2Snapshot = v2.toUpperCase().contains(SNAPSHOT);
             if (isV1Snapshot == isV2Snapshot) {
-                return -NaturalOrderComparator.INSTANCE.compare(v1, v2);
+                String[] v1Parts = v1.split("[^\\d]", 4);
+                String[] v2Parts = v2.split("[^\\d]", 4);
+                return -compare(v1Parts, v2Parts);
             } else if (isV1Snapshot) {
                 return 1;
             } else {
                 return -1;
             }
+        }
+    }
+
+    private int compare(String[] v1Parts, String[] v2Parts) {
+        int len = Math.max(v1Parts.length, v2Parts.length);
+        for (int i = 0; i < len; i++) {
+            if (i == v1Parts.length) {
+                return -1;
+            }
+            if (i == v2Parts.length) {
+                return 1;
+            }
+
+            String p1 = v1Parts[i];
+            String p2 = v2Parts[i];
+            int n1 = parse(p1);
+            int n2 = parse(p2);
+            if (n1 != -1 && n2 != -1) {
+                if (n1 != n2) {
+                    return compare(n1, n2);
+                }
+            } else if (n1 == -1 && n2 != -1) {
+                return 1;
+            } else if (n1 != -1 && n2 == -1) {
+                return -1;
+            } else {
+                int cmp = p1.compareTo(p2);
+                if (cmp != 0) {
+                    return cmp;
+                }
+            }
+        }
+        return 0;
+    }
+
+    //Replace with Integer.compare in J7
+    private int compare(int n1, int n2) {
+        if (n1 == n2) {
+            return 0;
+        } else if (n1 < n2) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    private int parse(String p) {
+        try {
+            return Integer.parseInt(p);
+        } catch (NumberFormatException e) {
+            return -1;
         }
     }
 }
