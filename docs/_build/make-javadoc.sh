d3c0469df2d9113a88b3e26e8b10f8ca48e7bc57
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2014-12-17 11:05:48 +0000

    update javadoc datestamp and simplify it, and remove old stuff from help file

--- docs/_build/make-javadoc.sh
+++ docs/_build/make-javadoc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -x make-javadoc.sh ]; then
+  echo This command must be run from the _build directory, not its parent.
+  exit 1
+fi
+
 if [ -z "$GROOVY_CMD" ] ; then
   if [ ! -z "$GROOVY_HOME" ] ; then
     GROOVY_CMD=$GROOVY_HOME/bin/groovysh
@@ -13,11 +18,19 @@ if [ ! -x "$GROOVY_CMD" ] ; then
   exit 1
 fi
 
-export SOURCE_PATHS=`find ../.. -name java | grep "src/main/java$" | grep -v web-console/plugins | grep -v "^../../sandbox"`
+if [ -z "$BROOKLYN_JAVADOC_SOURCE_PATHS" ]; then
+  echo detecting source paths for javadoc
+  export SOURCE_PATHS=`find ../.. -name java | grep "src/main/java$" | grep -v web-console/plugins | grep -v "^../../sandbox"`
+else
+  echo using pre-defined source paths $BROOKLYN_JAVADOC_SOURCE_PATHS
+  export SOURCE_PATHS=$BROOKLYN_JAVADOC_SOURCE_PATHS
+fi
 
 rm -rf target/apidocs/
 
-echo "building javadoc from "$SOURCE_PATHS
+export DATESTAMP=`date "+%Y-%m-%d"`
+echo "building javadoc from $SOURCE_PATH at $DATESTAMP"
+
 
 $GROOVY_CMD -q << END
 sourcePaths = System.env['SOURCE_PATHS'].split('\\\\s+').join(':')
@@ -32,13 +45,15 @@ ant.groovydoc(
     windowtitle  : "\${title}",
     doctitle     : "\${title}",
     header       : "\${title}",
-    footer       : '<b>Apache Brooklyn - Multi-Cloud Application Management Platform</b> <br/> <a href="http://brooklyn.io/" target="_top">brooklyn.io</a>. Apache License. &copy; 2014.',
+    footer       : '<b>Apache Brooklyn - Multi-Cloud Application Management Platform</b> <br/> <a href="http://brooklyn.io/" target="_top">brooklyn.io</a>. Apache License. &copy; '+System.env['DATESTAMP']+'.',
     private      : "false")
 println "\njavadoc built in target/apidocs\n" 
 END
 
 if (($!)) ; then echo ; echo ; echo "Groovy docs had an error." ; exit 1 ; fi
 echo ; echo
+
+if [ -z 'ls target/apidocs' ]; then echo ; echo ; echo "Error - no content. Are the paths right?" ; exit 1 ; fi
 
 if [ -d ../_site/use/api/ ] ; then
   echo "API directory detected in parent, installing docs there"
