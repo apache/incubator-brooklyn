51ffe808258a964710776b820ddce29ec50c34e6
Aled Sage <aled.sage@gmail.com>
2014-12-18 10:30:25 +0000

    Extracts AbstractBlueprintTest and marks MongoDbBlueprintTest as WIP

--- usage/launcher/src/test/java/brooklyn/launcher/blueprints/MongoDbBlueprintTest.java
+++ usage/launcher/src/test/java/brooklyn/launcher/blueprints/MongoDbBlueprintTest.java
@@ -18,81 +18,34 @@
  */
 package brooklyn.launcher.blueprints;
 
-import static org.testng.Assert.assertNotEquals;
-
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import brooklyn.entity.Application;
-import brooklyn.entity.Entity;
-import brooklyn.entity.basic.Attributes;
-import brooklyn.entity.basic.Entities;
-import brooklyn.entity.basic.Lifecycle;
-import brooklyn.launcher.BrooklynLauncher;
-import brooklyn.launcher.SimpleYamlLauncherForTests;
-import brooklyn.test.Asserts;
-import brooklyn.test.EntityTestUtils;
-
-public class MongoDbBlueprintTest {
+public class MongoDbBlueprintTest extends AbstractBlueprintTest {
 
     // TODO Some tests are failing! Needs investigated.
     
-    private SimpleYamlLauncherForTests launcher;
-    
-    // The "viewer" is just for having a Brooklyn web-console available for visual inspection;
-    // could be removed without affecting behaviour
-    private BrooklynLauncher viewer;
-    
-    @BeforeMethod(alwaysRun=true)
-    public void setUp() throws Exception {
-        launcher = new SimpleYamlLauncherForTests();
-        viewer = BrooklynLauncher.newInstance().managementContext(launcher.getManagementContext()).start();
+    @Test(groups={"Integration", "WIP"})
+    public void testMongoSharded() throws Exception {
+        runTest("mongo-sharded.yaml");
     }
-    
-    @AfterMethod(alwaysRun=true)
-    public void tearDown() throws Exception {
-        if (launcher != null) launcher.destroyAll();
-        if (viewer != null) viewer.terminate();
+
+    @Test(groups={"Integration", "WIP"})
+    public void testMongoReplicaSet() throws Exception {
+        runTest("mongo-blueprint.yaml");
     }
-    
-    @Test(groups="Integration")
-    public void testMongoSharded() {
-        runAppAndAssertNoFires("mongo-sharded.yaml");
+
+    @Test(groups={"Integration", "WIP"})
+    public void testMongoClientAndSingleServer() throws Exception {
+        runTest("mongo-client-single-server.yaml");
+    }
+
+    @Test(groups={"Integration", "WIP"})
+    public void testMongoScripts() throws Exception {
+        runTest("mongo-scripts.yaml");
     }
 
     @Test(groups="Integration")
-    public void testMongoReplicaSet() {
-        runAppAndAssertNoFires("mongo-blueprint.yaml");
-    }
-
-    @Test(groups="Integration")
-    public void testMongoClientAndSingleServer() {
-        runAppAndAssertNoFires("mongo-client-single-server.yaml");
-    }
-
-    @Test(groups="Integration")
-    public void testMongoScripts() {
-        runAppAndAssertNoFires("mongo-scripts.yaml");
-    }
-
-    @Test(groups="Integration")
-    public void testMongoSingleServer() {
-        runAppAndAssertNoFires("mongo-single-server-blueprint.yaml");
-    }
-
-    protected void runAppAndAssertNoFires(String yamlFile) {
-        final Application app = launcher.launchAppYaml(yamlFile);
-
-        EntityTestUtils.assertAttributeEqualsEventually(app, Attributes.SERVICE_UP, true);
-        EntityTestUtils.assertAttributeEqualsEventually(app, Attributes.SERVICE_STATE_ACTUAL, Lifecycle.RUNNING);
-        
-        Asserts.succeedsEventually(new Runnable() {
-            public void run() {
-                for (Entity entity : Entities.descendants(app)) {
-                    assertNotEquals(entity.getAttribute(Attributes.SERVICE_STATE_ACTUAL), Lifecycle.ON_FIRE);
-                    assertNotEquals(entity.getAttribute(Attributes.SERVICE_UP), false);
-                }
-            }});
+    public void testMongoSingleServer() throws Exception {
+        runTest("mongo-single-server-blueprint.yaml");
     }
 }
