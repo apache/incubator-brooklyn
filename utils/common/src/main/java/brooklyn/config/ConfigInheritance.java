12ccc014c29f90c0e2d4891d0222205299076f80
Andrew Kennedy <grkvlt@apache.org>
2015-03-24 19:15:03 +0000

    Make ConfigInheritance classes serializable

--- utils/common/src/main/java/brooklyn/config/ConfigInheritance.java
+++ utils/common/src/main/java/brooklyn/config/ConfigInheritance.java
@@ -18,9 +18,11 @@
  */
 package brooklyn.config;
 
+import java.io.Serializable;
+
 import com.google.common.annotations.Beta;
 
-public abstract class ConfigInheritance {
+public abstract class ConfigInheritance implements Serializable {
 
     public static final ConfigInheritance ALWAYS = new Always();
     public static final ConfigInheritance NONE = new None();
