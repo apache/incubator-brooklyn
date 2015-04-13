d7723b02bf02b13660253af11acc940c12a1d945
Martin Harris <github@nakomis.com>
2015-04-13 10:48:58 +0100

    Adds (WIP) outline for VanillaWindowsProcess

--- software/base/src/main/java/brooklyn/entity/basic/VanillaWindowsProcessDriver.java
+++ software/base/src/main/java/brooklyn/entity/basic/VanillaWindowsProcessDriver.java
@@ -18,6 +18,6 @@
  */
 package brooklyn.entity.basic;
 
-public interface VanillaSoftwareProcessDriver extends AbstractVanillaSoftwareProcessDriver {
+public interface VanillaWindowsProcessDriver extends AbstractVanillaSoftwareProcessDriver {
 
 }
--- software/base/src/main/java/brooklyn/entity/basic/VanillaWindowsProcessImpl.java
+++ software/base/src/main/java/brooklyn/entity/basic/VanillaWindowsProcessImpl.java
@@ -18,6 +18,21 @@
  */
 package brooklyn.entity.basic;
 
-public interface VanillaSoftwareProcessDriver extends AbstractVanillaSoftwareProcessDriver {
+public class VanillaWindowsProcessImpl extends SoftwareProcessImpl implements VanillaWindowsProcess {
+    @Override
+    public Class getDriverInterface() {
+        return VanillaWindowsProcessDriver.class;
+    }
 
+    @Override
+    protected void connectSensors() {
+        super.connectSensors();
+        connectServiceUpIsRunning();
+    }
+
+    @Override
+    protected void disconnectSensors() {
+        disconnectServiceUpIsRunning();
+        super.disconnectSensors();
+    }
 }
