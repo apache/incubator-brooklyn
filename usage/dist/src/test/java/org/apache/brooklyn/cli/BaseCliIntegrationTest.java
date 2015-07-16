713b62891dfdff893ef21d6b6b3943b8af969f71
Ciprian Ciubotariu <cheepeero@gmx.net>
2015-07-16 18:10:16 +0300

    brooklyn-dist: add org.apache package prefix

--- usage/dist/src/test/java/org/apache/brooklyn/cli/BaseCliIntegrationTest.java
+++ usage/dist/src/test/java/org/apache/brooklyn/cli/BaseCliIntegrationTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package brooklyn.cli;
+package org.apache.brooklyn.cli;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
--- usage/dist/src/test/java/org/apache/brooklyn/cli/CliIntegrationTest.java
+++ usage/dist/src/test/java/org/apache/brooklyn/cli/CliIntegrationTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package brooklyn.cli;
+package org.apache.brooklyn.cli;
 
 import org.testng.annotations.Test;
 
