8cfe58e52c031126489a46a89872cd2c9c696552
Mike Zaccardo <mike.zaccardo@cloudsoftcorp.com>
2015-08-03 13:55:11 -0400

    Address PR comments

--- software/nosql/src/main/java/brooklyn/entity/nosql/redis/RedisClusterImpl.java
+++ software/nosql/src/main/java/brooklyn/entity/nosql/redis/RedisClusterImpl.java
@@ -23,21 +23,24 @@ import java.util.Collection;
 import brooklyn.enricher.Enrichers;
 import brooklyn.entity.Entity;
 import brooklyn.entity.basic.AbstractEntity;
-import brooklyn.entity.basic.Entities;
+import brooklyn.entity.basic.Lifecycle;
+import brooklyn.entity.basic.ServiceStateLogic;
+import brooklyn.entity.basic.ServiceStateLogic.ServiceProblemsLogic;
 import brooklyn.entity.group.DynamicCluster;
 import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.entity.trait.Startable;
 import brooklyn.event.AttributeSensor;
 import brooklyn.event.basic.Sensors;
 import brooklyn.location.Location;
+import brooklyn.util.exceptions.Exceptions;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 public class RedisClusterImpl extends AbstractEntity implements RedisCluster {
 
-    private AttributeSensor<RedisStore> MASTER = Sensors.newSensor(RedisStore.class, "redis.master");
-    private AttributeSensor<DynamicCluster> SLAVES = Sensors.newSensor(DynamicCluster.class, "redis.slaves");
+    private static AttributeSensor<RedisStore> MASTER = Sensors.newSensor(RedisStore.class, "redis.master");
+    private static AttributeSensor<DynamicCluster> SLAVES = Sensors.newSensor(DynamicCluster.class, "redis.slaves");
 
     public RedisClusterImpl() {
     }
@@ -51,44 +54,65 @@ public class RedisClusterImpl extends AbstractEntity implements RedisCluster {
     public DynamicCluster getSlaves() {
         return getAttribute(SLAVES);
     }
-    
-    @Override
-    public void start(Collection<? extends Location> locations) {
-        RedisStore master = getMaster();
-        if (master == null) {
-            master = addChild(EntitySpec.create(RedisStore.class));
-            setAttribute(MASTER, master);
-        }
 
-        Entities.manage(master);
-        master.invoke(RedisStore.START, ImmutableMap.<String, Object>of("locations", ImmutableList.copyOf(locations))).getUnchecked();
+    public void init() {
+        super.init();
 
-        DynamicCluster slaves = getSlaves();
-        if (slaves == null) {
-            slaves = addChild(EntitySpec.create(DynamicCluster.class)
-                    .configure(DynamicCluster.MEMBER_SPEC, EntitySpec.create(RedisSlave.class).configure(RedisSlave.MASTER, master)));
-            setAttribute(SLAVES, slaves);
-        }
-        
-        slaves.invoke(DynamicCluster.START, ImmutableMap.<String, Object>of("locations", ImmutableList.copyOf(locations))).getUnchecked();
+        RedisStore master = addChild(EntitySpec.create(RedisStore.class));
+        setAttribute(MASTER, master);
 
-        setAttribute(Startable.SERVICE_UP, calculateServiceUp());
+        DynamicCluster slaves = addChild(EntitySpec.create(DynamicCluster.class)
+                .configure(DynamicCluster.MEMBER_SPEC, EntitySpec.create(RedisSlave.class).configure(RedisSlave.MASTER, master)));
+        setAttribute(SLAVES, slaves);
 
         addEnricher(Enrichers.builder()
                 .propagating(RedisStore.HOSTNAME, RedisStore.ADDRESS, RedisStore.SUBNET_HOSTNAME, RedisStore.SUBNET_ADDRESS, RedisStore.REDIS_PORT)
                 .from(master)
                 .build());
     }
+    
+    @Override
+    public void start(Collection<? extends Location> locations) {
+        ServiceStateLogic.setExpectedState(this, Lifecycle.STARTING);
+        ServiceProblemsLogic.clearProblemsIndicator(this, START);
+        try {
+            doStart(locations);
+            ServiceStateLogic.setExpectedState(this, Lifecycle.RUNNING);
+        } catch (Exception e) {
+            ServiceProblemsLogic.updateProblemsIndicator(this, START, "Start failed with error: "+e);
+            ServiceStateLogic.setExpectedState(this, Lifecycle.ON_FIRE);
+            throw Exceptions.propagate(e);
+        } finally {
+            setAttribute(Startable.SERVICE_UP, calculateServiceUp());
+        }
+    }
+
+    private void doStart(Collection<? extends Location> locations) {
+        RedisStore master = getMaster();
+        master.invoke(RedisStore.START, ImmutableMap.<String, Object>of("locations", ImmutableList.copyOf(locations))).getUnchecked();
+
+        DynamicCluster slaves = getSlaves();
+        slaves.invoke(DynamicCluster.START, ImmutableMap.<String, Object>of("locations", ImmutableList.copyOf(locations))).getUnchecked();
+    }
 
     @Override
     public void stop() {
-        DynamicCluster slaves = getSlaves();
-        RedisStore master = getMaster(); 
-        
-        if (slaves != null) slaves.invoke(DynamicCluster.STOP, ImmutableMap.<String, Object>of()).getUnchecked();
-        if (master != null) master.invoke(RedisStore.STOP, ImmutableMap.<String, Object>of()).getUnchecked();
+        ServiceStateLogic.setExpectedState(this, Lifecycle.STOPPING);
+        try {
+            doStop();
+            ServiceStateLogic.setExpectedState(this, Lifecycle.STOPPED);
+        } catch (Exception e) {
+            ServiceProblemsLogic.updateProblemsIndicator(this, STOP, "Stop failed with error: "+e);
+            ServiceStateLogic.setExpectedState(this, Lifecycle.ON_FIRE);
+            throw Exceptions.propagate(e);
+        } finally {
+            setAttribute(Startable.SERVICE_UP, calculateServiceUp());
+        }
+    }
 
-        setAttribute(Startable.SERVICE_UP, false);
+    private void doStop() {
+        getSlaves().invoke(DynamicCluster.STOP, ImmutableMap.<String, Object>of()).getUnchecked();
+        getMaster().invoke(RedisStore.STOP, ImmutableMap.<String, Object>of()).getUnchecked();
     }
 
     @Override
@@ -96,11 +120,12 @@ public class RedisClusterImpl extends AbstractEntity implements RedisCluster {
         throw new UnsupportedOperationException();
     }
 
-    protected boolean calculateServiceUp() {
-        boolean up = false;
-        for (Entity member : getSlaves().getMembers()) {
-            if (Boolean.TRUE.equals(member.getAttribute(SERVICE_UP))) up = true;
-        }
-        return up;
+    private boolean calculateServiceUp() {
+        if (!Boolean.TRUE.equals(getMaster().getAttribute(SERVICE_UP))) return false;
+
+        for (Entity member : getSlaves().getMembers())
+            if (!Boolean.TRUE.equals(member.getAttribute(SERVICE_UP))) return false;
+
+        return true;
     }
 }
