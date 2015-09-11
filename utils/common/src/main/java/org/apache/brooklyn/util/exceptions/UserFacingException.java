02d86fce841a83e5e809dbe719e27db5be4ec23a
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2015-09-11 10:19:05 +0100

    don't show type when collapsing UserFacingExceptions

--- utils/common/src/main/java/org/apache/brooklyn/util/exceptions/UserFacingException.java
+++ utils/common/src/main/java/org/apache/brooklyn/util/exceptions/UserFacingException.java
@@ -19,7 +19,7 @@
 package org.apache.brooklyn.util.exceptions;
 
 /** marker interface, to show that an exception is suitable for pretty-printing to an end-user,
- * without including a stack trace */
+ * without the prefix (type) including a stack trace */
 public class UserFacingException extends RuntimeException {
 
     private static final long serialVersionUID = 2216885527195571323L;
