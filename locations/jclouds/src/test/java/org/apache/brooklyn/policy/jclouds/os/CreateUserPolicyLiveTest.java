8e102ed2e913bb4f73cdfa73f26b244e1dbd8a96
Yavor Yanchev <yavor@yanchev.com>
2015-09-14 11:15:46 +0300

    Adjusting the test so it doesn't reset the password when executed   locally

--- locations/jclouds/src/test/java/org/apache/brooklyn/policy/jclouds/os/CreateUserPolicyLiveTest.java
+++ locations/jclouds/src/test/java/org/apache/brooklyn/policy/jclouds/os/CreateUserPolicyLiveTest.java
@@ -73,7 +73,6 @@ public class CreateUserPolicyLiveTest extends BrooklynAppLiveTestSupport {
             app.createAndManageChild(EntitySpec.create(TestEntity.class)
                     .policy(PolicySpec.create(CreateUserPolicy.class)
                             .configure(CreateUserPolicy.GRANT_SUDO, true)
-                            .configure(CreateUserPolicy.RESET_LOGIN_USER, true)
                             .configure(CreateUserPolicy.VM_USERNAME, newUsername)));
             TestEntity entity = (TestEntity) Iterables.getOnlyElement(app.getChildren());
 
